bc0754a2ce31fc411f6e9c9ce58eb5fdbd4e492c
zhusijia <zhusijia@hotoa.com>
2017-12-15 15:45:55 +0800

    添加svg icons

--- creator/packages/steedos-lightning-design-system/package.js
+++ creator/packages/steedos-lightning-design-system/package.js
@@ -10,6 +10,12 @@ Package.onUse(function(api) {
 	api.versionsFrom('METEOR@1.3');
 
 	api.addFiles('client/styles/salesforce-lightning-design-system.css', 'client');
+
+	api.addAssets('client/icons/action-sprite/symbols.svg', 'client');
+	api.addAssets('client/icons/custom-sprite/symbols.svg', 'client');
+	api.addAssets('client/icons/doctype-sprite/symbols.svg', 'client');
+	api.addAssets('client/icons/standard-sprite/symbols.svg', 'client');
+	api.addAssets('client/icons/utility-sprite/symbols.svg', 'client');
 	api.addAssets('client/images/themes/oneSalesforce/banner-brand-default.png', 'client');
 });
 
