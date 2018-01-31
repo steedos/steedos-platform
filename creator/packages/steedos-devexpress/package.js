6393148d79b7fcaf240c756d9b1529de2a6fe136
yinlianghui <yinlianghui@hotoa.com>
2018-01-31 17:37:26 +0800

    #119 fix 引用devexpress 的icons文件解决图标未显示出来 的问题

--- creator/packages/steedos-devexpress/package.js
+++ creator/packages/steedos-devexpress/package.js
@@ -7,6 +7,13 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
+	api.addAssets([
+		'client/css/icons/dxicons.ttf',
+		'client/css/icons/dxicons.woff',
+		'client/css/icons/dxiconsios.ttf',
+		'client/css/icons/dxiconsios.woff'
+	], 'client');
+
 	api.addFiles('client/jszip/jszip.js', 'client');
 	api.addFiles('client/css/dx.spa.css', 'client');
 	api.addFiles('client/css/dx.common.css', 'client');
