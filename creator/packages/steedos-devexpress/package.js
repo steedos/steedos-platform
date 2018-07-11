07aa8468e9b1044b3aa6c1726db1ed9e0d43e613
yinlianghui <yinlianghui@hotoa.com>
2018-07-11 18:51:40 +0800

    使用devexpress源码

--- creator/packages/steedos-devexpress/package.js
+++ creator/packages/steedos-devexpress/package.js
@@ -18,7 +18,7 @@ Package.onUse(function (api) {
 	api.addFiles('client/css/dx.spa.css', 'client');
 	api.addFiles('client/css/dx.common.css', 'client');
 	api.addFiles('client/css/dx.light.css', 'client');
-	api.addFiles('client/js/dx.all.js', 'client');
+	api.addFiles('client/js/dx.all.debug.js', 'client');
 	api.addFiles('client/js/dx.messages.zh.js', 'client');
 	api.addFiles('client/js/dx.date.formatter.js', 'client');
 	api.addFiles('client/js/dx.date.locale.js', 'client');
