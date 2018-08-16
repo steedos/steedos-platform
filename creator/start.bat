731b5968abbb5a507f84f20c521675afe9d7f3d0
Jack Zhuang <zhuangjianguo@me.com>
2018-08-16 13:10:49 +0800

    fix 内存溢出

--- creator/start.bat
+++ creator/start.bat
@@ -3,4 +3,5 @@ set MONGO_URL=mongodb://%DB_SERVER%/steedos
 set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
 set MULTIPLE_INSTANCES_COLLECTION_NAME=creator_instances
 set ROOT_URL=http://127.0.0.1:5000
+set TOOL_NODE_FLAGS="--max-old-space-size=1800"
 meteor run --port 5000
