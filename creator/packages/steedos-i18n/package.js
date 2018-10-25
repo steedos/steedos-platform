ef1bdd7112e535597fa9a7aba2e6a441e8b64cf1
Jack Zhuang <zhuangjianguo@me.com>
2018-10-25 13:15:39 +0800

    资源文件迁移到creator维护

---
+++ creator/packages/steedos-i18n/package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'steedos:i18n',
+  version: '0.0.11',
+  summary: 'i18n for Steedos',
+  git: 'https://github.com/steedos/framework/packages/steedos-i18n'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@1.3');
+
+  api.use('coffeescript');
+  api.use('blaze-html-templates');
+  api.use('underscore');
+  api.use('reactive-var');
+  api.use('tracker');
+  api.use('templating', 'client');
+
+  api.use('tap:i18n@1.8.2');
+  tapi18nFiles = ['i18n/en.i18n.json', 'i18n/zh-CN.i18n.json']
+  api.addFiles(tapi18nFiles);
+
+});
+
+Package.onTest(function(api) {
+
+});
