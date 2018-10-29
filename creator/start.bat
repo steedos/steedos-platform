ee6b2d21321f253d36044ff270077e23ab444927
Jack Zhuang <zhuangjianguo@me.com>
2018-10-29 19:41:29 +0800

    upgrade 1.6

--- creator/start.bat
+++ creator/start.bat
@@ -12,6 +12,6 @@ REM set ROOT_URL=http://192.168.0.195:5000/creator
 set ROOT_URL=http://192.168.0.81:5000
 REM meteor run --port 5000
 REM set MAIL_URL=smtp://AKIAJPRG5K6Z2XDWUM2A:AglrBWID%%2Fp2kCIByvcGE6gSbewe93HNFZXvGZs8lbk9I@email-smtp.us-east-1.amazonaws.com:465/
-set TOOL_NODE_FLAGS="--max-old-space-size=1800"
+set TOOL_NODE_FLAGS="--max-old-space-size=3800"
 set SOCKET_IO_PORT=8080
 meteor run --port 5000 --settings settings.json
