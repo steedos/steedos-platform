26beaf936cf09cfc06b89bc090c8971e49017b62
sunhaolin <sunhaolin@hotoa.com>
2019-02-26 18:45:40 +0800

    apps npm.require 清理及发包到creator和apps
    https://github.com/steedos/apps/issues/1972

--- creator/packages/steedos-formbuilder/checkNpm.js
+++ creator/packages/steedos-formbuilder/checkNpm.js
@@ -1,3 +1,6 @@
+// fix warning: xxx not installed
+require("formBuilder/package.json");
+
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 checkNpmVersions({
 	formBuilder: "3.1.x"
