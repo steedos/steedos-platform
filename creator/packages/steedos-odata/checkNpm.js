93c16935914c2da071a76c33e9af9afee3479276
sunhaolin <sunhaolin@hotoa.com>
2019-02-26 16:18:42 +0800

    apps npm.require 清理及发包到creator和apps
    https://github.com/steedos/apps/issues/1972

--- creator/packages/steedos-odata/checkNpm.js
+++ creator/packages/steedos-odata/checkNpm.js
@@ -1,3 +1,6 @@
+// fix warning: xxx not installed
+require("basic-auth/package.json");
+
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 checkNpmVersions({
 	'basic-auth': '2.0.x',
--- creator/packages/steedos-odata/package.js
+++ creator/packages/steedos-odata/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'steedos:odata',
-	version: '0.0.5',
+	version: '0.0.6',
 	summary: 'Steedos odata libraries',
 	git: '',
 	documentation: null
