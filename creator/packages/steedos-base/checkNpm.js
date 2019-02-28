bfbf61978aa14c41a1989d397c72681331c63440
sunhaolin <sunhaolin@hotoa.com>
2019-02-28 17:29:53 +0800

    weixin-pay
     去掉对不必要包的直接引用，比如 aliyun-sdk #1067

--- creator/packages/steedos-base/checkNpm.js
+++ creator/packages/steedos-base/checkNpm.js
@@ -1,9 +1,16 @@
-import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
+import {
+	checkNpmVersions
+} from 'meteor/tmeasday:check-npm-versions';
 checkNpmVersions({
 	"node-schedule": "^1.3.1",
 	cookies: "^0.6.2",
-	"weixin-pay": "^1.1.7",
 	"xml2js": "^0.4.19",
 	mkdirp: "^0.3.5",
 	"sprintf-js": "^1.0.3",
 }, 'steedos:base');
+
+if (Meteor.settings && Meteor.settings.billing) {
+	checkNpmVersions({
+		"weixin-pay": "^1.1.7"
+	}, 'steedos:base');
+}
