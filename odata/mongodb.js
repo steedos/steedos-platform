1b2f0fcd777279fa25694185465b5a6d6acdbaab
Jack Zhuang <steedos@users.noreply.github.com>
2019-03-04 17:49:13 +0800

    初始化

---
+++ odata/mongodb.js
@@ -0,0 +1,29 @@
+import { builtinModules } from "module";
+
+var find = function(object_name, select, filter, expand, top, skip, count){
+    
+}
+
+var findOne = function(object_name, _id){
+
+}
+
+var insert = function(object_name, _id, doc){
+
+}
+
+var update = function(object_name, _id, doc){
+
+}
+
+var deleteOne = function(object_name, _id){
+
+}
+
+module.exports = {
+    find,
+    findOne,
+    insert,
+    update,
+    deleteOne
+}
