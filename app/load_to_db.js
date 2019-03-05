1352862267d99a1e805eeaac3fc81b2b591ea849
sunhaolin <sunhaolin@hotoa.com>
2019-03-05 11:10:56 +0800

    schema/objects

--- app/load_to_db.js
+++ app/load_to_db.js
@@ -1,5 +1,29 @@
 // fix: Meteor code must always run within a Fiber.
-var Fiber = require("fibers");
+const Fiber = require("fibers");
+const _ = require('underscore');
+
+const _validateObject = function (obj, objName) {
+    var cObj = _.clone(obj);
+    delete cObj._id; // 不校验_id
+    var object = Creator.getObject(objName);
+    if (object) {
+        var schema = object.schema;
+        if (schema) {
+            var validationContext = schema.newContext();
+            validationContext.validate(cObj);
+            if (!validationContext.isValid()) {
+                var err = validationContext.getErrorObject();
+                console.error(err);
+                throw new Error(`对象${objName}数据校验未通过：${err.message}`);
+            }
+        } else {
+            throw new Error(`对象${objName}未定义SimpleSchema`);
+        }
+    } else {
+        throw new Error('未能获取到对象：', objName);
+    }
+}
+
 
 exports.loadObject = function (obj) {
     var collection = Creator.getCollection('objects');
@@ -24,6 +48,7 @@ exports.loadTrigger = function (trigger) {
 }
 
 exports.loadReports = function (report) {
+    _validateObject(report, 'reports');
     var collection = Creator.getCollection('reports');
     if (!report.space) {
         report.space = 'global';
