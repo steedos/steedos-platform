caca91e70db7f4d6e13ec6bbc6ff7a66a29ba96a
baozhoutao <baozhoutao@hotoa.com>
2019-03-08 14:04:21 +0800

    完成trigger loadFile

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { Objects, ObjectManager } from './object';
 export { Apps, AppManager } from './app';
 export { Reports, ReportManager } from './report';
+export { Triggers, TriggerManager } from './trigger';
 export * from "./module";
 
 
--- src/trigger.ts
+++ src/trigger.ts
@@ -1,4 +1,7 @@
 var util = require("./util");
+var _ = require("underscore");
+
+import {Objects, ObjectManager} from './object';
 
 import { Dictionary, JsonMap, getString } from '@salesforce/ts-types';
 
@@ -10,17 +13,44 @@ export const TriggerManager = {
         return TriggerManager.loadJSON(json);
     },
 
-    loadJSON(json: JsonMap) {
-        console.log('TriggerManager loadJSON', json);
-        if (TriggerManager.validate(json)){
-            let name = getString(json, "name");
+    loadJSON(trigger: JsonMap) {
+        if (TriggerManager.validate(trigger)){
+            let name = getString(trigger, "name");
             if (name)
-                Triggers[name] = json
+                Triggers[name] = trigger
+
+            var object_name: any = trigger.object_name
+            if(!object_name){
+                console.error(`load trigger error：Missing attribute 'object_name' /r ${name}`)
+                return
+            }
+
+            let object: any = Objects[object_name]
+            if(!object){
+                console.error(`load trigger error：Invalid 'object_name' /r ${name}`)
+                return
+            }
+
+            if(!object.triggers){
+                object.triggers = {}
+            }
+
+            _.each(trigger, (attr: any, key: string)=>{
+                let tm = triggerMapping[key]
+                if(!_.isEmpty(tm)){
+                    let tkey: string = `_${key}`.toLocaleUpperCase();
+                    object.triggers[tkey] = _.extend({name: `_${trigger.type}_${tkey}`, object: object_name}, tm, {
+                        todo: attr
+                    })
+                }
+            })
+            ObjectManager.loadJSON(object)
         }
     },
     
+    //TODO 处理格式校验
     validate(json: JsonMap): boolean {
-        let name = getString(json, "name");
+        let name = getString(json, "object_name");
         if (name)
             return true
         else
@@ -35,4 +65,32 @@ export const TriggerManager = {
     loadStandardTriggers() {
     }
 
+}
+
+//TODO afterUndelete，beforeSubmit
+const triggerMapping: JsonMap = {
+    beforeInsert: {
+        on: 'server',
+        when: 'before.insert'
+    },
+    beforeUpdate: {
+        on: 'server',
+        when: 'before.update'
+    },
+    beforeDelete: {
+        on: 'server',
+        when: 'before.remove'
+    },
+    afterInsert: {
+        on: 'server',
+        when: 'after.insert'
+    },
+    afterUpdate: {
+        on: 'server',
+        when: 'after.update'
+    },
+    afterDelete: {
+        on: 'server',
+        when: 'after.remove'
+    }
 }
