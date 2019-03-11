d49f50697a484d64a90d60f7ed3ffb0e2e1aee5e
Jack Zhuang <steedos@users.noreply.github.com>
2019-03-11 23:29:04 +0800

    https://github.com/typestack/routing-controllers

---
+++ src/odata/ODataController.ts
@@ -0,0 +1,43 @@
+
+import {Controller, Req, Res, Param, Body, Get, Post, Put, Delete} from "routing-controllers";
+import { getConnection } from "../";
+
+@Controller()
+export class ODataController {
+
+    @Get("/api/odata/v4/:spaceId/:objectName")
+    async getAll(@Req() request: any, @Res() response: any) {
+       
+      // const spaceId = request.params.spaceId;
+      const entityName = request.params.objectName;
+      // get a repository
+      const repository = getConnection().getRepository(entityName);
+
+      // load records
+      const records = await repository.find();
+
+      // return loaded records
+      response.send(records);
+    }
+
+    @Get("/api/odata/v4/:spaceId/:objectName/:id")
+    getOne(@Param("id") id: number) {
+       return "This action returns record #" + id;
+    }
+
+    @Post("/api/odata/v4/:spaceId/:objectName")
+    post(@Body() user: any) {
+       return "Saving record...";
+    }
+
+    @Put("/api/odata/v4/:spaceId/:objectName/:id")
+    put(@Param("id") id: number, @Body() user: any) {
+       return "Updating a record...";
+    }
+
+    @Delete("/api/odata/v4/:spaceId/:objectName/:id")
+    remove(@Param("id") id: number) {
+       return "Removing record...";
+    }
+
+}
--- src/odata/server.ts
+++ src/odata/server.ts
@@ -1,32 +1,13 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { Request, Response } from "express";
-import * as express from "express";
 import * as bodyParser from "body-parser";
-import { Routes } from "./routes";
+import "reflect-metadata"; // this shim is required
+import {createExpressServer} from "routing-controllers";
+import {ODataController} from "./ODataController";
 
-// create connection with database
-// note that it's not active database connection
-// TypeORM creates connection pools and uses them for your requests
-createConnection().then(async connection => {
+// creates express app, registers all controller routes and returns you express app instance
+const app = createExpressServer({
+   controllers: [ODataController] // we specify controllers we want to use
+});
+app.use(bodyParser.json());
 
-    // create express app
-    const app = express();
-    app.use(bodyParser.json());
-
-    // register all application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.path, (request: Request, response: Response, next: Function) => {
-            route.action(request, response)
-                .then(() => next)
-                .catch(err => next(err));
-        });
-    });
-
-    // run app
-    app.listen(3000);
-
-    console.log("Express application is up and running on port 3000");
-
-}).catch(error => console.log("TypeORM connection error: ", error));
-
+// run express application on port 3000
+app.listen(3000);
