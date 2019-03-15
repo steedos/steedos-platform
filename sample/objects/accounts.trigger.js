3aeec559adb5b6f44598c0b89e1c99b8f975f7ee
Jack Zhuang <steedos@users.noreply.github.com>
2019-03-15 14:16:45 +0800

    trigger sample

---
+++ sample/objects/accounts.trigger.js
@@ -0,0 +1,23 @@
+var accountTriggers = {
+    object_name: "accounts",
+    beforeInsert: function(){
+
+    },
+    beforeUpdate: function(){
+
+    },
+    beforeDelete: function(){
+
+    },
+    afterInsert: function(){
+
+    },
+    afterUpdate: function(){
+
+    },
+    afterDelete: function(){
+
+    },
+}
+
+export default accountTriggers;
