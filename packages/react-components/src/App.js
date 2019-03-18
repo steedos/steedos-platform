fc08a75488602a74ee8fc964dfda305ba41db512
yinlainghui <yinlianghui@hotoa.com>
2019-03-18 13:39:08 +0800

    fix run error : You should not use <Switch> outside a <Router>

--- packages/react-components/src/App.js
+++ packages/react-components/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Route from 'react-router-dom/Route';
-import Switch from 'react-router-dom/Switch';
+import { Route, Switch } from 'react-router-dom';
+
 import Home from './Home';
 import ReportDesigner from './components/report-designer';
 import ReportViewer from './components/report-viewer';
