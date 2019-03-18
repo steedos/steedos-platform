5f3078107827e4773e99e53401ff43836a70c90c
Jack Zhuang <steedos@users.noreply.github.com>
2019-03-18 15:15:20 +0800

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Steedos Objects', // Title for your website.
   tagline: '',
-  url: 'https://steedos.github.io/steedos-core/', // Your website URL
+  url: 'https://steedos.github.io/', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Steedos Objects',
-  organizationName: 'Steedos',
+  projectName: 'steedos.github.io',
+  organizationName: 'steedos',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
