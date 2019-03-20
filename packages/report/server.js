81c6df2cde9e806a0623cbd699114b9fb625c142
Jack Zhuang <steedos@users.noreply.github.com>
2019-03-20 18:31:32 +0800

    Update server.js

--- packages/report/server.js
+++ packages/report/server.js
@@ -2,13 +2,24 @@ var express = require('express');
 var path = require('path');
 var ReportRouter = require('./router');
 
-const port = 3600;
-const server = express();
+process.env.PORT=3600;
 
 stimulsoftAssets = path.join(path.dirname(require.resolve("@steedos/stimulsoft-report")), "assets");
 
-server
-  .disable('x-powered-by')
-  .use('/assets/stimulsoft-report/', express.static(stimulsoftAssets))
-  .use('/api', ReportRouter.routes)
-  .listen(port, () => console.log('report server listening on port ' + port))
+var server = require('@steedos/meteor-bundle-runner');
+
+server.Fiber(function () {
+    server.Profile.run("Server startup", function () {
+        server.loadServerBundles();
+        server.callStartupHooks();
+
+        var app = express();
+        app
+            .disable('x-powered-by')
+            .use('/assets/stimulsoft-report/', express.static(stimulsoftAssets))
+            .use('/api', ReportRouter.routes)
+            WebApp.connectHandlers.use(app);
+
+        server.runMain();
+    });
+}).run();
