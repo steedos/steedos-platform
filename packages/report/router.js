27502b5fabef5b4aab9c10e684d2c7d804c53b81
yinlainghui <yinlianghui@hotoa.com>
2019-03-20 18:15:08 +0800

    report server

---
+++ packages/report/router.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var reporter = require('./reporter');
+
+var routes = express();
+
+routes.get('/report/mrt/:report_id', function (req, res) {
+  let report = reporter.core.getReport();
+  let simpleList = reporter.core.getReportMrt(report);
+  res.send(simpleList);
+});
+
+routes.get('/report/data/:report_id', function (req, res) {
+  let data = reporter.core.getData();
+  res.send(data);
+});
+
+module.exports.routes = routes;
