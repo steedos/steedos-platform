6655fea8be54c00284fdda56fce4a3efb184143a
Jack Zhuang <steedos@users.noreply.github.com>
2019-03-22 15:54:04 +0800

    Update driverTest.ts

--- packages/core/test/unit/driverTest.ts
+++ packages/core/test/unit/driverTest.ts
@@ -4,12 +4,12 @@ import { expect } from 'chai';
 describe('Test connection', () => {
     it('should return true', async () => {
       
-        let driver = new SteedosMongoDriver( "mongodb://127.0.0.1/steedos" );
+        let mongo = new SteedosMongoDriver( "mongodb://127.0.0.1/steedos" );
         let queryOptions = {
             fields: ["_id"],
             filters: ["_id", "=", "-10"]
         };
-        let result = await driver.find("users", queryOptions);
+        let result = await mongo.find("users", queryOptions);
         expect(result).to.be.length(0);
   
     });
