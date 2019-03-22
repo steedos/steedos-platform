f47bc9bd2a4baf0356deacabcd1c8438f806b497
Jack Zhuang <steedos@users.noreply.github.com>
2019-03-22 16:20:23 +0800

    Update index.ts

--- packages/core/src/graphql/index.ts
+++ packages/core/src/graphql/index.ts
@@ -1,6 +1,16 @@
 import { GraphQLSchema, GraphQLSchemaConfig } from "graphql";
 import { SteedosSchema } from "../types";
 
+
+/*
+    var query = '{  }';
+    schema  = new ObjectQLSchema()
+
+    graphql(schema, query).then(result => {
+        console.log(result);
+    });
+
+*/
 export class ObjectQLSchema extends GraphQLSchema{
     constructor(steedosSchema: SteedosSchema){
         //...
