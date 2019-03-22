12747046a22b10d932e4a701ef3325a80d820d4d
Jack Zhuang <steedos@users.noreply.github.com>
2019-03-22 16:03:31 +0800

    Create index.ts

---
+++ packages/core/src/graphql/index.ts
@@ -0,0 +1,8 @@
+import { SteedosSchema } from "../types";
+import { GraphQLSchema } from "graphql";
+
+export class ObjectQL {
+    getGraphQLSchema(schema: SteedosSchema): GraphQLSchema {
+        
+    }
+}
