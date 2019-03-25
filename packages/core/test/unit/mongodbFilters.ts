50d2f79be6fdf9c7c76cc9fee83b5538145ee569
yinlainghui <yinlianghui@hotoa.com>
2019-03-25 16:09:40 +0800

    过滤条件单元测试改为用本地数据库

--- packages/core/test/unit/mongodbFilters.ts
+++ packages/core/test/unit/mongodbFilters.ts
@@ -1,17 +1,23 @@
 import { SteedosMongoDriver } from "../../src/driver";
 import { expect } from 'chai';
 
-describe('respond with an array of apps_auths', () => {
-    it('respond with an array of apps_auths', async () => {
+describe('respond with an array of app', () => {
+    it('respond with an array of app', async () => {
 
-        let mongo = new SteedosMongoDriver({url:"mongodb://192.168.0.21/fssh20181214"});
+        let mongo = new SteedosMongoDriver({ url: "mongodb://127.0.0.1/steedos" });
         await mongo.connect();
+        await mongo.insert("app", { _id: "ptr", name: "ptr", title: "PTR" })
+        await mongo.insert("app", { _id: "cnpc", name: "cnpc", title: "CNPC" })
+        
         let queryOptions = {
             fields: ["_id","name"],
             filters: [["name", "=", "ptr"], ["title", "=", "PTR"]]
         };
-        let result = await mongo.find("apps_auths", queryOptions);
-        expect(result).to.be.length.greaterThan(0);
+        let result = await mongo.find("app", queryOptions);
+        
+        await mongo.delete("app", "ptr")
+        await mongo.delete("app", "cnpc")
+        expect(result).to.be.length(1);
 
     });
 });
