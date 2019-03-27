eaead6779b3f0871d17a79d56bd92e1be463538d
yinlainghui <yinlianghui@hotoa.com>
2019-03-27 18:37:51 +0800

    最新driver代码从core中copy到objectql

--- packages/objectql/src/driver/devextreme.d.ts
+++
@@ -1,1 +0,0 @@
-declare var DevExpress: any;
--- packages/objectql/src/driver/filter.ts
+++ packages/objectql/src/driver/filter.ts
@@ -1,28 +1,160 @@
 // import { getFormatter } from "../date.formatter";
 // import getFormatter = require("../date.formatter");
-// const getLDMLFormatter = require("../date.formatter").getFormatter;
+const getLDMLFormatter = require("./date.formatter").getFormatter;
 // const defaultDateNames = require("../default_date_names");
+import DevExpress = require("devextreme/bundles/modules/core");
+import DevExpressData = require("devextreme/bundles/modules/data");
+// import DevExpressOData = require("devextreme/bundles/modules/data.odata");
+// declare var DevExpress: any;
+
+// <reference path="../../../../node_modules/devextreme/bundles/dx.all.d.ts" />
+// <reference path="../../types/devexpress/dx.all.d.ts" />
+// <reference path="../../types/devextreme/index.d.ts" />
+// declare global {
+//     interface DevExpress {
+
+//     }
+// }
+// import DevExpress from "devextreme/bundles/dx.all.d";
+// import DevExpress from "devextreme/bundles/modules/core";
+// declare var DevExpress: any;
+
+// import DevExpressData from "devextreme/bundles/modules/data";
+// import DevExpressOData from "devextreme/bundles/modules/data.odata";
+
+// DevExpress.data = DevExpressData;
+// DevExpress.data.odata = DevExpressData;
+// import DevExpress = require("devextreme/bundles/dx.all");
+// import DevExpress = require("devextreme/bundles/dx.all.d");
+// <reference path="devextreme/bundles/dx.all.d.ts" />
+// <reference path="../../../../node_modules/devextreme/bundles/dx.all.d.ts" />
+
+// declare var DevExpress: any;
 // const DevExpress = require("devextreme/bundles/modules/core");
 // const DevExpressData = require("devextreme/bundles/modules/data");
 // const DevExpressOData = require("devextreme/bundles/modules/data.odata");
 
-// DevExpress.data = DevExpressData;
-// DevExpress.data.odata = DevExpressData;
+// import * as DevExpress from "../../types/devexpress/dx.all.d.ts";
+// import * as DevExpress from "../../types/devexpress/index";
+// import * as DevExpress from 'devextreme';
 
-const DevExpressData = {
-    isConjunctiveOperator(criterion:any):any{
-    },
-    normalizeBinaryCriterion(criteria: any): any {
-    },
-    isUnaryOperation(criteria: any): any {
-    }
-};
+
+// const DevExpressData = {
+//     isConjunctiveOperator(criterion:any):any{
+//     },
+//     normalizeBinaryCriterion(criteria: any): any {
+//     },
+//     isUnaryOperation(criteria: any): any {
+//     }
+// };
+
+DevExpressData.isUnaryOperation = (crit: any[]): boolean => {
+    return crit[0] === "!" && Array.isArray(crit[1]);
+}
+
+const defaultDateNames = DevExpress.localization.date;
+
 const DevExpressOData = {
-    serializePropName(fieldName: any): any {
+    serializePropName(propName: string): string {
+        return propName.replace(/\./g, "/");
     },
-    serializeValue(value: any, protocolVersion: any): any {
+    serializeValue(value: any, protocolVersion: number): any {
+        switch (protocolVersion) {
+            case 2:
+                return this.serializeValueV2(value);
+            case 3:
+                return this.serializeValueV2(value);
+            case 4:
+                return this.serializeValueV4(value);
+            default: 
+                return this.serializeValueV4(value);
+        }
+    },
+    serializeValueV2(value: any): any {
+        if (value instanceof Date) {
+            return this.serializeDate(value);
+        }
+        // if (value instanceof Guid) {
+        //     return "guid'" + value + "'";
+        // }
+        // if (value instanceof EdmLiteral) {
+        //     return value.valueOf();
+        // }
+        if (typeof value === "string") {
+            return this.serializeString(value);
+        }
+        return String(value);
+    },
+    serializeValueV4(value: any): any {
+        if (value instanceof Date) {
+            return this.formatISO8601(value, false, false);
+        }
+        // if (value instanceof Guid) {
+        //     return value.valueOf();
+        // }
+        if (Array.isArray(value)) {
+            return "[" + value.map(function (item) {
+                return this.serializeValueV4(item);
+            }).join(",") + "]";
+        }
+        return this.serializeValueV2(value);
+    },
+    serializeDate(value: any, serializationFormat?: any): any {
+        if (!serializationFormat) {
+            return value;
+        }
+
+        if (!(value instanceof Date)) {
+            return null;
+        }
+
+        if (serializationFormat === "number") {
+            return value && value.valueOf ? value.valueOf() : null;
+        }
+
+        return getLDMLFormatter(serializationFormat, defaultDateNames)(value);
+    },
+    formatISO8601(date: Date, skipZeroTime: boolean, skipTimezone: boolean): string {
+        var bag = [];
+
+        var isZeroTime = function () {
+            return date.getHours() + date.getMinutes() + date.getSeconds() + date.getMilliseconds() < 1;
+        };
+
+        bag.push(date.getFullYear());
+        bag.push("-");
+        bag.push(this.padLeft2(date.getMonth() + 1));
+        bag.push("-");
+        bag.push(this.padLeft2(date.getDate()));
+
+        if (!(skipZeroTime && isZeroTime())) {
+            bag.push("T");
+            bag.push(this.padLeft2(date.getHours()));
+            bag.push(":");
+            bag.push(this.padLeft2(date.getMinutes()));
+            bag.push(":");
+            bag.push(this.padLeft2(date.getSeconds()));
+
+            if (date.getMilliseconds()) {
+                bag.push(".");
+                bag.push(this.pad(date.getMilliseconds(), 3));
+            }
+
+            if (!skipTimezone) {
+                bag.push("Z");
+            }
+        }
+
+        return bag.join("");
     }
 };
+
+// const DevExpressData = DevExpress.data;
+// DevExpress.default.data.utils.odata
+console.log("===================11=====34=====33===");
+// console.log(DevExpress.data.utils);
+// console.log(DevExpress.default.data.utils.odata);
+console.log(DevExpress.localization);
 
 class SteedosDriverFilter {
 
@@ -284,12 +416,13 @@ class SteedosDriverFilter {
         return this.compileBinary(criteria);
     }
     
-
-
     formatFiltersToODataQuery(): string {
+        // 转换filters为odata串
         let filters = this.filters;
         console.log("formatFiltersToODataQuery=========", filters);
-        // 转换filters为odata串
+        // let query = this.compileCore(filters);
+        // console.log("formatFiltersToODataQuery====query=====", query);
+        // return query;
         return "(name eq 'ptr') and (title eq 'PTR')";
     }
 }
