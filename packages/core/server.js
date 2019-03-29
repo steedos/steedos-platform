87e95ca0d69923c9797baefdf395feaa17d7401c
baozhoutao <baozhoutao@hotoa.com>
2019-03-29 14:18:31 +0800

    update server.js

--- packages/core/server.js
+++ packages/core/server.js
@@ -5,7 +5,10 @@ const graphqlHTTP = require('express-graphql');
 
 let getRolesfromCreator = async (userId)=>{
     console.log(`get ${userId} roles`);
-    var roles = []
+    var roles = ['user']
+    // if(isSpaceAdmin(userId)){
+    //     roles.push('admin')
+    // }
     let userRoles = await steedosSchema.getObject("permission_set").find({filters: `users eq '${userId}'`, fields: ['_id', 'name']})
     if(userRoles){
         userRoles.forEach((role) => {
@@ -37,7 +40,7 @@ steedosSchema.connect().then(function(){
 
     app.use(function(req, res, next){
         //TODO 处理userId
-        // req.userId = 'test_user_id'
+        // req.userId = '5199da558e296a4ba0000001'
         next();
     })
 
