5c7b3c325e2743e857dc09812f2c41294a669843
yinlainghui <yinlianghui@hotoa.com>
2019-03-29 13:30:38 +0800

    add multi sort test

--- packages/objectql/test/unit/driver/mongo/sort.ts
+++ packages/objectql/test/unit/driver/mongo/sort.ts
@@ -58,4 +58,32 @@ describe('fetch records with sort arguments as a string that comply with odata-v
         expect(result[0].name).to.be.eq("ptr");
         expect(result[1].name).to.be.eq("cnpc");
     });
+
+    it('multi sort', async () => {
+
+        let mongo = new SteedosMongoDriver({ url: "mongodb://127.0.0.1/steedos" });
+        await mongo.connect();
+        await mongo.insert(tableName, { _id: "cnpc1", name: "cnpc", title: "CNPC", count: 68 })
+        await mongo.insert(tableName, { _id: "cnpc2", name: "cnpc", title: "CNPC", count: 30 })
+        await mongo.insert(tableName, { _id: "ptr1", name: "ptr", title: "PTR", count: 32 })
+        await mongo.insert(tableName, { _id: "ptr2", name: "ptr", title: "PTR", count: 96 })
+
+        let queryOptions = {
+            fields: ["name"],
+            sort: 'name desc,count'
+        };
+        let result = await mongo.find(tableName, queryOptions);
+        console.log("fetch records with sort arguments result:");
+        console.log(result);
+
+        await mongo.delete(tableName, "ptr1");
+        await mongo.delete(tableName, "cnpc1");
+        await mongo.delete(tableName, "ptr2");
+        await mongo.delete(tableName, "cnpc2");
+        expect(result).to.be.length(4);
+        expect(result[0]._id).to.be.eq("ptr1");
+        expect(result[1]._id).to.be.eq("ptr2");
+        expect(result[2]._id).to.be.eq("cnpc2");
+        expect(result[3]._id).to.be.eq("cnpc1");
+    });
 });
