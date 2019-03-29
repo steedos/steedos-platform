82e2c473860ae3cd045297c89d9b843c5d0bff5c
baozhoutao <baozhoutao@hotoa.com>
2019-03-29 14:10:09 +0800

    修改server.js

--- packages/core/server.js
+++ packages/core/server.js
@@ -2,12 +2,26 @@ var SteedosSchema = require("@steedos/objectql").SteedosSchema
 var _ = require('underscore');
 const graphqlHTTP = require('express-graphql');
 
+
+let getRolesfromCreator = async (userId)=>{
+    console.log(`get ${userId} roles`);
+    var roles = []
+    let userRoles = await steedosSchema.getObject("permission_set").find({filters: `users eq '${userId}'`, fields: ['_id', 'name']})
+    if(userRoles){
+        userRoles.forEach((role) => {
+            roles.push(role.name)
+        });
+    }
+    return roles
+}
+
 let steedosSchema = new SteedosSchema({
     objects: {},
     datasource: {
         driver: 'mongo',
         url: 'mongodb://127.0.0.1/steedos'
-    }
+    },
+    getRoles: getRolesfromCreator
 })
 
 steedosSchema.use(__dirname + "/../standard-objects");
@@ -20,6 +34,13 @@ steedosSchema.connect().then(function(){
 
     let express = require('express');
     let app = express();
+
+    app.use(function(req, res, next){
+        //TODO 处理userId
+        // req.userId = 'test_user_id'
+        next();
+    })
+
     app.use('/graphql', graphqlHTTP({
         schema: graphqlSchema,
         graphiql: true
