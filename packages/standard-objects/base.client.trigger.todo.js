99075d96a7ddfbf20cfa13785c0f51f896e697d6
baozhoutao <baozhoutao@hotoa.com>
2019-04-10 10:11:00 +0800

    添加base object sharing，audit，client相关trigger定义， 未启用

---
+++ packages/standard-objects/base.client.trigger.todo.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    name: 'baseClientTrigger',
+  
+    listenTo: 'base',
+
+    on: 'client',
+  
+    beforeInsert: function(userId, doc) {
+        return doc.space = Session.get("spaceId");
+    }
+  }
