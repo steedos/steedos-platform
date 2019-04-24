ebbc6ed18ebd7ee905044d3267fdf10840c0f252
yinlainghui <yinlianghui@hotoa.com>
2019-04-24 18:01:01 +0800

    #24 增加sort支持

---
+++ packages/objectql/test/unit/driver/sqlserver/sort.ts
@@ -0,0 +1,144 @@
+import { SteedosSchema, SteedosSqlServerDriver, SteedosQueryOptions, SteedosDatabaseDriverType } from '../../../../src';
+import { expect } from 'chai';
+
+let url = process.env.DRIVER_SQLSERVER_URL;//不提供url值时不运行单元测试
+let tableName = "TestSortForSqlserver";
+let driver: SteedosSqlServerDriver;
+
+describe('fetch records for sqlserver with sort arguments as a string that comply with odata-v4 protocol', () => {
+    if (!url) {
+        return true;
+    }
+    try {
+        require("mssql");
+    }
+    catch (ex) {
+        return true;
+    }
+    let result: any;
+    let expected: any;
+    let testIndex: number = 0;
+    
+    let tests = [
+        {
+            title: "sort asc as default",
+            options: {
+                fields: ["id", "name"],
+                sort: 'name'
+            },
+            expected:{
+                length: 4,
+                firstRecordId: "cnpc1"
+            }
+        },
+        {
+            title: "sort asc",
+            options: {
+                fields: ["id", "name"],
+                sort: 'name asc'
+            },
+            expected: {
+                length: 4,
+                firstRecordId: "cnpc1"
+            }
+        },
+        {
+            title: "sort desc",
+            options: {
+                fields: ["id", "name"],
+                sort: 'name desc'
+            },
+            expected: {
+                length: 4,
+                firstRecordId: "ptr1"
+            }
+        },
+        {
+            title: "multi sort",
+            options: {
+                fields: ["id", "name"],
+                sort: 'name,count desc'
+            },
+            expected: {
+                length: 4,
+                firstRecordId: "cnpc2"
+            }
+        },
+        {
+            title: "multi sort error correction",
+            options: {
+                fields: ["id", "name"],
+                sort: 'name, count desc,, '
+            },
+            expected: {
+                length: 4,
+                firstRecordId: "cnpc2"
+            }
+        }
+    ];
+
+    before(async () => {
+        let mySchema = new SteedosSchema({
+            datasources: {
+                default: {
+                    url: url,
+                    driver: SteedosDatabaseDriverType.SqlServer,
+                    objects: {
+                        test: {
+                            label: 'SqlServer Schema',
+                            tableName: tableName,
+                            fields: {
+                                id: {
+                                    label: '主键',
+                                    type: 'text',
+                                    primary: true
+                                },
+                                name: {
+                                    label: '名称',
+                                    type: 'text'
+                                },
+                                title: {
+                                    label: '标题',
+                                    type: 'text'
+                                },
+                                count: {
+                                    label: '数量',
+                                    type: 'number'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        const datasource = mySchema.getDataSource("default");
+        await datasource.createTables();
+        driver = <SteedosSqlServerDriver>datasource.adapter;
+    });
+
+    beforeEach(async () => {
+        await driver.insert(tableName, { id: "cnpc1", name: "cnpc", title: "CNPC", count: 68 });
+        await driver.insert(tableName, { id: "cnpc2", name: "cnpc", title: "CNPC", count: 130 });
+        await driver.insert(tableName, { id: "ptr1", name: "ptr", title: "PTR", count: 32 });
+        await driver.insert(tableName, { id: "ptr2", name: "ptr", title: "PTR", count: 96 });
+        
+        let queryOptions: SteedosQueryOptions = tests[testIndex].options;
+        expected = tests[testIndex].expected;
+        result = await driver.find(tableName, queryOptions);
+    });
+
+    afterEach(async () => {
+        await driver.delete(tableName, "cnpc1");
+        await driver.delete(tableName, "cnpc2");
+        await driver.delete(tableName, "ptr1");
+        await driver.delete(tableName, "ptr2");
+    });
+
+    tests.forEach(async (test) => {
+        it(`${test.title}`, async () => {
+            testIndex++;
+            expect(result).to.be.length(expected.length);
+            expect(result[0].id).to.be.eq(expected.firstRecordId);
+        });
+    });
+});
