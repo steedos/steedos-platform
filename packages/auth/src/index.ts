ba0a98855a3c671d39632292f76e3374909b9d01
sunhaolin <sunhaolin@hotoa.com>
2019-04-28 14:39:47 +0800

    提供getSeesion方法
    https://github.com/steedos/object-server/issues/40

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,9 +1,33 @@
+import crypto = require('crypto');
+import { getSteedosSchema } from '@steedos/objectql';
+
 const sessions = {};
 
-export function addSessionToCache(token, session){
-    sessions[token] = session;
+function _hashLoginToken(token) {
+  const hash = crypto.createHash('sha256');
+  hash.update(token);
+  return hash.digest('base64');
 }
 
-export function getSessionFromCache(token){
-    return sessions[token];
+export function addSessionToCache(token, session) {
+  sessions[token] = session;
+}
+
+export function getSessionFromCache(token) {
+  return sessions[token];
+}
+
+export async function getSession(userId, token) {
+  if (getSessionFromCache(token)) {
+    return getSessionFromCache(token);
+  } else {
+    let hashedToken = _hashLoginToken(token);
+    let filters = `(_id eq '${userId}') and ('services.resume.loginTokens.hashedToken' eq '${hashedToken}')`;
+    console.log('filters:', filters)
+    let user = await getSteedosSchema().getObject('users').find({ filters: filters, fields: ['name', 'steedos_id', 'email'] })
+    let session = { name: user.name, userId: user._id, steedos_id: user.steedos_id, email: user.email, token: token }
+    addSessionToCache(token, session)
+    return session
+  }
+
 }
