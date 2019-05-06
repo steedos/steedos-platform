e294073a1fdc9ffc851da2f740bc071f53966d7b
yinlainghui <yinlianghui@hotoa.com>
2019-05-06 14:23:28 +0800

    Update init.js

--- packages/objectql/test/init.js
+++ packages/objectql/test/init.js
@@ -9,4 +9,6 @@ process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json');
 // process.env.DRIVER_SQLSERVER_URL = "mssql://sa:hotoainc.@192.168.0.190/driver-test";
 // process.env.DRIVER_SQLSERVER_URL = "mssql://sa:hotoainc.@192.168.0.78/driver-test";
 // process.env.DRIVER_SQLSERVER_TDS = "7_2";//7_2为2005,7_4为2012+,默认为7_4
+// process.env.DRIVER_POSTGRESQL_URL = "postgresql://postgres:hotoainc.@192.168.0.195/driver-test";
+
 
