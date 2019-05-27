1846591f8982c66ecc54e6a20eccce36ea41bdfb
baozhoutao <baozhoutao@hotoa.com>
2019-05-27 18:09:34 +0800

    #71 支持steedos-config.yml

--- packages/meteor-bundle-runner/index.js
+++ packages/meteor-bundle-runner/index.js
@@ -37,7 +37,7 @@ var getServerDir = function(){
 
 var getSettings = function(){
   
-  var settingsFileName = 'settings.yml'
+  var settingsFileName = 'steedos-config.yml'
   var settingsPath = path.join(projectDir, settingsFileName)
   var settings = {
   };
@@ -45,7 +45,7 @@ var getSettings = function(){
     try {
       settings = yaml.safeLoad(fs.readFileSync(settingsPath, 'utf8'));
     } catch (error) {
-      console.error(`Invalid settings.yml`, error)
+      console.error(`Invalid steedos-config.yml`, error)
     }
   }
 
@@ -82,14 +82,54 @@ if (!rootUrl) {
 }
 
 var mongoUrl =  process.env.MONGO_URL;       
-if (!mongoUrl) {  
-  mongoUrl = "mongodb://127.0.0.1/steedos";
+if (!mongoUrl) {
+  var _mongoHost = '127.0.0.1';
+  var _mongoDatabase = 'steedos';
+  var _mongoPort = '';
+  var _mongoUrl = '';
+  if(settings.datasources && settings.datasources.default){
+    var defaultDataSource = settings.datasources.default.connection
+    if(defaultDataSource.url){
+      _mongoUrl = defaultDataSource.url
+    }else{
+      if(defaultDataSource.port){
+        _mongoPort = defaultDataSource.port
+      }
+      if(defaultDataSource.host){
+        _mongoHost = defaultDataSource.host
+      }
+      if(defaultDataSource.database){
+        _mongoDatabase = defaultDataSource.database
+      }
+    }
+  }
+  if(_mongoUrl){
+    mongoUrl = _mongoUrl
+  }else if(_mongoPort){
+    mongoUrl = `mongodb://${_mongoHost}:${_mongoPort}/${_mongoDatabase}`
+  }else{
+    mongoUrl = `mongodb://${_mongoHost}/${_mongoDatabase}`
+  }
   process.env.MONGO_URL = mongoUrl
 }
+
+var ref, ref1, ref2, ref3;
+var steedosStorageDir = "./storage"
+var cfsSettings = typeof settings !== "undefined" && settings !== null ? (ref = settings["public"]) != null ? ref.cfs : void 0 : void 0;
+if (cfsSettings && (cfsSettings != null ? (ref1 = cfsSettings.local) != null ? ref1.folder : void 0 : void 0)) {
+  steedosStorageDir = cfsSettings.local.folder;
+}
+
+if (!path.isAbsolute(steedosStorageDir)) {
+  steedosStorageDir = path.resolve(steedosStorageDir);
+}
+
+process.env.STEEDOS_STORAGE_DIR = steedosStorageDir
 
 __steedos_bootstrap__ = {
     projectDir: projectDir,
     serverDir: serverDir,
+    storageDir: steedosStorageDir,
     rootUrl: rootUrl,
     mongoUrl: mongoUrl,
     verbose: false
@@ -104,6 +144,10 @@ console.log(`*  ROOT_URL: ${process.env.ROOT_URL}`);
 console.log(`*  MONGO_URL: ${process.env.MONGO_URL}`);
 console.log(`*  PROJECT_DIR: ${projectDir}`);
 console.log(`*  SERVER_DIR: ${serverDir}`);
+var ref, ref1;
+if ((typeof settings !== "undefined" && settings !== null ? (ref2 = settings["public"]) != null ? (ref3 = ref2.cfs) != null ? ref3.store : void 0 : void 0 : void 0) === 'local') {
+  console.log(`*  STORAGE_DIR: ${steedosStorageDir}`);
+}
 console.log("*")
 console.log("*******************************************************************");
 
