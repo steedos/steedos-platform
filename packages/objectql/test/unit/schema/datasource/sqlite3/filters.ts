aa832220c425efaf25a807c91680ce1e711ba501
yinlainghui <yinlianghui@hotoa.com>
2019-06-04 12:04:40 +0800

    增加object.find的filter单元测试

---
+++ packages/objectql/test/unit/schema/datasource/sqlite3/filters.ts
@@ -0,0 +1,279 @@
+// import { SteedosSchema, SteedosSqlite3Driver, SteedosQueryOptions, SteedosDatabaseDriverType } from '../../../../src';
+import { SteedosSchema, SteedosQueryOptions, SteedosDatabaseDriverType, SteedosObjectType } from '../../../../../src';
+import { expect } from 'chai';
+import path = require("path");
+
+let databaseUrl = path.join(__dirname, "sqlite-test.db");
+// let databaseUrl = ':memory:';
+let tableName = "TestFiltersForSqlite3";
+let testObject: SteedosObjectType;
+
+describe('filters for sqlite3 database', () => {
+    try {
+        require("sqlite3");
+    }
+    catch (ex) {
+        return true;
+    }
+    let result: any;
+    let expected: any;
+    let testIndex: number = 0;
+
+    let tests = [
+        {
+            title: "filter records with filters",
+            options: {
+                fields: ["id", "name"],
+                filters: [["name", "=", "ptr"], ["title", "=", "PTR"]]
+            },
+            expected: {
+                length: 1
+            }
+        },
+        {
+            title: "filter records with odata query string",
+            options: {
+                fields: ["id", "name"],
+                filters: "(name eq 'ptr') and (title eq 'PTR')"
+            },
+            expected: {
+                length: 1
+            }
+        },
+        {
+            title: "filter records with empty array",
+            options: {
+                fields: ["id", "name"],
+                filters: []
+            },
+            expected: {
+                length: 2
+            }
+        },
+        {
+            title: "filter records with empty string",
+            options: {
+                fields: ["id", "name"],
+                filters: ""
+            },
+            expected: {
+                length: 2
+            }
+        },
+        {
+            title: "filter records with operator: >",
+            options: {
+                fields: ["id", "name"],
+                filters: [["count", ">", 20]]
+            },
+            expected: {
+                length: 1
+            }
+        },
+        {
+            title: "filter records with operator: >=",
+            options: {
+                fields: ["id", "name"],
+                filters: [["count", ">=", 120]]
+            },
+            expected: {
+                length: 1
+            }
+        },
+        {
+            title: "filter records with operator: <",
+            options: {
+                fields: ["id", "name"],
+                filters: [["count", "<", 100]]
+            },
+            expected: {
+                length: 1
+            }
+        },
+        {
+            title: "filter records with operator: <=",
+            options: {
+                fields: ["id", "name"],
+                filters: [["count", "<=", 18]]
+            },
+            expected: {
+                length: 1
+            }
+        },
+        // {
+        //     title: "filter records with operator: between",
+        //     options: {
+        //         fields: ["id", "name"],
+        //         filters: [["count", "between", [10, 100]]]
+        //     },
+        //     expected: {
+        //         length: 1
+        //     }
+        // },
+        {
+            title: "filter records with operator: startswith",
+            options: {
+                fields: ["id", "name"],
+                filters: [["name", "startswith", "cn"]]
+            },
+            expected: {
+                length: 1
+            }
+        },
+        {
+            title: "filter records with operator: notstartswith",
+            options: {
+                fields: ["id", "name"],
+                filters: [["name", "notstartswith", "cn"]]
+            },
+            expected: {
+                length: 1
+            }
+        },
+        {
+            title: "filter records with operator: endswith",
+            options: {
+                fields: ["id", "name"],
+                filters: [["name", "endswith", "pc"]]
+            },
+            expected: {
+                length: 1
+            }
+        },
+        {
+            title: "filter records with operator: notendswith",
+            options: {
+                fields: ["id", "name"],
+                filters: [["name", "notendswith", "pc"]]
+            },
+            expected: {
+                length: 1
+            }
+        },
+        {
+            title: "filter records with operator: contains",
+            options: {
+                fields: ["id", "name"],
+                filters: [["name", "contains", "p"]]
+            },
+            expected: {
+                length: 2
+            }
+        },
+        {
+            title: "filter records with operator: notcontains",
+            options: {
+                fields: ["id", "name"],
+                filters: [["name", "notcontains", "cn"]]
+            },
+            expected: {
+                length: 1
+            }
+        },
+        {
+            title: "records count with filters",
+            function: "count",
+            options: {
+                fields: ["id", "name"],
+                filters: [["name", "=", "ptr"], ["title", "=", "PTR"]]
+            },
+            expected: {
+                eq: 1
+            }
+        },
+        {
+            title: "records count with odata query string",
+            function: "count",
+            options: {
+                fields: ["id", "name"],
+                filters: "(name eq 'ptr') and (title eq 'PTR')"
+            },
+            expected: {
+                eq: 1
+            }
+        }
+    ];
+
+    before(async () => {
+        let mySchema = new SteedosSchema({
+            datasources: {
+                default: {
+                    driver: SteedosDatabaseDriverType.Sqlite,
+                    url: databaseUrl,
+                    objects: {
+                        test: {
+                            label: 'Sqlite3 Schema',
+                            tableName: tableName,
+                            fields: {
+                                id: {
+                                    label: '主键',
+                                    type: 'text',
+                                    primary: true
+                                },
+                                name: {
+                                    label: '名称',
+                                    type: 'text'
+                                },
+                                title: {
+                                    label: '标题',
+                                    type: 'text'
+                                },
+                                count: {
+                                    label: '数量',
+                                    type: 'number'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        const datasources = mySchema.getDataSources();
+        for (let name in datasources) {
+            await datasources[name].dropEntities();
+            await datasources[name].createTables();
+        }
+        testObject = mySchema.getObject('test');
+    });
+
+    beforeEach(async () => {
+        await testObject.insert({ id: "ptr", name: "ptr", title: "PTR", count: 120 });
+        await testObject.insert({ id: "cnpc", name: "cnpc", title: "CNPC", count: 18 });
+
+        let queryOptions: SteedosQueryOptions = tests[testIndex].options;
+        expected = tests[testIndex].expected;
+        let functionName: string = tests[testIndex].function;
+        try {
+            if (functionName){
+                result = await testObject[functionName](queryOptions);
+            }
+            else{
+                result = await testObject.find(queryOptions).catch((ex: any) => { console.error(ex); return false; });
+            }
+        }
+        catch (ex) {
+            result = ex;
+        }
+    });
+
+    afterEach(async () => {
+        await testObject.delete("ptr");
+        await testObject.delete("cnpc");
+    });
+
+    tests.forEach(async (test) => {
+        it(`${test.title}`, async () => {
+            testIndex++;
+            if (expected.error !== undefined) {
+                expect(result.message).to.be.eq(expected.error);
+            }
+            if (expected.length !== undefined) {
+                expect(result).to.be.length(expected.length);
+            }
+            if (expected.eq !== undefined) {
+                expect(result).to.be.eq(expected.eq);
+            }
+        });
+    });
+});
+
