3c2259b6fd1ebaec6285716f7648c673a4e31450
yinlainghui <yinlianghui@hotoa.com>
2019-06-06 10:04:34 +0800

    #65 实现简单的报表列表，点击即可进入报表详细

--- packages/react-components/src/components/report-list/index.js
+++ packages/react-components/src/components/report-list/index.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
-
+import './index.css';
 class ReportList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: []
+        };
+    }
+
     render() {
-        return <div id="report-list"></div>;
+        let list = this.state.list;
+        var items = list.map(function (item) {
+            return (
+                <a className="report-list-item" key={item._id} href={item._id}>
+                    {item.name}
+                </a>
+            );
+        }, this);
+        return (
+            <div className="report-list">
+                {items}
+            </div>
+        );
     }          
 
     async componentDidMount(){
@@ -11,7 +30,14 @@ class ReportList extends Component {
                 'Content-Type': 'application/json'
             }
         });
-        let reportList = await response.json();
+        let reports = await response.json();
+        let list = [];
+        for (let key in reports){
+            list.push(reports[key]);
+        }
+        this.setState(state => ({
+            list: list
+        }));
     }
 }
 
