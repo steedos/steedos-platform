2b2456e770b6ae5fc6c5a6ff6c77e77dc1f43bbf
sunhaolin <sunhaolin@hotoa.com>
2019-06-14 20:53:22 +0800

    @steedos/standard-objects@0.0.4
    creator到华信的推送接口

--- packages/standard-objects/core.objectwebhooks.trigger.js
+++ packages/standard-objects/core.objectwebhooks.trigger.js
@@ -4,25 +4,227 @@ module.exports = {
 
     listenTo: 'core',
 
-    afterInsert: function (userId, doc) {
-        var Fiber = require("fibers");
-        var object_name = this.object_name;
-        Fiber(function () {
-            Creator.objectWebhooksPreSend(userId, doc, object_name, 'create')
-        }).run();
+    afterInsert: async function () {
+
+        if (!ObjectWebhooksQueue) {
+            console.error('not found ObjectWebhooksQueue');
+            return;
+        }
+
+        let owCollection = this.getObject('object_webhooks');
+        if (!owCollection) {
+            console.error('not found collection object_webhooks');
+            return;
+        }
+
+        let action = 'create';
+        let userId = this.userId;
+        let doc = this.doc;
+        let object_name = this.datasource_name + '.' + this.object_name;
+        let obj = this.getObject(object_name);
+        let recordId = this.doc[obj.idFieldName];
+        let userObj = this.getObject('users');
+        let actionUserInfo = await userObj.findOne(userId, {
+            fields: ['name']
+        });
+        let redirectUrl = Steedos.absoluteUrl(Creator.getObjectUrl(object_name, recordId, object_name));
+        let objWebhooks = await owCollection.find({
+            filters: `(object_name eq '${object_name}') and (active eq true) and (events eq '${action}')`
+        });
+
+        for (let idx = 0; idx < objWebhooks.length; idx++) {
+            let oh = objWebhooks[idx];
+            var data;
+            data = {};
+            if (!oh.fields || oh.fields.length === 0) {
+                data = doc;
+            } else {
+                for (let i = 0; i < oh.fields.length; i++) {
+                    var objField, refCollection, refFilterFields, refNameFieldKey, refRecord;
+                    var fieldName = oh.fields[i];
+                    objField = obj.getField(fieldName);
+                    if (objField.type === 'lookup' && _.isString(objField.reference_to) && !objField.multiple) {
+                        refCollection = this.getObject(objField.reference_to);
+                        if (refCollection) {
+                            refNameFieldKey = refCollection.NAME_FIELD_KEY;
+                            refFilterFields = [];
+                            refFilterFields.push(refNameFieldKey);
+                            refRecord = await refCollection.findOne(doc[fieldName], {
+                                fields: refFilterFields
+                            });
+                            if (refRecord) {
+                                data[fieldName] = refRecord[refNameFieldKey];
+                            }
+                        }
+                    } else {
+                        data[fieldName] = doc[fieldName];
+                    }
+                };
+            }
+
+            data._id = recordId;
+            var Fiber = require("fibers");
+            Fiber(function () {
+                ObjectWebhooksQueue.send({
+                    data: data,
+                    payload_url: oh.payload_url,
+                    content_type: oh.content_type,
+                    action: action,
+                    actionUserInfo: actionUserInfo,
+                    objectName: object_name,
+                    objectDisplayName: obj.label,
+                    nameFieldKey: obj.NAME_FIELD_KEY,
+                    redirectUrl: redirectUrl
+                });
+            }).run();
+        }
     },
-    afterUpdate: function (userId, doc, fieldNames, modifier, options) {
-        var Fiber = require("fibers");
-        var object_name = this.object_name;
-        Fiber(function () {
-            Creator.objectWebhooksPreSend(userId, doc, object_name, 'update')
-        }).run();
+    afterUpdate: async function () {
+        if (!ObjectWebhooksQueue) {
+            console.error('not found ObjectWebhooksQueue');
+            return;
+        }
+
+        let owCollection = this.getObject('object_webhooks');
+        if (!owCollection) {
+            console.error('not found collection object_webhooks');
+            return;
+        }
+
+        let action = 'update';
+        let userId = this.userId;
+        let doc = this.doc;
+        let object_name = this.datasource_name + '.' + this.object_name;
+        let recordId = this.id;
+        let obj = this.getObject(object_name);
+        let userObj = this.getObject('users');
+        let actionUserInfo = await userObj.findOne(userId, {
+            fields: ['name']
+        });
+        let redirectUrl = Steedos.absoluteUrl(Creator.getObjectUrl(object_name, recordId, object_name));
+        let objWebhooks = await owCollection.find({
+            filters: `(object_name eq '${object_name}') and (active eq true) and (events eq '${action}')`
+        });
+
+        for (let idx = 0; idx < objWebhooks.length; idx++) {
+            let oh = objWebhooks[idx];
+            var data;
+            data = {};
+            if (!oh.fields || oh.fields.length === 0) {
+                data = doc;
+            } else {
+                for (let i = 0; i < oh.fields.length; i++) {
+                    var objField, refCollection, refFilterFields, refNameFieldKey, refRecord;
+                    var fieldName = oh.fields[i];
+                    objField = obj.getField(fieldName);
+                    if (objField.type === 'lookup' && _.isString(objField.reference_to) && !objField.multiple) {
+                        refCollection = this.getObject(objField.reference_to);
+                        if (refCollection) {
+                            refNameFieldKey = refCollection.NAME_FIELD_KEY;
+                            refFilterFields = [];
+                            refFilterFields.push(refNameFieldKey);
+                            refRecord = await refCollection.findOne(doc[fieldName], {
+                                fields: refFilterFields
+                            });
+                            if (refRecord) {
+                                data[fieldName] = refRecord[refNameFieldKey];
+                            }
+                        }
+                    } else {
+                        data[fieldName] = doc[fieldName];
+                    }
+                };
+            }
+
+            data._id = recordId;
+            var Fiber = require("fibers");
+            Fiber(function () {
+                ObjectWebhooksQueue.send({
+                    data: data,
+                    payload_url: oh.payload_url,
+                    content_type: oh.content_type,
+                    action: action,
+                    actionUserInfo: actionUserInfo,
+                    objectName: object_name,
+                    objectDisplayName: obj.label,
+                    nameFieldKey: obj.NAME_FIELD_KEY,
+                    redirectUrl: redirectUrl
+                });
+            }).run();
+        }
     },
-    afterDelete: function (userId, doc) {
-        var Fiber = require("fibers");
-        var object_name = this.object_name;
-        Fiber(function () {
-            Creator.objectWebhooksPreSend(userId, doc, object_name, 'delete')
-        }).run();
+    afterDelete: async function () {
+
+        if (!ObjectWebhooksQueue) {
+            console.error('not found ObjectWebhooksQueue');
+            return;
+        }
+
+        let owCollection = this.getObject('object_webhooks');
+        if (!owCollection) {
+            console.error('not found collection object_webhooks');
+            return;
+        }
+
+        let action = 'delete';
+        let userId = this.userId;
+        let doc = this.previousDoc;
+        let object_name = this.datasource_name + '.' + this.object_name;
+        let recordId = this.id;
+        let obj = this.getObject(object_name);
+        let userObj = this.getObject('users');
+        let actionUserInfo = await userObj.findOne(userId, {
+            fields: ['name']
+        });
+        let redirectUrl = Steedos.absoluteUrl(Creator.getObjectUrl(object_name, recordId, object_name));
+        let objWebhooks = await owCollection.find({
+            filters: `(object_name eq '${object_name}') and (active eq true) and (events eq '${action}')`
+        });
+        for (let idx = 0; idx < objWebhooks.length; idx++) {
+            let oh = objWebhooks[idx];
+            var data;
+            data = {};
+            if (!oh.fields || oh.fields.length === 0) {
+                data = doc;
+            } else {
+                for (let i = 0; i < oh.fields.length; i++) {
+                    var objField, refCollection, refFilterFields, refNameFieldKey, refRecord;
+                    var fieldName = oh.fields[i];
+                    objField = obj.getField(fieldName);
+                    if (objField.type === 'lookup' && _.isString(objField.reference_to) && !objField.multiple) {
+                        refCollection = this.getObject(objField.reference_to);
+                        if (refCollection) {
+                            refNameFieldKey = refCollection.NAME_FIELD_KEY;
+                            refFilterFields = [];
+                            refFilterFields.push(refNameFieldKey);
+                            refRecord = await refCollection.findOne(doc[fieldName], {
+                                fields: refFilterFields
+                            });
+                            if (refRecord) {
+                                data[fieldName] = refRecord[refNameFieldKey];
+                            }
+                        }
+                    } else {
+                        data[fieldName] = doc[fieldName];
+                    }
+                };
+            }
+
+            data._id = recordId;
+            var Fiber = require("fibers");
+            Fiber(function () {
+                ObjectWebhooksQueue.send({
+                    data: data,
+                    payload_url: oh.payload_url,
+                    content_type: oh.content_type,
+                    action: action,
+                    actionUserInfo: actionUserInfo,
+                    objectName: object_name,
+                    objectDisplayName: obj.label,
+                    nameFieldKey: obj.NAME_FIELD_KEY,
+                    redirectUrl: redirectUrl
+                });
+            }).run();
+        }
     }
 }
