41ab063c9e79ff6cbf49849bf48ab3ca004d2b80
baozhoutao <baozhoutao@hotoa.com>
2019-06-17 17:34:12 +0800

    清理无用的属性

--- packages/cli/src/create/IProjectOptions.ts
+++ packages/cli/src/create/IProjectOptions.ts
@@ -1,12 +1,3 @@
 export class IProjectOptions {
-    public host: string = "";
-    public port: number = 0;
-    public databaseName: string = "";
-    public user: string = "";
-    public password: string = "";
-    public databaseType: string = "";
-    public schemaName: string = "";
-    public ssl: boolean = false;
     public name: string = '';
-    public vsersion: string = '';
 }
