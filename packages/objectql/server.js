f45bf7a383ba8a4063b07932f44a6d72549804cb
sunhaolin <sunhaolin@hotoa.com>
2019-07-18 14:13:13 +0800

    @steedos/objectql@0.1.2

--- packages/objectql/server.js
+++ packages/objectql/server.js
@@ -1,31 +1,59 @@
 var objectql = require("@steedos/objectql")
 var _ = require('underscore');
 const graphqlHTTP = require('express-graphql');
+const Cookies = require('cookies');
+const steedosAuth = require("@steedos/auth");
 
 // const schemaConfig = require(process.cwd() + '/steedos.config.js')
 
-let steedosSchema = new objectql.SteedosSchema({
-    datasources: {
-        default: {
-            driver: 'meteor-mongo',
-            url: 'mongodb://127.0.0.1/steedos',
-            objectFiles: [__dirname + "/../standard-objects"]
-        }
-    },
+let steedosSchema = objectql.getSteedosSchema();
+steedosSchema.addDataSource('default', {
+    driver: 'mongo',
+    url: 'mongodb://127.0.0.1/qhd-beta',
+    objectFiles: [__dirname + "/../standard-objects"]
 })
+
 let express = require('express');
 let app = express();
-app.use(function (req, res, next) {
-    //TODO 处理userId
-    next();
+router = express.Router();
+
+router.use('/:dataSourceName/:spaceId', async function (req, res, next) {
+    var authToken, user, cookies;
+    cookies = new Cookies(req, res);
+    authToken = req.headers['x-auth-token'] || cookies.get("X-Auth-Token");
+    let spaceId = req.params.spaceId;
+    console.log('spaceId:::::::::: ', spaceId);
+    user = null;
+    if (authToken) {
+        user = await steedosAuth.getSession(authToken, spaceId);
+    }
+    if (user) {
+        console.log('user::::::::::::::: ', user);
+        // 因为没有spaceId,无法获取roles，暂不启用权限校验
+        req.userSession = user;
+        return next();
+    } else {
+        return res.status(401).send({
+            errors: [
+                {
+                    'message': 'You must be logged in to do this.'
+                }
+            ]
+        });
+    }
 })
 
 _.each(steedosSchema.getDataSources(), function (datasource, name) {
-    app.use(`/graphql/${name}`, graphqlHTTP({
+    console.log('name::::::::: ', name);
+    console.log('datasource::::::::: ', datasource.driver);
+    router.use(`/${name}/:spaceId`, graphqlHTTP({
         schema: datasource.buildGraphQLSchema(),
         graphiql: true
     }));
 })
 
+app.use('/graphql', router);
 
-app.listen(process.env.PORT || 3000)
+
+app.listen(process.env.PORT || 5000)
+console.log('app listenning porst 5000!');
