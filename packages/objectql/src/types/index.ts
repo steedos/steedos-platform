982f41f595c7afee6e804c66a026286965ef5e55
sunhaolin <sunhaolin@hotoa.com>
2019-08-10 15:47:39 +0800

    @steedos/objectql@0.2.3

--- packages/objectql/src/types/index.ts
+++ packages/objectql/src/types/index.ts
@@ -8,12 +8,12 @@ export { SteedosFieldType, SteedosFieldTypeConfig } from "./field";
 export { SteedosListenerConfig } from './listeners'
 export { SteedosTriggerType } from './trigger'
 export { SteedosObjectListViewTypeConfig, SteedosObjectListViewType } from "./list_view";
-export { SteedosIDType } from  "./field_types";
+export { SteedosIDType } from "./field_types";
 export { SteedosQueryOptions } from "./query";
 export { SteedosObjectPermissionType, SteedosObjectPermissionTypeConfig } from "./object_permission";
-export { SteedosActionType, SteedosActionTypeConfig} from './action'
-export { SteedosAppType, SteedosAppTypeConfig} from './app'
-export { SteedosUserSession} from './userSession';
+export { SteedosActionType, SteedosActionTypeConfig } from './action'
+export { SteedosAppType, SteedosAppTypeConfig } from './app'
+export { SteedosUserSession, SteedosUserSessionSpace, SteedosUserSessionCompany, SteedosUserSessionOrganization } from './userSession';
 export { SteedosQueryFilters } from './query';
 export { SteedosReportType, SteedosReportTypeConfig } from './report';
 
--- packages/objectql/src/types/userSession.ts
+++ packages/objectql/src/types/userSession.ts
@@ -1,12 +1,34 @@
 import { SteedosIDType } from ".";
 
+export type SteedosUserSessionSpace = {
+    _id: string,
+    name: string
+}
+
+export type SteedosUserSessionCompany = {
+    _id: string,
+    name: string
+}
+
+export type SteedosUserSessionOrganization = {
+    _id: string,
+    name: string,
+    fullname: string
+}
+
 export type SteedosUserSession = {
     userId: SteedosIDType,
     spaceId: string,
-    roles: string[],
     name: string,
-    steedos_id?: string,
+    username?: string,
+    mobile?: string,
     email?: string,
-    companyId?: string,
-    companyIds?: string[]
+    utcOffset?: number,
+    roles: string[],
+    space: SteedosUserSessionSpace,
+    spaces: SteedosUserSessionSpace[],
+    company?: SteedosUserSessionCompany,
+    companies?: SteedosUserSessionCompany[],
+    organization: SteedosUserSessionOrganization,
+    organizations: SteedosUserSessionOrganization[],
 }
