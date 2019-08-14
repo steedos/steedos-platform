d3a3bc984e06c995aa32201adbe037b5218d10c3
yinlainghui <yinlianghui@hotoa.com>
2019-08-14 10:25:32 +0800

    fix DevExtreme的 formatISO8601函数没用使用utc时间--使用npm-postinstall方式

--- creator/.scripts/npm-postinstall.js
+++ creator/.scripts/npm-postinstall.js
@@ -3,3 +3,6 @@ const { execSync } = require('child_process');
 console.log("Running npm-postinstall.js");
 
 //execSync('cp node_modules/devextreme/dist/css/fonts/* public/fonts/devextreme/');
+
+// fix DevExtreme的 formatISO8601函数没用使用utc时间
+execSync('cp -r .scripts/devextreme/* node_modules/devextreme/');
