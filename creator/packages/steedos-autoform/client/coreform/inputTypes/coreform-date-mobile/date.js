04400374fc3fa79356811cd926a09bb73595dbb9
yinlainghui <yinlianghui@hotoa.com>
2019-08-16 15:07:22 +0800

    #1140 fix iphone safari有时区问题

--- creator/packages/steedos-autoform/client/coreform/inputTypes/coreform-date-mobile/date.js
+++ creator/packages/steedos-autoform/client/coreform/inputTypes/coreform-date-mobile/date.js
@@ -16,6 +16,21 @@ AutoForm.addInputType("steedos-date-mobile", {
       val = moment(val).format("YYYY-MM-DDTHH:mm:ss");
     }
     return val;
+  },
+  valueOut: function () {
+    // 这里需要写valueOut相关逻辑代码是因为iphone手机上safari浏览器有时区问题,android上看着本来就是好的
+    let value = this.val()
+    let type = this.attr("type");
+    if (type === "date") {
+      // 日期字段数据库中存储的是utc的0点，直接返回即可
+      return value;
+    }
+    else {
+      let m = moment.tz(AutoForm.Utility.dateToNormalizedLocalDateAndTimeString(value), "utc");
+      value = m.toDate();
+      value.setHours(value.getHours() + (value.getTimezoneOffset() / 60));
+      return value
+    }
   }
 });
 
