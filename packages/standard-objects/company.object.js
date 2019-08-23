d6db3bda8c02dadb429198646ca81d33b1707fea
yinlainghui <yinlianghui@hotoa.com>
2019-08-23 13:42:46 +0800

    #124 新增单位时，如果未设置 organization值，自动在根节点创建对应的组织节点与之关联，但是不处理组织节点与company_id关联逻辑。

---
+++ packages/standard-objects/company.object.js
@@ -0,0 +1,39 @@
+Creator.Objects['company'].triggers = {
+    "before.insert.server.default": {
+        on: "server",
+        when: "before.insert",
+        todo: function (userId, doc) {
+            console.log("=======company=========before.insert.server.default====userId========", userId);
+            console.log("=======company=========before.insert.server.default====doc========", doc);
+            
+            var existsCount = Creator.getCollection("company").find({
+                space: doc.space,
+                name: doc.name
+            }).count();
+            if (existsCount > 0) {
+                // throw new Meteor.Error(400, "company_error_company_name_exists");
+                // 还不支持i18n
+                throw new Meteor.Error(400, "该单位名称已经存在");
+            }
+            if (!doc.organization){
+                // 未指定所属组织时自动在根节点新建一个组织，对应上关系
+                var rootOrg = db.organizations.findOne({
+                    space: doc.space,
+                    parent: null
+                }, {
+                    fields: {
+                        _id: 1
+                    }
+                });
+                // 组织的其他属性，比如fullname，parents等在organizations.before.insert，organizations.after.insert处理
+                // 组织的company_id属性，由用户手动点击“更新组织”按钮来触发actions处理
+                doc.organization = db.organizations.insert({
+                    name: doc.name,
+                    parent: rootOrg._id,
+                    space: doc.space,
+                    owner: userId
+                });
+            }
+        }
+    }
+}
