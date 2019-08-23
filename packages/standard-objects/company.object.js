5fbb6264ccc4d34db9f883db93682d15189aed43
yinlainghui <yinlianghui@hotoa.com>
2019-08-23 18:20:02 +0800

    # 124 如果同意，则更新 organizations 表，该节点的company_id。

--- packages/standard-objects/company.object.js
+++ packages/standard-objects/company.object.js
@@ -46,3 +46,66 @@ Creator.Objects['company'].triggers = {
         }
     }
 }
+
+Creator.Objects['company'].methods = {
+    updateOrgs: async function (params) {
+        console.log("====methods=updateOrgs======");
+        console.log("====methods=params======", params);
+        console.log("====methods=this======", this);
+        let company = await this.getObject("company").findOne(this.record_id, {
+            fields: ["organization"]
+        });
+
+        if (!company.organization){
+            throw new Meteor.Error(400, "该单位的关联组织未设置");
+        }
+
+        // let org = await this.getObject("organizations").findOne(company.organization, {
+        //     fields: ["_id"]
+        // });
+
+        await this.getObject("organizations").update(company.organization,{
+            company_id: this.record_id
+        }, this.userSession);
+    }
+}
+
+Creator.Objects['company'].actions = {
+    updateOrgs: {
+        label: "更新组织",
+        visible: true,
+        on: "record",
+        todo: async function (object_name, record_id, fields){
+            var userSession = Creator.USER_CONTEXT;
+            var url = `/api/odata/v4/${userSession.spaceId}/company/${record_id}/updateOrgs`;
+            try {
+                let authorization = `Bearer ${userSession.spaceId},${userSession.authToken}`;
+                let fetchParams = {};
+                const res = await fetch(url, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': authorization
+                    },
+                    method: 'POST',
+                    body: JSON.stringify(fetchParams)
+                });
+                let reJson = await res.json();
+                if(reJson.error){
+                    console.error(reJson.error);
+                    if (reJson.error.reason){
+                        toastr.error(reJson.error.reason)
+                    }
+                    else if (reJson.error.message) {
+                        toastr.error(reJson.error.message)
+                    }
+                }
+                else{
+                    toastr.success("已成功更新组织")
+                }
+            } catch (err) {
+                console.error(err);
+                toastr.error(err)
+            }
+        }
+  }
+}
