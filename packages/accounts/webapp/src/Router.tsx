a728692dae23c4e209994f0e12110134de9b76b0
Jack Zhuang <steedos@users.noreply.github.com>
2019-08-24 23:37:42 +0800

    css to js

--- packages/accounts/webapp/src/Router.tsx
+++ packages/accounts/webapp/src/Router.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { CssBaseline, Grid, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { green, lightBlue } from '@material-ui/core/colors';
 
 import Logo from './components/Logo';
 import Signup from './components/Signup';
@@ -11,8 +14,33 @@ import ResetPassword from './components/ResetPassword';
 import VerifyEmail from './components/VerifyEmail';
 import TwoFactor from './components/TwoFactor';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: lightBlue,
+  },
+});
+
 const useStyles = makeStyles({
   root: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    display: "flex",
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    backgroundImage: "url(" + require("./assets/background.jpg") + ")",
+    backgroundSize: "cover",
+    height: "100%",
+  },
+  rootBackgroundFade: {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+  },
+  rootGrid: {
     margin: 'auto',
     maxWidth: 380,
     minWidth: 320,
@@ -27,22 +55,27 @@ const Router = () => {
 
   return (
     <BrowserRouter basename="/accounts">
-      <Grid container className={classes.root}>
-        <Grid item xs={12}>
-          <Logo></Logo>
-          <Paper className={classes.container}>
-            <CssBaseline />
-            <Route exact path="/" component={Home} />
-            <Route path="/two-factor" component={TwoFactor} />
+      <ThemeProvider theme={theme}>
+        <div className={classes.root}>
+          <Grid container className={classes.rootGrid}>
+            <Grid item xs={12}>
+              <Logo></Logo>
+              <Paper className={classes.container}>
+                <CssBaseline />
+                <Route exact path="/" component={Home} />
+                <Route path="/two-factor" component={TwoFactor} />
 
-            <Route path="/signup" component={Signup} />
-            <Route path="/login" component={Login} />
-            <Route exact path="/reset-password" component={ResetPassword} />
-            <Route path="/reset-password/:token" component={ResetPassword} />
-            <Route path="/verify-email/:token" component={VerifyEmail} />
-          </Paper>
-        </Grid>
-      </Grid>
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route exact path="/reset-password" component={ResetPassword} />
+                <Route path="/reset-password/:token" component={ResetPassword} />
+                <Route path="/verify-email/:token" component={VerifyEmail} />
+              </Paper>
+            </Grid>
+          </Grid>
+          <div className={classes.rootBackgroundFade}></div>
+        </div>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
--- packages/accounts/webapp/src/components/Logo.tsx
+++ packages/accounts/webapp/src/components/Logo.tsx
@@ -6,11 +6,23 @@ interface Props {
   logoUrl?: string;
 }
 
+const useStyles = makeStyles({
+  logo: {
+    width: "100%",
+    height: "auto",
+    maxWidth: 320,
+    display: "block",
+    margin: "0 auto",
+    paddingBottom: 10,
+  },
+});
+
 const Logo = ({ logoUrl }: Props) => {
+  const classes = useStyles();
   if (!logoUrl) 
     logoUrl = require("../assets/logo.png")
   return (
-    <img src={logoUrl} id="logo"></img>
+    <img src={logoUrl} className={classes.logo}></img>
   )
 };
 
