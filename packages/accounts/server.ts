bfaa1819470c28897504c1fe5034a865ed190393
baozhoutao <baozhoutao@hotoa.com>
2019-08-30 10:56:58 +0800

    支持以 steedos plugins 方式运行

--- packages/accounts/server.ts
+++ packages/accounts/server.ts
@@ -2,19 +2,19 @@ import * as express from 'express';
 import * as path from 'path';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
-import router from "./src"
+import { init } from "./src"
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-
 app.get('/', (req, res) => {
   res.redirect("/accounts");
   res.end();
 });
-app.use("/accounts", router);
+
+init({app});
 
 app.listen(4000, () => {
   console.log('Server listening on port 4000');
--- packages/accounts/src/index.ts
+++ packages/accounts/src/index.ts
@@ -1,45 +1,65 @@
 import * as express from 'express';
 import * as path from 'path';
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors';
 import * as mongoose from 'mongoose';
 import { AccountsServer } from '@accounts/server';
 import { AccountsPassword } from '@accounts/password';
-import accountsExpress, { userLoader } from './rest-express';
+import accountsExpress from './rest-express';
 import MongoDBInterface from './database-mongo';
 
-let MONGO_URL = process.env.MONGO_URL;
-if (!MONGO_URL)
-  MONGO_URL = "mongodb://127.0.0.1/steedos"
 
-mongoose.connect(MONGO_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
 
-const accountsServer = new AccountsServer(
-  {
-    db: new MongoDBInterface(db, {
-      convertUserIdToMongoObjectId: false,
-      convertSessionIdToMongoObjectId: false,
-      timestamps: {
-        createdAt: 'created',
-        updatedAt: 'modified',
-      },
-    }),
-    tokenSecret: 'secret',
-  },
-  {
-    password: new AccountsPassword(),
+function getAccountsServer (context){
+  let MONGO_URL = process.env.MONGO_URL;
+  if (!MONGO_URL)
+    MONGO_URL = "mongodb://127.0.0.1/steedos"
+  
+  mongoose.connect(MONGO_URL, { useNewUrlParser: true });
+  const db = mongoose.connection;
+  
+  const accountsServer = new AccountsServer(
+    {
+      db: new MongoDBInterface(db, {
+        convertUserIdToMongoObjectId: false,
+        convertSessionIdToMongoObjectId: false,
+        timestamps: {
+          createdAt: 'created',
+          updatedAt: 'modified',
+        },
+      }),
+      tokenSecret: 'secret',
+    },
+    {
+      password: new AccountsPassword(),
+    }
+  );
+
+  return accountsServer;
+}
+
+function getAccountsRouter(context){
+
+  let accountsServer = getAccountsServer(context)
+
+  const router = accountsExpress(accountsServer, {
+    path: '/api',
+  });
+  
+  router.use("/", express.static(path.join(__dirname, '..', 'webapp', 'build')));
+  router.use("/i18n", express.static(path.join(__dirname, '..', 'webapp', 'src', 'i18n')));
+  router.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'webapp', 'build', 'index.html'));
+  });
+
+  return router
+}
+
+export function init(context){
+  if(context.settings){
+    if(!context.settings.public){
+        context.settings.public = {webservices: {}}
+    }
+    context.settings.public.webservices.accounts = '/accounts'
   }
-);
-
-const router = accountsExpress(accountsServer, {
-  path: '/api',
-});
-
-router.use("/", express.static(path.join(__dirname, '..', 'webapp', 'build')));
-router.use("/i18n", express.static(path.join(__dirname, '..', 'webapp', 'src', 'i18n')));
-router.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'webapp', 'build', 'index.html'));
-});
-
-export default router;
+  let accountsRouter = getAccountsRouter(context)
+  context.app.use("/accounts", accountsRouter)
+}
