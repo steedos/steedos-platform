262c3f72b32b2342864302414603c10f7c29f0f2
庄建国 <zhuangjianguo@hotoa.com>
2019-09-17 15:50:12 +0800

    use objectql.getSteedosConfig()

--- packages/accounts/src/config/index.ts
+++ packages/accounts/src/config/index.ts
@@ -1,28 +1,4 @@
-const path = require("path");
-const fs = require("fs");
-const yaml = require('js-yaml');
+import { getSteedosConfig } from '@steedos/objectql'
+const config = getSteedosConfig();
 
-const SteedosConfig = {}
-const configName = 'steedos-config.yml'
-
-
-function getBaseDirectory(){
-    return require('app-root-path').path
-}
-
-function getSteedosConfig(){
-    let config: any;
-    let configPath = path.join(getBaseDirectory(), configName)
-    if (fs.existsSync(configPath) && !fs.statSync(configPath).isDirectory()) {
-        config = yaml.load(fs.readFileSync(configPath, 'utf8'));
-    }else{
-        console.info('not found steedos-config.yml', configPath);
-    }
-    return config;
-}
-
-export function initConfig(options?){
-    Object.assign(SteedosConfig, getSteedosConfig(), options)
-}
-
-export default SteedosConfig
+export default config;
