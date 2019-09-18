19ea1528e68f245aa6c7fcfc8ebd31bbd8d16fba
庄建国 <zhuangjianguo@hotoa.com>
2019-09-18 17:47:41 +0800

    从 localStorage 中获取 spaceId

--- packages/accounts/webapp/src/actions/tenant.js
+++ packages/accounts/webapp/src/actions/tenant.js
@@ -4,8 +4,11 @@ export function loadTenant() {
     return (dispatch) => {
 
         const searchParams = new URLSearchParams(window.location.search);
-        const tenant = searchParams.get("tenant");
-        if (tenant)
+        let tenant = searchParams.get("tenant");
+        if (!tenant) 
+            tenant = localStorage.getItem("spaceId")
+        if (tenant) {
+            
             accountsRest.fetch("/tenant/" + tenant).then((tenantDoc) => {
                 dispatch({
                     type: "RECEIVED_TENANT",
@@ -14,5 +17,6 @@ export function loadTenant() {
             }).catch((error) => {
                 console.warn('Actions - loadTenant - recreived error: ', error)
             }); // eslint-disable-line no-empty-function
+        }
     }
 }
