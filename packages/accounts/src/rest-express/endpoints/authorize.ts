b8c1a4e5cbf51be094230b12f82de958e4f027b2
庄建国 <zhuangjianguo@hotoa.com>
2019-09-19 14:52:32 +0800

    Update authorize.ts

--- packages/accounts/src/rest-express/endpoints/authorize.ts
+++ packages/accounts/src/rest-express/endpoints/authorize.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { URL } from 'url';
 import { get, isEmpty } from 'lodash';
 import { AccountsServer } from '@accounts/server';
 import { sendError } from '../utils/send-error';
@@ -13,14 +14,22 @@ export const authorize = (accountsServer: AccountsServer) => async (
   const client_id = req.query.client_id || "steedos"
   const connection = req.query.connection || "steedos"
   const state = req.query.state || ""
-  const redirect_uri = req.query.redirect_uri || "/"
+  const redirect_uri = req.query.redirect_uri
   const query = req.url.substring("/authorize".length)
 
   let userId = (req as any).userId
   let userIdCookie = get(req.cookies, 'X-User-Id') 
+  let userAccessTokenCookie = get(req.cookies, 'X-Access-Token') 
   if (userId && (userIdCookie== userId)) {
-    res.redirect(redirect_uri);
-    res.end();
+    if (redirect_uri) {
+      const redirectURL = new URL(redirect_uri);
+      redirectURL.searchParams.set("token", userAccessTokenCookie)
+      res.redirect(redirectURL.toString());
+      res.end();
+    } else {
+      res.redirect("/");
+      res.end();
+    }
   } else {
     clearAuthCookies(req, res);
     res.redirect("/accounts/a/login" + query);
