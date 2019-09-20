472266240ea2ce46cc73f81803aea6021fda7ede
庄建国 <zhuangjianguo@hotoa.com>
2019-09-20 09:38:11 +0800

    优化样式

--- packages/accounts/webapp/src/theme.ts
+++ packages/accounts/webapp/src/theme.ts
@@ -2,14 +2,11 @@
 import { createMuiTheme } from '@material-ui/core';
 
 const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: "#0070d2"
-      },
-      secondary: {
-        main: "#0070d2"
-      },
+  palette: {
+    primary: {
+      main: '#0070d2',
     },
+  },  
 });
 
 export default theme;
