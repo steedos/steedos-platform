8cd789fef2fdb21f432ea6d5c0a993dcbdc2d02a
hotlong <zhuangjianguo@hotoa.com>
2019-09-30 10:42:52 +0800

    优化 build 脚本

--- creator/.scripts/build-after.js
+++ creator/.scripts/build-after.js
@@ -8,4 +8,7 @@ execSync('rm -rf .dist/cfs');
 execSync('rm -rf .dist/bundle/programs/server/npm');
 execSync('rm -rf .dist/bundle/programs/server/*.js');
 
+execSync('rm -rf ../object-server/server/bundle/*');
+
 execSync('cp -rf .dist/bundle/* ../object-server/server/bundle/');
+execSync('cp -rf .scripts/build-fix/* ../object-server/server/bundle/');
