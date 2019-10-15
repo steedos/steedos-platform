0d11e3bc8d47866871d9c5d79c46126db4ac3171
yinlainghui <yinlianghui@hotoa.com>
2019-10-15 15:27:57 +0800

    @steedos/filters包去掉DevExpress依赖

--- packages/filters/src/devextreme.js
+++
@@ -1,8 +0,0 @@
-const DevExpress = require("devextreme/bundles/modules/core");
-const DevExpressData = require("devextreme/bundles/modules/data");
-const DevExpressODataStore = require("devextreme/bundles/modules/data.odata");
-
-DevExpress.data = DevExpressData;
-DevExpress.data.odata = DevExpressODataStore;
-
-export default DevExpress;
