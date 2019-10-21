d4b5685a13ad21e62d2a7f68d6a4d5516f8646bf
hotlong <zhuangjianguo@hotoa.com>
2019-10-21 11:08:55 +0800

    当webservices.steedos配置的url不以 / 结尾，首页背景图出不来 #13

--- packages/accounts/src/rest-express/endpoints/steedos/get-tenant.ts
+++ packages/accounts/src/rest-express/endpoints/steedos/get-tenant.ts
@@ -14,19 +14,21 @@ export const getTenant = (accountsServer: AccountsServer) => async (
 
     const spaceId = req.params.id;
     if (!spaceId)
-        throw new Error("accounts.tenant_id_required")
+      throw new Error("accounts.tenant_id_required")
     
     const spaceDoc = await db.findOne("spaces", spaceId, {fields: ["name", "avatar", "avatar_dark", "background", "enable_register", "enable_forget_password", "enable_create_tenant"]})
 
     if (config.webservices && config.webservices.steedos) {
-        if (spaceDoc.avatar_dark) {
-            spaceDoc.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar_dark
-        } else if (spaceDoc.avatar) {
-            spaceDoc.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar
-        } 
-        if (spaceDoc.background) {
-            spaceDoc.background_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.background
-        }
+      if (!config.webservices.steedos.endsWith("/"))
+        config.webservices.steedos += "/"
+      if (spaceDoc.avatar_dark) {
+        spaceDoc.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar_dark
+      } else if (spaceDoc.avatar) {
+        spaceDoc.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar
+      } 
+      if (spaceDoc.background) {
+        spaceDoc.background_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.background
+      }
     }
     res.json(spaceDoc);
   } catch (err) {
--- packages/accounts/src/rest-express/endpoints/steedos/settings.ts
+++ packages/accounts/src/rest-express/endpoints/steedos/settings.ts
@@ -9,23 +9,25 @@ export const getSettings = (accountsServer: AccountsServer) => async (
     req: express.Request,
     res: express.Response
   ) => {
-    let spaceDoc = config.tenant?config.tenant:{};
-    if (config.tenant && config.tenant._id) {
-        spaceDoc = await db.findOne("spaces", config.tenant._id, {fields: ["name", "avatar", "avatar_dark", "background", "enable_register", "enable_forget_password", "enable_create_tenant"]})
+  let spaceDoc = config.tenant?config.tenant:{};
+  if (config.tenant && config.tenant._id) {
+    spaceDoc = await db.findOne("spaces", config.tenant._id, {fields: ["name", "avatar", "avatar_dark", "background", "enable_register", "enable_forget_password", "enable_create_tenant"]})
 
-        if (config.webservices && config.webservices.steedos) {
-            if (spaceDoc.avatar_dark) {
-                spaceDoc.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar_dark
-            } else if (spaceDoc.avatar) {
-                spaceDoc.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar
-            } 
-            if (spaceDoc.background) {
-                spaceDoc.background_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.background
-            }
-        }
+    if (config.webservices && config.webservices.steedos) {
+      if (!config.webservices.steedos.endsWith("/"))
+        config.webservices.steedos += "/"
+      if (spaceDoc.avatar_dark) {
+        spaceDoc.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar_dark
+      } else if (spaceDoc.avatar) {
+        spaceDoc.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar
+      } 
+      if (spaceDoc.background) {
+        spaceDoc.background_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.background
+      }
     }
-    res.json({
-        tenant: spaceDoc,
-        password: config.password?config.password:{},
-    })
+  }
+  res.json({
+    tenant: spaceDoc,
+    password: config.password?config.password:{},
+  })
 }
