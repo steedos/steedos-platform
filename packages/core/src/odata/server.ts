3c5ae6c971432568171a4ac5316410fb70517907
sunhaolin <sunhaolin@hotoa.com>
2019-10-23 14:37:51 +0800

    odata接口优化：datasource为default和非default的url都使用/api/v4 #163

--- packages/core/src/odata/server.ts
+++ packages/core/src/odata/server.ts
@@ -1,9 +1,75 @@
 import { getFromContainer } from "../container";
 import { ODataManager } from "./ODataManager";
+import { SteedosDatabaseDriverType, getSteedosSchema } from "@steedos/objectql";
+import { oDataExpressMiddleware } from "./ODataExpressMiddleware";
+import { meteorODataExpressMiddleware } from './MeteorODataExpressMiddleware';
 
 /**
  * Gets a ODataManager.
  */
 export function getODataManager(): ODataManager {
    return getFromContainer(ODataManager);
+}
+
+export async function getObjectList(req, res) {
+   if (_isMeteorDriver(req)) {
+      return await meteorODataExpressMiddleware.getObjectList(req, res);
+   }
+   else {
+      return await oDataExpressMiddleware.getObjectList(req, res);
+   }
+}
+export async function getObjectRecent(req, res) {
+   if (_isMeteorDriver(req)) {
+      return await meteorODataExpressMiddleware.getObjectRecent(req, res);
+   }
+   else {
+      return await oDataExpressMiddleware.getObjectRecent(req, res);
+   }
+}
+export async function createObjectData(req, res) {
+   if (_isMeteorDriver(req)) {
+      return await meteorODataExpressMiddleware.createObjectData(req, res);
+   }
+   else {
+      return await oDataExpressMiddleware.createObjectData(req, res);
+   }
+}
+export async function getObjectData(req, res) {
+   if (_isMeteorDriver(req)) {
+      return await meteorODataExpressMiddleware.getObjectData(req, res);
+   }
+   else {
+      return await oDataExpressMiddleware.getObjectData(req, res);
+   }
+}
+export async function updateObjectData(req, res) {
+   if (_isMeteorDriver(req)) {
+      return await meteorODataExpressMiddleware.updateObjectData(req, res);
+   }
+   else {
+      return await oDataExpressMiddleware.updateObjectData(req, res);
+   }
+}
+export async function deleteObjectData(req, res) {
+   if (_isMeteorDriver(req)) {
+      return await meteorODataExpressMiddleware.deleteObjectData(req, res);
+   }
+   else {
+      return await oDataExpressMiddleware.deleteObjectData(req, res);
+   }
+}
+export async function excuteObjectMethod(req, res) {
+   if (_isMeteorDriver(req)) {
+      return await meteorODataExpressMiddleware.excuteObjectMethod(req, res);
+   }
+
+}
+
+function _isMeteorDriver(req) {
+   let urlParams = req.params;
+   let key = urlParams.objectName;
+   let collection = getSteedosSchema().getObject(key);
+   let driveName = collection.datasource.driver;
+   return (driveName == SteedosDatabaseDriverType.MeteorMongo || driveName == SteedosDatabaseDriverType.Mongo);
 }
