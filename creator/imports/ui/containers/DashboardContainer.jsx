3894a39f35f94d596574797a5cc916ff606d8c47
yinlainghui <yinlianghui@hotoa.com>
2019-11-02 11:53:03 +0800

    dashboard，使用windows全局变量中的组件，不用Import

--- creator/imports/ui/containers/DashboardContainer.jsx
+++ creator/imports/ui/containers/DashboardContainer.jsx
@@ -1,18 +1,11 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { Dashboard, Bootstrap, store } from '@steedos/react';
-import { IconSettings } from '@salesforce/design-system-react';
-
-var iconPath = `/creator/assets/icons`;
-
 function DashboardContainer(prop){
 	return (
-		<Provider store={store}>
-			<Bootstrap>
-					<Dashboard config={prop.config}/>
-			</Bootstrap>
-		</Provider>
-	)
+		<ReactRedux.Provider store={ReactSteedos.store}>
+			<ReactSteedos.Bootstrap>
+				<ReactSteedos.Dashboard config={prop.config} />
+			</ReactSteedos.Bootstrap>
+		</ReactRedux.Provider>
+	);
 }
 
 export default DashboardContainer;
