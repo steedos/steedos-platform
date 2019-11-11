2d6997cb402f107509553c998ae04bcbc55a99d1
baozhoutao <baozhoutao@hotoa.com>
2019-11-11 11:10:33 +0800

    将 PROJECT_ROOT/public 文件夹映射为 服务端 /
    https://github.com/steedos/steedos-project-dzug/issues/131

--- packages/core/src/init/index.ts
+++ packages/core/src/init/index.ts
@@ -3,6 +3,7 @@ import { Core, initCreator, initDesignSystem } from './core'
 import { Plugins } from './plugins';
 import { getSteedosSchema } from '@steedos/objectql';
 import * as migrate from '@steedos/migrate';
+import {initPublicStaticRouter} from '../routes'
 
 export async function init() {
     getSteedosSchema();
@@ -12,5 +13,6 @@ export async function init() {
     initCreator();
     await Datasources.init();
     await migrate.init();
+    initPublicStaticRouter();
     Core.run();
 }
