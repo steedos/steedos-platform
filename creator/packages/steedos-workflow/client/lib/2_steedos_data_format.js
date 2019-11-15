2f571edc886ac7f780df6ce3186bf68e66080f72
sunhaolin <sunhaolin@hotoa.com>
2019-11-15 11:11:57 +0800

    审批表单增加字段备注功能
    https://github.com/steedos/workflow/issues/2050

--- creator/packages/steedos-workflow/client/lib/2_steedos_data_format.js
+++ creator/packages/steedos-workflow/client/lib/2_steedos_data_format.js
@@ -44,7 +44,7 @@ WorkflowManager_format = {};
 // };
 
 var number_step = function (digits) {
-    if (digits && typeof(digits) == 'number' && digits > 0) {
+    if (digits && typeof (digits) == 'number' && digits > 0) {
         var step = '0.';
 
         for (var i = 0; i < (digits - 1); i++) {
@@ -65,10 +65,10 @@ var s_autoform = function (schema, field) {
 
     var optionsArr = []
 
-	if(InstanceMacro.check(options)){
-		optionsArr = InstanceMacro.run(options);
-	}else if(options != null && options.length > 0){
-		optionsArr = options.split("\n");
+    if (InstanceMacro.check(options)) {
+        optionsArr = InstanceMacro.run(options);
+    } else if (options != null && options.length > 0) {
+        optionsArr = options.split("\n");
     }
 
     var permission = field.permission == 'editable' ? 'editable' : 'readonly';
@@ -82,13 +82,13 @@ var s_autoform = function (schema, field) {
 
     //字段类型转换
     switch (type) {
-        case 'input' :
+        case 'input':
             schema.type = String;
             autoform.disabled = (permission == 'readonly');
 
-            if(options != null && options.length > 0){
+            if (options != null && options.length > 0) {
                 autoform.type = 'typeahead';
-            }else{
+            } else {
                 if (field.is_textarea) {
                     autoform.type = 'coreform-textarea';
                     autoform.rows = field.rows;
@@ -97,23 +97,23 @@ var s_autoform = function (schema, field) {
                 }
             }
             break;
-        case 'section' : //div
+        case 'section': //div
             schema.type = String;
             autoform.disabled = true;
             autoform.type = 'section';
             break;
-        case 'geolocation' : //地理位置
+        case 'geolocation': //地理位置
             schema.type = String;
             autoform.disabled = (permission == 'readonly');
             autoform.type = 'text';
             break;
-        case 'number' :
+        case 'number':
             schema.type = Number;
             autoform.disabled = (permission == 'readonly');
             autoform.type = 'coreform-number';
             autoform.step = number_step(field.digits); //控制有效位数
             break;
-        case 'date' :
+        case 'date':
             schema.type = String;
             autoform.disabled = (permission == 'readonly');
             if (Steedos.isMobile() || Steedos.isPad())
@@ -125,13 +125,13 @@ var s_autoform = function (schema, field) {
                     showClear: true,
                     format: "YYYY-MM-DD",
                     locale: Session.get("TAPi18n::loaded_lang"),
-                    widgetPositioning:{
+                    widgetPositioning: {
                         // horizontal: 'right'
                     }
                 }
             }
             break;
-        case 'dateTime' :
+        case 'dateTime':
             schema.type = Date;
             autoform.disabled = (permission == 'readonly');
             if (Steedos.isMobile() || Steedos.isPad())
@@ -142,18 +142,18 @@ var s_autoform = function (schema, field) {
                     showClear: true,
                     format: "YYYY-MM-DD HH:mm",
                     locale: Session.get("TAPi18n::loaded_lang"),
-                    widgetPositioning:{
+                    widgetPositioning: {
                         // horizontal: 'right'
                     }
                 }
             }
             break;
-        case 'checkbox' :
+        case 'checkbox':
             schema.type = Boolean;
             autoform.disabled = (permission == 'readonly');
             autoform.type = 'coreform-checkbox';
             break;
-        case 'select' :
+        case 'select':
             if (is_multiselect) {
                 schema.type = [String];
                 autoform.multiple = true;
@@ -164,17 +164,17 @@ var s_autoform = function (schema, field) {
             autoform.type = (permission == 'readonly') ? 'text' : 'select';
             autoform.disabled = (permission == 'readonly');
             break;
-        case 'radio' :
+        case 'radio':
             schema.type = [String];
             autoform.disabled = (permission == 'readonly');
             autoform.type = 'coreform-radio';
             break;
-        case 'multiSelect' :
+        case 'multiSelect':
             schema.type = [String];
             autoform.disabled = (permission == 'readonly');
             autoform.type = 'coreform-multiSelect';
             break;
-        case 'user' :
+        case 'user':
             if (is_multiselect) {
                 schema.type = [String];
                 autoform.multiple = true;
@@ -184,19 +184,19 @@ var s_autoform = function (schema, field) {
             autoform.disabled = (permission == 'readonly');
             autoform.type = "selectuser";
             break;
-        case 'email' :
+        case 'email':
             schema.type = String;
             autoform.type = "steedosEmail";
             autoform.readonly = (permission == 'readonly');
             autoform.disabled = (permission == 'readonly');
             break;
-        case 'url' :
+        case 'url':
             schema.type = String;
             autoform.type = "steedosUrl";
             autoform.readonly = (permission == 'readonly');
             autoform.disabled = (permission == 'readonly');
             break;
-        case 'group' :
+        case 'group':
             if (is_multiselect) {
                 schema.type = [String];
                 autoform.multiple = true;
@@ -209,22 +209,22 @@ var s_autoform = function (schema, field) {
 
             break;
         case 'odata':
-			if (is_multiselect) {
-				schema.type = [Object];
-				autoform.multiple = true;
-			}else{
-				schema.type = Object;
+            if (is_multiselect) {
+                schema.type = [Object];
+                autoform.multiple = true;
+            } else {
+                schema.type = Object;
             }
-			schema.blackbox = true;
-			autoform.type = "steedos-selectize";
-			autoform.readonly = (permission === 'readonly');
-			autoform.disabled = (permission === 'readonly');
-			autoform.related_object = field.related_object;
-			autoform.url = field.url;
-			autoform.filters = field.filters;
-			autoform.formula = field.formula;
-			autoform.search_field = field.search_field;
-			break;
+            schema.blackbox = true;
+            autoform.type = "steedos-selectize";
+            autoform.readonly = (permission === 'readonly');
+            autoform.disabled = (permission === 'readonly');
+            autoform.related_object = field.related_object;
+            autoform.url = field.url;
+            autoform.filters = field.filters;
+            autoform.formula = field.formula;
+            autoform.search_field = field.search_field;
+            break;
         default:
             schema.type = String;
             autoform.readonly = (permission == 'readonly');
@@ -238,7 +238,10 @@ var s_autoform = function (schema, field) {
         var afoptions = new Array();
 
         for (var s = 0; s < optionsArr.length; s++) {
-            afoptions.push({label: optionsArr[s], value: optionsArr[s]});
+            afoptions.push({
+                label: optionsArr[s],
+                value: optionsArr[s]
+            });
         }
 
         autoform.options = afoptions;
@@ -248,7 +251,8 @@ var s_autoform = function (schema, field) {
 
 var s_schema = function (label, field) {
 
-    var fieldType = field.type, is_required = field.is_required;
+    var fieldType = field.type,
+        is_required = field.is_required;
 
     schema = {};
 
@@ -269,22 +273,22 @@ var s_schema = function (label, field) {
 
 
 
-    if(schema.autoform.disabled == false){
+    if (schema.autoform.disabled == false) {
 
-		if(!field.default_value || (_.isString(field.default_value) && field.default_value.indexOf("auto_number(") < 0)){
-			schema.autoform.defaultValue = field.default_value;
-		}
-		if(InstanceMacro.check(field.default_value) || _.isFunction(field.default_value)){
-			schema.autoform.defaultValue = InstanceMacro.run(field.default_value);
-		}
+        if (!field.default_value || (_.isString(field.default_value) && field.default_value.indexOf("auto_number(") < 0)) {
+            schema.autoform.defaultValue = field.default_value;
+        }
+        if (InstanceMacro.check(field.default_value) || _.isFunction(field.default_value)) {
+            schema.autoform.defaultValue = InstanceMacro.run(field.default_value);
+        }
 
-		if(field.default_value && _.isString(field.default_value) && field.default_value.indexOf("auto_number(") > -1){
+        if (field.default_value && _.isString(field.default_value) && field.default_value.indexOf("auto_number(") > -1) {
 
-			schema.autoform["data-new-number"] = true
+            schema.autoform["data-new-number"] = true
 
-			schema.autoform["data-formula"] = field.default_value
+            schema.autoform["data-formula"] = field.default_value
 
-			schema.autoform.defaultValue = ""
+            schema.autoform.defaultValue = ""
 
         }
 
@@ -292,10 +296,10 @@ var s_schema = function (label, field) {
 
     if (fieldType === 'section') {
         schema.autoform.description = field.description;
-		schema.autoform.label = field.name;
+        schema.autoform.label = field.name;
     }
 
-	schema.autoform.title = schema.label;
+    schema.autoform.title = field.description || schema.label;
 
     return schema;
 };
@@ -359,21 +363,24 @@ WorkflowManager_format.getAutoformSchema = function (steedosForm) {
                 fieldSchema[field.code].autoform.editable = false
             }
 
-            fieldSchema[field.code + ".$"] = {type: Object, label: label}
+            fieldSchema[field.code + ".$"] = {
+                type: Object,
+                label: label
+            }
 
-            if(field.sfields){
-				for (var si = 0; si < field.sfields.length; si++) {
+            if (field.sfields) {
+                for (var si = 0; si < field.sfields.length; si++) {
 
-					var tableField = field.sfields[si];
+                    var tableField = field.sfields[si];
 
-					label = (tableField.name != null && tableField.name.length > 0) ? tableField.name : tableField.code;
+                    label = (tableField.name != null && tableField.name.length > 0) ? tableField.name : tableField.code;
 
-					tableField_schema = new s_schema(label, tableField);
+                    tableField_schema = new s_schema(label, tableField);
 
-					fieldSchema[field.code + ".$." + tableField.code] = tableField_schema;
+                    fieldSchema[field.code + ".$." + tableField.code] = tableField_schema;
 
-				}
-			}
+                }
+            }
 
         } else {
 
