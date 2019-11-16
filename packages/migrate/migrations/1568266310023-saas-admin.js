dc8803225162e2c0cd5a49089b577f30113b6121
yinlainghui <yinlianghui@hotoa.com>
2019-11-16 14:28:24 +0800

    fix migrate error:  TypeError: Cannot read property 'filters' of undefined

--- packages/migrate/migrations/1568266310023-saas-admin.js
+++ packages/migrate/migrations/1568266310023-saas-admin.js
@@ -5,7 +5,6 @@ let db = require("../db");
 // TODO: 创建云管理工作区
 // https://github.com/steedos/creator/issues/233
 module.exports.up = async function (next) {
-  let space_users = await db.findOne("space_users");
 }
 
 module.exports.down = async function (next) {
