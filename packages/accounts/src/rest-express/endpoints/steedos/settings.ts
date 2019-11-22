d85322631e44581a1490bb022c5ce972203ab486
hotlong <zhuangjianguo@hotoa.com>
2019-11-22 14:50:15 +0800

    默认显示企业注册界面

--- packages/accounts/src/rest-express/endpoints/steedos/settings.ts
+++ packages/accounts/src/rest-express/endpoints/steedos/settings.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as _ from 'lodash';
 import { AccountsServer } from '@accounts/server';
 import { getSteedosConfig } from '@steedos/objectql'
 import { db } from '../../../db';
@@ -9,25 +10,36 @@ export const getSettings = (accountsServer: AccountsServer) => async (
     req: express.Request,
     res: express.Response
   ) => {
-  let spaceDoc = config.tenant?config.tenant:{};
+  let tenant = {
+    name: "Steedos",
+    logo_url: undefined,
+    background_url: undefined,
+    enable_create_tenant: true,
+    enable_register: true,
+    enable_forget_password: true
+  }
   if (config.tenant && config.tenant._id) {
-    spaceDoc = await db.findOne("spaces", config.tenant._id, {fields: ["name", "avatar", "avatar_dark", "background", "enable_register", "enable_forget_password", "enable_create_tenant"]})
+    let spaceDoc = await db.findOne("spaces", config.tenant._id, {fields: ["name", "avatar", "avatar_dark", "background", "enable_register", "enable_forget_password", "enable_create_tenant"]})
 
     if (config.webservices && config.webservices.steedos) {
       if (!config.webservices.steedos.endsWith("/"))
         config.webservices.steedos += "/"
+      
+        _.assignIn(tenant, spaceDoc);
       if (spaceDoc.avatar_dark) {
-        spaceDoc.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar_dark
+        tenant.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar_dark
       } else if (spaceDoc.avatar) {
-        spaceDoc.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar
+        tenant.logo_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.avatar
       } 
       if (spaceDoc.background) {
-        spaceDoc.background_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.background
+        tenant.background_url = config.webservices.steedos + "api/files/avatars/" + spaceDoc.background
       }
     }
+  } else if (config.tenant) {
+      _.assignIn(tenant, config.tenant)
   }
   res.json({
-    tenant: spaceDoc,
+    tenant: tenant,
     password: config.password?config.password:{},
   })
 }
