ef20440b3cac6cd95142eb4ebe0773c15617b4b6
baozhoutao <baozhoutao@hotoa.com>
2019-11-30 18:30:31 +0800

    #181 防止配置的共享规则filters 为空

--- packages/objectql/src/util/permission_shares.ts
+++ packages/objectql/src/util/permission_shares.ts
@@ -7,7 +7,9 @@ export function getUserObjectSharesFilters(object_name: string, userSession: Ste
     if(userSession){
         _.each(_.filter(userSession.permission_shares, (permission_share)=>{return permission_share.object_name === object_name}), function(share){
             let shareDoc = getConfig('permission_shares', share._id);
-            sharesFilters.push(`(${shareDoc.filters})`)
+            if(!_.isEmpty(shareDoc.filters)){
+                sharesFilters.push(`(${shareDoc.filters})`)
+            }
         })
     }
     return sharesFilters;
