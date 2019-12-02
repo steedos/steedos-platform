1701b3c0db489b3d055a2d37a28337444c2f8ef4
殷亮辉 <yinlianghui@hotoa.com>
2019-12-02 15:02:16 +0800

    通知中心：“全部标记为已读” method实现
    https://github.com/steedos/creator/issues/1287

---
+++ packages/standard-objects/notifications.object.js
@@ -0,0 +1,31 @@
+const objectql = require('@steedos/objectql')
+
+Creator.Objects['notifications'].methods = {
+    markReadAll: async function (req, res) {
+        let userSession = req.user;
+        let error;
+        let updatedCount = await objectql.getObject("notifications").updateMany([
+            ["space", "=", userSession.spaceId],
+            ["owner", "=", userSession.userId],
+            [["is_read", "=", null], 'or', ["is_read", "=", false]]
+        ], {
+            is_read: true
+        }, userSession.userId).catch((ex) => {
+            console.error(ex);
+            error = ex;
+            return 0;
+        });
+        if(error){
+            res.status(500).send({
+                "error": error,
+                "success": false
+            });
+        }
+        else{
+            return res.send({
+                markedCount: updatedCount,
+                "success": true
+            });
+        }
+    }
+}
