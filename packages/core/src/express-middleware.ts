e615d265b3ac67154418fd4ad0ba70a8ff5fce34
殷亮辉 <yinlianghui@hotoa.com>
2019-12-04 18:48:25 +0800

    通知中心: /api/v4/notifications/:id/read 迁移到object脚本中
    https://github.com/steedos/creator/issues/1287

--- packages/core/src/express-middleware.ts
+++ packages/core/src/express-middleware.ts
@@ -1,7 +1,4 @@
 const express = require('express');
 
-import { read } from "./endpoints/notifications";
-
 export const coreExpress = express.Router();
 
-coreExpress.get('/api/v4/notifications/:id/read', read);
--- packages/standard-objects/notifications.object.js
+++ packages/standard-objects/notifications.object.js
@@ -43,3 +43,55 @@ Meteor.publish('my_notifications', function(spaceId){
     }
     return collection.find({space: spaceId, owner: this.userId}, {fields: {space: 1, owner: 1, name: 1, is_read: 1}})
 })
+
+
+const express = require('express');
+const Auth = require('@steedos/auth');
+const Core = require('@steedos/core');
+const Objectql = require('@steedos/objectql');
+const auth = Auth.auth;
+const getSteedosSchema = Objectql.getSteedosSchema;
+const addRouterConfig = Objectql.addRouterConfig;
+const util = Core.Util;
+
+const read = async (req, res) => {
+    let id = req.params.id;
+    let userSession = await auth(req, res);
+    if (userSession.userId) {
+        let record = await getSteedosSchema().getObject("notifications").findOne(id, { fields: ['owner', 'is_read', 'related_to', 'space', 'url'] });
+        if(!record){
+            return res.status(404).send({
+                "error": "Validate Request -- Not Found",
+                "success": false
+            });
+        }
+        if(record.owner !== userSession.userId){
+            return res.status(401).send({
+                "error": "Validate Request -- Missing Access",
+                "success": false
+            });
+        }
+        if(!record.related_to && !record.url){
+            return res.status(401).send({
+                "error": "Validate Request -- Missing related_to or url",
+                "success": false
+            });
+        }
+        if(!record.is_read){
+            await getSteedosSchema().getObject('notifications').update(id, { 'is_read': true })
+        }
+        let redirectUrl = record.url ? record.url : util.getObjectRecordUrl(record.related_to.o, record.related_to.ids[0], record.space);
+        return res.redirect(redirectUrl);
+    }
+    return res.status(401).send({
+        "error": "Validate Request -- Missing X-Auth-Token",
+        "success": false
+    })
+}
+
+const coreExpress = express.Router();
+coreExpress.get('/api/v4/notifications/:id/read', read);
+
+const prefix = "/";
+addRouterConfig(prefix, coreExpress);
+
