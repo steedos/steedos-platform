7204582b818b4bbe208dff31849c0dc55c6388a8
baozhoutao <baozhoutao@hotoa.com>
2019-12-05 16:10:17 +0800

    #190 共享规则中的对象字段改为多选

--- packages/objectql/src/util/permission_shares.ts
+++ packages/objectql/src/util/permission_shares.ts
@@ -5,7 +5,16 @@ const _ = require('underscore');
 export function getUserObjectSharesFilters(object_name: string, userSession: SteedosUserSession){
     let sharesFilters = []
     if(userSession){
-        _.each(_.filter(userSession.permission_shares, (permission_share)=>{return permission_share.object_name === object_name}), function(share){
+        _.each(_.filter(userSession.permission_shares, (permission_share)=>{
+            let shareObjectName =  permission_share.object_name;
+            if(_.isString(shareObjectName)){
+                return shareObjectName === object_name
+            }
+            if(_.isArray(shareObjectName)){
+                return _.contains(shareObjectName, object_name);
+            }
+            return false
+        }), function(share){
             let shareDoc = getConfig('permission_shares', share._id);
             if(!_.isEmpty(shareDoc.filters)){
                 sharesFilters.push(`(${shareDoc.filters})`)
