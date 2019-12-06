15f2d261eb4493e534b1fab83d8a293bcfa702a4
baozhoutao <baozhoutao@hotoa.com>
2019-12-06 18:10:00 +0800

    #164 添加测试文件

---
+++ packages/objectql/test/unit/loadConfigFiles.test.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+let objectql = require("../../src")
+let path = require("path");
+
+
+describe('Load loadConfigFiles Test', () => {
+    it('loadConfigFiles return true', async () => {
+        let datasource = objectql.getDataSource()
+        objectql.addObjectConfigFiles(path.join(__dirname, "../../apps/contracts/src/**"), 'default');
+        datasource.init()
+        let permission_set = objectql.getObject("permission_set")
+        objectql.addAppConfigFiles(path.join(process.cwd(), "../../apps/contracts/src/**"))
+        let apps = objectql.getAppConfigs()
+        let record = objectql.getConfigs('spaces');
+        expect(permission_set != undefined).to.equal(true) && expect(apps != undefined).to.equal(true) && expect(record != undefined).to.equal(true)
+    });
+  });
--- packages/objectql/test/unit/loadSteedosConfig.test.ts
+++ packages/objectql/test/unit/loadSteedosConfig.test.ts
@@ -1,17 +1,11 @@
 import { expect } from 'chai';
 let objectql = require("../../src")
-let path = require("path");
 
 
-describe('Load StedosConfig Test', () => {
-    it('LoadStedosConfigTest return true', async () => {
-        let datasource = objectql.getDataSource()
-        objectql.addObjectConfigFiles(path.join(__dirname, "../../apps/contracts/src/**"), 'default');
-        datasource.init()
-        let permission_set = objectql.getObject("permission_set")
-        objectql.addAppConfigFiles(path.join(process.cwd(), "../../apps/contracts/src/**"))
-        let apps = objectql.getAppConfigs()
-        let record = objectql.getConfigs('spaces');
-        expect(permission_set != undefined).to.equal(true) && expect(apps != undefined).to.equal(true) && expect(record != undefined).to.equal(true)
+describe('Load loadSteedosConfig Test', () => {
+    it('loadSteedosConfig return true', async () => {
+        let config = objectql.getSteedosConfig();
+        let config2 = objectql.getSteedosConfig();
+        expect(config).to.equal(config2)  && expect(config.datasources.default.connection.url).to.equal("mongodb://127.0.0.1/steedos")
     });
   });
