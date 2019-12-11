e3f3e5ec1fc149adeb2fe5fd452524187e431009
殷亮辉 <yinlianghui@hotoa.com>
2019-12-11 15:56:21 +0800

    新通知到达后，自动弹出客户端消息 #1454

--- packages/standard-objects/notifications.client.js
+++ packages/standard-objects/notifications.client.js
@@ -3,8 +3,8 @@ var loadNotificationsData = ReactSteedos.loadNotificationsData;
 
 Creator.subs["CreatorNotifications"] = new SubsManager();
 
-Meteor.startup(function(c){
-	Meteor.autorun(function(c){
+Meteor.startup(function(){
+    Meteor.autorun(function(c){
         spaceId = Session.get("spaceId");
         if(spaceId){
             Creator.subs["CreatorNotifications"].subscribe("my_notifications", spaceId);
@@ -23,3 +23,67 @@ Meteor.startup(function(c){
     });
 });
 
+Meteor.startup(function(c){
+    if(!Steedos.isMobile()){
+        if(Push.debug){
+            console.log("init my_notifications observeChanges");
+        }
+        Meteor.autorun(function(){
+            if(Creator.subs["CreatorNotifications"].ready("my_notifications") && Creator.bootstrapLoaded.get()){
+                var query = Creator.getCollection("notifications").find();
+                // 发起获取发送通知权限请求
+                onRequestSuccess = function(){
+                    console.log("Request my_notifications push permission success.")
+                }
+                onRequestFailed = function(){
+                    console.log("Request my_notifications push permission failed.")
+                }
+                Steedos.Push.Permission.request(onRequestSuccess, onRequestFailed);
+                
+                handle = query.observeChanges({
+                    added: function(id, notification){
+                        console.log(notification);
+                        // 非主窗口不弹推送消息
+                        if(window.opener)
+                            return;
+
+                        var options = {
+                            iconUrl: '',
+                            title: notification.name,
+                            body: notification.text,
+                            timeout: 15 * 1000,
+                            onClick: function(event){
+                                console.log(event)
+                                if(event.target.tag){
+                                    window.location.href =event.target.tag;
+                                }
+                                window.focus();
+                                this.close();
+                                return;
+                            }
+                        }
+
+                        if(options.title){
+                            options.tag = Steedos.absoluteUrl("/api/v4/notifications/" + id + "/read");
+                            Steedos.Push.create(options.title, options);
+                        }
+
+                        if(Steedos.isNode()){
+                            // 新版客户端
+                            if(nw.ipcRenderer){
+                                if(notification.badge != undefined){
+                                    nw.ipcRenderer.sendToHost('onBadgeChange', false, 0, notification.badge, false, false);
+                                }
+                            }
+                            else{
+                                // 任务栏高亮显示
+                                nw.Window.get().requestAttention(3);
+                            }
+                        }
+                        return;
+                    }
+                });
+            }
+        });
+    }
+});
