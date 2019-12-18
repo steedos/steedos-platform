6b82713573aa96df3deb6944cbe1831b5b700782
殷亮辉 <yinlianghui@hotoa.com>
2019-12-18 18:38:08 +0800

    新建单位时，如果选择了关联组织，不应该再自动创建组织 #212，把所有_id相同的逻辑都去掉了并优化了性能

--- packages/standard-objects/company.object.js
+++ packages/standard-objects/company.object.js
@@ -1,38 +1,88 @@
 const objectql = require('@steedos/objectql')
 
+const checkRepeatCompany = function(orgId, spaceId){
+    console.log("==checkRepeatCompany====orgId=", orgId);
+    console.log("==checkRepeatCompany====spaceId=", spaceId);
+    var repeatCompany = Creator.getCollection("company").findOne({
+        space: spaceId,
+        organization: orgId
+    }, {
+        fields: {
+            _id: 1
+        }
+    });
+    if (repeatCompany) {
+        /* 要判断选中的关联组织是否已经被其他单位关联了，如果有就提示不能修改，而且不用处理其is_company、company_id值逻辑 */
+        throw new Error("该关联组织，已经被其他单位占用了，不能重复关联该组织");
+    }
+}
+
 Creator.Objects['company'].triggers = {
+    "before.insert.server.company": {
+        on: "server",
+        when: "before.insert",
+        todo: function (userId, doc) {
+            if (doc.organization) {
+                checkRepeatCompany(doc.organization, doc.space);
+            }
+            else {
+                /* 允许不设置关联组织值， after.insert触发器中会自动新建关联组织*/
+            }
+        }
+    },
     "after.insert.server.company": {
         on: "server",
         when: "after.insert",
         todo: function (userId, doc) {
-            // 自动在根节点新建一个组织，对应上关系
-            var rootOrg = Creator.getCollection("organizations").findOne({
-                space: doc.space,
-                parent: null
-            }, {
-                fields: {
-                    _id: 1
-                }
-            });
+            if (doc.organization) {
+                /* 设置了关联组织值，则更新相关属性*/
+                Creator.getCollection("organizations").update({
+                    _id: doc.organization
+                }, {
+                    $set: {
+                        company_id: doc._id,
+                        is_company: true
+                    }
+                });
+                Creator.getCollection("company").update({
+                    _id: doc._id
+                }, {
+                    $set: {
+                        company_id: doc._id
+                    }
+                });
+            }
+            else {
+                /* 允许未设置关联组织值，自动新建关联组织*/
+                // 自动在根节点新建一个组织，对应上关系
+                var rootOrg = Creator.getCollection("organizations").findOne({
+                    space: doc.space,
+                    parent: null
+                }, {
+                    fields: {
+                        _id: 1
+                    }
+                });
 
-            // 自动新建根组织下对应的组织关联到新单位，就算存在同名组织，也要新建，同名的老组织用户应该手动删除
-            // 组织的其他属性，比如fullname，parents等在organizations.before.insert，organizations.after.insert处理
-            var orgId = Creator.getCollection("organizations").insert({
-                _id: doc._id,
-                name: doc.name,
-                parent: rootOrg._id,
-                space: doc.space,
-                company_id: doc._id,
-                is_company: true
-            });
-            Creator.getCollection("company").update({
-                _id: doc._id
-            }, {
-                $set: {
-                    organization: orgId,
-                    company_id: doc._id
-                }
-            });
+                // 自动新建根组织下对应的组织关联到新单位，就算存在同名组织，也要新建，同名的老组织用户应该手动删除
+                // 组织的其他属性，比如fullname，parents等在organizations.before.insert，organizations.after.insert处理
+                // 因为没办法保证company与organizations表的关联记录_id值一定相同，所以不再把它们_id值设置为相同值
+                var orgId = Creator.getCollection("organizations").insert({
+                    name: doc.name,
+                    parent: rootOrg._id,
+                    space: doc.space,
+                    company_id: doc._id,
+                    is_company: true
+                });
+                Creator.getCollection("company").update({
+                    _id: doc._id
+                }, {
+                    $set: {
+                        organization: orgId,
+                        company_id: doc._id
+                    }
+                });
+            }
         }
     },
     "before.remove.server.company": {
@@ -54,9 +104,9 @@ Creator.Objects['company'].triggers = {
                 throw new Meteor.Error(400, "关联组织有下级组织，请先删除相关下级组织");
             }
             else {
-                // 删除关联组织，只删除单位与组织_id值相等的关联记录，_id值不相等的记录不执行级联删除
+                // 有关联数据时，比如组织下有用户，触发器中已作判断了会自动报错
                 Creator.getCollection("organizations").remove({
-                    _id: doc._id
+                    _id: doc.organization
                 });
             }
         }
@@ -66,26 +116,42 @@ Creator.Objects['company'].triggers = {
         when: "before.update",
         todo: function (userId, doc, fields, options) {
             if (options.$set.organization !== doc.organization) {
-                if (doc.organization === doc._id) {
-                    /* 只允许单位的organization值为空或单位的organization值不等于其_id值的记录可以编辑其organization属性值 */
-                    throw new Error("不允许编辑关联组织，只有同步过来的单位才可以编辑其关联组织属性")
-                }
                 if (options.$set.organization) {
-                    var repeatCompany = Creator.getCollection("company").findOne({
-                        space: doc.space,
-                        organization: options.$set.organization
-                    }, {
-                        fields: {
-                            _id: 1
-                        }
-                    });
-                    if (repeatCompany) {
-                        /* 要判断选中的关联组织是否已经被其他单位关联了，如果有就提示不能修改，而且不用处理其is_company、company_id值逻辑 */
-                        throw new Error("该关联组织，已经被其他单位占用了，不能重复关联该组织");
-                    }
+                    checkRepeatCompany(options.$set.organization, doc.space);
                 }
                 else {
                     /* 允许清除关联组织值，而且不用处理其is_company、company_id值逻辑 */
+                }
+            }
+        }
+    },
+    "after.update.server.company": {
+        on: "server",
+        when: "after.update",
+        todo: function (userId, doc, fieldNames, modifier, options) {
+            console.log("==after.update.server.company====doc.organization==", doc.organization);
+            console.log("==after.update.server.company====this.previous.organization==", this.previous.organization);
+            if (this.previous.organization !== doc.organization) {
+                if (doc.organization) {
+                    /* 设置了关联组织值，则更新相关属性*/
+                    Creator.getCollection("organizations").update({
+                        _id: doc.organization
+                    }, {
+                        $set: {
+                            company_id: doc._id,
+                            is_company: true
+                        }
+                    });
+                    Creator.getCollection("company").update({
+                        _id: doc._id
+                    }, {
+                        $set: {
+                            company_id: doc._id
+                        }
+                    });
+                }
+                else {
+                    /* 允许清除关联组织值，组织相关属性is_company、company_id值的修正需要手动执行“更新组织”操作 */
                 }
             }
         }
@@ -95,7 +161,7 @@ Creator.Objects['company'].triggers = {
 let _ = require("underscore");
 
 // 根据当前space_users的organizations/organization值，计算其company_ids及company_id值
-let update_su_company_ids = async function (_id, su) {
+let update_su_company_ids = async function (_id, su, cachedOrganizations) {
     var company_ids, orgs, org;
     if (!su) {
         su = await this.getObject("space_users").findOne(_id, {
@@ -106,16 +172,15 @@ let update_su_company_ids = async function (_id, su) {
         console.error("db.space_users.update_company_ids,can't find space_users by _id of:", _id);
         return;
     }
-    orgs = await this.getObject("organizations").find({
-        filters: [["_id", "in", su.organizations]],
-        fields: ["company_id"]
+    orgs = cachedOrganizations.filter(function(item){
+        return su.organizations.indexOf(item._id) > -1;
     });
     company_ids = _.pluck(orgs, 'company_id');
     // company_ids中的空值就空着，不需要转换成根组织ID值
     company_ids = _.uniq(_.compact(company_ids));
 
-    org = await this.getObject("organizations").findOne(su.organization, {
-        fields: ["company_id"]
+    org = cachedOrganizations.find(function(item){
+        return su.organization === item._id;
     });
 
     let updateDoc = {
@@ -124,45 +189,32 @@ let update_su_company_ids = async function (_id, su) {
     if (org && org.company_id) {
         updateDoc.company_id = org.company_id;
     }
-    await this.getObject("space_users").updateOne(_id, updateDoc, this.userSession);
+    await this.getObject("space_users").directUpdate(_id, updateDoc);
 };
 
 // 循环执行当前组织及其子组织children的company_id值计算
-let update_org_company_id = async function (_id, company_id, space_id, updated = { count: 0 }) {
+let update_org_company_id = async function (_id, company_id, space_id, cachedCompanys, updated = { count: 0 }) {
     const org = await this.getObject("organizations").findOne(_id, {
-        fields: ["children", "is_company", "company_id", "space"]
+        fields: ["children", "is_company"]
     });
 
     if (org.is_company) {
-        if (org.company_id === _id){
-            // company_id为自身_id值说明单company是新建出来的，不是其他方式同步过来的数据，不需要再从company表中查询其company_id值
-            company_id = _id;
-        }
-        else {
-            const companys = await this.getObject("company").find({
-                filters: [["space", "=", space_id], ["organization", "=", _id]],
-                fields: ["_id"]
-            });
-            if (companys.length) {
-                company_id = companys[0]._id;
-            }
-            await this.getObject("organizations").updateOne(_id, {
-                company_id: company_id
-            }, this.userSession);
-            updated.count += 1;
+        const orgCompany = cachedCompanys.find(function(company){
+            return company.organization === _id;
+        });
+        if (orgCompany) {
+            company_id = orgCompany._id;
         }
     }
-    else {
-        await this.getObject("organizations").updateOne(_id, {
-            company_id: company_id
-        }, this.userSession);
-        updated.count += 1;
-    }
+    await this.getObject("organizations").directUpdate(_id, {
+        company_id: company_id
+    });
+    updated.count += 1;
 
     const children = org.children;
     if (children && children.length) {
         for (let child of children) {
-            await update_org_company_id.call(this, child, company_id, space_id, updated);
+            await update_org_company_id.call(this, child, company_id, space_id, cachedCompanys, updated);
         }
     }
 }
@@ -170,19 +222,21 @@ let update_org_company_id = async function (_id, company_id, space_id, updated =
 // 执行更新组织前先把所有company的直属关联组织is_company及is_company设置对，
 // 即把直属关联组织is_company设置为true，company_id设置为关联单位_id
 // 只需要处理organization值不等于其_id值的company记录，这些记录不是新建出来的，而是其他方式同步过来的数据
-let update_all_company_org = async function (space_id) {
-    let companys = await this.getObject("company").find({
-        filters: [["space", "=", space_id]],
-        fields: ["organization"]
-    });
+let update_all_company_org = async function (space_id, cachedCompanys) {
+    if(!cachedCompanys){
+        cachedCompanys = await this.getObject("company").find({
+            filters: [["space", "=", space_id]],
+            fields: ["organization"]
+        });
+    }
 
-    for (let company of companys) {
-        if (company.organization && company._id !== company.organization) {
+    for (let company of cachedCompanys) {
+        if (company.organization) {
             // company.organization为自身_id值说明单company是新建出来的，不是其他方式同步过来的数据，不需要再修正其is_company、company值
-            await this.getObject("organizations").updateOne(company.organization, {
+            await this.getObject("organizations").directUpdate(company.organization, {
                 is_company: true,
                 company_id: company._id
-            }, this.userSession);
+            });
         }
     }
 
@@ -192,17 +246,13 @@ let update_all_company_org = async function (space_id) {
     });
     // 清除未被company引用的organizations记录的is_company值为false
     for (let org of orgs) {
-        if (org.company_id !== org._id) {
-            // company_id为自身_id值说明单company是新建出来的，不是其他方式同步过来的数据，不需要再从company表中查询确认其是否被引用
-            const companys = await this.getObject("company").find({
-                filters: [["space", "=", space_id], ["organization", "=", org._id]],
-                fields: ["_id"]
+        const orgCompany = cachedCompanys.find(function(company){
+            return company.organization === org._id;
+        });
+        if (!orgCompany) {
+            await this.getObject("organizations").directUpdate(org._id, {
+                is_company: false
             });
-            if (!companys.length) {
-                await this.getObject("organizations").updateOne(org._id, {
-                    is_company: false
-                }, this.userSession);
-            }
         }
     }
 }
@@ -220,22 +270,32 @@ Creator.Objects['company'].methods = {
             fields: ["organization", "space"]
         });
 
-        await update_all_company_org.call(callThis, company.space);
-
         if (!company.organization) {
             throw new Meteor.Error(400, "该单位的关联组织未设置");
         }
 
+        const cachedCompanys = await objectql.getObject("company").find({
+            filters: [["space", "=", company.space]],
+            fields: ["_id", "organization"]
+        });
+
+        await update_all_company_org.call(callThis, company.space, cachedCompanys);
+
         let updatedOrgs = { count: 0 };
-        await update_org_company_id.call(callThis, company.organization, record_id, company.space, updatedOrgs);
+        await update_org_company_id.call(callThis, company.organization, record_id, company.space, cachedCompanys, updatedOrgs);
 
         sus = await objectql.getObject("space_users").find({
             filters: [["organizations_parents", "=", company.organization]],
             fields: ["organizations", "organization", "company_id", "space"]
         });
 
+        const cachedOrganizations = await objectql.getObject("organizations").find({
+            filters: [["space", "=", company.space]],
+            fields: ["company_id", "_id"]
+        });
+
         for (let su of sus) {
-            await update_su_company_ids.call(callThis, su._id, su);
+            await update_su_company_ids.call(callThis, su._id, su, cachedOrganizations);
         }
 
         return res.send({
