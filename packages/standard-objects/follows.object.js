8d2a9e979143997e27cde20a18e43d5d4ff3e117
baozhoutao <baozhoutao@hotoa.com>
2019-12-21 12:28:03 +0800

    Creator.getRecordFollowers 支持共享规则权限
    https://github.com/steedos/steedos-project-dzug/issues/258

--- packages/standard-objects/follows.object.js
+++ packages/standard-objects/follows.object.js
@@ -1,3 +1,8 @@
+const getSessionByUserId = require('@steedos/auth').getSessionByUserId
+const objectql = require('@steedos/objectql')
+const getObject = objectql.getObject
+const wrapAsync = objectql.wrapAsync
+
 Meteor.publish('myFollows', function(space_id){
   var collection = Creator.getCollection("follows", space_id)
   if(!this.userId){
@@ -26,6 +31,18 @@ if(Meteor.isServer){
           var recordPermissions = Creator.getRecordPermissions(object_name, record, objectFollower.owner, spaceId);
           if(recordPermissions.allowRead){
             recordFollowers.push(objectFollower.owner);
+          }else{
+            var getSessionFn = function(){
+              return getSessionByUserId(objectFollower.owner, spaceId);
+            }
+            var ownerSession = wrapAsync(getSessionFn, {});
+            var recordFn = function(){
+              return getObject(object_name).findOne(recordId, {fields: {_id:1}}, ownerSession)
+            }
+            var _record = wrapAsync(recordFn, {});
+            if(_record){
+              recordFollowers.push(objectFollower.owner);
+            }
           }
         }
       })
