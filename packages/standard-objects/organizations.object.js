020587a64c0621ba93f71242612246996067d61f
殷亮辉 <yinlianghui@hotoa.com>
2019-12-28 10:09:55 +0800

    编辑根组织时报错提示：请选择上级部门 #216

--- packages/standard-objects/organizations.object.js
+++ packages/standard-objects/organizations.object.js
@@ -422,7 +422,7 @@ if (Meteor.isServer) {
             //     }
             // }
         }
-        if(!modifier.$set.parent || (modifier.$unset && modifier.$unset.parent)){
+        if(doc.parent && (!modifier.$set.parent || (modifier.$unset && modifier.$unset.parent))){
             throw new Meteor.Error(400, "organizations_error_organizations_parent_required");
         }
         // else if (modifier.$set.name != doc.name)
