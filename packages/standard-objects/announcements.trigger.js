cdd50ce5796bee29db79053a7f8740d07be0dac9
殷亮辉 <yinlianghui@hotoa.com>
2020-02-26 12:03:37 +0800

    参考原来cms信息对象中的发布对象功能，增加公告对象，但只可发布给人，不能发布给组织 #246

---
+++ packages/standard-objects/announcements.trigger.js
@@ -0,0 +1,80 @@
+const _ = require("underscore");
+
+const addNotifications = function (userId, doc, members) {
+    members = _.difference(members, [userId]);
+    if (!members.length) {
+        return;
+    }
+    const fromUser = Creator.getCollection("users").findOne({
+        _id: userId
+    }, {
+            fields: {
+                name: 1
+            }
+        });
+    const notificationTitle = fromUser.name;
+    const now = new Date();
+    let notificationDoc = {
+        name: notificationTitle,
+        body: doc.name,
+        related_to: {
+            o: "announcements",
+            ids: [doc._id]
+        },
+        related_name: doc.name,
+        from: userId,
+        space: doc.space,
+        created: now,
+        modified: now,
+        created_by: userId,
+        modified_by: userId
+    };
+    const collection = Creator.getCollection("notifications");
+    let bulk = collection.rawCollection().initializeUnorderedBulkOp();
+    members.forEach(function (member) {
+        let bulkDoc = _.clone(notificationDoc);
+        bulkDoc._id = collection._makeNewID();
+        bulkDoc.owner = member;
+        return bulk.insert(bulkDoc);
+    });
+    return bulk.execute().catch(function (error) {
+        console.error("公告通知数据插入失败，错误信息：", error);
+    });
+}
+
+const removeNotifications = function (doc, members) {
+    const collection = Creator.getCollection("notifications");
+    let bulk = collection.rawCollection().initializeUnorderedBulkOp();
+    members.forEach(function (member) {
+        bulk.find({
+            "related_to.o": "announcements",
+            "related_to.ids": doc._id,
+            owner: member
+        }).remove();
+    });
+    return bulk.execute().catch(function (error) {
+        console.error("公告通知数据删除失败，错误信息：", error);
+    });
+}
+
+module.exports = {
+    listenTo: 'announcements',
+    afterInsert: async function (userId, doc) {
+        const members = doc.members;
+        if (members && members.length) {
+            addNotifications(userId, doc, members);
+        }
+    },
+    beforeUpdate: async function (userId, doc, fieldNames, modifier, options) {
+        let oldMembers = doc.members;
+        let newMembers = modifier.$set.members;
+        let addMembers = _.difference(newMembers, oldMembers);
+        let subMembers = _.difference(oldMembers, newMembers);
+        if (addMembers.length) {
+            addNotifications(userId, doc, addMembers);
+        }
+        if (subMembers.length) {
+            removeNotifications(doc, subMembers);
+        }
+    }
+}
