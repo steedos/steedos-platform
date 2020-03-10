ed3c4eafc06948591c90db054c7b520a3c7d1a77
baozhoutao <baozhoutao@hotoa.com>
2020-03-10 10:34:18 +0800

    build 1.18.3

--- server/bundle/programs/server/packages/modules.js
+++ server/bundle/programs/server/packages/modules.js
@@ -129,6 +129,7 @@ install("spacebars");
 install("html-tools");
 install("blaze-tools");
 install("spacebars-compiler");
+install("standard-minifier-css");
 install("standard-minifier-js");
 install("less");
 install("accounts-ui");
@@ -860,7 +861,7 @@ module.useNode();
 module.exports = {
   "name": "@steedos/core",
   "private": false,
-  "version": "1.18.1",
+  "version": "1.18.2",
   "description": "",
   "main": "lib/index.js",
   "scripts": {
@@ -877,10 +878,10 @@ module.exports = {
     "@salesforce/dev-config": "^1.4.4",
     "@salesforce/kit": "^1.0.4",
     "@salesforce/ts-types": "^1.1.1",
-    "@steedos/auth": "^1.18.1",
-    "@steedos/migrate": "^1.18.1",
-    "@steedos/objectql": "^1.18.1",
-    "@steedos/standard-objects": "^1.18.1",
+    "@steedos/auth": "^1.18.2",
+    "@steedos/migrate": "^1.18.2",
+    "@steedos/objectql": "^1.18.2",
+    "@steedos/standard-objects": "^1.18.2",
     "@types/express": "^4.16.1",
     "@types/mongodb": "^3.1.22",
     "@types/node": "^11.10.4",
@@ -910,7 +911,7 @@ module.exports = {
   },
   "devDependencies": {
     "@salesforce/dev-scripts": "0.3.12",
-    "@steedos/meteor-bundle-runner": "^1.18.1",
+    "@steedos/meteor-bundle-runner": "^1.18.2",
     "@steedos/migrate": "^1.6.2",
     "@types/chai": "^4.1.7",
     "@types/chai-as-promised": "7.1.0",
@@ -953,7 +954,7 @@ module.useNode();
                                                                                                     //
 module.exports = {
   "name": "@steedos/auth",
-  "version": "1.18.1",
+  "version": "1.18.2",
   "main": "lib/index.js"
 };
 
@@ -1013,7 +1014,7 @@ module.useNode();
                                                                                                     //
 module.exports = {
   "name": "@steedos/filters",
-  "version": "1.18.1",
+  "version": "1.18.2",
   "main": "lib/index.js"
 };
 
