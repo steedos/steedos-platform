9ac54f9053e2523634798394e513b28d2ee8765e
殷亮辉 <yinlianghui@hotoa.com>
2020-03-13 16:40:19 +0800

    Merge branch 'dev' of https://github.com/steedos/creator into dev

--- creator/packages/steedos-smsqueue/package.js
+++ creator/packages/steedos-smsqueue/package.js
@@ -21,10 +21,6 @@ Package.onUse(function(api) {
 	api.use('matb33:collection-hooks@0.8.4');
 	api.use('mongo', 'server');
 
-	api.use('universe:i18n@1.13.0');
-	tapi18nFiles = ['i18n/en.i18n.json', 'i18n/zh-CN.i18n.json'];
-	api.addFiles(tapi18nFiles, ['client', 'server']);
-
 	api.addFiles('checkNpm.js', 'server');
 
 	// Common api
