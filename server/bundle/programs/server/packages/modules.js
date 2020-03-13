cdebb7871ca726ffdcb6c1a11cd558808ca6274d
baozhoutao <baozhoutao@hotoa.com>
2020-03-13 13:50:08 +0800

    build 1.18.5

--- server/bundle/programs/server/packages/modules.js
+++ server/bundle/programs/server/packages/modules.js
@@ -86,7 +86,6 @@ install("tracker");
 install("retry", "meteor/retry/retry.js");
 install("ddp-common");
 install("email");
-install("fastclick");
 install("url", "meteor/url/url_server.js");
 install("http", "meteor/http/httpcall_server.js");
 install("jquery");
@@ -607,7 +606,7 @@ module.useNode();
                                                                                                     //
 module.exports = {
   "name": "react",
-  "version": "16.12.0",
+  "version": "16.13.0",
   "main": "index.js"
 };
 
@@ -768,7 +767,7 @@ module.useNode();
                                                                                                     //
 module.exports = {
   "name": "qs",
-  "version": "6.9.0",
+  "version": "6.9.1",
   "main": "lib/index.js"
 };
 
@@ -844,7 +843,7 @@ module.useNode();
 module.exports = {
   "name": "@steedos/core",
   "private": false,
-  "version": "1.18.3",
+  "version": "1.18.4",
   "description": "",
   "main": "lib/index.js",
   "scripts": {
@@ -861,10 +860,10 @@ module.exports = {
     "@salesforce/dev-config": "^1.4.4",
     "@salesforce/kit": "^1.0.4",
     "@salesforce/ts-types": "^1.1.1",
-    "@steedos/auth": "^1.18.3",
-    "@steedos/migrate": "^1.18.3",
-    "@steedos/objectql": "^1.18.3",
-    "@steedos/standard-objects": "^1.18.3",
+    "@steedos/auth": "^1.18.4",
+    "@steedos/migrate": "^1.18.4",
+    "@steedos/objectql": "^1.18.4",
+    "@steedos/standard-objects": "^1.18.4",
     "@types/express": "^4.16.1",
     "@types/mongodb": "^3.1.22",
     "@types/node": "^11.10.4",
@@ -894,7 +893,7 @@ module.exports = {
   },
   "devDependencies": {
     "@salesforce/dev-scripts": "0.3.12",
-    "@steedos/meteor-bundle-runner": "^1.18.3",
+    "@steedos/meteor-bundle-runner": "^1.18.4",
     "@steedos/migrate": "^1.6.2",
     "@types/chai": "^4.1.7",
     "@types/chai-as-promised": "7.1.0",
@@ -937,7 +936,7 @@ module.useNode();
                                                                                                     //
 module.exports = {
   "name": "@steedos/auth",
-  "version": "1.18.3",
+  "version": "1.18.4",
   "main": "lib/index.js"
 };
 
@@ -997,7 +996,7 @@ module.useNode();
                                                                                                     //
 module.exports = {
   "name": "@steedos/filters",
-  "version": "1.18.3",
+  "version": "1.18.4",
   "main": "lib/index.js"
 };
 
@@ -1158,7 +1157,7 @@ module.useNode();
                                                                                                     //
 module.exports = {
   "name": "xml2js",
-  "version": "0.4.22",
+  "version": "0.4.23",
   "main": "./lib/xml2js"
 };
 
@@ -1504,7 +1503,7 @@ module.useNode();
                                                                                                     //
 module.exports = {
   "name": "ejs",
-  "version": "2.7.1",
+  "version": "2.7.4",
   "main": "./lib/ejs.js"
 };
 
