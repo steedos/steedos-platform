b2e6d5ce7240f54772cbf72c0c19d7bcf5a1b1f3
殷亮辉 <yinlianghui@hotoa.com>
2020-03-14 14:46:58 +0800

    新建修改用户时邮件字段保持小写格式
    https://github.com/steedos/creator/issues/1658

--- packages/standard-objects/space_users.object.js
+++ packages/standard-objects/space_users.object.js
@@ -124,7 +124,9 @@ Meteor.startup(function () {
         };
         db.space_users.before.insert(function (userId, doc) {
             var creator, currentUserPhonePrefix, email, id, options, organization, phone, phoneNumber, user, userObj, userObjs;
-            
+            if(doc.email){
+                doc.email = doc.email.toLowerCase();
+            }
             db.space_users.insertVaildate(userId, doc);
 
             if(doc.user){
@@ -297,6 +299,9 @@ Meteor.startup(function () {
         db.space_users.before.update(function (userId, doc, fieldNames, modifier, options) {
             var currentUserPhonePrefix, email_val, emails, emails_val, euser, isEmailCleared, isMobileCleared, lang, newEmail, newMobile, number, organization, paramString, params, ref, ref1, ref2, steedos_id, user_set, user_unset;
             modifier.$set = modifier.$set || {};
+            if(modifier.$set.email){
+                modifier.$set.email = modifier.$set.email.toLowerCase();
+            }
             db.space_users.updatevaildate(userId, doc, modifier);
             if (modifier.$set.organizations && modifier.$set.organizations.length > 0) {
                 // 修改所有组织后，强制把主组织自动设置为第一个组织
