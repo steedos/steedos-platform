2cae155fcfa4f7458ec50480bd17dca558dabed6
baozhoutao <baozhoutao@hotoa.com>
2020-03-16 18:27:26 +0800

    build 1.18.8

--- server/bundle/programs/server/packages/modules.js
+++ server/bundle/programs/server/packages/modules.js
@@ -843,7 +843,7 @@ module.useNode();
 module.exports = {
   "name": "@steedos/core",
   "private": false,
-  "version": "1.18.5",
+  "version": "1.18.6",
   "description": "",
   "main": "lib/index.js",
   "scripts": {
@@ -860,10 +860,10 @@ module.exports = {
     "@salesforce/dev-config": "^1.4.4",
     "@salesforce/kit": "^1.0.4",
     "@salesforce/ts-types": "^1.1.1",
-    "@steedos/auth": "^1.18.5",
-    "@steedos/migrate": "^1.18.5",
-    "@steedos/objectql": "^1.18.5",
-    "@steedos/standard-objects": "^1.18.5",
+    "@steedos/auth": "^1.18.6",
+    "@steedos/migrate": "^1.18.6",
+    "@steedos/objectql": "^1.18.6",
+    "@steedos/standard-objects": "^1.18.6",
     "@types/express": "^4.16.1",
     "@types/mongodb": "^3.1.22",
     "@types/node": "^11.10.4",
@@ -893,7 +893,7 @@ module.exports = {
   },
   "devDependencies": {
     "@salesforce/dev-scripts": "0.3.12",
-    "@steedos/meteor-bundle-runner": "^1.18.5",
+    "@steedos/meteor-bundle-runner": "^1.18.6",
     "@steedos/migrate": "^1.6.2",
     "@types/chai": "^4.1.7",
     "@types/chai-as-promised": "7.1.0",
@@ -936,7 +936,7 @@ module.useNode();
                                                                                                     //
 module.exports = {
   "name": "@steedos/auth",
-  "version": "1.18.5",
+  "version": "1.18.6",
   "main": "lib/index.js"
 };
 
@@ -996,7 +996,7 @@ module.useNode();
                                                                                                     //
 module.exports = {
   "name": "@steedos/filters",
-  "version": "1.18.5",
+  "version": "1.18.6",
   "main": "lib/index.js"
 };
 
