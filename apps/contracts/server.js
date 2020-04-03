0cb2a65f45fede775487071c7fbd53ee5c83850d
baozhoutao <baozhoutao@hotoa.com>
2020-04-03 12:02:12 +0800

    清理log
    https://github.com/steedos/steedos-project-saas/issues/104

--- apps/contracts/server.js
+++ apps/contracts/server.js
@@ -3,11 +3,6 @@ dotenv.config()
 
 var server = require('@steedos/meteor-bundle-runner');
 var steedos = require('@steedos/core');
-var i18n = require('@steedos/i18n');
-console.log('i18n', i18n);
-console.log('default key', i18n.t('key'))
-console.log('zh-CN key', i18n.t('key', {lng: 'zh-CN'}));
-console.log('zh-CN key getFixedT', i18n.getFixedT('zh-CN')('key'));
 server.Fiber(function () {
     try {
         server.Profile.run("Server startup", function () {
