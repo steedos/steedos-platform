2fe4b0b0844a869b9c57e3405c0a78534b8ec7fc
baozhoutao <baozhoutao@hotoa.com>
2020-04-18 11:32:03 +0800

    #269 项目模板新增百度统计

---
+++ packages/project-template-empty/public/accounts/a/dynamic.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6bdb41ab7e3b884e6dfdd331bcdd2622";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
