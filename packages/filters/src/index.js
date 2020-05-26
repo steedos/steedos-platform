d5678530c755782afc54fa04e12268bbc94befbc
殷亮辉 <yinlianghui@hotoa.com>
2020-05-26 16:37:30 +0800

    @steedos/filters 包中增加工具函数formatFiltersToGraphqlQuery
    https://github.com/steedos/creator/issues/1791

--- packages/filters/src/index.js
+++ packages/filters/src/index.js
@@ -3,6 +3,7 @@ const SteedosFilter = require("./filter");
 const format = require('./format');
 const utils = require('./utils');
 const formula = require('./formula');
+const graphql = require('./graphql');
 
 exports.SteedosFilter = SteedosFilter;
-Object.assign(exports, utils, formula, format);
+Object.assign(exports, utils, formula, format, graphql);
---
+++ packages/filters/test/unit/format_graphql.js
@@ -0,0 +1,27 @@
+const formatFiltersToGraphqlQuery = require('../../lib/index').formatFiltersToGraphqlQuery;
+const expect = require('chai').expect;
+
+describe('format filter and fields to graphql query', () => {
+    it('fields is a array', async () => {
+        let filters = [["create_date","between","this_year"]];
+        let fields = ["a","a.b.a","x.y","c"];
+        let result = formatFiltersToGraphqlQuery(JSON.stringify(filters), fields);
+        console.log("graphql filters query result:", result);
+        expect(result).to.be.include(`contracts(filters:[["create_date","between","this_year"]])`);
+        expect(result).to.be.include("    a {");
+        expect(result).to.be.include("        b {");
+        expect(result).to.be.include("    x {");
+        expect(result).to.be.include("    c");
+    });
+    it('fields is a string', async () => {
+        let filters = [["create_date","between","this_year"]];
+        let fields = "a,a.b.a,x.y,c";
+        let result = formatFiltersToGraphqlQuery(JSON.stringify(filters), fields);
+        console.log("graphql filters query result:", result);
+        expect(result).to.be.include(`contracts(filters:[["create_date","between","this_year"]])`);
+        expect(result).to.be.include("    a {");
+        expect(result).to.be.include("        b {");
+        expect(result).to.be.include("    x {");
+        expect(result).to.be.include("    c");
+    });
+});
