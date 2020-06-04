e4494d10d6b3767328577fed4786e4fe71797b80
baozhoutao <baozhoutao@hotoa.com>
2020-06-04 18:44:30 +0800

    space_users的profile字段值存储permission set的name

--- packages/standard-objects/permission_set.trigger.js
+++ packages/standard-objects/permission_set.trigger.js
@@ -1,4 +1,5 @@
 const _ = require('underscore');
+const odataMongodb = require("odata-v4-mongodb");
 
 const permissions = {
     allowEdit: false,
@@ -19,6 +20,24 @@ const internalPermissionSet = [
     {_id: 'customer', name: 'customer', label: 'customer',...baseRecord}
 ];
 
+function parserFilters(filters){
+    let query = {};
+    if(_.isArray(filters)){
+        _.each(filters,function(filter){
+            Object.assign(query, parserFilters(filter))
+        })
+    }else{
+        _.each(filters, function (v, k) {
+            if (k === '$and') {
+                Object.assign(query, parserFilters(v))
+            } else {
+                Object.assign(query, {[k]: v})
+            }
+        })
+    }
+    return query;
+}
+
 const getInternalPermissionSet = function(spaceId){
     if(!spaceId){
         return internalPermissionSet;
@@ -35,22 +54,63 @@ const getInternalPermissionSet = function(spaceId){
     return perms;
 }
 
+const geByFilters = function(spaceId, filtersFun){
+    let all = getInternalPermissionSet(spaceId);
+    let pers = []
+    _.each(all, function(doc){
+        if(filtersFun(doc)){
+            pers.push(doc)
+        }
+    })
+    return pers;
+}
+
+const find = function(query){
+    let filters = parserFilters(odataMongodb.createFilter(query.filters));
+    console.log('filters', JSON.stringify(filters));
+
+    return geByFilters(query.space, (doc)=>{
+        let rev = true;
+        _.each(filters, function(v, k){
+            if(_.isString(v) && _.has(doc, k)){
+                rev = doc[k] === v
+            }
+        })
+        return rev;
+    })
+}
+
 module.exports = {
     beforeFind: async function () {
         console.log('beforeFind......');
     },
     afterFind: async function () {
-        let spaceId = this.spaceId;
-        console.log('afterFind......spaceId ', spaceId);
         if(_.isArray(this.data.values)){
-            _.forEach(getInternalPermissionSet(spaceId), (doc)=>{
+            // let filters = parserFilters(odataMongodb.createFilter(this.query.filters));
+            // if(filters._id && _.include(['admin','user','supplier','customer'], filters._id)){
+            //     let dbPerms = Creator.getCollection("permission_set").find({space: filters.spaceId, name: filters._id}).fetch();
+            //     if(dbPerms){
+            //         this.data.values = dbPerms
+            //         return ;
+            //     }
+            // }
+            _.forEach(getInternalPermissionSet(this.spaceId), (doc)=>{
                 this.data.values.unshift(doc)
             })
         }
     },
     afterFindOne: async function () {
         let id = this.id;
+        let spaceId = this.spaceId;
+        console.log('afterFindOne this ---> ', this);
         if(id && _.isEmpty(this.data.values)){
+            if(_.include(['admin','user','supplier','customer'], id) && spaceId){
+                let dbPerms = Creator.getCollection("permission_set").findOne({space: spaceId, name: id});
+                if(dbPerms){
+                    this.data.values = dbPerms
+                    return ;
+                }
+            }
             Object.assign(this.data.values, _.find(internalPermissionSet, (doc)=>{
                 return doc._id === id
             }))
