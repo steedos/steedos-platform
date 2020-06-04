8c08bd736f06103cab5d33588932aa41290836b4
殷亮辉 <yinlianghui@hotoa.com>
2020-06-04 13:43:50 +0800

    增加其他类型报表字段必填校验
    https://github.com/steedos/steedos-project-fssh/issues/41

--- packages/standard-objects/reports.object.trigger.js
+++ packages/standard-objects/reports.object.trigger.js
@@ -5,18 +5,58 @@ module.exports = {
     listenTo: 'reports',
 
     beforeInsert: async function () {
-        var doc = this.doc
-        if (doc.report_type === "matrix") {
+        var doc = this.doc;
+        if (doc.report_type === "tabular") {
+            if(!(doc.columns && doc.columns.length)){
+                throw new Error("creator_report_error_tabular_miss_fields");
+            }
+        }
+        else if (doc.report_type === "summary") {
+            if(!(doc.columns && doc.columns.length)){
+                throw new Error("creator_report_error_summary_miss_fields");
+            }
+        }
+        else if (doc.report_type === "matrix") {
             if(!(doc.rows && doc.rows.length) && !(doc.columns && doc.columns.length)){
                 throw new Error("creator_report_error_matrix_miss_fields");
             }
         }
+        else if (doc.report_type === "stimulsoft-report") {
+            if(!(doc.fields && doc.fields.length)){
+                throw new Error("creator_report_error_stimulsoft_report_miss_fields");
+            }
+        }
+        else if (doc.report_type === "jsreport") {
+            if(!(doc.fields && doc.fields.length)){
+                throw new Error("creator_report_error_jsreport_miss_fields");
+            }
+        }
     },
     beforeUpdate: async function () {
-        var doc = this.doc
-        if (doc.report_type === "matrix") {
+        var doc = this.doc;
+        if (doc.report_type === "tabular") {
+            if(!(doc.columns && doc.columns.length)){
+                throw new Error("creator_report_error_tabular_miss_fields");
+            }
+        }
+        else if (doc.report_type === "summary") {
+            if(!(doc.columns && doc.columns.length)){
+                throw new Error("creator_report_error_summary_miss_fields");
+            }
+        }
+        else if (doc.report_type === "matrix") {
             if(!(doc.rows && doc.rows.length) && !(doc.columns && doc.columns.length)){
                 throw new Error("creator_report_error_matrix_miss_fields");
+            }
+        }
+        else if (doc.report_type === "stimulsoft-report") {
+            if(!(doc.fields && doc.fields.length)){
+                throw new Error("creator_report_error_stimulsoft_report_miss_fields");
+            }
+        }
+        else if (doc.report_type === "jsreport") {
+            if(!(doc.fields && doc.fields.length)){
+                throw new Error("creator_report_error_jsreport_miss_fields");
             }
         }
     }
