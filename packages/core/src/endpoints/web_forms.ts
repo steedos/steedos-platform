7836e2a1af4e085984ed00693ba6325668654f63
殷亮辉 <yinlianghui@hotoa.com>
2020-06-18 10:10:41 +0800

    #377 实现表单发送后发通知功能

--- packages/core/src/endpoints/web_forms.ts
+++ packages/core/src/endpoints/web_forms.ts
@@ -1,5 +1,34 @@
 import * as express from 'express';
 import { getSteedosSchema } from '@steedos/objectql';
+const Fiber = require('fibers');
+
+const addNotifications:any = function (object:any, doc:any, members:any) {
+    console.log("===addNotifications====members===", members);
+    if (!members || !members.length) {
+        return;
+    }
+    const nameKey = object.NAME_FIELD_KEY;
+    const notificationTitle = object.label;
+    let notificationBody = doc[nameKey];
+    if(!notificationBody){
+        notificationBody = "在线表单有一条新记录提交。";
+    }
+    let notificationDoc = {
+        name: notificationTitle,
+        body: notificationBody,
+        related_to: {
+            o: object.name,
+            ids: [doc._id]
+        },
+        related_name: doc[nameKey],
+        from: null,
+        space: doc.space
+    };
+    console.log("===addNotifications====notificationDoc===", notificationDoc);
+    Fiber(function () {
+        Creator.addNotifications(notificationDoc, null, members);
+    }).run();
+}
 
 export const postObjectWebForm = async (req: express.Request, res: express.Response) => {
     try {
@@ -16,7 +45,7 @@ export const postObjectWebForm = async (req: express.Request, res: express.Respo
         let bodyParams = req.body;
         const formId = bodyParams.steedos_form_id;
         const objectWebForms = getSteedosSchema().getObject("web_forms");
-        let formDoc = await objectWebForms.findOne(formId, { fields: ["space", "object_name", "record_owner"]});
+        let formDoc = await objectWebForms.findOne(formId, { fields: ["space", "object_name", "record_owner", "notification_users"]});
         if (!formDoc) {
             return res.status(401).send({
                 "error": `Validate Request -- The Web Form '${formId}' Not Fount`,
@@ -37,7 +66,16 @@ export const postObjectWebForm = async (req: express.Request, res: express.Respo
             let body = {};
             body['entity'] = entity;
             body['success'] = true;
-            res.status(200).send(body);
+            try{
+                addNotifications(object, entityDoc, formDoc.notification_users);
+                res.status(200).send(body);
+            }
+            catch (error) {
+                return res.status(401).send({
+                    "error": `Add notifications failed after post web form: ${error}`,
+                    "success": false
+                })
+            }
         }
     } catch (error) {
         return res.status(401).send({
