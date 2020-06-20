ef22fa3c166eb2d04e8dd8fbe12b76418f0672e2
baozhoutao <baozhoutao@hotoa.com>
2020-06-20 17:51:44 +0800

    优化权限组自定义按钮功能：应该是直接新建记录并打开

--- packages/standard-objects/permission_set.action.js
+++ packages/standard-objects/permission_set.action.js
@@ -1,11 +1,12 @@
 module.exports = {
     customize: function (object_name, record_id, fields) {
         var doc = Creator.odata.get(object_name, record_id)
-        Session.set('cmDoc', doc);
-        Session.set('cmShowAgainDuplicated', true);
-        Meteor.defer(function(){
-            $(".creator-add").click()
-        })
+        Creator.odata.insert(object_name, {name: doc.name, label: doc.label, type: doc.type}, function(result, error){
+            if(result){
+                FlowRouter.go(`/app/-/${object_name}/view/${result._id}`)
+            }
+        });
+            
     },
     customizeVisible: function(object_name, record_id, record_permissions, record){
         return Creator.baseObject.actions.standard_new.visible() && record.is_system;
