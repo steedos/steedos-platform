d0c1bcfc9448a16965d93583141362627146c1ac
baozhoutao <baozhoutao@hotoa.com>
2020-07-13 16:25:10 +0800

    fix：报表列表中显示代码中定义的系统报表 #515

--- packages/standard-objects/reports.object.trigger.js
+++ packages/standard-objects/reports.object.trigger.js
@@ -1,5 +1,28 @@
 const _ = require('underscore');
+const clone = require("clone");
 const objectql = require("@steedos/objectql");
+const auth = require("@steedos/auth");
+const internalData = require("./core/internalData")
+const permissions = {
+    allowEdit: false,
+    allowDelete: false,
+    allowRead: true,
+}
+
+const baseRecord = {
+    is_system:true,
+    record_permissions:permissions
+}
+
+//.report.yml 中的文件 应该包括 html、script、helper、mrt
+//暂时不支持jsReport、stimulsoft-report
+const getReoprts = function(){
+    return _.filter(clone(objectql.getConfigs("report")), function(report){
+        if(_.include(["tabular", "summary", "matrix"], report.report_type)){
+            return true
+        }
+    })
+}
 
 module.exports = {
 
@@ -62,12 +85,31 @@ module.exports = {
         }
     },
     afterFind: async function(){
-        this.data.values = this.data.values.concat(objectql.getConfigs("report"))
+        let reports = getReoprts();
+        let self = this;
+        _.each(reports, function(report){
+            self.data.values.push(Object.assign({}, report, baseRecord));
+        })
+    },
+    afterCount: async function(){
+        let filters = internalData.parserFilters(this.query.filters)
+        if(filters && filters.space === 'global'){
+            return
+        }
+        let result = await objectql.getObject('reports').find(this.query, await auth.getSessionByUserId(this.userId, this.spaceId))
+        this.data.values = result.length
     },
     afterFindOne: async function(){
         let id = this.id;
         if(id && _.isEmpty(this.data.values)){
-            this.data.values = objectql.getConfigs("report")[0]
+            let reports = getReoprts();
+            let report = _.find(reports, function(_f){                
+                return _f._id === id
+            })
+            if(report){
+                this.data.values = Object.assign({}, report, baseRecord)
+            }
+            
         }
     }
 }
