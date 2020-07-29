e308f4d4b64226867d97dc62fef8d513b7367363
殷亮辉 <yinlianghui@hotoa.com>
2020-07-29 11:44:30 +0800

    #521 简化cms权限功能，站点成员只有私有站点有用

--- packages/standard-objects/cms/cms_posts.client.js
+++ packages/standard-objects/cms/cms_posts.client.js
@@ -83,74 +83,74 @@ Meteor.autorun(function(){
     });
 
     // standard_new、standard_edit等baseObject中的actions用上面的`Object.assign(actions`写法不会生效，只能单独拿出来写
-    actions.standard_new.visible = function (object_name, record_id, record_permissions) {
-      var site = Session.get("site");
-      var allowCreate = Creator.baseObject.actions.standard_new.visible.apply(this, arguments);
-      if(!allowCreate){
-          // permissions配置没有权限则不给权限
-          return false
-      }
-      // 管理员要单独判断，只给到有对应站点成员的权限
-      if(Steedos.isSpaceAdmin()){
-          return true;
-      }
-      else{
-        var userId = Steedos.userId();
-        var isSiteAdmin = site && site.admins && site.admins.indexOf(userId) > -1;
-        return isSiteAdmin;
-      }
-    }
+    // actions.standard_new.visible = function (object_name, record_id, record_permissions) {
+    //   var site = Session.get("site");
+    //   var allowCreate = Creator.baseObject.actions.standard_new.visible.apply(this, arguments);
+    //   if(!allowCreate){
+    //       // permissions配置没有权限则不给权限
+    //       return false
+    //   }
+    //   // 管理员要单独判断，只给到有对应站点成员的权限
+    //   if(Steedos.isSpaceAdmin()){
+    //       return true;
+    //   }
+    //   else{
+    //     var userId = Steedos.userId();
+    //     var isSiteAdmin = site && site.admins && site.admins.indexOf(userId) > -1;
+    //     return isSiteAdmin;
+    //   }
+    // }
 
-    actions.standard_edit.visible = function (object_name, record_id, record_permissions) {
-      var site = Session.get("site");
-      var allowEdit = Creator.baseObject.actions.standard_edit.visible.apply(this, arguments);
-      if(!allowEdit){
-          // permissions配置没有权限则不给权限
-          return false
-      }
-      // 管理员要单独判断，只给到有对应站点成员的权限
-      if(Steedos.isSpaceAdmin()){
-          return true;
-      }
-      else{
-        var userId = Steedos.userId();
-        if(!site){
-          // 文档详细界面拿不到当前选中站点时，只能请求记录本身所属站点的信息来判断
-          var record = Creator.getObjectRecord(object_name, record_id, "site");
-          if(record && record.site){
-            var siteId = typeof record.site === "string" ? record.site : record.site._id;
-            site = Creator.odata.get("cms_sites", siteId, "admins");
-          }
-        }
-        var isSiteAdmin = site && site.admins && site.admins.indexOf(userId) > -1;
-        return isSiteAdmin;
-      }
-    }
+    // actions.standard_edit.visible = function (object_name, record_id, record_permissions) {
+    //   var site = Session.get("site");
+    //   var allowEdit = Creator.baseObject.actions.standard_edit.visible.apply(this, arguments);
+    //   if(!allowEdit){
+    //       // permissions配置没有权限则不给权限
+    //       return false
+    //   }
+    //   // 管理员要单独判断，只给到有对应站点成员的权限
+    //   if(Steedos.isSpaceAdmin()){
+    //       return true;
+    //   }
+    //   else{
+    //     var userId = Steedos.userId();
+    //     if(!site){
+    //       // 文档详细界面拿不到当前选中站点时，只能请求记录本身所属站点的信息来判断
+    //       var record = Creator.getObjectRecord(object_name, record_id, "site");
+    //       if(record && record.site){
+    //         var siteId = typeof record.site === "string" ? record.site : record.site._id;
+    //         site = Creator.odata.get("cms_sites", siteId, "admins");
+    //       }
+    //     }
+    //     var isSiteAdmin = site && site.admins && site.admins.indexOf(userId) > -1;
+    //     return isSiteAdmin;
+    //   }
+    // }
 
-    actions.standard_delete.visible = function (object_name, record_id, record_permissions) {
-      var site = Session.get("site");
-      var allowDelete = Creator.baseObject.actions.standard_delete.visible.apply(this, arguments);
-      if(!allowDelete){
-          // permissions配置没有权限则不给权限
-          return false
-      }
-      // 管理员要单独判断，只给到有对应站点成员的权限
-      if(Steedos.isSpaceAdmin()){
-          return true;
-      }
-      else{
-        var userId = Steedos.userId();
-        if(!site){
-          // 文档详细界面拿不到当前选中站点时，只能请求记录本身所属站点的信息来判断
-          var record = Creator.getObjectRecord(object_name, record_id, "site");
-          if(record && record.site){
-            var siteId = typeof record.site === "string" ? record.site : record.site._id;
-            site = Creator.odata.get("cms_sites", siteId, "admins");
-          }
-        }
-        var isSiteAdmin = site && site.admins && site.admins.indexOf(userId) > -1;
-        return isSiteAdmin;
-      }
-    }
+    // actions.standard_delete.visible = function (object_name, record_id, record_permissions) {
+    //   var site = Session.get("site");
+    //   var allowDelete = Creator.baseObject.actions.standard_delete.visible.apply(this, arguments);
+    //   if(!allowDelete){
+    //       // permissions配置没有权限则不给权限
+    //       return false
+    //   }
+    //   // 管理员要单独判断，只给到有对应站点成员的权限
+    //   if(Steedos.isSpaceAdmin()){
+    //       return true;
+    //   }
+    //   else{
+    //     var userId = Steedos.userId();
+    //     if(!site){
+    //       // 文档详细界面拿不到当前选中站点时，只能请求记录本身所属站点的信息来判断
+    //       var record = Creator.getObjectRecord(object_name, record_id, "site");
+    //       if(record && record.site){
+    //         var siteId = typeof record.site === "string" ? record.site : record.site._id;
+    //         site = Creator.odata.get("cms_sites", siteId, "admins");
+    //       }
+    //     }
+    //     var isSiteAdmin = site && site.admins && site.admins.indexOf(userId) > -1;
+    //     return isSiteAdmin;
+    //   }
+    // }
   }
 });
