75f02e65faf7d9ed42356486cc5a9b5db31c885a
殷亮辉 <yinlianghui@hotoa.com>
2020-08-01 10:38:56 +0800

    #639 效验时间值格式

--- packages/standard-objects/business_hours.object.trigger.js
+++ packages/standard-objects/business_hours.object.trigger.js
@@ -1,11 +1,46 @@
 const objectql = require("@steedos/objectql");
 
+const getTimeValue = (str)=>{
+    str = str.trim();
+    if(!/^\d{1,2}:\d{1,2}$/.test(str)){
+        return false;
+    }
+    const splits = str.split(":");
+    const h = parseInt(splits[0]);
+    const m = parseInt(splits[1]);
+    if(h > 24 || m > 60){
+        return false;
+    }
+    else if(h === 24 && m > 0){
+        return false;
+    }
+    return { h, m };
+}
+
+const validateStartEnd = (start, end)=>{
+    // 验证start, end为HH:MM格式，且结束时间必须晚于开始时间
+    start = start.trim();
+    end = end.trim();
+    const timeValueStart = getTimeValue(start)
+    if(!timeValueStart){
+        throw new Error("business_hours_error_start_format");
+    }
+    const timeValueEnd = getTimeValue(end)
+    if(!timeValueEnd){
+        throw new Error("business_hours_error_end_format");
+    }
+    if(parseInt(end.replace(":", "")) <= parseInt(start.replace(":", ""))){
+        throw new Error("business_hours_error_start_lt_end");
+    }
+}
+
 module.exports = {
 
     listenTo: 'business_hours',
 
     beforeInsert: async function () {
         var doc = this.doc;
+        validateStartEnd(doc.start, doc.end);
         if (doc.is_default) {
             const repeats = await objectql.getObject("business_hours").find({
                 filters: [["space", "=", doc.space], ["is_default", "=", true]],
@@ -20,6 +55,7 @@ module.exports = {
     beforeUpdate: async function () {
         // 因为afterUpdate中没有this.doc._id，所以把this.id集成过去
         const doc = Object.assign({}, this.doc, {_id: this.id});
+        validateStartEnd(doc.start, doc.end);
         if (doc.is_default) {
             const repeats = await objectql.getObject("business_hours").find({
                 filters: [["space", "=", doc.space], ["is_default", "=", true], ["_id", "!=", doc._id]],
