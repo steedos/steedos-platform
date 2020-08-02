7c7fff1d3d6fdc99bb7e5ac902cb2d9697f047dd
baozhoutao <baozhoutao@hotoa.com>
2020-08-02 18:25:26 +0800

    调整标准对象
    https://github.com/steedos/steedos-project-huayan/issues/5

--- packages/standard-objects/license.object.action.js
+++ packages/standard-objects/license.object.action.js
@@ -7,69 +7,54 @@ module.exports = {
     spaceId = Session.get("spaceId");
     spaceName = Creator.getCollection("spaces").findOne(spaceId).name;
     licenseServer = 'https://community.trial.steedos.com:8443';
-    licenseSpaceId = 'LHXoSJWEtDKuHFyge';
-    window.open(licenseServer + "/accounts/a/#/signup?X-Space-Id=" + licenseSpaceId + "&redirect_uri=" + encodeURIComponent(licenseServer + "/api/v4/product_license/apply/new?q=" + spaceId + "&n=" + spaceName));
-    swal({
-      title: TAPi18n.__('license_trial_header_swal_title'),
-      type: "warning",
-      showCancelButton: true,
-      cancelButtonText: TAPi18n.__('license_trial_header_swal_cancelButtonText'),
-      confirmButtonColor: "#DD6B55",
-      confirmButtonText: TAPi18n.__('license_trial_header_swal_confirmButtonText'),
-      closeOnConfirm: true
-    }, function (isConfirm) {
-      var authToken, authorization, headers, url, userSession;
-      if (isConfirm) {
-        $("body").addClass("loading");
-        userSession = Creator.USER_CONTEXT;
-        spaceId = userSession.spaceId;
-        if (userSession.authToken) {
-          authToken = userSession.authToken;
-        } else {
-          authToken = userSession.user.authToken;
-        }
-        url = "/api/v4/spaces/" + spaceId + "/clean_license";
-        url = Steedos.absoluteUrl(url);
-        authorization = "Bearer " + spaceId + "," + authToken;
-        headers = [
-          {
-            name: 'Content-Type',
-            value: 'application/json'
-          },
-          {
-            name: 'Authorization',
-            value: authorization
-          }
-        ];
-        return $.ajax({
-          type: "get",
-          url: url,
-          dataType: "json",
-          contentType: 'application/json',
-          beforeSend: function (XHR) {
-            if (headers && headers.length) {
-              return headers.forEach(function (header) {
-                return XHR.setRequestHeader(header.name, header.value);
-              });
-            }
-          },
-          success: function (data) {
-            $("body").removeClass("loading");
-            return window.location.reload();
-          },
-          error: function (XMLHttpRequest, textStatus, errorThrown) {
-            $("body").removeClass("loading");
-            if (XMLHttpRequest.responseJSON && XMLHttpRequest.responseJSON.error) {
-              return toastr.error(t(XMLHttpRequest.responseJSON.error.replace(/:/g, '：')));
-            } else {
-              return toastr.error(XMLHttpRequest.responseJSON);
-            }
-          }
-        });
-      }
-    });
+    toastr.info("升级完成后，请点击同步按钮");
+    window.open(licenseServer + "/accounts/a/#/signup?redirect_uri=" + encodeURIComponent(licenseServer));
   },
   sync: function (object_name, record_id, fields) {
-    console.log('sync....');
+    let userSession = Creator.USER_CONTEXT;
+    let spaceId = userSession.spaceId;
+    if (userSession.authToken) {
+      authToken = userSession.authToken;
+    } else {
+      authToken = userSession.user.authToken;
+    }
+    let authorization = "Bearer " + spaceId + "," + authToken;
+    let headers = [
+      {
+        name: 'Content-Type',
+        value: 'application/json'
+      },
+      {
+        name: 'Authorization',
+        value: authorization
+      }
+    ];
+    let url = "/api/v4/license/" + spaceId + "/sync";
+    url = Steedos.absoluteUrl(url);
+    $.ajax({
+      type: "get",
+      url: url,
+      dataType: "json",
+      contentType: 'application/json',
+      beforeSend: function (XHR) {
+        if (headers && headers.length) {
+          return headers.forEach(function (header) {
+            return XHR.setRequestHeader(header.name, header.value);
+          });
+        }
+      },
+      success: function (data) {
+        $("body").removeClass("loading");
+        return window.location.reload();
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+        $("body").removeClass("loading");
+        if (XMLHttpRequest.responseJSON && XMLHttpRequest.responseJSON.error) {
+          return toastr.error(t(XMLHttpRequest.responseJSON.error.replace(/:/g, '：')));
+        } else {
+          return toastr.error(XMLHttpRequest.responseJSON);
+        }
+      }
+    });
   }
 }
