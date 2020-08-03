f4b59ec20da45ac73d61c066fa63729156ea5bae
殷亮辉 <yinlianghui@hotoa.com>
2020-08-03 15:09:50 +0800

    #639 工作时间表增加两个午休字段，并在触发器是校验格式

--- packages/standard-objects/business_hours.object.trigger.js
+++ packages/standard-objects/business_hours.object.trigger.js
@@ -26,8 +26,8 @@ const getTimeValue = (str)=>{
     return { h, m, value};
 }
 
-const validateStartEnd = (start, end)=>{
-    // 验证start, end为HH:MM格式，且结束时间必须晚于开始时间
+const validateStartEnd = (start, end, lunch_start, lunch_end)=>{
+    // 验证start, end为HH:MM格式，结束时间必须晚于开始时间，午休时间必须在开始结束时间范围内
     start = start.trim();
     end = end.trim();
     const timeValueStart = getTimeValue(start)
@@ -41,6 +41,24 @@ const validateStartEnd = (start, end)=>{
     if(timeValueStart.value >= parseInt(timeValueEnd.value)){
         throw new Error("business_hours_error_start_lt_end");
     }
+
+    lunch_start = lunch_start.trim();
+    lunch_end = lunch_end.trim();
+    const timeValueLunchStart = getTimeValue(lunch_start)
+    if(!timeValueLunchStart){
+        throw new Error("business_hours_error_lunch_start_format");
+    }
+    const timeValueLunchEnd = getTimeValue(lunch_end)
+    if(!timeValueLunchEnd){
+        throw new Error("business_hours_error_lunch_end_format");
+    }
+    if(timeValueLunchStart.value >= parseInt(timeValueLunchEnd.value)){
+        throw new Error("business_hours_error_lunch_start_lt_lunch_end");
+    }
+
+    if(timeValueStart.value >= parseInt(timeValueLunchStart.value) || timeValueEnd.value <= parseInt(timeValueLunchEnd.value)){
+        throw new Error("business_hours_error_lunch_start_end_overflow");
+    }
 }
 
 module.exports = {
@@ -49,7 +67,7 @@ module.exports = {
 
     beforeInsert: async function () {
         var doc = this.doc;
-        validateStartEnd(doc.start, doc.end);
+        validateStartEnd(doc.start, doc.end, doc.lunch_start, doc.lunch_end);
         if (doc.is_default) {
             const repeats = await objectql.getObject("business_hours").find({
                 filters: [["space", "=", doc.space], ["is_default", "=", true]],
@@ -64,7 +82,7 @@ module.exports = {
     beforeUpdate: async function () {
         // 因为afterUpdate中没有this.doc._id，所以把this.id集成过去
         const doc = Object.assign({}, this.doc, {_id: this.id});
-        validateStartEnd(doc.start, doc.end);
+        validateStartEnd(doc.start, doc.end, doc.lunch_start, doc.lunch_end);
         if (doc.is_default) {
             const repeats = await objectql.getObject("business_hours").find({
                 filters: [["space", "=", doc.space], ["is_default", "=", true], ["_id", "!=", doc._id]],
