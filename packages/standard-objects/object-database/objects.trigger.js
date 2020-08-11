6f0147510a872f0784daaf7db0e3dc3ecb270852
baozhoutao <baozhoutao@hotoa.com>
2020-08-11 17:47:02 +0800

    fix: 国际化 #686

--- packages/standard-objects/object-database/objects.trigger.js
+++ packages/standard-objects/object-database/objects.trigger.js
@@ -2,16 +2,18 @@ const InternalData = require('../core/internalData');
 const _ = require('underscore');
 module.exports = {
     afterFind: async function(){
+        let userId = this.userId
         _.each(this.data.values, function(doc){
-            doc.fields =  Object.assign({}, doc.fields, InternalData.getDefaultSysFields(doc.name, this.userId)) ;
+            doc.fields =  Object.assign({}, doc.fields, InternalData.getDefaultSysFields(doc.name, userId)) ;
         })
-        this.data.values = this.data.values.concat(InternalData.findObjects(this.userId, this.query.filters))
+        this.data.values = this.data.values.concat(InternalData.findObjects(userId, this.query.filters))
     },
     afterAggregate: async function(){
+        let userId = this.userId
         _.each(this.data.values, function(doc){
-            doc.fields =  Object.assign({}, doc.fields, InternalData.getDefaultSysFields(doc.name, this.userId)) ;
+            doc.fields =  Object.assign({}, doc.fields, InternalData.getDefaultSysFields(doc.name, userId)) ;
         })
-        this.data.values = this.data.values.concat(InternalData.findObjects(this.userId, this.query.filters))
+        this.data.values = this.data.values.concat(InternalData.findObjects(userId, this.query.filters))
     },
     afterCount: async function(){
         this.data.values = this.data.values + InternalData.findObjects(this.userId, this.query.filters).length
