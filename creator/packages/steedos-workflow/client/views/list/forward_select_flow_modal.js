543a96d9843ed2919c1cdbd8345b173e347b8130
baozhoutao <baozhoutao@hotoa.com>
2020-08-25 14:45:50 +0800

    fix: 分发支持免费版； #755

--- creator/packages/steedos-workflow/client/views/list/forward_select_flow_modal.js
+++ creator/packages/steedos-workflow/client/views/list/forward_select_flow_modal.js
@@ -98,10 +98,6 @@ Template.forward_select_flow_modal.events({
 		var related = false;
 
 		if (action_type == 'forward') {
-			if (!Steedos.isLegalVersion('', "workflow.professional")) {
-				Steedos.spaceUpgradedModal()
-				return;
-			}
 			selectedUsers = [Meteor.userId()];
 		} else if (action_type == 'distribute') {
 			if (!Steedos.isLegalVersion('', "workflow.enterprise")) {
