10d7b191814db02dccc522005b93c0e531994226
baozhoutao <baozhoutao@hotoa.com>
2020-09-02 19:57:15 +0800

    优化提交审批按钮显示 #780

---
+++ packages/standard-objects/process/process_instance/process_instance_history.action.js
@@ -0,0 +1,14 @@
+module.exports = {
+    approve: function(object_name, record_id, fields){
+        
+    },
+    reject: function(object_name, record_id, fields){
+        
+    },
+    reassign: function(object_name, record_id, fields){
+        
+    },
+    remove: function(object_name, record_id, fields){
+        
+    }
+  }
