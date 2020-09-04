27e63a53f0387e791f048a219afe7ffd32321628
yinlianghui <yinlianghui@hotoa.com>
2020-09-04 16:10:42 +0800

    odata 删除接口，如果记录不存在时，直接return 。 #794 #791

--- packages/core/src/odata/MeteorODataExpressMiddleware.ts
+++ packages/core/src/odata/MeteorODataExpressMiddleware.ts
@@ -416,6 +416,9 @@ const deleteObjectData = async function (req: Request, res: Response) {
         }
         let permissions = await collection.getUserObjectPermission(userSession);
         let recordData = await collection.findOne(recordId, { fields: ['owner', 'company_id'] });
+        if(!recordData){
+            res.send({});
+        }
         let record_owner = recordData.owner;
         // let companyId = recordData.company_id;
         let isAllowed = (permissions.modifyAllRecords && permissions.allowDelete) || (permissions.modifyCompanyRecords && permissions.allowDelete) || (permissions.allowDelete && record_owner === userId);
