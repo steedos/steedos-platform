302f1ac3e64b221dcd350834e04f7406412439c9
殷亮辉 <yinlianghui@hotoa.com>
2020-09-04 14:14:32 +0800

    #782 #638 删除对象时应该跟停用对象逻辑一样，重新初始化数据源

--- packages/objectql/src/types/datasource.ts
+++ packages/objectql/src/types/datasource.ts
@@ -131,6 +131,7 @@ export class SteedosDataSourceType implements Dictionary {
         delete this._objectsConfig[object_name];
         delete this._objects[object_name];
         this.schema.removeObjectMap(object_name);
+        this.init();
     }
 
     initDriver() {
