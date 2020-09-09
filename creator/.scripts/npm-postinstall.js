68078012a994202409eff48d82a55164302fa353
hotlong <zhuangjianguo@hotoa.com>
2020-09-09 21:22:14 +0800

    ln 替代 yarn os

--- creator/.scripts/npm-postinstall.js
+++ creator/.scripts/npm-postinstall.js
@@ -8,10 +8,18 @@ console.log("Running npm-postinstall.js");
 execSync('cp -r .scripts/devextreme/* node_modules/devextreme/');
 execSync('rm -rf ./node_modules/crypto');
 
-if (process.platform == "darwin")
+if (process.platform == "darwin"){
     execSync('cp -r .scripts/bcrypt-57-darwin-x64/* node_modules/bcrypt/');
-else if (process.platform == "win32")
+    execSync('cp -r .scripts/bcrypt-57-darwin-x64/* ../node_modules/bcrypt/');
+}
+else if (process.platform == "win32") {
     execSync('cp -r .scripts/bcrypt-57-win32-x64/* node_modules/bcrypt/');
+    execSync('cp -r .scripts/bcrypt-57-win32-x64/* ../node_modules/bcrypt/');
+}
 
 // 修正 旧版 windows 客户端
 execSync('cp -r .scripts/iconv-lite/* node_modules/iconv-lite/');
+
+execSync('rm -rf node_modules/@steedos');
+
+execSync('ln -s ' + process.cwd() + '/../node_modules/@steedos ' + process.cwd() + '/node_modules/@steedos' );
