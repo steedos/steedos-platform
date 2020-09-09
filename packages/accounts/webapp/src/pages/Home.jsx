462b62d28893557230a60a07cea068f06e1041fe
hotlong <zhuangjianguo@hotoa.com>
2020-09-09 16:41:26 +0800

    Update Home.jsx

--- packages/accounts/webapp/src/pages/Home.jsx
+++ packages/accounts/webapp/src/pages/Home.jsx
@@ -44,75 +44,36 @@ class Home extends React.PureComponent {
     if (!currentUser || !currentSpace) {
       return null;
     }
-
-    const emailVerified = () => {
-      if (currentUser && currentUser.email_verified)
-        return (
-        <div className="mx-2 flex">
-          <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-          </svg>
-          <span>邮箱已验证</span>
-        </div>
-        )
-      else return (
-        <div className="mx-2 flex">
-          <span>邮箱未验证</span>
-        </div>
-      )
-    }
-
-    const mobileVerified = () => {
-      if (currentUser && currentUser.mobile_verified)
-        return (
-        <div className="mx-2 flex">
-          <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-          </svg>
-          <span>手机已验证</span>
-        </div>
-        )
-      else return (
-        <div className="mx-2 flex">
-          <span>手机未验证</span>
-        </div>
-      )
-    }
     return (
       <div>
         <Navbar user={currentUser}/>
-        {/* <h4>
-          <FormattedMessage
-              id='accounts.welcome'
-              defaultMessage='Welcome' 
-          /> {user.email || user.name}
-        </h4>
-        {!(!user.spaces || user.spaces.length === 0) && <Button onClick={onHome} variant="contained" color="primary">
-          <FormattedMessage
-              id='accounts.home'
-              defaultMessage='Home' 
-          /> 
-        </Button>
-        }
-        {(!user.spaces || user.spaces.length === 0) && <Typography variant="body2" gutterBottom><FormattedMessage
-              id='accounts.notFindSpaces'
-              defaultMessage='您没有所属公司，请联系系统管理员' 
-            /></Typography>
-        }
-        <br/>
-        <Button onClick={onLogout}>
-          <FormattedMessage
-              id='accounts.logout'
-              defaultMessage='Logout' 
-          /> 
-        </Button> */}
-
+<div class="px-4 sm:px-6 lg:px-8 py-6 border-b border-gray-200 md:flex md:items-center md:justify-between">
+  <div class="flex-1 min-w-0">
+    <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+      {this.props.currentSpace && this.props.currentSpace.name}
+    </h2>
+  </div>
+  <div class="mt-4 flex md:mt-0 md:ml-4">
+    {/* <span class="shadow-sm rounded-md">
+      <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 transition duration-150 ease-in-out">
+        切换企业
+      </button>
+    </span> */}
+    <span class="shadow-sm rounded-md">
+      <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-700 active:bg-blue-700 transition duration-150 ease-in-out"
+        onClick={this.onHome}>
+          进入首页
+      </button>
+    </span>
+  </div>
+</div>
+        
+{/* 
         <div className="bg-white overflow-hidden sm:shadow sm:rounded-lg sm:px-6 sm:mx-8 sm:my-8 md:max-w-5xl md:mx-auto">
           <div className="px-4 py-5 sm:p-6">
             <div className="py-6 md:flex md:items-center md:justify-between">
               <div className="flex-1 min-w-0">
                 <div className="flex items-center">
-                  {/* <img className="hidden h-15 w-15 rounded-full sm:block" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.6&w=256&h=256&q=80" alt=""/> */}
                   <span className="inline-block h-15 w-15 rounded-full overflow-hidden bg-gray-100 text-gray-500">
                     <svg className="h-full w-full" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
@@ -124,7 +85,7 @@ class Home extends React.PureComponent {
                         {this.props.currentSpace && this.props.currentSpace.name}
                       </h1>
                     </div>
-                    {/* <dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
+                    <dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
                       <dt className="sr-only">Company</dt>
                       <dd className="flex items-center text-sm leading-5 text-cool-gray-500 font-medium capitalize sm:mr-6">
                         <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-cool-gray-400" viewBox="0 0 20 20" fill="currentColor">
@@ -137,7 +98,7 @@ class Home extends React.PureComponent {
                         {emailVerified()}
                         {mobileVerified()}
                       </dd>
-                    </dl> */}
+                    </dl>
                   </div>
                 </div>
               </div>
@@ -148,15 +109,15 @@ class Home extends React.PureComponent {
                     进入首页
                   </button>
                 </span>
-                {/* <span className="shadow-sm rounded-md">
+                <span className="shadow-sm rounded-md">
                   <a href="#/preference" className="inline-flex items-center px-4 py-2 border border-cool-gray-300 text-sm leading-5 font-medium rounded-md text-cool-gray-700 bg-white hover:text-cool-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-cool-gray-800 active:bg-cool-gray-50 transition duration-150 ease-in-out">
                     账户设置
                   </a>
-                </span> */}
+                </span>
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
