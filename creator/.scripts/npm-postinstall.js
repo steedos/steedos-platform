addeeb4c4d1e89519bbe2767f469f4e9e5a5c0fc
baozhoutao <baozhoutao@hotoa.com>
2020-09-11 15:28:46 +0800

    npm-postinstall.js: 支持window

--- creator/.scripts/npm-postinstall.js
+++ creator/.scripts/npm-postinstall.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
-
+const path = require('path');
 console.log("Running npm-postinstall.js");
 
 //execSync('cp node_modules/devextreme/dist/css/fonts/* public/fonts/devextreme/');
@@ -25,5 +25,8 @@ else if (process.platform == "win32") {
 execSync('cp -r .scripts/iconv-lite/* node_modules/iconv-lite/');
 
 execSync('rm -rf node_modules/@steedos');
-
-execSync('ln -s ' + process.cwd() + '/../node_modules/@steedos ' + process.cwd() + '/node_modules/@steedos' );
+if (process.platform == "win32") {
+    execSync('mklink /J '+path.join(process.cwd(), '/node_modules/@steedos')+' '+ path.join(process.cwd(), '/../node_modules/@steedos'));
+}else{
+    execSync('ln -s ' + process.cwd() + '/../node_modules/@steedos ' + process.cwd() + '/node_modules/@steedos' );
+}
