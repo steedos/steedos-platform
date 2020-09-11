79de16e3c0083ab1ff2a6f3adf761ced03cdaaf9
hotlong <zhuangjianguo@hotoa.com>
2020-09-11 14:25:06 +0800

    修复 creator

--- creator/.scripts/npm-postinstall.js
+++ creator/.scripts/npm-postinstall.js
@@ -9,10 +9,14 @@ execSync('cp -r .scripts/devextreme/* node_modules/devextreme/');
 execSync('rm -rf ./node_modules/crypto');
 
 if (process.platform == "darwin"){
+    try {execSync('mkdir ./node_modules/bcrypt/');} catch(e) {}
+    execSync('cp -r ../node_modules/bcrypt/* ./node_modules/bcrypt/');
     execSync('cp -r .scripts/bcrypt-57-darwin-x64/* node_modules/bcrypt/');
     execSync('cp -r .scripts/bcrypt-57-darwin-x64/* ../node_modules/bcrypt/');
 }
 else if (process.platform == "win32") {
+    try {execSync('mkdir ./node_modules/bcrypt/');} catch(e) {}
+    execSync('cp -r ../node_modules/bcrypt/* ./node_modules/bcrypt/');
     execSync('cp -r .scripts/bcrypt-57-win32-x64/* node_modules/bcrypt/');
     execSync('cp -r .scripts/bcrypt-57-win32-x64/* ../node_modules/bcrypt/');
 }
