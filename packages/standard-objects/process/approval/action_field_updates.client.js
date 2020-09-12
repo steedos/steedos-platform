fc79152f1a9330326d4ad2b373cc3ab6e70b35a8
baozhoutao <baozhoutao@hotoa.com>
2020-09-12 12:08:08 +0800

    fix： process 设置优化完成 #780

--- packages/standard-objects/process/approval/action_field_updates.client.js
+++ packages/standard-objects/process/approval/action_field_updates.client.js
@@ -3,14 +3,59 @@ Steedos.ProcessFieldUpdatesManager = {};
 Steedos.ProcessFieldUpdatesManager.changeSchema = function(doc, schema){
     var objectSchema = Creator.getObjectSchema(Creator.getObject("action_field_updates"));
     var operation = doc.operation;
-    if(operation === 'null'){
-        schema._schema.formula.autoform.type = 'hidden';
-        schema._schema.literal_value.autoform.type = 'hidden';
+
+    var hiddenField = function(fieldName){
+      schema._schema[fieldName].autoform.omit = true;
+      schema._schema[fieldName].autoform.type = 'hidden';
+    }
+
+    if(doc.process_definition){
+      hiddenField('process_node');
+      schema._schema.when.autoform.options = _.filter(objectSchema.when.autoform.options, function(option){return _.include(['final_approval','final_rejection','recall','initial_submission'], option.value)});
+      var pid = doc.process_definition;
+      if(_.isObject(pid)){
+        if(_.has(pid, 'ids') && pid.ids.length > 0){
+          pid = pid.ids[0];
+        }else{
+          pid = pid._id;
+        }
       }
+      var object_name = Creator.odata.get("process_definition", pid, "object_name").object_name;
+      schema._schema.object_name.optionsFunction = function(){var _obj = Creator.getObject(object_name); return [{label: _obj.label, value: _obj.name, icon: _obj.icon}]}
+      doc.object_name = object_name;
+    }else{
+      schema._schema.process_node = objectSchema.process_node;
+      schema._schema.when.autoform.options = _.filter(objectSchema.when.autoform.options, function(option){return _.include(['approval','rejection'], option.value)})
+    }
+
+    if(doc.process_node){
+      hiddenField('process_definition');
+      var pnid = doc.process_node;
+      if(_.isObject(pnid)){
+        if(_.has(pnid, 'ids') && pnid.ids.length > 0){
+          pnid = pnid.ids[0];
+        }else{
+          pnid = pnid._id;
+        }
+      }
+      var processDefinitionId = Creator.odata.get("process_node", pnid, "process_definition").process_definition;
+      var object_name = Creator.odata.get("process_definition", processDefinitionId, "object_name").object_name;
+      schema._schema.object_name.optionsFunction = function(){var _obj = Creator.getObject(object_name); return [{label: _obj.label, value: _obj.name, icon: _obj.icon}]}
+      doc.object_name = object_name;
+      schema._schema.when.autoform.options = _.filter(objectSchema.when.autoform.options, function(option){return _.include(['approval','rejection'], option.value)})
+    }else{
+      schema._schema.process_definition = objectSchema.process_definition;
+      schema._schema.when.autoform.options = _.filter(objectSchema.when.autoform.options, function(option){return _.include(['final_approval','final_rejection','recall','initial_submission'], option.value)})
+    }
+
+    if(operation === 'null'){
+      hiddenField('formula');
+      hiddenField('literal_value');
+    }
 
       if(operation === 'formula'){
         schema._schema.formula = objectSchema.formula;
-        schema._schema.literal_value.autoform.type = 'hidden';
+        hiddenField('literal_value');
       }
 
       if(operation === 'literal'){
--- packages/standard-objects/process/approval/process_node.client.js
+++ packages/standard-objects/process/approval/process_node.client.js
@@ -3,20 +3,27 @@ Steedos.ProcessNodeManager = {};
 Steedos.ProcessNodeManager.changeSchema = function(doc, schema){
     var objectSchema = Creator.getObjectSchema(Creator.getObject("process_node"));
 
+    var hiddenField = function(fieldName){
+        schema._schema[fieldName].autoform.omit = true;
+        schema._schema[fieldName].autoform.type = 'hidden';
+    }
+
     if(doc.filtrad){
-        schema._schema.entry_criteria.autoform.omit = true;
-        schema._schema.if_criteria_not_met.autoform.omit = true;
+        hiddenField('entry_criteria');
+        hiddenField('if_criteria_not_met');
     }else{
         schema._schema.entry_criteria = objectSchema.entry_criteria;
         schema._schema.if_criteria_not_met = objectSchema.if_criteria_not_met;
     }
 
     if(doc.approver === 'submitter_choose'){
-        schema._schema.assigned_approver_users.autoform.omit = true;
-        schema._schema.assigned_approver_roles.autoform.omit = true;
-        schema._schema.assigned_approver_flow_roles.autoform.omit = true;
-        schema._schema.assigned_approver_user_field.autoform.omit = true;
-        schema._schema.when_multiple_approvers.autoform.omit = true;
+        hiddenField('assigned_approver_users');
+        hiddenField('assigned_approver_roles');
+        hiddenField('assigned_approver_flow_roles');
+        hiddenField('assigned_approver_user_field');
+        hiddenField('when_multiple_approvers');
+
+        schema._schema.when_multiple_approvers.optional = true;
     }else{
         schema._schema.assigned_approver_users = objectSchema.assigned_approver_users;
         schema._schema.assigned_approver_roles = objectSchema.assigned_approver_roles;
