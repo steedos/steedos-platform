4f6a8db89ae6dcdd81dc83b3074c6f869fabce0b
baozhoutao <baozhoutao@hotoa.com>
2020-09-12 14:32:11 +0800

    fix：开始步骤国际化 #780

--- packages/standard-objects/process/process_instance/process_instance_history.trigger.js
+++ packages/standard-objects/process/process_instance/process_instance_history.trigger.js
@@ -1,21 +1,35 @@
 const _ = require("underscore");
+
+
+const getLng = function(userId){
+  return Steedos.locale(userId, true);
+}
+
+const getEmptyNodeTitle = function(userId){
+  let lng = getLng(userId);
+  return TAPi18n.__('process_approval_empty_node_title', {}, lng);
+}
+
 module.exports = {
   afterFindOne: async function () {
+    let userId = this.userId;
     if(this.data.values && !this.data.values.step_node){
-      this.data.values.name = 'Approval Request Submitted';
+      this.data.values.name = getEmptyNodeTitle(userId);
     }
   },
   afterFind: async function () {
+    let userId = this.userId;
     _.each(this.data.values, function(value){
       if(!value.step_node){
-        value.name = 'Approval Request Submitted';
+        value.name = getEmptyNodeTitle(userId);
       }
     })
   },
   afterAggregate: async function () {
+    let userId = this.userId;
     _.each(this.data.values, function(value){
       if(!value.step_node){
-        value.name = 'Approval Request Submitted';
+        value.name = getEmptyNodeTitle(userId);
       }
     })
   },
