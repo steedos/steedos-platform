6cb0e68b100c885ddb9e0cd57bae7c81ede90a0e
baozhoutao <baozhoutao@hotoa.com>
2020-09-18 19:44:24 +0800

    字段更新表调整 相应调整业务代码  #833

--- packages/process/src/platform_action_manager.ts
+++ packages/process/src/platform_action_manager.ts
@@ -31,14 +31,57 @@ const runAction = async (action: any, recordId: any, userSession: any)=>{
 }
 
 export const runProcessNodeAction = async (processNodeId: string, when: string, recordId: string, userSession: any)=>{
-    const actions = await objectql.getObject("action_field_updates").find({filters: [["process_node", "=", processNodeId], ["when", "=", when], ["space", "=", userSession.spaceId]]})
+    const processNode = await objectql.getObject("process_node").findOne(processNodeId);
+    const filters = [];
+    switch (when) {
+        case 'approval':
+            if(!_.isEmpty(processNode.approval_updates_field_actions) && _.isArray(processNode.approval_updates_field_actions)){
+                filters.push(['_id', 'in', processNode.approval_updates_field_actions])
+            }
+            break;
+        case 'rejection':
+            if(!_.isEmpty(processNode.rejection_updates_field_actions) && _.isArray(processNode.rejection_updates_field_actions)){
+                filters.push(['_id', 'in', processNode.rejection_updates_field_actions])
+            }
+            break;
+        default:
+            throw new Error(`无效的参数when: ${when}`, );
+    }
+    
+    const actions = await objectql.getObject("action_field_updates").find({filters: filters})
     for (const action of actions) {
         await runAction(action, recordId, userSession)
     }
 }
 
 export const runProcessAction = async (processId: string, when: string, recordId: string, userSession: any)=>{
-    const actions = await objectql.getObject("action_field_updates").find({filters: [["process_definition", "=", processId], ["when", "=", when], ["space", "=", userSession.spaceId]]})
+    const process = await objectql.getObject("process_definition").findOne(processId);
+    const filters = [];
+    switch (when) {
+        case 'initial_submission':
+            if(!_.isEmpty(process.initial_submission_updates_field_actions) && _.isArray(process.initial_submission_updates_field_actions)){
+                filters.push(['_id', 'in', process.initial_submission_updates_field_actions])
+            }
+            break;
+        case 'final_approval':
+            if(!_.isEmpty(process.final_approval_updates_field_actions) && _.isArray(process.final_approval_updates_field_actions)){
+                filters.push(['_id', 'in', process.final_approval_updates_field_actions])
+            }
+            break;
+        case 'final_rejection':
+            if(!_.isEmpty(process.final_rejection_record_lock) && _.isArray(process.final_rejection_record_lock)){
+                filters.push(['_id', 'in', process.final_rejection_record_lock])
+            }
+            break;
+        case 'recall':
+            if(!_.isEmpty(process.recall_updates_field_actions) && _.isArray(process.recall_updates_field_actions)){
+                filters.push(['_id', 'in', process.recall_updates_field_actions])
+            }
+            break;
+        default:
+            throw new Error(`无效的参数when: ${when}`, );
+    }
+    const actions = await objectql.getObject("action_field_updates").find({filters: filters})
     for (const action of actions) {
         await runAction(action, recordId, userSession)
     }
