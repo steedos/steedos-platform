3a99ea100fd0d2b7aaab209966d0f1d7c8b3805c
baozhoutao <baozhoutao@hotoa.com>
2020-09-22 13:55:11 +0800

    fix bug: 背景图错了

--- packages/core/src/init/core.ts
+++ packages/core/src/init/core.ts
@@ -196,10 +196,13 @@ export const initPublic = () => {
     let publicPath = require.resolve("@steedos/webapp/package.json")
     publicPath = publicPath.replace("package.json", 'build')
     let routerPath = "/"
+    if(__meteor_runtime_config__.ROOT_URL_PATH_PREFIX){
+        routerPath = __meteor_runtime_config__.ROOT_URL_PATH_PREFIX
+    }
     const cacheTime = 86400000*1; // one day
     router.use(routerPath, express.static(publicPath, { maxAge: cacheTime }));
-    // WebApp.rawConnectHandlers.use(router);
-    WebApp.connectHandlers.use(router);
+    WebApp.rawConnectHandlers.use(router);
+    // WebApp.connectHandlers.use(router);
 }
 
 export const initDesignSystem = () => {
