b483fd2262c7f812cbf0f3411fbe24d1abb2cb62
baozhoutao <baozhoutao@hotoa.com>
2020-09-25 18:12:56 +0800

    add formulon && add getSubstitutionDataType function

---
+++ packages/objectql/src/formula/params.ts
@@ -0,0 +1,103 @@
+import _ = require('underscore');
+import { getObject } from '../types/object';
+
+const enum FormulonDataType {
+    Text = 'text',
+    Number = 'number',
+    Date = 'date',
+    Datetime = 'datetime',
+    Time = 'time',
+    Checkbox = 'checkbox',
+    Picklist = 'picklist',
+    Multipicklist = 'multipicklist',
+    Geolocation = 'geolocation'
+}
+
+function getFieldSteedosType(field: any){
+    if(_.include(['formula', 'summary'], field.type)){
+        return field.formula_type
+    }
+    return field.type
+}
+
+function getField(objectName: string, fieldName: string){
+    return getObject(objectName).getField(fieldName);
+}
+
+function getSubstitutionDataType(objectName: string, fieldName: string){
+
+    const field = getField(objectName, fieldName);
+
+    const steedosType = getFieldSteedosType(field);
+
+    switch (steedosType) {
+        case 'text':
+            return FormulonDataType.Text;
+        case 'textarea':
+            return FormulonDataType.Text;
+        case 'html':
+            return FormulonDataType.Text;
+        case 'password':
+            return FormulonDataType.Text;
+        case 'autonumber':
+            return FormulonDataType.Text;
+        case 'url':
+            return FormulonDataType.Text;
+        case 'email':
+            return FormulonDataType.Text;
+        case 'number':
+            return FormulonDataType.Number;
+        case 'currency':
+            return FormulonDataType.Number;
+        case 'date':
+            return FormulonDataType.Date;
+        case 'datetime':
+            return FormulonDataType.Datetime;
+        case 'boolean':
+            return FormulonDataType.Checkbox;
+        case 'select':
+            if(field.multiple){
+                return FormulonDataType.Multipicklist;
+            }else{
+                return FormulonDataType.Picklist;
+            }
+        case 'lookup':
+            if(field.multiple){
+                return FormulonDataType.Multipicklist;
+            }else{
+                return FormulonDataType.Picklist;
+            }
+        case 'master_detail':
+            return FormulonDataType.Picklist;
+        case 'geolocation':
+            return FormulonDataType.Geolocation;
+        default:
+            break;
+    }
+}
+
+function getSubstitutionOptions(objectName: string, fieldName: string, dataType: string){
+    const field = getField(objectName, fieldName);
+    switch (dataType) {
+        case 'number':
+            if(_.has(field, 'scale')){
+                return {scale: field.scale}
+            }else{
+                return {scale: 2}
+            }
+        case 'text':
+            return {length: Number.MAX_VALUE};
+        default:
+            return ;
+    }
+}
+
+export function getFieldSubstitution(objectName: string, fieldName: string, value: any){
+    let fieldSubstitution: any = {
+        type: 'literal',
+        value: value
+    }
+    fieldSubstitution.dataType = getSubstitutionDataType(objectName, fieldName);
+    fieldSubstitution.options = getSubstitutionOptions(objectName, fieldName, fieldSubstitution.dataType);
+    return fieldSubstitution;
+}
