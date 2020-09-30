328a49772540a753432fca1e54948d461a348ac2
baozhoutao <baozhoutao@hotoa.com>
2020-09-30 11:46:26 +0800

    Update development.js

--- creator/packages/steedos-base/server/development.js
+++ creator/packages/steedos-base/server/development.js
@@ -1,4 +1,5 @@
 if(Meteor.isDevelopment){
+	//Meteor 版本升级到1.9 及以上时(node 版本 11+)，可以删除此代码
 	Object.defineProperty(Array.prototype, 'flat', {
 		value: function(depth = 1) {
 			return this.reduce(function (flat, toFlatten) {
