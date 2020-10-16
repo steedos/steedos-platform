8dd7eaf08f412641fc90f3967cd99c3ca3bc9ad1
殷亮辉 <yinlianghui@hotoa.com>
2020-10-16 11:11:06 +0800

    登录界面在xp那个版本的老华炎办公客户端上没对齐 #982

--- packages/webapp/src/components/Card.tsx
+++ packages/webapp/src/components/Card.tsx
@@ -6,8 +6,8 @@ import Logo from './Logo';
 class Card extends React.Component {
   render() {
     return (
-<div className="flex sm:items-center justify-center mx-auto overflow-auto p-10 h-full">
-  <div className="absolute rounded p-11 sm:shadow-md bg-white w-screen max-w-md">
+<div className="flex sm:items-center justify-center mx-auto overflow-auto md:p-10 h-full">
+  <div className="relative rounded p-11 sm:shadow-md bg-white w-screen max-w-md">
     {this.props.children}
   </div>
 </div>
