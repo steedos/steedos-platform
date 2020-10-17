6075d9f76411637846330567d964a77835ab7684
baozhoutao <baozhoutao@hotoa.com>
2020-10-17 11:40:10 +0800

    #986 优化提示消息

--- packages/standard-objects/object-database/permission_objects.core.js
+++ packages/standard-objects/object-database/permission_objects.core.js
@@ -2,28 +2,30 @@ var objectql = require('@steedos/objectql');
 var objectCore = require('./objects.lib.js');
 
 function loadObjectPermission(doc){
-    try {
-        var dbObject = objectCore.getObjectFromDB(doc.object_name);
-        var objectDataSourceName = objectCore.getDataSourceName(dbObject);
+    var dbObject = objectCore.getObjectFromDB(doc.object_name);
+    var objectDataSourceName = objectCore.getDataSourceName(dbObject);
 
-        if(!dbObject){
+    if(!dbObject){
+        try {
             objectDataSourceName = objectql.getObject(doc.object_name).datasource.name;
-        }
-
-        if(dbObject && !objectCore.canLoadObject(dbObject.name, objectDataSourceName)){
+        } catch (error) {
             console.warn('warn: Not loaded. Invalid custom permission_objects -> ', doc.name, doc.object_name);
             return;
         }
-        const pset = Creator.getCollection("permission_set").findOne({_id: doc.permission_set_id, space: doc.space});
-        if(pset){
-            const datasource = objectql.getDataSource(objectDataSourceName);
-            if(datasource){
-                datasource.setObjectSpacePermission(doc.object_name, doc.space, Object.assign({}, doc, {name: pset.name}));
-            }
-        }
-    } catch (error) {
-        console.error(error);
     }
+
+    if(dbObject && !objectCore.canLoadObject(dbObject.name, objectDataSourceName)){
+        console.warn('warn: Not loaded. Invalid custom permission_objects -> ', doc.name, doc.object_name);
+        return;
+    }
+    const pset = Creator.getCollection("permission_set").findOne({_id: doc.permission_set_id, space: doc.space});
+    if(pset){
+        const datasource = objectql.getDataSource(objectDataSourceName);
+        if(datasource){
+            datasource.setObjectSpacePermission(doc.object_name, doc.space, Object.assign({}, doc, {name: pset.name}));
+        }
+    }
+    
 }
 
 function removeObjectPermission(doc){
