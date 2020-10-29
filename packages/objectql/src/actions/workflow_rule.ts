25797d8082f68c6b572cf576069aa71111e8d9fe
baozhoutao <baozhoutao@hotoa.com>
2020-10-29 17:16:43 +0800

    #1033 启用工作流规则功能

--- packages/objectql/src/actions/workflow_rule.ts
+++ packages/objectql/src/actions/workflow_rule.ts
@@ -3,38 +3,56 @@ import { computeFormula } from '../formula'
 import { WorkflowRule } from './types/workflow_rule';
 import { JsonMap } from "@salesforce/ts-types";
 import { runFieldUpdateActions } from './field_updates'
+import _ = require('underscore');
 
-export function runObjectWorkflowRules(objectName, event, record, userSession, previousRecord) {
+export async function runObjectWorkflowRules(objectName, event, record, userSession, previousRecord) {
+    if(!_.include(['insert', 'update'], event)){
+        return ;
+    }
+
+    let filters = [['object_name', '=', objectName], ['space', '=', record.space], ['active', '=', true]];
+
     switch (event) {
         case 'insert':
+            filters.push([['trigger_type', '=', 'onCreateOnly'], 'or', ['trigger_type', '=', 'onAllChanges'], 'or', ['trigger_type', '=', 'onCreateOrTriggeringUpdate']])
             break;
         case 'update':
+            filters.push([['trigger_type', '=', 'onAllChanges'], 'or', ['trigger_type', '=', 'onCreateOrTriggeringUpdate']])
             break;
         default:
             break;
     }
-}
 
-export async function runObjectWorkflowRulesOnCreateOnly(objectName, record, userSession) {
-    const wfRules = await getObject('workflow_rule').find({ filters: [['object_name', '=', objectName], ['space', '=', record.space], ['active', '=', true], ['trigger_type', '=', 'onCreateOnly']] });
+    const wfRules = await getObject('workflow_rule').find({ filters: filters});
     for (const wfRule of wfRules) {
-        await runWFRule(wfRule, record, userSession)
+        if(wfRule.trigger_type === 'onCreateOrTriggeringUpdate'){
+            await runWFRule(wfRule, record, userSession, previousRecord)
+        }else{
+            await runWFRule(wfRule, record, userSession)
+        }
     }
 }
 
-export async function runObjectWorkflowRulesOnAllChanges(objectName, record, userSession) {
-    const wfRules = await getObject('workflow_rule').find({ filters: [['object_name', '=', objectName], ['space', '=', record.space], ['active', '=', true], ['trigger_type', '=', 'onCreateOnly']] });
-    for (const wfRule of wfRules) {
-        await runWFRule(wfRule, record, userSession)
-    }
-}
+// export async function runObjectWorkflowRulesOnCreateOnly(objectName, record, userSession) {
+//     const wfRules = await getObject('workflow_rule').find({ filters: [['object_name', '=', objectName], ['space', '=', record.space], ['active', '=', true], ['trigger_type', '=', 'onCreateOnly']] });
+//     for (const wfRule of wfRules) {
+//         await runWFRule(wfRule, record, userSession)
+//     }
+// }
 
-export async function runObjectWorkflowRulesOnCreateOrTriggeringUpdate(objectName, record, userSession, previousRecord) {
-    const wfRules = await getObject('workflow_rule').find({ filters: [['object_name', '=', objectName], ['space', '=', record.space], ['active', '=', true], ['trigger_type', '=', 'onCreateOnly']] });
-    for (const wfRule of wfRules) {
-        await runWFRule(wfRule, record, userSession)
-    }
-}
+// export async function runObjectWorkflowRulesOnAllChanges(objectName, record, userSession) {
+//     const wfRules = await getObject('workflow_rule').find({ filters: [['object_name', '=', objectName], ['space', '=', record.space], ['active', '=', true], ['trigger_type', '=', 'onAllChanges']] });
+//     for (const wfRule of wfRules) {
+//         await runWFRule(wfRule, record, userSession)
+//     }
+// }
+
+// export async function runObjectWorkflowRulesOnCreateOrTriggeringUpdate(objectName, record, userSession, previousRecord) {
+//     const wfRules = await getObject('workflow_rule').find({ filters: [['object_name', '=', objectName], ['space', '=', record.space], ['active', '=', true], ['trigger_type', '=', 'onCreateOrTriggeringUpdate']] });
+//     for (const wfRule of wfRules) {
+//         await runWFRule(wfRule, record, userSession, previousRecord)
+//     }
+// }
 
 async function runWFRule(rule: WorkflowRule, record: JsonMap, userSession, previousRecord?: JsonMap) {
     let previousIsTrue = false;
