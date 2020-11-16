409d22cf2c378c7e29534790406b390444b5e01b
baozhoutao <baozhoutao@hotoa.com>
2020-11-16 15:56:15 +0800

    fix bug: 停用的账户禁止进入工作区

--- packages/auth/src/endpoints/validate.ts
+++ packages/auth/src/endpoints/validate.ts
@@ -1,22 +1,27 @@
 
 import * as express from 'express';
 import { auth } from '../session';
-import { setAuthCookies } from '../utils';
+import { setAuthCookies, clearAuthCookies } from '../utils';
 import { getSteedosSchema } from '@steedos/objectql';
 
 export const validate = async (req: express.Request, res: express.Response) => {
     let utcOffset = req.body.utcOffset;
     let userSession = await auth(req, res);
     if (userSession.userId) {
-        setAuthCookies(req, res, userSession.userId, userSession.authToken, userSession.spaceId);
+        let spaceUser = await getSteedosSchema().getObject('space_users').find({filters: [['space', '=', userSession.spaceId], ['user', '=', userSession.userId], ['user_accepted', '=', true]]});
+        if(spaceUser.length > 0){
+            setAuthCookies(req, res, userSession.userId, userSession.authToken, userSession.spaceId);
 
-        let user = await getSteedosSchema().getObject('users').findOne(userSession.userId, { fields: ['utcOffset','password_expired'] });
-
-        if (!user.hasOwnProperty('utcOffset')) {
-            await getSteedosSchema().getObject('users').update(userSession.userId, { 'utcOffset': utcOffset })
+            let user = await getSteedosSchema().getObject('users').findOne(userSession.userId, { fields: ['utcOffset','password_expired'] });
+    
+            if (!user.hasOwnProperty('utcOffset')) {
+                await getSteedosSchema().getObject('users').update(userSession.userId, { 'utcOffset': utcOffset })
+            }
+    
+            return res.send(Object.assign({}, userSession, {password_expired: user.password_expired}));
+        }else{
+            clearAuthCookies(req, res)
         }
-
-        return res.send(Object.assign({}, userSession, {password_expired: user.password_expired}));
     }
     return res.status(401).send({
         "error": "Validate Request -- Missing X-Auth-Token",
