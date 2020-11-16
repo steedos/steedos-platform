5e27e0f507e534f4c0e4751fc30ff7cff0187a69
baozhoutao <baozhoutao@hotoa.com>
2020-11-16 15:54:51 +0800

    build creator

--- server/bundle/programs/server/packages/steedos_autoform-lookup.js
+++ server/bundle/programs/server/packages/steedos_autoform-lookup.js
@@ -1,8 +1,5 @@
 (function () {
 
-/* Package-scope variables */
-var val, ordered_val, createFunction, _getOptions, options, _val, isReadonly, ref, data, UniSelectize, values, cf;
-
 
 
 /* Exports */
