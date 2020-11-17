bbeb267a273602b03887eaefb83a89a7383702a1
baozhoutao <baozhoutao@hotoa.com>
2020-11-17 11:12:57 +0800

    【1.22】简档添加密码策略：强制密码历史、最大无效登录尝试次数、锁定有效期间 #1123

--- creator/packages/steedos-base/server/methods/set_space_user_password.coffee
+++ creator/packages/steedos-base/server/methods/set_space_user_password.coffee
@@ -22,6 +22,9 @@ Meteor.methods
 		if this.userId == user_id
 			logout = false
 		Accounts.setPassword(user_id, password, {logout: logout})
+		changedUserInfo = db.users.findOne({_id: user_id})
+		if changedUserInfo
+			db.users.update({_id: user_id}, {$push: {'services.password_history': changedUserInfo.services?.password?.bcrypt}})
 
 		# 如果用户手机号通过验证，就发短信提醒
 		if userCP.mobile && userCP.mobile_verified
--- packages/auth/src/endpoints/validate.ts
+++ packages/auth/src/endpoints/validate.ts
@@ -1,22 +1,28 @@
 
 import * as express from 'express';
 import { auth } from '../session';
-import { setAuthCookies } from '../utils';
+import { setAuthCookies, clearAuthCookies } from '../utils';
 import { getSteedosSchema } from '@steedos/objectql';
 
 export const validate = async (req: express.Request, res: express.Response) => {
     let utcOffset = req.body.utcOffset;
     let userSession = await auth(req, res);
+    let spaceUser = await getSteedosSchema().getObject('space_users').find({filters: [['space', '=', userSession.spaceId], ['user', '=', userSession.userId], ['user_accepted', '=', true]]});
     if (userSession.userId) {
-        setAuthCookies(req, res, userSession.userId, userSession.authToken, userSession.spaceId);
-
-        let user = await getSteedosSchema().getObject('users').findOne(userSession.userId, { fields: ['utcOffset','password_expired'] });
-
-        if (!user.hasOwnProperty('utcOffset')) {
-            await getSteedosSchema().getObject('users').update(userSession.userId, { 'utcOffset': utcOffset })
+        if(spaceUser.length > 0){
+            let user = await getSteedosSchema().getObject('users').findOne(userSession.userId, { fields: ['utcOffset','password_expired','lockout'] });
+            if(user.lockout){
+                clearAuthCookies(req, res)
+            }else{
+                setAuthCookies(req, res, userSession.userId, userSession.authToken, userSession.spaceId);
+                if (!user.hasOwnProperty('utcOffset')) {
+                    await getSteedosSchema().getObject('users').update(userSession.userId, { 'utcOffset': utcOffset })
+                }
+                return res.send(Object.assign({}, userSession, {password_expired: user.password_expired}));
+            }
+        }else{
+            clearAuthCookies(req, res)
         }
-
-        return res.send(Object.assign({}, userSession, {password_expired: user.password_expired}));
     }
     return res.status(401).send({
         "error": "Validate Request -- Missing X-Auth-Token",
