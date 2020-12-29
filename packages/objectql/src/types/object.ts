18b2e1613489156d8ad1aff79085e4585de24dbf
殷亮辉 <yinlianghui@hotoa.com>
2020-12-29 23:49:22 +0800

    累计汇总1.23最新版本源码，新建子表记录时未正确触发主表的累计汇总重算 #1279
    #1242

--- packages/objectql/src/types/object.ts
+++ packages/objectql/src/types/object.ts
@@ -743,6 +743,7 @@ export class SteedosObjectType extends SteedosObjectProperties {
         if(["insert", "update", "updateMany", "delete"].indexOf(method) > -1){
             // 因下面的代码，比如函数dealWithMethodPermission可能改写args变量，所以需要提前从args取出对应变量值。
             if(method === "insert"){
+                // 此处doc不带_id值，得执行完adapterMethod.apply后，doc中才有_id属性，所以这里的doc及recordId都不准确
                 doc = args[1];
                 recordId = <string>doc._id;
             }
@@ -799,6 +800,11 @@ export class SteedosObjectType extends SteedosObjectProperties {
             }
             await runObjectWorkflowRules(this.name, method, returnValue, userSession, afterTriggerContext.previousDoc);
             if(returnValue){
+                if(method === "insert"){
+                    // 当为insert时，上面代码执行后的doc不带_id，只能从returnValue中取
+                    doc = returnValue;
+                    recordId = <string>doc._id;
+                }
                 // 一定要先运行公式再运行汇总，以下两个函数顺序不能反
                 await this.runRecordFormula(method, objectName, recordId, doc, userSession);
                 await this.runRecordSummaries(method, objectName, recordId, doc, previousDoc, userSession);
