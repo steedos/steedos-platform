0213ee0ad9da68b09c3a1e6d56b5edeaf3dacfb6
殷亮辉 <yinlianghui@hotoa.com>
2020-12-31 17:10:27 +0800

    Update field_updates.ts

--- packages/objectql/src/actions/field_updates.ts
+++ packages/objectql/src/actions/field_updates.ts
@@ -1,4 +1,6 @@
 import { getObject, computeFormula, SteedosError } from '../index';
+import { runQuotedByObjectFieldFormulas } from '../formula';
+import { runQuotedByObjectFieldSummaries } from '../summary';
 const _ = require('underscore');
 
 const getFieldValue = async (action: any, recordId: string, userSession: any)=>{
@@ -13,20 +15,35 @@ const getFieldValue = async (action: any, recordId: string, userSession: any)=>{
             return;
     }
 }
+
 export async function runFieldUpdateAction(action: any, recordId: any, userSession: any){
+    console.log("runFieldUpdateAction==", action);
     const record = await getObject(action.object_name).findOne(recordId, null);
     let mainObjectName = null;
+    let recordIdToUpdate;
     if(action.target_object && action.target_object != action.object_name){
         mainObjectName = getObject(action.object_name).getField(action.target_object).reference_to;
         if(!_.isString(mainObjectName)){
             throw new SteedosError('target_object must be a string');
         }
+        recordIdToUpdate = record[action.target_object];
         const fieldValue = await getFieldValue(action, recordId, userSession);
-        await getObject(mainObjectName).directUpdate(record[action.target_object], {[action.field_name]: fieldValue});
+        await getObject(mainObjectName).directUpdate(recordIdToUpdate, {[action.field_name]: fieldValue});
     }else{
+        mainObjectName = action.object_name;
+        recordIdToUpdate = record._id;
         const fieldValue = await getFieldValue(action, recordId, userSession);
-        await getObject(action.object_name).directUpdate(record._id, {[action.field_name]: fieldValue});
+        await getObject(mainObjectName).directUpdate(recordIdToUpdate, {[action.field_name]: fieldValue});
     }
+
+    // 字段更新后，需要找到引用了该字段的公式字段并更新其值
+    await runQuotedByObjectFieldFormulas(mainObjectName, recordIdToUpdate, userSession, {
+        fieldNames: [action.field_name]
+    });
+    // 字段更新后，需要找到引用了该字段的累计汇总字段并更新其值
+    await runQuotedByObjectFieldSummaries(mainObjectName, recordIdToUpdate, null, userSession, {
+        fieldNames: [action.field_name]
+    });
 }
 
 export async function runFieldUpdateActions(ids: Array<string>, recordId: any, userSession: any){
