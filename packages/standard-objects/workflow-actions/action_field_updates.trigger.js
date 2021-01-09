b35d62f3be71865ce3fadfac7751f08351f7b6a9
殷亮辉 <yinlianghui@hotoa.com>
2021-01-09 11:17:25 +0800

    #1300 参考sf，字段更新配置的是更新父对象上的字段时不允许勾选“字段更改后重新评估工作流规则”

--- packages/standard-objects/workflow-actions/action_field_updates.trigger.js
+++ packages/standard-objects/workflow-actions/action_field_updates.trigger.js
@@ -2,14 +2,21 @@ const _ = require("underscore");
 const util = require('../util');
 
 
+const checkReevaluateParent = (doc)=>{
+    if(doc.reevaluate_on_change && doc.target_object && doc.object_name && doc.target_object != doc.object_name){
+        throw new Error('action_field_updates_field__error_reevaluate_parent');
+    }
+}
+
 module.exports = {
     beforeInsert: async function () {
         await util.checkAPIName(this.object_name, 'name', this.doc.name);
-
+        checkReevaluateParent(this.doc);
     },
     beforeUpdate: async function () {
         if (_.has(this.doc, 'name')) {
             await util.checkAPIName(this.object_name, 'name', this.doc.name, this.id);
         }
+        checkReevaluateParent(this.doc);
     }
 }
