71582610e7db26da5988de6e61cd59d180e23a8b
baozhoutao <baozhoutao@hotoa.com>
2021-01-15 14:20:26 +0800

    fix api key bug

--- packages/standard-objects/api_keys-app/main/default/objects/api_keys/functions/generator.function.js
+++ packages/standard-objects/api_keys-app/main/default/objects/api_keys/functions/generator.function.js
@@ -3,17 +3,21 @@ const objectql = require("@steedos/objectql");
 module.exports = {
     listenTo: 'api_keys',
     generator: async function(req, res){
-        const params = req.params;
-        const userSession = req.user;
-        const spaceId = userSession.spaceId;
-        const userId = userSession.userId;
+        try {
+            const params = req.params;
+            const userSession = req.user;
+            const spaceId = userSession.spaceId;
+            const userId = userSession.userId;
 
-        const record = await objectql.getObject('api_keys').insert({
-            api_key: Random.secret(),
-            space: spaceId,
-            owner: userId,
-            active: true
-        });
-        res.status(200).send(record);
+            const record = await objectql.getObject('api_keys').insert({
+                api_key: Random.secret(),
+                space: spaceId,
+                owner: userId,
+                active: true
+            });
+            res.status(200).send(record);
+        } catch (error) {
+            res.status(500).send({ status: 'error', message: error.message });
+        }
     }
   }
