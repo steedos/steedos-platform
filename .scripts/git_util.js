6af55eda79a6d5cca8f23cab1d214ce5d18fc9bb
殷亮辉 <yinlianghui@hotoa.com>
2021-01-25 18:21:05 +0800

    apps下包更新代码时的脚本也默认使用master分支

--- .scripts/git_util.js
+++ .scripts/git_util.js
@@ -1,12 +1,3 @@
 exports.getAppBranch = function(appPath){
-    switch (appPath) {
-        case 'steedos-app-admin':
-            return '1.23';
-        case 'steedos-app-contract':
-            return '1.23';
-        case 'steedos-app-crm':
-            return '1.23';
-        default:
-            return 'master';
-    }
+    return "master";
 }
