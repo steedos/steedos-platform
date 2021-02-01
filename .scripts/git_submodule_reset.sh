c195cd3498e17d28ca66a5c8b38fb509372e54b9
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-02-01 18:05:58 +0800

    update submodules

--- .scripts/git_submodule_reset.sh
+++ .scripts/git_submodule_reset.sh
@@ -3,3 +3,5 @@
 git submodule foreach --recursive git clean -xfd
 git submodule foreach --recursive git reset --hard
 git submodule update --init --recursive
+git submodule foreach --recursive git checkout master
+git submodule foreach --recursive git pull
