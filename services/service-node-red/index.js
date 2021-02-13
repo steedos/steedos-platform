2eaba4ff70944d5015233420e8b8d20d13392506
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2021-02-13 22:21:08 +0800

    node-red

---
+++ services/service-node-red/index.js
@@ -0,0 +1,115 @@
+"use strict";
+
+const express = require('express');
+const http = require('http');
+const path = require('path');
+var RED = require("node-red");
+
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+module.exports = {
+	name: "node-red",
+
+	/**
+	 * Settings
+	 */
+	settings: {
+        port: 3100,
+        credentialSecret: "3b905ca2dbb6921f3c98a21eeb0e3ef1bWs",
+        httpAdminRoot:"/admin/",
+        httpNodeRoot: "/",
+        userDir: path.join(process.cwd(), "steedos-packages", "node-red"),
+        flowFile: path.join("flows.json"),
+        functionGlobalContext: {
+        },    // enables global context
+        editorTheme: {
+            page: {
+                title: "Steedos Flow Builder",
+                // favicon: "/absolute/path/to/theme/icon",
+                // css: "/absolute/path/to/custom/css/file",
+                // scripts: [ "/absolute/path/to/custom/script/file", "/another/script/file"]
+            },
+            header: {
+                title: "Steedos Flow Builder",
+                image: "/images/logo.png", // or null to remove image
+                url: "/" // optional url to make the header text/image a link to this url
+            },
+            projects: {
+                enabled: false // Enable the projects feature
+            }
+        },
+	},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	async started() {
+
+
+        // Create an Express app
+        this.app = express();
+
+        // Add a simple route for static content served from 'public'
+        // app.use("/",express.static("public"));
+
+        // Create a server
+        this.server = http.createServer(this.app);
+
+
+        // Initialise the runtime with a server and settings
+        RED.init(this.server,this.settings);
+
+        // Serve the editor UI from /red
+        this.app.use(this.settings.httpAdminRoot, RED.httpAdmin);
+
+        // Serve the http nodes UI from /api
+        this.app.use(this.settings.httpNodeRoot, RED.httpNode);
+
+        this.server.listen(this.settings.port);
+
+        // Start the runtime
+        RED.start();
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	async stopped() {
+
+	}
+};
