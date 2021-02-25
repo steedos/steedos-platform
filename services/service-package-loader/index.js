14a6f748bcf63417aca8f1c402fe6d87202dfa80
Sunhaolin <sunhaolin@hotoa.com>
2021-02-25 18:30:38 +0800

    service-package-loader trigger.js加载

---
+++ services/service-package-loader/index.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const triggerLoader = require('./triggerLoader');
+
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+module.exports = {
+	name: "service-package-loader",
+
+	/**
+	 * Settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+		console.log('service package loader created!!!');
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	async started() {
+		let settings = this.settings;
+		triggerLoader.load(this.broker, settings.packagePath, settings.packageServiceName);
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	async stopped() {
+
+	}
+};
---
+++ services/service-package-loader/triggerLoader/index.js
@@ -0,0 +1,42 @@
+const _ = require("underscore");
+const path = require("path");
+const globby = require("globby");
+const clone = require("clone");
+const objectql = require("@steedos/objectql");
+const { getMD5 } = require("@steedos/objectql");
+module.exports = {
+  load: function(broker, packagePath, packageServiceName) {
+    console.log("packagePath : " + packagePath);
+    let actions = {};
+
+    let filePath = path.join(packagePath, "**");
+    objectql.loadObjectTriggers(filePath);
+
+    let objListeners = objectql.getLazyLoadListeners();
+    console.log(objListeners);
+    _.each(objListeners, (listeners, objectName) => {
+      _.forEach(listeners, (l) => {
+        let action = createAction(l);
+        actions[action.name] = action;
+      });
+    });
+    console.log(actions);
+    let service = {
+      name: `${packageServiceName}-triggers`,
+      actions: actions,
+    };
+    broker.createService(service);
+  },
+};
+
+function createAction(listener) {
+  let action = {};
+  let name = listener.name || objectql.getMD5(JSON.stringify(listener));
+  action.name = `$trigger.${listener.listenTo}.${name}`;
+  action.handler = function(ctx) {
+    listener.handler.call({}, ctx);
+  };
+  //
+  action.when = listener.when; // TODO check when value
+  return action;
+}
