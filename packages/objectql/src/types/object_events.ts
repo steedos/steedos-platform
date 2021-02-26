8377977d92a4fabed7736b864de8349fdcff25cd
Sunhaolin <sunhaolin@hotoa.com>
2021-02-26 15:14:55 +0800

    调整 brokeEmitEvents

--- packages/objectql/src/types/object_events.ts
+++ packages/objectql/src/types/object_events.ts
@@ -6,21 +6,30 @@ export async function brokeEmitEvents(objectName: string, method: string, contex
         let schema = getSteedosSchema();
         let when = "";
         let id;
+        let isInsert = false;
+        let isUpdate = false;
+        let isDelete = false;
         if (method == 'insert') {
-            when = 'created';
+            when = 'inserted';
             id = context.doc._id;
+            isInsert = true;
         } else if (method == 'update') {
             when = 'updated';
             id = context.id;
+            isUpdate = true;
         } else if (method == 'delete') {
             when = 'deleted';
             id = context.id;
+            isDelete = true;
         }
         let payload = {
             objectApiName: objectName,
             recordId: id,
             userId: context.userId,
-            spaceId: context.spaceId
+            spaceId: context.spaceId,
+            isInsert,
+            isUpdate,
+            isDelete
         };
         if (schema.broker) {
             await schema.broker.emit(`#${objectName}.${when}`, payload);
