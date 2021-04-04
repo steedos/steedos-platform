d4b86294526322bcdc092bc41c3b74e1cd2d3936
Sunhaolin <sunhaolin@hotoa.com>
2021-04-04 20:08:44 +0800

    删除废弃包

--- packages/metadata-service/src/handler/permission.ts
+++
@@ -1,7 +0,0 @@
-import { Permission, MetadataPermission } from '../types/permission';
-export interface IPermission{
-    add(config: Permission): boolean,
-    change(newConfig: Permission, oldConfig: Permission): boolean,
-    delete(config: Permission): boolean,
-    getPermissions(objectAPIName): Promise<Array<MetadataPermission>>
-}
--- packages/metadata-service/src/interface/metadataService.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 此规则应该属于 objectService 规范
- */
-export interface MetadataService{
-    find(),
-    findOne(),
-    aggregate()
-}
