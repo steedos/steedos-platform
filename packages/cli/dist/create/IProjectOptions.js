7d32c4ee6426c4f9807b220e4e4e4b674735bbf2
chenzhipei <18605199364@163.com>
2021-04-23 14:52:14 +0800

    #1636 兼容documentDB

---
+++ packages/cli/dist/create/IProjectOptions.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.IProjectOptions = void 0;
+class IProjectOptions {
+    constructor() {
+        this.name = '';
+    }
+}
+exports.IProjectOptions = IProjectOptions;
+//# sourceMappingURL=IProjectOptions.js.map
