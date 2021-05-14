b9a818ebec11bae4392c83ef7b73df39bbc9604e
baozhoutao <baozhoutao@hotoa.com>
2021-05-14 10:18:10 +0800

    优化对象服务创建次数 #1649

--- packages/objectql/src/metadata-register/objectServiceManager.ts
+++ packages/objectql/src/metadata-register/objectServiceManager.ts
@@ -1,6 +1,11 @@
+import * as _ from 'underscore';
 const objectService = require('../services/objectService');
 
+const LocalObjectServices = [];
 export async function createObjectService(broker, serviceName, objectConfig) {
+    if(_.include(LocalObjectServices, serviceName)){
+        return ;
+    }
     let service = broker.createService({
         name: serviceName,
         mixins: [objectService],
@@ -11,5 +16,6 @@ export async function createObjectService(broker, serviceName, objectConfig) {
     if (!broker.started) { //如果broker未启动则手动启动service
         await broker._restartService(service)
     }
+    LocalObjectServices.push(serviceName);
     return
 }
