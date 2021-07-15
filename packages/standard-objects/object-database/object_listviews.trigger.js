37e70377459c970522df66aa646445323fdd3877
逸捷 <zyj805@users.noreply.github.com>
2021-07-15 17:51:44 +0800

    #1879 处理_id

--- packages/standard-objects/object-database/object_listviews.trigger.js
+++ packages/standard-objects/object-database/object_listviews.trigger.js
@@ -1,31 +1,69 @@
 const InternalData = require('../core/internalData');
-
 module.exports = {
     afterFind: async function(){
         let filters = InternalData.parserFilters(this.query.filters)
-        if(filters.object_name){
-            let views = await InternalData.getObjectListViews(filters.object_name, this.userId);
-            if(views){
-                this.data.values = this.data.values.concat(views)
+        let views = []
+        if(filters._id && !filters._id.$ne){
+            let id = filters._id
+            id = id.replace(/\\/g, '');
+            if(_.isString(id)){
+                let objectName = id.substr(0, id.indexOf("."));
+                if(objectName){
+                    let view = await InternalData.getObjectListView(objectName, this.userId, id);
+                    if(view){
+                        views = [view];
+                    }
+                }
             }
+        }else if(filters.object_name){
+            views = await InternalData.getObjectListViews(filters.object_name, this.userId);
+        }
+        if(views){
+            this.data.values = this.data.values.concat(views)
         }
     },
     afterAggregate: async function(){
         let filters = InternalData.parserFilters(this.query.filters)
-        if(filters.object_name){
-            let views = await InternalData.getObjectListViews(filters.object_name, this.userId);
-            if(views){
-                this.data.values = this.data.values.concat(views)
+        let views = []
+        if(filters._id && !filters._id.$ne){
+            let id = filters._id
+            id = id.replace(/\\/g, '');
+            if(_.isString(id)){
+                let objectName = id.substr(0, id.indexOf("."));
+                if(objectName){
+                    let view = await InternalData.getObjectListView(objectName, this.userId, id);
+                    if(view){
+                        views = [view];
+                    }
+                }
             }
+        }else if(filters.object_name){
+            views = await InternalData.getObjectListViews(filters.object_name, this.userId);
+        }
+        if(views){
+            this.data.values = this.data.values.concat(views)
         }
     },
     afterCount: async function(){
         let filters = InternalData.parserFilters(this.query.filters)
-        if(filters.object_name){
-            let views = await InternalData.getObjectListViews(filters.object_name, this.userId);
-            if(views){
-                this.data.values = this.data.values + views.length
+        let views = []
+        if(filters._id && !filters._id.$ne){
+            let id = filters._id
+            id = id.replace(/\\/g, '');
+            if(_.isString(id)){
+                let objectName = id.substr(0, id.indexOf("."));
+                if(objectName){
+                    let view = await InternalData.getObjectListView(objectName, this.userId, id);
+                    if(view){
+                        views = [view];
+                    }
+                }
             }
+        }else if(filters.object_name){
+            views = await InternalData.getObjectListViews(filters.object_name, this.userId);
+        }
+        if(views){
+            this.data.values = this.data.values + views.length
         }
     },
     afterFindOne: async function(){
