8ee5e5ee4561354caf0b81ae28549e39f203856c
baozhoutao <baozhoutao@hotoa.com>
2021-07-27 17:40:50 +0800

    修改按钮描述 #1858

--- services/service-package-registry/main/default/objects/steedos_packages/buttons/reload.button.js
+++ services/service-package-registry/main/default/objects/steedos_packages/buttons/reload.button.js
@@ -1,6 +1,6 @@
 module.exports = {
     reload: function (object_name, record_id) {
-        toastr.info('重载中，请稍后...', null, {timeOut: false});
+        toastr.info('重新加载中，请稍后...', null, {timeOut: false});
         const record = Creator.odata.get(object_name,record_id);
         Steedos.authRequest(Steedos.absoluteUrl('/api/nodes/reload'), {type: 'post', async: false, data: JSON.stringify({
             module: record.name
@@ -10,7 +10,7 @@ module.exports = {
                 SteedosUI.reloadRecord(object_name, record_id)
             }
             toastr.clear();
-            toastr.success('已重载');
+            toastr.success('已重新加载');
             FlowRouter.reload()
         }, 1000 * 5)
         
