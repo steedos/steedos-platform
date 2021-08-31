5a5d9ea5960e81bce612f220b8432a4c81b88d6f
Byran <sunhaolin@hotoa.com>
2021-08-31 11:15:02 +0800

    添加下载模板按钮 对象数据导入功能梳理 #2052

--- packages/data-import/src/importRecords.router.ts
+++ packages/data-import/src/importRecords.router.ts
@@ -5,6 +5,11 @@ const express = require("express");
 const router = express.Router();
 const Fiber = require('fibers');
 
+const json2xls = require('json2xls');
+import { getObject } from '@steedos/objectql';
+import _ from 'lodash';
+
+
 // declare var Creator: any;
 // declare var Meteor: any;
 declare var Steedos: any;
@@ -29,7 +34,7 @@ const initiateImport = async function (req, res) {
         } catch (error) {
             return res.status(500).send({ status: 'failed', message: error.message });
         }
-        
+
     } catch (error) {
         return res.status(500).send({ status: 'failed', error: error.message });
     }
@@ -39,6 +44,31 @@ router.post('/api/data/initiateImport', requireAuthentication, function (req, re
     return Fiber(function () {
         return initiateImport(req, res);
     }).run();;
+});
+
+router.get('/api/data/download/template/:record_id', requireAuthentication, async function (req, res) { //requireAuthentication
+    try {
+        const { record_id } = req.params;
+        let queueImportDoc = await getObject("queue_import").findOne(record_id);
+        let fieldMaps = queueImportDoc.field_mappings;
+        if (_.isEmpty(fieldMaps)) {
+            return;
+        }
+        let json = {};
+        for (const fMap of fieldMaps) {
+            json[fMap.header] = null;
+        }
+        let xls = json2xls([json]);
+        res.writeHead(200, {
+            'Content-Type': 'application/octet-stream',
+            'Content-Disposition': 'attachment;filename=' + encodeURI(queueImportDoc.description + '.xlsx'),
+        });
+        res.end(xls, 'binary');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: error.message });
+    }
+
 });
 
 exports.default = router;
