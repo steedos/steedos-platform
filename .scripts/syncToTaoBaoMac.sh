bcc8cfdba6b3e67101ecfd5d26823dd2d03dae79
Byran <sunhaolin@hotoa.com>
2021-09-16 14:07:50 +0800

    淘宝源同步npm包

--- .scripts/syncToTaoBaoMac.sh
+++ .scripts/syncToTaoBaoMac.sh
@@ -1,49 +1,54 @@
- open https://npm.taobao.org/sync/@steedos/app-admin
- open https://npm.taobao.org/sync/@steedos/app-contracts
- open https://npm.taobao.org/sync/@steedos/app-crm
- open https://npm.taobao.org/sync/@steedos/app-oa
- open https://npm.taobao.org/sync/@steedos/app-okr-management
- open https://npm.taobao.org/sync/@steedos/app-project-management
- open https://npm.taobao.org/sync/@steedos/accounts
- open https://npm.taobao.org/sync/@steedos/auth
- open https://npm.taobao.org/sync/@steedos/client
- open https://npm.taobao.org/sync/@steedos/core
- open https://npm.taobao.org/sync/create-steedos-app
- open https://npm.taobao.org/sync/@steedos/design-system-react
- open https://npm.taobao.org/sync/@steedos/filters
- open https://npm.taobao.org/sync/@steedos/formula
- open https://npm.taobao.org/sync/@steedos/i18n
- open https://npm.taobao.org/sync/@steedos/meteor-bundle-dependencies
- open https://npm.taobao.org/sync/@steedos/meteor-bundle-runner
- open https://npm.taobao.org/sync/@steedos/migrate
- open https://npm.taobao.org/sync/@steedos/objectql
- open https://npm.taobao.org/sync/@steedos/plugin-enterprise
- open https://npm.taobao.org/sync/@steedos/process
- open https://npm.taobao.org/sync/@steedos/project-template-empty
- open https://npm.taobao.org/sync/@steedos/react
- open https://npm.taobao.org/sync/@steedos/schemas
- open https://npm.taobao.org/sync/@steedos/standard-objects
- open https://npm.taobao.org/sync/@steedos/plugin-dingtalk
- open https://npm.taobao.org/sync/@steedos/plugin-qywx
- open https://npm.taobao.org/sync/@steedos/steedos-plugin-schema-builder
- open https://npm.taobao.org/sync/@steedos/tailwind
- open https://npm.taobao.org/sync/@steedos/webapp
- open https://npm.taobao.org/sync/@steedos/workflow
- open https://npm.taobao.org/sync/steedos-server
- open https://npm.taobao.org/sync/@steedos/service-api
- open https://npm.taobao.org/sync/@steedos/service-metadata-apps
- open https://npm.taobao.org/sync/@steedos/service-metadata-objects
- open https://npm.taobao.org/sync/@steedos/service-metadata-permissionsets
- open https://npm.taobao.org/sync/@steedos/service-metadata-server
- open https://npm.taobao.org/sync/@steedos/service-metadata-translations
- open https://npm.taobao.org/sync/@steedos/service-metadata-triggers
- open https://npm.taobao.org/sync/@steedos/service-metadata
- open https://npm.taobao.org/sync/@steedos/service-meteor-package-loader
- open https://npm.taobao.org/sync/@steedos/service-mongodb-server
- open https://npm.taobao.org/sync/@steedos/service-node-red
- open https://npm.taobao.org/sync/@steedos/service-package-loader
- open https://npm.taobao.org/sync/@steedos/service-steedos-server
- open https://npm.taobao.org/sync/@steedos/service-charts
- open https://npm.taobao.org/sync/@steedos/service-package-registry
- open https://npm.taobao.org/sync/@steedos/service-packages
- open https://npm.taobao.org/sync/@steedos/service-pages
+open https://npm.taobao.org/sync/@steedos/app-admin
+open https://npm.taobao.org/sync/@steedos/app-contracts
+open https://npm.taobao.org/sync/@steedos/app-crm
+open https://npm.taobao.org/sync/@steedos/app-oa
+open https://npm.taobao.org/sync/@steedos/app-okr-management
+open https://npm.taobao.org/sync/@steedos/app-project-management
+open https://npm.taobao.org/sync/@steedos/accounts
+open https://npm.taobao.org/sync/@steedos/auth
+open https://npm.taobao.org/sync/@steedos/client
+open https://npm.taobao.org/sync/@steedos/core
+open https://npm.taobao.org/sync/create-steedos-app
+open https://npm.taobao.org/sync/@steedos/design-system-react
+open https://npm.taobao.org/sync/@steedos/filters
+open https://npm.taobao.org/sync/@steedos/formula
+open https://npm.taobao.org/sync/@steedos/i18n
+open https://npm.taobao.org/sync/@steedos/meteor-bundle-dependencies
+open https://npm.taobao.org/sync/@steedos/meteor-bundle-runner
+open https://npm.taobao.org/sync/@steedos/migrate
+open https://npm.taobao.org/sync/@steedos/objectql
+open https://npm.taobao.org/sync/@steedos/plugin-enterprise
+open https://npm.taobao.org/sync/@steedos/process
+open https://npm.taobao.org/sync/@steedos/project-template-empty
+open https://npm.taobao.org/sync/@steedos/react
+open https://npm.taobao.org/sync/@steedos/schemas
+open https://npm.taobao.org/sync/@steedos/standard-objects
+open https://npm.taobao.org/sync/@steedos/plugin-dingtalk
+open https://npm.taobao.org/sync/@steedos/plugin-qywx
+open https://npm.taobao.org/sync/@steedos/steedos-plugin-schema-builder
+open https://npm.taobao.org/sync/@steedos/tailwind
+open https://npm.taobao.org/sync/@steedos/webapp
+open https://npm.taobao.org/sync/@steedos/workflow
+open https://npm.taobao.org/sync/steedos-server
+open https://npm.taobao.org/sync/@steedos/service-api
+open https://npm.taobao.org/sync/@steedos/service-metadata-apps
+open https://npm.taobao.org/sync/@steedos/service-metadata-objects
+open https://npm.taobao.org/sync/@steedos/service-metadata-permissionsets
+open https://npm.taobao.org/sync/@steedos/service-metadata-server
+open https://npm.taobao.org/sync/@steedos/service-metadata-translations
+open https://npm.taobao.org/sync/@steedos/service-metadata-triggers
+open https://npm.taobao.org/sync/@steedos/service-metadata
+open https://npm.taobao.org/sync/@steedos/service-meteor-package-loader
+open https://npm.taobao.org/sync/@steedos/service-mongodb-server
+open https://npm.taobao.org/sync/@steedos/service-node-red
+open https://npm.taobao.org/sync/@steedos/service-package-loader
+open https://npm.taobao.org/sync/@steedos/service-steedos-server
+open https://npm.taobao.org/sync/@steedos/service-charts
+open https://npm.taobao.org/sync/@steedos/service-package-registry
+open https://npm.taobao.org/sync/@steedos/service-packages
+open https://npm.taobao.org/sync/@steedos/service-pages
+open https://npm.taobao.org/sync/@steedos/service-metadata-tabs
+open https://npm.taobao.org/sync/@steedos/service-fields-indexs
+open https://npm.taobao.org/sync/@steedos/service-cloud-init
+open https://npm.taobao.org/sync/@steedos/metadata-api
+open https://npm.taobao.org/sync/@steedos/service-accounts
