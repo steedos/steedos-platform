cd12535cd4339d7b83d8345926bcd6b7fe399764
Byran <sunhaolin@hotoa.com>
2021-09-17 17:19:57 +0800

    设置-许可证-列表 添加 同步许可证按钮
    https://github.com/steedos/steedos-project-k8s/issues/113

--- services/service-cloud-init/package.service.js
+++ services/service-cloud-init/package.service.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const bcrypt = require('bcryptjs');
 const createHash = require('crypto').createHash;
 const steedosLicense = require("@steedos/license");
+const objectql = require('@steedos/objectql');
 // const randomstring = require("randomstring");
 const project = require('./package.json');
 const packageName = project.name;
@@ -37,27 +38,49 @@ module.exports = {
 		},
 		/**
 		 * 调用主控接口获取许可证
-		 * @param {*} spaceName 
 		 * @param {*} spaceId 
 		 * @param {*} apiKey 
 		 * @param {*} consoleUrl 
 		 */
-		getLicense: async function (spaceId, apiKey, consoleUrl) {
+		getLicenses: async function (spaceId, apiKey, consoleUrl) {
 			let result = await axios({
 				url: `${consoleUrl}/api/saas/space/init/license/${spaceId}`,
 				method: 'get',
 				data: {},
 				headers: { "Content-Type": "application/json", "Authorization": `Bearer apikey,${apiKey}` }
 			});
-			return result.data.license;
-		}
+			if (!result.data.success) {
+				throw new Error(result.data.error);
+			}
+			return result.data.licenses;
+		},
+		/**
+		 * 获取工作区初始化信息
+		 * @param {*} spaceId 
+		 * @param {*} apiKey 
+		 * @param {*} consoleUrl 
+		 * @returns 
+		 */
+		getInitInfo: async function (spaceId, apiKey, consoleUrl) {
+			let result = await axios({
+				url: `${consoleUrl}/api/saas/trial/application/space/init/info`,
+				method: 'post',
+				data: {
+					cloudSpaceId: spaceId
+				},
+				headers: { "Content-Type": "application/json", "Authorization": `Bearer apikey,${apiKey}` }
+			})
+			if (!result.data.success) {
+				throw new Error(result.data.error);
+			}
+			return result.data.info;
+		},
 
 	},
 	events: {
 		'steedos-server.started': async function (ctx) {
 			// console.log(chalk.blue('steedos-server.started'));
 			const settings = this.settings;
-			const objectql = require('@steedos/objectql');
 			const spaceObj = objectql.getObject('spaces');
 			// 查询库中工作区记录，如果有工作区记录则不初始化
 			let spacesCount = await spaceObj.count({});
@@ -85,21 +108,7 @@ module.exports = {
 			}
 
 			// 调用接口获取初始化信息
-			let infoResult = await axios({
-				url: `${consoleUrl}/api/saas/trial/application/space/init/info`,
-				method: 'post',
-				data: {
-					cloudSpaceId: spaceId
-				},
-				headers: { "Content-Type": "application/json", "Authorization": `Bearer apikey,${apiKey}` }
-			})
-			// console.log(chalk.blue(JSON.stringify(infoResult.data)));
-			if (!infoResult.data.success) {
-				console.log(chalk.red('获取初始化信息失败'));
-				console.log(chalk.red(infoResult.data.error));
-				return;
-			}
-			const { spaceName, adminName, adminPhone, adminPassword } = infoResult.data.info;
+			const { spaceName, adminName, adminPhone, adminPassword } = await this.getInitInfo(spaceId, apiKey, consoleUrl);
 
 			if (!spaceName || !adminName || !adminPhone) {
 				console.log(chalk.red('缺少初始化工作区信息 工作区名称、管理员姓名、管理员手机号，请检查'));
@@ -164,14 +173,7 @@ module.exports = {
 				await apiKeysObj.insert(newApiKeyDoc);
 
 				// 给工作区添加许可证，调用导入许可证接口
-				const license = await this.getLicense(spaceId, apiKey, consoleUrl);
-				// console.log(license);
-				const licenseInfo = license.split(',');
-				let license_decrypt = steedosLicense.verifyLicenseFile(licenseInfo[0], licenseInfo[1], spaceId);
-				if (license_decrypt.verify_error) {
-					throw new Error(license_decrypt.verify_error);
-				}
-				await steedosLicense.save({ license: licenseInfo[0], is_local: license_decrypt.is_local, key: licenseInfo[1], verify_status: license_decrypt.verify_status, verify_error: license_decrypt.verify_erro, license_last_verify: new Date(), _id: license_decrypt._id, product: license_decrypt.product }, spaceId);
+				await this.actions.saveLicenses({ spaceId, apiKey, consoleUrl }, { parentCtx: ctx });
 
 				try {
 					await ctx.broker.call('~packages-project-server.installPurchasedPackages', {}, {
@@ -214,6 +216,85 @@ module.exports = {
 				}
 			}
 
+		}
+	},
+	actions: {
+		manualSyncSpaceLicense: {
+			rest: {
+				method: "POST",
+				path: "/manualSyncSpaceLicense"
+			},
+			params: {},
+			handler: async function (ctx) {
+				try {
+					console.log(chalk.blue('manualSyncSpaceLicense...'));
+					const settings = this.settings;
+					// 配置主控地址
+					const consoleUrl = settings.STEEDOS_CLOUD_URL;
+					if (!consoleUrl) {
+						throw new Error('请配置主控地址环境变量STEEDOS_CLOUD_URL');
+					}
+					// 获取环境变量中工作区信息
+					const spaceId = settings.STEEDOS_CLOUD_SPACE_ID;
+					const apiKey = settings.STEEDOS_CLOUD_API_KEY;
+					if (!spaceId || !apiKey) {
+						throw new Error('缺少环境变量STEEDOS_CLOUD_SPACE_ID或STEEDOS_CLOUD_API_KEY，请检查');
+					}
+					const userSession = ctx.meta.user;
+					const userId = userSession.userId;
+					// 给工作区添加许可证，调用导入许可证接口
+					await this.actions.saveLicenses({ spaceId, apiKey, consoleUrl }, { parentCtx: ctx });
+					// 同步工作区信息（公司名称、华炎云中api_key字段、api_key不存在则创建）
+					const { spaceName, adminName, adminPhone, adminPassword } = await this.getInitInfo(spaceId, apiKey, consoleUrl);
+					const spaceObj = objectql.getObject('spaces');
+					const apiKeysObj = objectql.getObject('api_keys');
+					await spaceObj.directUpdate(spaceId, { name: spaceName, api_key: apiKey });
+					const apiKeyCount = await apiKeysObj.count({ filters: [ ['api_key', '=', apiKey] ] });
+					if (apiKeyCount == 0) {
+						const now = new Date();
+						const baseInfo = {
+							owner: userId,
+							created: now,
+							modified: now,
+							created_by: userId,
+							modified_by: userId
+						};
+						const newApiKeyDoc = {
+							_id: apiKey,
+							name: 'admin api key',
+							api_key: apiKey,
+							active: true,
+							space: spaceId,
+							...baseInfo
+						};
+						await apiKeysObj.insert(newApiKeyDoc);
+					}
+					return { success: true };
+				} catch (error) {
+					return { success: false, error: error.message };
+				}
+			}
+		},
+
+		saveLicenses: {
+			parms: {
+				spaceId: { type: 'string', optional: false },
+				apiKey: { type: 'string', optional: false },
+				consoleUrl: { type: 'string', optional: false }
+			},
+			handler: async function (ctx) {
+				const { spaceId, apiKey, consoleUrl } = ctx.params;
+				// 给工作区添加许可证，调用导入许可证接口
+				const licenses = await this.getLicenses(spaceId, apiKey, consoleUrl);
+				for (const license of licenses) {
+					const licenseInfo = license.split(',');
+					let license_decrypt = steedosLicense.verifyLicenseFile(licenseInfo[0], licenseInfo[1], spaceId);
+					if (license_decrypt.verify_error) {
+						throw new Error(license_decrypt.verify_error);
+					}
+					await steedosLicense.save({ license: licenseInfo[0], is_local: license_decrypt.is_local, key: licenseInfo[1], verify_status: license_decrypt.verify_status, verify_error: license_decrypt.verify_erro, license_last_verify: new Date(), _id: license_decrypt._id, product: license_decrypt.product }, spaceId);
+				}
+			}
 		}
 	}
 }
