6f5b3f03c8fc97f1e4048056afcff5d99335e49f
baozhoutao <baozhoutao@hotoa.com>
2021-10-12 14:58:37 +0800

    调整脚本 #2312

--- .scripts/.git_push_upgrade_template.js
+++ .scripts/.git_push_upgrade_template.js
@@ -3,13 +3,13 @@ const path = require('path');
 const _ = require('underscore');
 const fs = require('fs');
 
-let appsPath = path.join(process.cwd(), 'apps')
+let appsPath = path.join(process.cwd(), 'packages', 'create-steedos-app', 'templates')
 let apps = fs.readdirSync(appsPath);
 let versionInfo = require('../lerna.json');
 _.each(apps, function(item){
     let fPath = path.join(appsPath, item);
     let stat = fs.statSync(fPath);
     if(stat.isDirectory() === true) {
-        execSync(`cd ${fPath} && git add . && git commit -m upgrade template @${versionInfo.version} && git push`);
+        execSync(`cd ${fPath} && git add . && git commit -m "upgrade template @${versionInfo.version}" && git push`);
     }
 })
