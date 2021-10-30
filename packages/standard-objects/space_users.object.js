4c846765ab5f99dca26c90c9c0103ea3d408a7da
yinlianghui <yinlianghui@hotoa.com>
2021-10-30 23:02:58 +0800

    #2395 修改密码功能，当是组织管理员时，应该跟工作区管理员一样直接修改密码而不是输入原密码

--- packages/standard-objects/space_users.object.js
+++ packages/standard-objects/space_users.object.js
@@ -834,6 +834,25 @@ Meteor.startup(function () {
     }
 });
 
+let isCompanyAdmin = function(object_name, record_id, organization){
+    var userId = Steedos.userId();
+    if(organization){
+        //当前选中组织所属分部的管理员才有权限
+        if(organization.company_id && organization.company_id.admins){
+            return organization.company_id.admins.indexOf(userId) > -1;
+        }
+    }
+    else{
+        // 用户详细界面拿不到当前选中组织时，只能从记录本身所属分部的管理员中判断，只要当前用户是任何一个所属分部的管理员则有权限
+        var record = Creator.getObjectRecord(object_name, record_id);
+        if(record && record.company_ids && record.company_ids.length){
+            return _.any(record.company_ids,function(item){
+                return item.admins && item.admins.indexOf(userId) > -1
+            });
+        }
+    }
+}
+
 let actions = {
     import: {
         label: "导入",
@@ -912,27 +931,17 @@ let actions = {
                 return true;
             }
             else{
-                var userId = Steedos.userId();
-                if(organization){
-                    //当前选中组织所属分部的管理员才有权限
-                    if(organization.company_id && organization.company_id.admins){
-                        return organization.company_id.admins.indexOf(userId) > -1;
-                    }
-                }
-                else{
-                    // 用户详细界面拿不到当前选中组织时，只能从记录本身所属分部的管理员中判断，只要当前用户是任何一个所属分部的管理员则有权限
-                    var record = Creator.getObjectRecord(object_name, record_id);
-                    if(record && record.company_ids && record.company_ids.length){
-                        return _.any(record.company_ids,function(item){
-                            return item.admins && item.admins.indexOf(userId) > -1
-                        });
-                    }
-                }
+                return isCompanyAdmin(object_name, record_id, organization);
             }
         },
         todo: function (object_name, record_id, fields) {
+            var organization = Session.get("organization");
+            var isAdmin = Creator.isSpaceAdmin();
+            if(!isAdmin){
+                isAdmin = isCompanyAdmin(object_name, record_id, organization);
+            }
 
-            if(!Creator.isSpaceAdmin()){
+            if(!isAdmin){
                 // Modal.show("reset_password_modal");
                 Steedos.openWindow(Steedos.absoluteUrl("/accounts/a/#/update-password"))
                 return;
@@ -1022,22 +1031,7 @@ let actions = {
                 return true;
             }
             else{
-                var userId = Steedos.userId();
-                if(organization){
-                    //当前选中组织所属分部的管理员才有权限
-                    if(organization.company_id && organization.company_id.admins){
-                        return organization.company_id.admins.indexOf(userId) > -1;
-                    }
-                }
-                else{
-                    // 用户详细界面拿不到当前选中组织时，只能从记录本身所属分部的管理员中判断，只要当前用户是任何一个所属分部的管理员则有权限
-                    var record = Creator.getObjectRecord(object_name, record_id);
-                    if(record && record.company_ids && record.company_ids.length){
-                        return _.any(record.company_ids,function(item){
-                            return item.admins && item.admins.indexOf(userId) > -1
-                        });
-                    }
-                }
+                return isCompanyAdmin(object_name, record_id, organization);
             }
         }
     },
