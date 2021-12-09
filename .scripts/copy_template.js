f3d2e43787b08fe245c09413ad6329dad64bf4d9
baozhoutao <baozhoutao@hotoa.com>
2021-12-09 20:01:09 +0800

    .npmrc .yarnrc 不需要提交

--- .scripts/copy_template.js
+++ .scripts/copy_template.js
@@ -5,6 +5,8 @@ cpy([
     '**',
     '.steedos/**',
     '!.steedos/node_modules/**',
+    '!.steedos/.npmrc',
+    '!.steedos/.yarnrc',
     '.vscode/**',
     '!**/node_modules/**',
     '!logs/**',
@@ -15,26 +17,26 @@ cpy([
     '!init_home.sh',
     '.env',
 ], '../../packages/create-steedos-app/templates/default', {
-    parents: true, 
+    parents: true,
     flat: false,
     cwd: path.join(process.cwd(), 'steedos-projects', 'project-template'),
     rename: (name) => {
         switch (name) {
-          case '.env':
-            return 'env'
-          case '.gitignore': {
-            return 'gitignore'
-          }
-          case 'package.json': {
-            return '_package.json'
-          }
-          default: {
-            return name
-          }
+            case '.env':
+                return 'env'
+            case '.gitignore': {
+                return 'gitignore'
+            }
+            case 'package.json': {
+                return '_package.json'
+            }
+            default: {
+                return name
+            }
         }
-      }
-}).then(()=>{
+    }
+}).then(() => {
     console.log(`copy default template end: packages/create-steedos-app/templates/default`);
-}).catch((result)=>{
+}).catch((result) => {
     console.log(`error`, result);
 })
