1f54151b73290d0cc8cdc59f33b984d9a49e5485
Byran <sunhaolin@hotoa.com>
2022-02-09 06:21:19 +0000

    流程增加按钮：‘更新审批单’，将 审批单字段（grid）、审批单子表（grid） 最新内容同步至表单 #2587

--- packages/standard-objects/workflow/flows.action.js
+++ packages/standard-objects/workflow/flows.action.js
@@ -11,7 +11,7 @@ module.exports = {
             initialValues: {
                 "object_name": objectName,
                 "instance_fields": record.instance_fields,
-                "instance_table_fields":  record.instance_table_fields
+                "instance_table_fields": record.instance_table_fields
             },
             objectSchema: {
                 fields: {
@@ -104,9 +104,9 @@ module.exports = {
                                         return [];
                                     }
                                     const objectName = _.isObject(values.object_name) ? values.object_name.name : values.object_name;
-        
+
                                     const options = [];
-        
+
                                     const { detailsInfo } = Steedos.authRequest('/am/forms/getDetailsInfo', {
                                         type: 'post',
                                         async: false,
@@ -114,7 +114,7 @@ module.exports = {
                                             objectName
                                         })
                                     });
-        
+
                                     _.each(detailsInfo, function (related) {
                                         let foo = related.split('.');
                                         let rObjectName = foo[0];
@@ -176,6 +176,11 @@ module.exports = {
                         toastr.success('添加成功');
                         Modal.hide();
                         $(document.body).removeClass('loading');
+                        if (Template.creator_view.currentInstance) {
+                            Template.creator_view.currentInstance.onEditSuccess();
+                        } else {
+                            FlowRouter.reload();
+                        }
                     },
                     error: function (XMLHttpRequest, textStatus, errorThrown) {
                         console.error(XMLHttpRequest.responseJSON);
@@ -193,190 +198,43 @@ module.exports = {
         return record && record.object_name;
     },
 
-    // 添加字段
-    addFormFieldFromObject: function (object_name, record_id, fields) {
-        var record = Creator.getObjectRecord(object_name, record_id, 'name,form,object_name');
+    // 更新字段
+    updateFormFields: function (object_name, record_id, fields) {
+        const record = Creator.getObjectRecord();
         var formId = _.isObject(record.form) ? record.form._id : record.form;
-        let objectName = _.isObject(record.object_name) ? record.object_name.name : record.object_name;
-        let objectInfo = Creator.getObject(objectName);
-        let objectFields = objectInfo.fields;
-        const rows = [];
-        for (const key in objectFields) {
-            if (Object.hasOwnProperty.call(objectFields, key)) {
-                const field = objectFields[key];
-                if (!['grid', 'object'].includes(field.type) && !(field.name.indexOf('.') > -1 || field.name.indexOf('$') > -1)) {
-                    rows.push(field);
-                }
-            }
-        }
-        const props = {
-            title: `选择 对象字段`,
-            objectApiName: "object_fields",
-            listSchema: {
-                columns: ["label", "name", "type", "group", "sort_no"]
-            },
-            multiple: true,
-            rows: rows,
-            rowKey: 'name',
-            onFinish: async (selectedRowKeys, selectedRows) => {
-                console.log("selectedRowKeys:", selectedRowKeys);
-                console.log("selectedRows:", selectedRows);
-                if (selectedRowKeys.length < 1) {
-                    return;
-                }
-                $(document.body).addClass('loading');
-                let url = '/am/forms/addFieldFromObject';
-                let options = {
-                    type: 'post',
-                    async: true,
-                    data: JSON.stringify({
-                        fields: selectedRows,
-                        formId: formId
-                    }),
-                    success: function (data) {
-                        toastr.success('添加成功');
-                        Modal.hide();
-                        $(document.body).removeClass('loading');
-                    },
-                    error: function (XMLHttpRequest, textStatus, errorThrown) {
-                        console.error(XMLHttpRequest.responseJSON);
-                        toastr.error(XMLHttpRequest.responseJSON.error.replace(/:/g, '：'))
-                        $(document.body).removeClass('loading');
-                    }
-                };
-                Steedos.authRequest(url, options);
-                return true;
-            },
-            sort: [{ field_name: "sort_no", order: "asc" }]
-        };
-        SteedosUI.showModal(window.stores.ComponentRegistry.components.ObjectTable, props)
-    },
-    addFormFieldFromObjectVisible: function (object_name, record_id, record_permissions, record) {
-        // var record = Creator.getObjectRecord(object_name, record_id, 'object_name');
-        // if (record && record.object_name) {
-        //     return true;
-        // }
-        return false;
-    },
-
-    // 添加子表
-    addFormTableFromObject: function (object_name, record_id, fields) {
-        var record = Creator.getObjectRecord(object_name, record_id, 'name,form,object_name');
-        var formId = _.isObject(record.form) ? record.form._id : record.form;
-        let objectName = _.isObject(record.object_name) ? record.object_name.name : record.object_name;
-
-        var _showFeildsModal = function (selectedObj, rows) {
-            SteedosUI.showModal(window.stores.ComponentRegistry.components.ObjectTable, {
-                title: `选择 ${selectedObj.label}的字段`,
-                objectApiName: "object_fields",
-                listSchema: {
-                    columns: ["label", "name", "type", "group", "sort_no"]
-                },
-                multiple: true,
-                rows: rows,
-                rowKey: 'name',
-                onFinish: async (selectedRowKeys, selectedRows) => {
-                    console.log("selectedRowKeys:", selectedRowKeys);
-                    console.log("selectedRows:", selectedRows);
-                    if (selectedRowKeys.length < 1) {
-                        return;
-                    }
-                    $(document.body).addClass('loading');
-                    let url = '/am/forms/addTableFromObject';
-                    let options = {
-                        type: 'post',
-                        async: true,
-                        data: JSON.stringify({
-                            tables: [{ 'name': selectedObj.name, 'label': selectedObj.label, 'fields': selectedRows }],
-                            formId: formId
-                        }),
-                        success: function (data) {
-                            toastr.success('添加成功');
-                            Modal.hide();
-                            $(document.body).removeClass('loading');
-                        },
-                        error: function (XMLHttpRequest, textStatus, errorThrown) {
-                            console.error(XMLHttpRequest.responseJSON);
-                            toastr.error(XMLHttpRequest.responseJSON.error.replace(/:/g, '：'))
-                            $(document.body).removeClass('loading');
-                        }
-                    };
-                    Steedos.authRequest(url, options);
-                    return true;
-                },
-                sort: [{ field_name: "sort_no", order: "asc" }]
-            })
-        }
-
-        Steedos.authRequest('/am/forms/getDetails', {
+        const objectName = _.isObject(record.object_name) ? record.object_name.name : record.object_name;
+        $(document.body).addClass('loading');
+        let url = '/am/forms/addFieldsFromObject';
+        let options = {
             type: 'post',
             async: true,
             data: JSON.stringify({
-                objectName
+                formId: formId,
+                object_name: objectName,
+                instance_fields: record.instance_fields,
+                instance_table_fields: record.instance_table_fields
             }),
             success: function (data) {
-                const { details } = data;
-                let detailsInfo = [];
-                for (const dObjName of details) {
-                    const dObj = Creator.getObject(dObjName);
-                    if (dObj) {
-                        detailsInfo.push({ name: dObj.name, label: dObj.label });
-                    }
+                toastr.success('更新成功');
+                Modal.hide();
+                $(document.body).removeClass('loading');
+                if (Template.creator_view.currentInstance) {
+                    Template.creator_view.currentInstance.onEditSuccess();
+                } else {
+                    FlowRouter.reload();
                 }
-                SteedosUI.showModal(stores.ComponentRegistry.components.ObjectTable, {
-                    title: `请选择子表`,
-                    listSchema: {
-                        columns: [
-                            { field: "label" },
-                        ],
-                    },
-                    objectSchema: {
-                        fields: {
-                            'name': { type: 'text', label: '对象名' },
-                            'label': { type: 'text', label: '对象' },
-                        }
-                    },
-                    rows: detailsInfo,
-                    rowSelection: 'single',
-                    rowKey: 'name',
-                    onFinish: async (selectedObjKeys, selectedObjs) => {
-                        if (selectedObjKeys.length < 1) {
-                            throw new Error(`请选择对象。`);
-                        }
-                        console.log('selectedObjs: ', selectedObjs);
-                        var selectedObj = selectedObjs[0];
-                        let objectInfo = Creator.getObject(selectedObj.name);
-                        let objectFields = objectInfo.fields;
-                        const rows = [];
-                        for (const key in objectFields) {
-                            if (Object.hasOwnProperty.call(objectFields, key)) {
-                                const field = objectFields[key];
-                                if (!['grid', 'object'].includes(field.type) && !(field.name.indexOf('.') > -1 || field.name.indexOf('$') > -1)) {
-                                    rows.push(field);
-                                }
-                            }
-                        }
-
-                        setTimeout(function () {
-                            _showFeildsModal(selectedObj, rows);
-                        }, 100);
-                        return true;
-                    }
-                })
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
                 console.error(XMLHttpRequest.responseJSON);
                 toastr.error(XMLHttpRequest.responseJSON.error.replace(/:/g, '：'))
+                $(document.body).removeClass('loading');
             }
-        });
-
-
+        };
+        Steedos.authRequest(url, options);
     },
-    addFormTableFromObjectVisible: function (object_name, record_id, record_permissions, record) {
-        // var record = Creator.getObjectRecord(object_name, record_id, 'object_name');
-        // if (record && record.object_name) {
-        //     return true;
-        // }
-        return false;
-    }
+    updateFormFieldsVisible: function (object_name, record_id, record_permissions, record) {
+        console.log('record: ', record);
+        return record && record.object_name;
+    },
+
 }
