99dd1a10aa19f8a9d3428fd7f05ae616a53a7090
baozhoutao <baozhoutao@hotoa.com>
2022-03-27 18:18:57 +0800

    tabs列表支持搜素
    https://github.com/steedos/steedos-frontend/issues/336

--- services/service-pages/main/default/routes/page_design.router.js
+++ services/service-pages/main/default/routes/page_design.router.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const core = require('@steedos/core');
+const fs = require('fs');
 
-router.get('/api/pageDesign', async function (req, res) {
+router.get('/api/pageDesign', core.requireAuthentication, async function (req, res) {
     try {
         res.set('Content-Type', 'text/html');
+        const userSession = req.user;
+        const steedosBuilderHost = `https://builder.steedos.com/amis?assetUrl=https://unpkg.com/@steedos-ui/builder-widgets/dist/assets.js&rootUrl=${__meteor_runtime_config__.ROOT_URL}&authToken=${userSession.authToken}&userId=${userSession.userId}&tenantId=${userSession.spaceId}&__q=`;
+
+        let data = fs.readFileSync(__dirname+'/design.html', 'utf8');
+        res.send(data.replace('SteedosBuilderHost',steedosBuilderHost));
+
+
         res.sendFile(__dirname+'/design.html') 
     } catch (error) {
         res.status(500).send({ message: error.message });
