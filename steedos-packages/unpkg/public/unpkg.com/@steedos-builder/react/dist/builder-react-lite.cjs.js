e379ebf0dce1a562d0e2cd08298d193021260e5b
baozhoutao <baozhoutao@hotoa.com>
2022-03-30 23:23:29 +0800

    升级 @steedos-builder/react

---
+++ steedos-packages/unpkg/public/unpkg.com/@steedos-builder/react/dist/builder-react-lite.cjs.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=builder-react-lite.cjs.js.map
---
+++ steedos-packages/unpkg/public/unpkg.com/@steedos-builder/react/dist/src/assets.d.ts
@@ -0,0 +1,9 @@
+declare const _default: {
+    packages: {
+        package: string;
+        library: string;
+        urls: string[];
+    }[];
+    components: never[];
+};
+export default _default;
