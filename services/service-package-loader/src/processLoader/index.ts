36a28c9298084d43a1b47343885970b7f22c6f04
Byran <sunhaolin@hotoa.com>
2022-03-30 21:45:04 +0800

    支持加载流程图元数据

--- services/service-package-loader/src/processLoader/index.ts
+++ services/service-package-loader/src/processLoader/index.ts
@@ -2,13 +2,15 @@
  * @Author: sunhaolin@hotoa.com
  * @Date: 2022-03-30 11:49:53
  * @LastEditors: sunhaolin@hotoa.com
- * @LastEditTime: 2022-03-30 14:34:23
+ * @LastEditTime: 2022-03-30 21:34:47
  * @Description: 
  */
 import * as _ from "underscore";
 import * as path from "path";
+import * as glob from 'glob';
+import * as fs from 'fs';
 import { registerProcess } from "@steedos/objectql";
-// import { Process } from "./types";
+import { Process } from "./types";
 import { LoadProcessFile } from '@steedos/metadata-core';
 const loadProcessFile = new LoadProcessFile();
 
@@ -18,36 +20,25 @@ export async function load(broker: any, packagePath: string, packageServiceName:
     if (_.isEmpty(processes)) {
         return;
     }
-    console.log(processes);
 
     const data = [];
     for (const apiName in processes) {
-        const process = processes[apiName];
+        const process: Process = processes[apiName];
+        // 加载对应的引擎元数据
+        const engineFileName = `${apiName}.process.${process.engine}.${process.ext}`;
+        const matchedPaths = glob.sync(path.join(filePath, loadProcessFile.metadataInfo.defaultDirectory, engineFileName));
+        if (matchedPaths.length > 1) {
+            throw new Error(`路径${filePath}下检测到多份元数据文件${engineFileName}，请检查。`);
+        }
+        const engineFilePath = matchedPaths[0];
+        if (engineFilePath) {
+            const engineFileContent = fs.readFileSync(engineFilePath);
+            process.schema = engineFileContent.toString();
+        }
         data.push(process);
     }
     if (data.length > 0) {
         await registerProcess.mregister(broker, packageServiceName, data)
     }
-
-    // for (const wt of wTriggers) {
-
-    //     if (_.isString(wt.listenTo)) {
-    //         for (const when of ENUM_WHEN) {
-    //             let handler = wt[when];
-    //             if (!handler) {
-    //                 continue;
-    //             }
-    //             let name = wt.name || getMD5(JSONStringify(wt));
-    //             let config: Trigger = {
-    //                 name: name,
-    //                 "listenTo": wt.listenTo == '*' ? LISTENTO_ALL_FLOWS : wt.listenTo,
-    //                 "when": when,
-    //                 "handler": handler.toString()
-    //             }
-    //             await registerProcessTrigger.register(broker, packageServiceName, config);
-    //         }
-
-    //     }
-    // }
 
 }
--- services/service-package-loader/src/processLoader/types/process.ts
+++ services/service-package-loader/src/processLoader/types/process.ts
@@ -2,10 +2,18 @@
  * @Author: sunhaolin@hotoa.com
  * @Date: 2022-03-30 11:49:53
  * @LastEditors: sunhaolin@hotoa.com
- * @LastEditTime: 2022-03-30 11:50:33
- * @Description: 
+ * @LastEditTime: 2022-03-30 21:24:32
+ * @Description: Process 元数据格式
  */
 export type Process = {
     name: string,
-    
+    label: string,
+    object_name: string,
+    engine: string,
+    is_active?: boolean,
+    description?: string,
+    entry_criteria: string,
+    when: 'afterInsert' | 'afterUpdate',
+    schema?: string,
+    ext: string
 }
