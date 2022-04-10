241a5b7e758b2329d086f268a5c50b2c945d5a39
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2022-04-10 14:39:58 +0800

    ubpkg: resolve file from node_modules

--- steedos-packages/unpkg/package.service.js
+++ steedos-packages/unpkg/package.service.js
@@ -8,6 +8,9 @@ const project = require('./package.json');
 const packageName = project.name;
 const packageLoader = require('@steedos/service-package-loader');
 const express = require('express');
+const fs = require('fs')
+const path = require('path')
+
 
 /**
  * @typedef {import('moleculer').Context} Context Moleculer's Context
@@ -25,7 +28,10 @@ module.exports = {
 			path: __dirname,
 			name: packageName
 		},
-		unpkgUrl: process.env.STEEDOS_PUBLIC_PAGE_UNPKGURL?process.env.STEEDOS_PUBLIC_PAGE_UNPKGURL: 'https://unpkg.com'
+		unpkgUrl: process.env.STEEDOS_UNPKG_URL? process.env.STEEDOS_UNPKG_URL : 'node_modules',
+		white_list: [],
+		black_list: [],
+		resolved_modules: [],
 	},
 
 	/**
@@ -52,13 +58,58 @@ module.exports = {
 	 */
 	methods: {
 
-		loadUnpkgRedirectRoutes: function() {
+		parsePackagePathname: function(pathname) {
+
+			const packagePathnameFormat = /^\/((?:@[^/@]+\/)?[^/@]+)(?:@([^/]+))?(\/.*)?$/;
+		
+			try {
+				pathname = decodeURIComponent(pathname);
+			} catch (error) {
+				return null;
+			}
+		
+			const match = packagePathnameFormat.exec(pathname);
+		
+			// Disallow invalid pathnames.
+			if (match == null) return null;
+		
+			const packageName = match[1];
+			const packageVersion = match[2] || 'latest';
+			const filename = (match[3] || '').replace(/\/\/+/g, '/');
+		
+			return {
+				// If the pathname is /@scope/name@version/file.js:
+				packageName, // @scope/name
+				packageVersion, // version
+				packageSpec: `${packageName}@${packageVersion}`, // @scope/name@version
+				filename // /file.js
+			};
+		},
+		
+		loadUnpkgRoutes: function() {
 			try {
 				const router = express.Router();
 				const unpkgUrl = this.settings.unpkgUrl
+				const resolved_modules = this.settings.resolved_modules;
 				router.get('/unpkg.com/*', (req, res) => {
 					const packageUrl = req.path.split('/unpkg.com')[1]
-					res.redirect(unpkgUrl + packageUrl);
+					if (unpkgUrl !== 'node_modules') {
+						res.redirect(unpkgUrl + packageUrl);
+						return
+					}
+					const parsed = this.parsePackagePathname(packageUrl)
+					try {
+						if (parsed && resolved_modules.indexOf(parsed.packageName)<0) {
+							const packageDir =  path.dirname(require.resolve(parsed.packageName + '/package.json')).replace('/package.json', '')
+							if (fs.existsSync(packageDir)) {
+								const cacheTime = 86400000 * 1; // one day
+								router.use(`/unpkg.com/${parsed.packageName}`, express.static(packageDir, { maxAge: cacheTime }));
+								resolved_modules.push(parsed.packageName)
+							}
+						}
+					}catch(error){
+						console.log(`Package not found: ${parsed.packageSpec}, you should add to you project.`)
+					}
 				});
 				WebApp.connectHandlers.use(router);
 			} catch (error) {
@@ -78,7 +129,7 @@ module.exports = {
 	 * Service started lifecycle event handler
 	 */
 	async started() {
-		this.loadUnpkgRedirectRoutes();
+		this.loadUnpkgRoutes();
 	},
 
 	/**
