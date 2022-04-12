0b7c7e151a65884c42a7e386aa4478044b32784f
Byran <sunhaolin@hotoa.com>
2022-04-12 16:47:51 +0800

    page_assignments和page_versions元数据同步
    https://github.com/steedos/steedos-plugin-cmbchina/issues/9

--- packages/metadata-api/src/metadata/collection/page.ts
+++ packages/metadata-api/src/metadata/collection/page.ts
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@hotoa.com
  * @Date: 2021-08-30 12:06:41
  * @LastEditors: sunhaolin@hotoa.com
- * @LastEditTime: 2022-04-11 15:34:46
+ * @LastEditTime: 2022-04-12 16:39:03
  * @Description: 
  */
 import { SteedosMetadataTypeInfoKeys as TypeInfoKeys } from '@steedos/metadata-core';
@@ -14,70 +14,74 @@ const metadataName = TypeInfoKeys.Page;
 const WIGETS_TABLE_NAME = "widgets";
 const ASSIGNMENTS_TABLE_NAME = "page_assignments";
 const PAGE_VERSION_TABLE_NAME = "page_versions";
-export class PageCollection extends MetadataBaseCollection{
-    constructor(){
+export class PageCollection extends MetadataBaseCollection {
+    constructor() {
         super(metadataName, [WIGETS_TABLE_NAME]);
     }
 
-    async find(dbManager){
+    async find(dbManager) {
         const pages = await dbManager.find(this.collectionName, {});
         for (const page of pages) {
             page.widgets = await this.getPageWidgets(dbManager, page.name);
             // 获取page_assignments和page_versions最新的一条记录的schema
-            page.pageAssignments = await this.getPageAssignments(dbManager, page._id);
+            page.pageAssignments = await this.getPageAssignments(dbManager, page._id, page.name);
             page.schema = await this.getLatestPageVersionSchema(dbManager, page._id);
         }
         return pages;
     }
 
-    async findOne(dbManager, metadataApiName){
-        const page = await dbManager.findOne(this.collectionName, {name: metadataApiName});
+    async findOne(dbManager, metadataApiName) {
+        const page = await dbManager.findOne(this.collectionName, { name: metadataApiName });
         page[WIGETS_TABLE_NAME] = await this.getPageWidgets(dbManager, page.name);
         // 获取page_assignments和page_versions最新的一条记录的schema
-        page.pageAssignments = await this.getPageAssignments(dbManager, page._id);
+        page.pageAssignments = await this.getPageAssignments(dbManager, page._id, page.name);
         page.schema = await this.getLatestPageVersionSchema(dbManager, page._id);
         return page;
     }
 
-    async save(dbManager, data){
-        var filter = {name: data.name};
+    async save(dbManager, data) {
+        var filter = { name: data.name };
         var record = await dbManager.findOne(this.collectionName, filter);
         const pageWidgets = data[WIGETS_TABLE_NAME];
         await this.savePageWidgets(dbManager, data.name, pageWidgets)
-        if(record == null){
+        // 保存pageAssignments
+        await this.savePageAssignments(dbManager, record._id, data.pageAssignments);
+        // 保存page_versions
+        await this.savePageVersions(dbManager, record._id, data.schema);
+        if (record == null) {
             return await dbManager.insert(this.collectionName, data);
-        }else{
+        } else {
             return await dbManager.update(this.collectionName, filter, data);
         }
     }
 
-    private async savePageWidgets(dbManager, pageApiName, pageWidgets){
-        if(pageWidgets && _.isArray(pageWidgets)){
+    private async savePageWidgets(dbManager, pageApiName, pageWidgets) {
+        if (pageWidgets && _.isArray(pageWidgets)) {
             for await (let widget of pageWidgets) {
-                if(!widget.name){
+                if (!widget.name) {
                     throw new Error('not find widget.name');
                 }
                 const widgetRecord = await this.getPageWidget(dbManager, pageApiName, widget.name);
                 widget.page = pageApiName;
-                if(widgetRecord){
-                    await dbManager.update(WIGETS_TABLE_NAME, {_id: widgetRecord._id}, widget);
-                }else{
+                if (widgetRecord) {
+                    await dbManager.update(WIGETS_TABLE_NAME, { _id: widgetRecord._id }, widget);
+                } else {
                     await dbManager.insert(WIGETS_TABLE_NAME, widget);
                 }
             }
         }
     }
 
-    private async getPageWidget(dbManager, pageApiName, widgetApiName){
-        const widgets = await dbManager.find(WIGETS_TABLE_NAME, {page: pageApiName, name: widgetApiName});
-        if(widgets && widgets.length > 0){
+    private async getPageWidget(dbManager, pageApiName, widgetApiName) {
+        const widgets = await dbManager.find(WIGETS_TABLE_NAME, { page: pageApiName, name: widgetApiName });
+        if (widgets && widgets.length > 0) {
             return widgets[0]
         }
     }
 
-    private async getPageWidgets(dbManager, pageApiName){
-        const widgets = await dbManager.find(WIGETS_TABLE_NAME, {page: pageApiName});
-        _.map(widgets, (widget)=>{
+    private async getPageWidgets(dbManager, pageApiName) {
+        const widgets = await dbManager.find(WIGETS_TABLE_NAME, { page: pageApiName });
+        _.map(widgets, (widget) => {
             delete widget.page;
             delete widget.id;
         })
@@ -85,22 +89,61 @@ export class PageCollection extends MetadataBaseCollection{
     }
 
     // 获取page_assignments返回必要属性
-    private async getPageAssignments(dbManager, pageId){
-        const pageAssignments = await dbManager.find(ASSIGNMENTS_TABLE_NAME, {page: pageId});
-        if(pageAssignments && pageAssignments.length > 0){
+    private async getPageAssignments(dbManager, pageId, pageName) {
+        const pageAssignments = await dbManager.find(ASSIGNMENTS_TABLE_NAME, { page: pageId });
+        if (pageAssignments && pageAssignments.length > 0) {
             for (const assignment of pageAssignments) {
                 deleteCommonAttribute(assignment);
+                delete assignment._id;
+                assignment.page = pageName;
             }
         }
         return pageAssignments;
     }
 
     // 获取page_versions最新版的schema
-    private async getLatestPageVersionSchema(dbManager, pageId){
-        const pageVersions = await dbManager.find(PAGE_VERSION_TABLE_NAME, {page: pageId}, {sort: {version: -1}});
-        if(pageVersions && pageVersions.length > 0){
+    private async getLatestPageVersionSchema(dbManager, pageId) {
+        const pageVersions = await dbManager.find(PAGE_VERSION_TABLE_NAME, { page: pageId }, { sort: { version: -1 } });
+        if (pageVersions && pageVersions.length > 0) {
             return pageVersions[0].schema;
         }
         return '';
+    }
+
+    // 保存page_assignments，清除旧的新增新的。
+    private async savePageAssignments(dbManager, pageId, pageAssignments) {
+        await dbManager.deleteMany(ASSIGNMENTS_TABLE_NAME, {page: pageId});
+        if (pageAssignments && _.isArray(pageAssignments)) {
+            for (const assignment of pageAssignments) {
+                assignment.page = pageId;
+                await dbManager.insert(ASSIGNMENTS_TABLE_NAME, assignment);
+            }
+        }
+    }
+
+    // 保存page_versions，如果最新版未发布则更新，否则新增一条
+    private async savePageVersions(dbManager, pageId, pageVersionSchema) {
+        const latestVersion = await this.getLatestPageVersion(dbManager, pageId);
+        if (latestVersion && !latestVersion.is_active) {
+            await dbManager.update(PAGE_VERSION_TABLE_NAME, { _id: latestVersion._id }, { schema: pageVersionSchema });
+        } else {
+            const pageVersionDoc = {
+                description: '',
+                is_active: false,
+                page: pageId,
+                schema: pageVersionSchema
+            }
+            await dbManager.insert(PAGE_VERSION_TABLE_NAME, pageVersionDoc);
+        }
+    }
+
+    // 获取page_versions最新版
+    private async getLatestPageVersion(dbManager, pageId) {
+        const pageVersions = await dbManager.find(PAGE_VERSION_TABLE_NAME, { page: pageId });
+        if (pageVersions) {
+            const sortedVersions = _.sortBy(pageVersions, 'version');
+            return _.last(sortedVersions);
+        }
+        return;
     }
 }
