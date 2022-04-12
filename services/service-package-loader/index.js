24abb9fb80ab35057b4f1f5ef735a81f9e7254ab
baozhoutao <baozhoutao@hotoa.com>
2022-04-12 21:20:00 +0800

    Update index.js

--- services/service-package-loader/index.js
+++ services/service-package-loader/index.js
@@ -141,7 +141,7 @@ module.exports = {
 					}
 					const cacheTime = 86400000 * 1; // one day
 					router.use(routerPath, express.static(publicPath, { maxAge: cacheTime }));
-					WebApp.rawConnectHandlers.use(router);
+					WebApp.connectHandlers.use(router);
 				} catch (error) {
 					console.error(error)
 					this.settings.loadedPackagePublicFiles = false;
