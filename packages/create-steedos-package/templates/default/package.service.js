0d82e2a1958dfbbce9eb241541f2b1a3b858b534
baozhoutao <baozhoutao@hotoa.com>
2022-04-16 13:58:45 +0800

    fix bug: create-steedos-package 内容缺失

--- packages/create-steedos-package/templates/default/package.service.js
+++ packages/create-steedos-package/templates/default/package.service.js
@@ -1,5 +1,69 @@
-/*
- * @Author: steedos
- * @Date: 2022-03-28 15:04:10
- * @Description: 
+"use strict";
+const project = require('./package.json');
+const packageName = project.name;
+const packageLoader = require('@steedos/service-package-loader');
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ * 软件包服务启动后也需要抛出事件。
  */
+module.exports = {
+	name: packageName,
+	namespace: "steedos",
+	mixins: [packageLoader],
+	/**
+	 * Settings
+	 */
+	settings: {
+		packageInfo: {
+			path: __dirname,
+			name: packageName
+		}
+	},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	async created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	async started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	async stopped() {
+
+	}
+};
