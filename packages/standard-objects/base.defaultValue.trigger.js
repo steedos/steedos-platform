b952c2331b9a1a069b3cc822c0e2ab8babd7bb3c
yinlianghui <yinlianghui@hotoa.com>
2022-04-28 12:47:20 +0800

    #3166 fix 默认值spaceId不生效

--- packages/standard-objects/base.defaultValue.trigger.js
+++ packages/standard-objects/base.defaultValue.trigger.js
@@ -4,18 +4,25 @@ const objectql = require('@steedos/objectql');
 /**
   把原来的默认值公式表达式转换成新的表达式语法，原来的表达式语法如下所示：
   ==========
-  取一个字段的值：使用“{”和“}” (注意都是半角)将字段名扩起来，如：{user.name}，注意不支持表单字段值，比如{price}
-  基于当前用户的系统变量：包括姓名、角色、部门等
+  - 取一个字段的值：使用“{”和“}” (注意都是半角)将字段名扩起来，如：{user.name}，注意不支持表单字段值，比如{price}
+  - 基于当前用户的系统变量：包括姓名、角色、部门等
   ID: {userId}
   姓名：{user.name}
   所在部门（当申请人属于多个部门时，为所在主部门的全路径）： {user.organization.fullname}
   所在部门（最底层部门名）： {user.organization.name}
-  角色名： {user.roles}
+  角色名： {user.roles} 不支持，user.roles写法是userSession的，公式引擎中user指向的space_users而不是userSession
   手机： {user.mobile}
   固定电话： {user.work_phone}
   职务： {user.position}
-  取当前工作区的信息
+  - 取当前工作区的信息
   ID: {spaceId}
+  - 取当前时间
+  {now}
+  - 双层大括号语法，只兼容基本语法，复杂的直接忽略默认值
+  {{global.userId}}
+  {{global.spaceId}}
+  {{global.user.xxx}}
+  {{global.now}}
   ===========
  * @param express 
  */
@@ -100,8 +107,9 @@ const setDefaultValues = async function (doc, fields, userId, spaceId) {
         else if (valueType === "string") {
             if (defaultValue.length) {
                 // 运行公式引擎
+                // console.log("==setDefaultValues=defaultValue===2222=", defaultValue);
                 defaultValue = getCompatibleDefaultValueExpression(defaultValue);
-                // console.log("==setDefaultValues=defaultValue====", defaultValue);
+                // console.log("==setDefaultValues=defaultValue===333=", defaultValue);
                 if(defaultValue){
                     doc[field.name] = await objectql.computeSimpleFormula(defaultValue, doc, userId, spaceId);
                     // console.log("==setDefaultValues=doc[field.name]====", field.name, doc[field.name]);
@@ -135,6 +143,5 @@ module.exports = {
             await setDefaultValues(doc, fields, userId, spaceId);
             // console.log("===before.insert=last==doc==", doc);
         }
-        // console.log("===before.insert=last==docs==", docs);
     }
 }
