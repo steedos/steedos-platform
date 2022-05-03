110ab6dd96e4542fd2584a03529044fe1ad89578
yinlianghui <yinlianghui@hotoa.com>
2022-05-03 15:04:13 +0800

    登录界面，禁用鼠标右键菜单，禁用ctrlKey/metaKey+c/v/a，复制、剪切、粘贴、全选，其中metaKey是mac系统中相关操作的辅助键
    https://github.com/steedos/steedos-frontend/issues/358

--- steedos-packages/webapp-accounts/webapp/src/components/InputPassword.tsx
+++ steedos-packages/webapp-accounts/webapp/src/components/InputPassword.tsx
@@ -34,6 +34,20 @@ const InputPassword = React.forwardRef((props: any, ref?: React.Ref<HTMLInputEle
 
     }
 
+    const handlePasswordKeyDown = (e: any) => {
+        // 禁用ctrlKey/metaKey+c/v/a，复制、剪切、粘贴、全选，其中metaKey是mac系统中相关操作的辅助键
+        if ((e.ctrlKey || e.metaKey) && [67, 88, 86, 65].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+            return false;
+        }
+    }
+
+    const handlePasswordContextMenu = (e: any) => {
+        // 禁用鼠标右键菜单
+        e.preventDefault();
+        return false;
+    }
+
     useEffect(() => {
       const timer = clearPasswordValueAttribute();
       return () => {
@@ -49,6 +63,8 @@ const InputPassword = React.forwardRef((props: any, ref?: React.Ref<HTMLInputEle
             ref={inputRef}
             onFocus={handlePasswordFocus}
             onBlur={handlePasswordBlur}
+            onKeyDown={handlePasswordKeyDown}
+            onContextMenu={handlePasswordContextMenu}
         />
     );
 });
