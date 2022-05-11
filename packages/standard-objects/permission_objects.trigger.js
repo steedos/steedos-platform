ce7964e0906c111cd673823f80942e6e99ed7c75
baozhoutao <baozhoutao@hotoa.com>
2022-05-11 16:17:32 +0800

    清理无用代码

--- packages/standard-objects/permission_objects.trigger.js
+++ packages/standard-objects/permission_objects.trigger.js
@@ -1,6 +1,5 @@
 const _ = require('underscore');
 const objectql = require("@steedos/objectql");
-const odataMongodb = require("odata-v4-mongodb");
 const InternalData = require("./core/internalData");
 const auth = require("@steedos/auth");
 
@@ -36,83 +35,6 @@ const getInternalPermissionObjects = async function(){
 }
 
 
-const getPermissionByFilters = async function(filtersFun){
-    let allPer = await getInternalPermissionObjects();
-    let pers = []
-    _.each(allPer, function(doc){
-        if(filtersFun(doc)){
-            pers.push(doc)
-        }
-    })
-    return pers;
-}
-
-const getPermissionById = async function(id){
-    let allPer = await getInternalPermissionObjects();
-    return _.find(allPer, (doc)=>{
-        return doc._id === id
-    })
-}
-
-function parserFilters(filters){
-    let query = {};
-    if(_.isArray(filters)){
-        _.each(filters,function(filter){
-            Object.assign(query, parserFilters(filter))
-        })
-    }else{
-        _.each(filters, function (v, k) {
-            if (k === '$and') {
-                Object.assign(query, parserFilters(v))
-            } else {
-                Object.assign(query, {[k]: v})
-            }
-        })
-    }
-    return query;
-}
-
-const getSourcePermissionSetsKeys = async function(type){
-    switch (type) {
-        case 'permission_set':
-            return await objectql.getSourcePermissionsetKeys();
-        case 'profile':
-            return await objectql.getSourceProfilesKeys();
-        default:
-            return (await objectql.getSourceProfilesKeys()).concat((await objectql.getSourcePermissionsetKeys()))
-    }
-}
-
-const find = async function(query){
-    let filters = parserFilters(odataMongodb.createFilter(query.filters));
-    let permissionSetId = filters.permission_set_id;
-    let sourcePermissionSetsKeys = await getSourcePermissionSetsKeys();
-    if(permissionSetId && !_.include(sourcePermissionSetsKeys, permissionSetId)){
-        var dbPerms = await objectql.getObject("permission_set").directFind({filters: ['_id', '=', permissionSetId], fields: ['_id', 'name']});
-        var dbPerm = null;
-        if(dbPerms && dbPerms.length > 0){
-            dbPerm = dbPerms[0];
-        }
-        if(dbPerm && _.include(sourcePermissionSetsKeys, dbPerm.name)){
-            permissionSetId = dbPerm.name
-        }
-    }
-    let objectName = filters.object_name;
-    let permissionObjects = await getPermissionByFilters((doc)=>{
-        if(permissionSetId && objectName){
-            return permissionSetId === doc.permission_set_id && objectName === doc.object_name
-        }
-        if(permissionSetId){
-            return permissionSetId === doc.permission_set_id
-        }
-        if(objectName){
-            return objectName === doc.object_name
-        }
-        return true;
-    });
-    return permissionObjects;
-}
-
 const initPermissionSet = async function(doc, userId, spaceId){
     const userSession = await auth.getSessionByUserId(userId, spaceId);
     if(!doc.permission_set_id){
@@ -146,133 +68,65 @@ const initPermissionSet = async function(doc, userId, spaceId){
 } 
 
 module.exports = {
-    afterFind: async function () {
-        let filters = parserFilters(odataMongodb.createFilter(this.query.filters));
-        let isSystem = filters.is_system;
-        if(!_.isEmpty(isSystem) || _.isBoolean(isSystem)){
-            if(_.isBoolean(isSystem) && isSystem){
-                this.data.values = await find(this.query);
-            }else{
-                return ;
-            }
-            if(isSystem.$ne){
-                return 
-            }
-        }else{
-            let permissionObjects = await find(this.query);
-            if(_.isArray(this.data.values)){
-                let that = this;
-                const _ids = [];
-                for (const record of that.data.values) {
-                    let permission_set_id = record.permission_set_id;
-                    if(record && record._id && !_.has(record, 'permission_set_id')){
-                        const perObj = await objectql.getObject('permission_objects').findOne(record._id);
-                        if(perObj){
-                            permission_set_id = perObj.permission_set_id
-                        }
-                    }
-                    const perSet = await objectql.getObject('permission_set').findOne(permission_set_id);
-                    if(perSet){
-                        const _record = await objectql.getObject('permission_objects').findOne(record._id, {fields: ['object_name']});
-                        _ids.push(`${_record.object_name}.${perSet.name}`)
-                    }
-                }
-                _.each(permissionObjects, function(_po){
-                    if(!_.include(_ids, `${_po.object_name}.${_po.permission_set_id}`)){
-                        that.data.values.push(_po);
-                    }
-                })
-            }
-        }
-        
+    beforeFind: async function () {
+        delete this.query.fields;
     },
-    afterAggregate: async function () {
-        let filters = parserFilters(odataMongodb.createFilter(this.query.filters));
-        let isSystem = filters.is_system;
-        if(!_.isEmpty(isSystem) || _.isBoolean(isSystem)){
-            if(_.isBoolean(isSystem) && isSystem){
-                this.data.values = await find(this.query);
-            }else{
-                return ;
-            }
-            if(isSystem.$ne){
-                return 
-            }
-        }else{
-            let permissionObjects = await find(this.query);
-            if(_.isArray(this.data.values)){
-                let that = this;
-                const _ids = [];
-                for (const record of that.data.values) {
-                    let permission_set_id = record.permission_set_id;
-                    if(record && record._id && !_.has(record, 'permission_set_id')){
-                        const perObj = await objectql.getObject('permission_objects').findOne(record._id);
-                        if(perObj){
-                            permission_set_id = perObj.permission_set_id
-                        }
-                    }
-                    const perSet = await objectql.getObject('permission_set').findOne(permission_set_id);
-                    if(perSet){
-                        const _record = await objectql.getObject('permission_objects').findOne(record._id, {fields: ['object_name']});
-                        _ids.push(`${_record.object_name}.${perSet.name}`)
-                    }
+
+    beforeAggregate: async function () {
+        delete this.query.fields;
+    },
+
+    afterFind: async function(){
+        const { spaceId } = this;
+        let dataList = await getInternalPermissionObjects();
+        if (!_.isEmpty(dataList)) {
+            dataList.forEach((doc) => {
+                if (!_.find(this.data.values, (value) => {
+                    return value.name === doc.name
+                })) {
+                    this.data.values.push(doc);
                 }
-                _.each(permissionObjects, function(_po){
-                    if(!_.include(_ids, `${_po.object_name}.${_po.permission_set_id}`)){
-                        that.data.values.push(_po);
-                    }
-                })
-            }
-        }
-        /*  TODO: 把push的数据去掉或者为push的数据增加过滤条件就可以删除以下再过滤的代码。
-            permission_objects.trigger.js 和 permission_set.trigger.js 同理。
-            对象权限 字段拿数据库中保存的值再过滤一遍。
-            这里写死了，根据字段配置了 reference_to_field: name 。
-        */
-        const allData = this.data.values;
-        const firstFilterKey = _.keys(filters)[0];
-        if(firstFilterKey === 'name'){
-            this.data.values = _.filter(allData, (item)=>{
-                return item[firstFilterKey] === filters[firstFilterKey];
             })
+            const records = objectql.getSteedosSchema().metadataDriver.find(this.data.values, this.query, spaceId);
+            if (records.length > 0) {
+                this.data.values = records;
+            } else {
+                this.data.values.length = 0;
+            }
         }
-        
+
     },
-    afterCount: async function () {
-        delete this.query.fields
-        let result = await objectql.getObject('permission_objects').find(this.query, await auth.getSessionByUserId(this.userId, this.spaceId))
-        this.data.values = result.length
-        // let filters = parserFilters(odataMongodb.createFilter(this.query.filters));
-        // let isSystem = filters.is_system;
-        // if(!_.isEmpty(isSystem) || _.isBoolean(isSystem)){
-        //     if(_.isBoolean(isSystem) && isSystem){
-        //         this.data.values = find(this.query).length;
-        //     }else{
-        //         return ;
-        //     }
-        //     if(isSystem.$ne){
-        //         return 
-        //     }
-        // }else{
-        //     let permissionObjects = find(this.query);
-        //     let filters = parserFilters(odataMongodb.createFilter(this.query.filters));
-        //     let permissionSetId = filters.permission_set_id;
-        //     let query = {};
-        //     let dbPOsCounts = 0;
-        //     if(permissionSetId){
-        //         query = {permission_set_id: permissionSetId}
-        //         dbPOsCounts = Creator.getCollection("permission_objects").direct.find(query).count();
-        //     }
-        //     if(permissionObjects.length > 0){
-        //         this.data.values = this.data.values + permissionObjects.length - dbPOsCounts
-        //     }
-        // }
-        
+    afterAggregate: async function(){
+        const { spaceId } = this;
+        let dataList = await getInternalPermissionObjects();
+        if (!_.isEmpty(dataList)) {
+            dataList.forEach((doc) => {
+                if (!_.find(this.data.values, (value) => {
+                    return value.name === doc.name
+                })) {
+                    this.data.values.push(doc);
+                }
+            })
+            const records = objectql.getSteedosSchema().metadataDriver.find(this.data.values, this.query, spaceId);
+            if (records.length > 0) {
+                this.data.values = records;
+            } else {
+                this.data.values.length = 0;
+            }
+        }
     },
-    afterFindOne: async function () {
-        let id = this.id;
-        if(id && _.isEmpty(this.data.values)){
-            Object.assign(this.data.values, await getPermissionById(id))
+    afterCount: async function(){
+        delete this.query.fields;
+        let result = await objectql.getObject(this.object_name).find(this.query, await auth.getSessionByUserId(this.userId, this.spaceId))
+        this.data.values = result.length;
+    },
+    afterFindOne: async function(){
+        if(_.isEmpty(this.data.values)){
+            const all = await getInternalPermissionObjects();
+            const id = this.id;
+            this.data.values = _.find(all, function(item){
+                return item._id === id
+            });
         }
     },
     beforeInsert: async function(){
