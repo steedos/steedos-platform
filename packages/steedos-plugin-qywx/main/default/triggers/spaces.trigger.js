6ae9d21847b66af8f6d78713001e3a54b41ee9ac
Byran <sunhaolin@hotoa.com>
2022-05-28 09:45:12 +0800

    如果验证文件不存在则创建   #2348

--- packages/steedos-plugin-qywx/main/default/triggers/spaces.trigger.js
+++ packages/steedos-plugin-qywx/main/default/triggers/spaces.trigger.js
@@ -2,7 +2,7 @@
  * @Author: sunhaolin@hotoa.com
  * @Date: 2022-05-27 10:56:55
  * @LastEditors: sunhaolin@hotoa.com
- * @LastEditTime: 2022-05-27 13:56:49
+ * @LastEditTime: 2022-05-28 09:42:36
  * @Description: 
  */
 
@@ -25,20 +25,24 @@ module.exports = {
             }
             const fileName = fileDoc.original.name;
             // 如果域名验证文件为txt则将文件下载后copy至项目根目录的public文件夹下
-            Fiber(function () {
-                // console.log('process.cwd(): ', process.cwd());
-                const file = cfs.files.findOne(qywx_domain_verify_file);
-                const readable = file.createReadStream('files');
-                const publicDir = path.join(process.cwd(), 'public');
-                // 如果public文件夹不存在则新建
-                if (!fs.existsSync(publicDir)) {
-                    fs.mkdirSync(publicDir);
-                }
-                // 创建文件
-                const writable = fs.createWriteStream(path.join(publicDir, fileName));
-                // 传输内容
-                readable.pipe(writable);
-            }).run();
+            // console.log('process.cwd(): ', process.cwd());
+            const publicDir = path.join(process.cwd(), 'public');
+            // 如果public文件夹不存在则新建
+            if (!fs.existsSync(publicDir)) {
+                fs.mkdirSync(publicDir);
+            }
+            // 如果验证文件不存在则创建
+            const filePath = path.join(publicDir, fileName);
+            if (!fs.existsSync(filePath)) {
+                Fiber(function () {
+                    const file = cfs.files.findOne(qywx_domain_verify_file);
+                    const readable = file.createReadStream('files');
+                    // 创建文件
+                    const writable = fs.createWriteStream(filePath);
+                    // 传输内容
+                    readable.pipe(writable);
+                }).run();
+            }
 
         }
 
