9c25eab6bdf44a91bdf151b5196971de1b90fb01
Byran <sunhaolin@hotoa.com>
2022-06-23 18:14:36 +0800

    记录审计日志，分拆为单独的软件包。对象开启 审计日志功能时校验并提示
    https://github.com/steedos/steedos-platform/issues/3466

---
+++ ee/audit-records/package.service.js
@@ -0,0 +1,81 @@
+/*
+ * @Author: sunhaolin@hotoa.com
+ * @Date: 2022-05-03 10:29:51
+ * @LastEditors: sunhaolin@hotoa.com
+ * @LastEditTime: 2022-06-23 15:38:35
+ * @Description: 
+ */
+"use strict";
+const project = require('./package.json');
+const packageName = project.name;
+const packageLoader = require('@steedos/service-package-loader');
+
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+module.exports = {
+    name: packageName,
+    namespace: "steedos",
+    mixins: [packageLoader],
+    /**
+     * Settings
+     */
+    settings: {
+        packageInfo: {
+            path: __dirname,
+            name: packageName
+        },
+    },
+
+    /**
+     * Dependencies
+     */
+    dependencies: ['~packages-standard-objects'],
+
+    /**
+     * Actions
+     */
+    actions: {
+
+    },
+
+    /**
+     * Events
+     */
+    events: {
+
+    },
+
+    /**
+     * Methods
+     */
+    methods: {
+
+    },
+
+    merged(schema) {
+
+    },
+
+    /**
+     * Service created lifecycle event handler
+     */
+    async created() {
+
+    },
+
+    /**
+     * Service started lifecycle event handler
+     */
+    async started() {
+
+
+    },
+
+    /**
+     * Service stopped lifecycle event handler
+     */
+    async stopped() {
+
+    }
+};
