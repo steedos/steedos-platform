612109f565b18d9eae8c45b4b7fe2327a0de522d
庄建国 <9340100@qq.com>
2022-07-01 11:20:22 +0000

    清理脚本

--- .scripts/git_reset.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-git clean -xfd
-git submodule foreach --recursive git clean -xfd
-git reset --hard
-git submodule foreach --recursive git reset --hard
-git submodule update --init --recursive
