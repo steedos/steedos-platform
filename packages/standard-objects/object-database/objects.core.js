884f441d0c385be3b23f5ed44eb6e5799fa05b7e
baozhoutao <baozhoutao@hotoa.com>
2022-07-05 11:28:24 +0800

    还原reInitDatasource参数

--- packages/standard-objects/object-database/objects.core.js
+++ packages/standard-objects/object-database/objects.core.js
@@ -250,7 +250,7 @@ function loadObject(doc, oldDoc) {
     }
 }
 
-function removeObject(doc, enforce) {
+function removeObject(doc, enforce, reInitDatasource) {
     if (!enforce && !objectql.hasObjectSuffix(doc.name, doc.space) && !doc.datasource) {
         console.warn('warn: Not deleted. Invalid custom object -> ', doc.name);
         return;
@@ -260,7 +260,9 @@ function removeObject(doc, enforce) {
     objectql.removeObjectConfig(doc.name, datasourceName);
     if (datasource) {
         datasource.removeObject(doc.name);
-        datasource.init();
+        if(reInitDatasource){
+            datasource.init();
+        }
     }
     if (!datasourceName || datasourceName == defaultDatasourceName) {
         Creator.removeObject(doc.name);
