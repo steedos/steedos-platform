76e066e61a2ad022513dfecc1e0ccbe7ad10e671
baozhoutao <baozhoutao@hotoa.com>
2022-07-06 18:38:43 +0800

    fix: sso 注册space users bug

--- ee/sso-oidc/src/context/index.ts
+++ ee/sso-oidc/src/context/index.ts
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-06-26 16:31:46
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-06-28 11:58:21
+ * @LastEditTime: 2022-07-06 17:43:45
  * @Description: 
  */
 import { getSteedosConfig } from '@steedos/objectql';
@@ -35,6 +35,6 @@ export const getOidcConfig = () => {
         configUrl: config.sso?.oidc?.config_url || process.env.SSO_OIDC_CONFIG_URL,
         clientID: config.sso?.oidc?.client_id || process.env.SSO_OIDC_CLIENT_ID,
         clientSecret: config.sso?.oidc?.client_secret || process.env.SSO_OIDC_CLIENT_SECRET,
-        requireLocalAccount: config.sso?.oidc?.require_local_account || validator.toBoolean(process.env.SSO_OIDC_REQUIRE_LOCAL_ACCOUNT || 'false', true) || false,
+        requireLocalAccount: validator.toBoolean(config.sso?.oidc?.require_local_account || 'false', true) || validator.toBoolean(process.env.SSO_OIDC_REQUIRE_LOCAL_ACCOUNT || 'false', true) || false,
     }
 }
