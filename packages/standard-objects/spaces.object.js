af162a5570892f6468aaae56a10dab0215172ff7
baozhoutao <baozhoutao@hotoa.com>
2022-07-14 15:23:54 +0800

    删除创建工作区时自动给db中写入data.json 数据功能

--- packages/standard-objects/spaces.object.js
+++ packages/standard-objects/spaces.object.js
@@ -484,36 +484,36 @@ if (Meteor.isServer) {
 }
 
 //仅考虑mongo-db数据源的对象数据
-function initSpaceData(spaceId, userId){
-    let spacesCollection = Creator.getCollection("spaces")
-    let datas = objectql.getAllObjectData();
-    let now = new Date();
-    let insertMap = {};
-    try {
-        for(let objectName in datas){
-            let records = datas[objectName];
-            if(_.indexOf(["spaces"], objectName) < 0){
-                for(let record of records){
-                    if(Creator.getCollection(objectName)){
-                        let docId = Creator.getCollection(objectName).direct.insert(Object.assign({}, record, {_id: record._id || spacesCollection._makeNewID(), space: spaceId, owner: userId, created: now, modified: now, created_by: userId, modified_by: userId}));
-                        if(insertMap[objectName]){
-                            insertMap[objectName].push(docId)
-                        }else{
-                            insertMap[objectName] = []
-                            insertMap[objectName].push(docId)
-                        }
-                        // await objectql.getObject(objectName).directInsert(Object.assign({}, record, {_id: spacesCollection._makeNewID(), space: spaceId}));
-                    }
-                }
-            }
-        }
-    } catch (error) {
-        _.each(insertMap, function(_ids,objectName){
-            Creator.getCollection(objectName).direct.remove({_id: {$in: _ids}})
-        })
-        throw new Error("初始化数据失败");
-    }
-}
+// function initSpaceData(spaceId, userId){
+//     let spacesCollection = Creator.getCollection("spaces")
+//     let datas = objectql.getAllObjectData();
+//     let now = new Date();
+//     let insertMap = {};
+//     try {
+//         for(let objectName in datas){
+//             let records = datas[objectName];
+//             if(_.indexOf(["spaces"], objectName) < 0){
+//                 for(let record of records){
+//                     if(Creator.getCollection(objectName)){
+//                         let docId = Creator.getCollection(objectName).direct.insert(Object.assign({}, record, {_id: record._id || spacesCollection._makeNewID(), space: spaceId, owner: userId, created: now, modified: now, created_by: userId, modified_by: userId}));
+//                         if(insertMap[objectName]){
+//                             insertMap[objectName].push(docId)
+//                         }else{
+//                             insertMap[objectName] = []
+//                             insertMap[objectName].push(docId)
+//                         }
+//                         // await objectql.getObject(objectName).directInsert(Object.assign({}, record, {_id: spacesCollection._makeNewID(), space: spaceId}));
+//                     }
+//                 }
+//             }
+//         }
+//     } catch (error) {
+//         _.each(insertMap, function(_ids,objectName){
+//             Creator.getCollection(objectName).direct.remove({_id: {$in: _ids}})
+//         })
+//         throw new Error("初始化数据失败");
+//     }
+// }
 
 Creator.Objects['spaces'].methods = {
     "tenant": function (req, res) {
@@ -556,9 +556,9 @@ Creator.Objects['spaces'].methods = {
                         name: spaceName, 
                         owner: userId
                     }
-                    if(!tenant.saas){
-                        initSpaceData(spaceId, userId);
-                    }
+                    // if(!tenant.saas){
+                    //     initSpaceData(spaceId, userId);
+                    // }
                     let newSpaceId = db.spaces.insert(spaceDoc);
                     let newSpace = db.spaces.findOne(newSpaceId);
                     return res.send(newSpace);
