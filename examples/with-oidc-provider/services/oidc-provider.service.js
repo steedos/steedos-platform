bd43c7322f6115c5f0706463cdaf62dbbf445cbd
庄建国 <9340100@qq.com>
2022-07-15 16:44:12 +0000

    oidc provider

--- examples/with-oidc-provider/services/oidc-provider.service.js
+++ examples/with-oidc-provider/services/oidc-provider.service.js
@@ -12,9 +12,9 @@ module.exports = {
 	settings: {
 		// ... see the available options in Configuration options section
 		clients: [{
-		  client_id: 'foo',
-		  client_secret: 'bar',
-		  redirect_uris: ['http://lvh.me:8080/cb'],
+		  client_id: 'steedos',
+		  client_secret: 'steedos',
+		  redirect_uris: ['https://www.steedos.cn'],
 		  // + other client properties
 		}],
 		// ...
@@ -43,7 +43,28 @@ module.exports = {
 	 * Methods
 	 */
 	methods: {
-
+		async findAccount(ctx, sub, token) {
+			// @param ctx - koa request context
+			// @param sub {string} - account identifier (subject)
+			// @param token - is a reference to the token used for which a given account is being loaded,
+			//   is undefined in scenarios where claims are returned from authorization endpoint
+			return {
+			  accountId: sub,
+			  // @param use {string} - can either be "id_token" or "userinfo", depending on
+			  //   where the specific claims are intended to be put in
+			  // @param scope {string} - the intended scope, while oidc-provider will mask
+			  //   claims depending on the scope automatically you might want to skip
+			  //   loading some claims from external resources or through db projection etc. based on this
+			  //   detail or not return them in ID Tokens but only UserInfo and so on
+			  // @param claims {object} - the part of the claims authorization parameter for either
+			  //   "id_token" or "userinfo" (depends on the "use" param)
+			  // @param rejected {Array[String]} - claim names that were rejected by the end-user, you might
+			  //   want to skip loading some claims from external resources or through db projection
+			  async claims(use, scope, claims, rejected) {
+				return { sub };
+			  },
+			};
+		}
 	},
 
 	/**
@@ -57,12 +78,15 @@ module.exports = {
 	 */
 	async started() {
 
-		this.oidc = new Provider('http://localhost:3000', this.settings);
+		this.oidc = new Provider(process.env.ROOT_URL, {
+			clients: this.settings.clients,
+			findAccount: this.findAccount
+		});
 		
 		// express/nodejs style application callback (req, res, next) for use with express apps, see /examples/express.js
 		WebApp.connectHandlers.use('/oidc', this.oidc.callback());
 		
-		this.logger.warn('oidc started, check /oidc/.well-known/openid-configuration');
+		this.logger.warn(`service started, check ${process.env.ROOT_URL}/oidc/.well-known/openid-configuration`);
 	},
 
 	/**
