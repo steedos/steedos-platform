eabb794ca473d68ce7be7fb31dde97d2eae8c9c5
庄建国 <9340100@qq.com>
2022-07-15 15:14:07 +0000

    with-nextjs-nextauth

---
+++ examples/with-nextjs-nextauth/pages/_app.js
@@ -0,0 +1,11 @@
+import { SessionProvider } from "next-auth/react"
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
