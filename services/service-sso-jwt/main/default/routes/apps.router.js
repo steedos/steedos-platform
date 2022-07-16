e02005bc34e1b5965b7b7c8eeb5feaf0c8a6faf5
baozhoutao <baozhoutao@hotoa.com>
2022-07-16 18:46:30 +0800

    fix bug

--- services/service-sso-jwt/main/default/routes/apps.router.js
+++ services/service-sso-jwt/main/default/routes/apps.router.js
@@ -5,14 +5,14 @@ const tslib_1 = require("tslib");
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-06-08 23:28:39
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-07-16 15:11:01
+ * @LastEditTime: 2022-07-16 16:18:31
  * @Description:
  */
 const express = require("express");
 const account_1 = require("./account");
 const passport = require('passport');
 const router = express.Router();
-router.use('/identity/jwt', passport.authenticate('jwt', { session: false }), (req, res) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
+router.use('/accounts/jwt/login', passport.authenticate('jwt', { session: false }), (req, res) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     account_1.Account.ssoLogin(req, res, { err: null, user: req.user, redirect: false, accessToken: null }).then((loginResult) => {
         delete loginResult.user.services;
         delete loginResult.user.thirdPartyUser;
