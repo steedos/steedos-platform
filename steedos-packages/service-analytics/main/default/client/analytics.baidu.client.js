6af4dd34e262e534dd8caa76e88d4d7b52e40c76
yinlianghui <yinlianghui@hotoa.com>
2022-07-22 13:18:26 +0800

    访问统计：改为通过软件包加载 #3656

--- steedos-packages/service-analytics/main/default/client/analytics.baidu.client.js
+++ steedos-packages/service-analytics/main/default/client/analytics.baidu.client.js
@@ -1,8 +1,18 @@
+/*
+ * @Author: yinlianghui@steedos.com
+ * @Date: 2022-07-22 09:50:56
+ * @LastEditors: yinlianghui@steedos.com
+ * @LastEditTime: 2022-07-22 11:08:35
+ * @Description: 
+ */
 var _hmt = _hmt || [];
 (function() {
   try {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?6bdb41ab7e3b884e6dfdd331bcdd2622";
+    var analyticsConfig = Meteor.settings.public.analytics;
+    var hmId = analyticsConfig && analyticsConfig.baidu && analyticsConfig.baidu.id;
+    var defaultHmId = "6bdb41ab7e3b884e6dfdd331bcdd2622";
+    hm.src = "https://hm.baidu.com/hm.js?" + (hmId || defaultHmId);
     var s = document.getElementsByTagName("script")[0]; 
     s.parentNode.insertBefore(hm, s);
   } catch (error) {
