9dcefaea5b3c3583362b703e03cc11312ba85816
baozhoutao <baozhoutao@hotoa.com>
2022-07-25 13:29:09 +0800

    fix: #3683

--- packages/objectql/src/util/suffix.ts
+++ packages/objectql/src/util/suffix.ts
@@ -1,18 +1,41 @@
 /*
  * @Author: sunhaolin@hotoa.com
  * @Date: 2022-05-28 11:07:57
- * @LastEditors: sunhaolin@hotoa.com
- * @LastEditTime: 2022-07-20 14:10:40
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2022-07-25 13:28:03
  * @Description: 
  */
 const validator = require('validator');
+const Future  = require('fibers/future');
 //TODO 将此代码转移到license项目
 const customObjectNameSuffix = '__c';
 const customFieldNameSuffix = '__c';
 declare var Steedos: any;
 
+function wrapAsync(fn, context){
+    let proxyFn = async function(_cb){
+        let value = null;
+        let error = null;
+        try {
+            value = await fn.apply({}, context)
+        } catch (err) {
+            error = err
+        }
+        _cb(error, value)
+    }
+    let fut = new Future();
+    let callback = fut.resolver();
+    let result = proxyFn.apply(this, [callback]);
+    return fut ? fut.wait() : result;
+}
+
+function hasProductSync(productKey, spaceId){
+    var options = wrapAsync(Steedos.hasProduct, [productKey, spaceId])
+    return options;
+}
+
 function standardObject(spaceId){
-    return validator.toBoolean(process.env.DEVELOPER_STANDARD_OBJECTS || 'false', true) && Steedos.hasFeatureSync('platform-enterprise.standard_object', spaceId);
+    return validator.toBoolean(process.env.DEVELOPER_STANDARD_OBJECTS || 'false', true) && hasProductSync('platform-enterprise', spaceId);
 }
 
 export const getObjectSuffix = (spaceId: string, internal?: boolean)=>{
