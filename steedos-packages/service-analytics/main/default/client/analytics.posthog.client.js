ad3fbcf963decceefeffe8058f87c8347090ecb2
殷亮辉 <yinlianghui@hotoa.com>
2022-08-05 08:05:13 +0000

    #3656 posthog产品分析默认值

--- steedos-packages/service-analytics/main/default/client/analytics.posthog.client.js
+++ steedos-packages/service-analytics/main/default/client/analytics.posthog.client.js
@@ -8,12 +8,14 @@
 (function () {
   try {
     var analyticsConfig = Meteor.settings.public.analytics;
-    var posthogConfig = analyticsConfig && analyticsConfig.posthog;
-    if (!posthogConfig || !posthogConfig.id) {
+    var defaultId = "phc_Hs5rJpeE5JK3GdR3NWOf75TvjEcnYShmBxNU2Y942HB";
+    var defaultApiHost = "https://posthog.steedos.cn";
+    var posthogConfig = analyticsConfig && analyticsConfig.posthog || { id: defaultId, api_host: defaultApiHost };
+    if (!posthogConfig) {
       return;
     }
-    var posthogId = posthogConfig.id;
-    var posthogHost = posthogConfig.api_host || "https://app.posthog.com";
+    var posthogId = posthogConfig.id || defaultId;
+    var posthogHost = posthogConfig.api_host || defaultApiHost;
     var scriptPosthog = $(`<script>
         !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
         posthog.init('${posthogId}',{api_host:'${posthogHost}'})
