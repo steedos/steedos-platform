ff810fc97084a7d1eb32d6e0ee796b6018fd5fc8
殷亮辉 <yinlianghui@hotoa.com>
2022-08-15 20:15:47 +0800

    #2256 页面布局弹出全屏窗口（4边50px间隔）

--- services/standard-object-database/main/default/objects/object_layouts.layouts.action.js
+++ services/standard-object-database/main/default/objects/object_layouts.layouts.action.js
@@ -15,5 +15,44 @@ module.exports = {
   },
   customizeVisible: function(object_name, record_id, record_permissions, record){
       return Creator.baseObject.actions.standard_new.visible() && record.is_system;
+  },
+  standard_new: function (object_name, record_id, fields){
+		var object = Creator.getObject(object_name);
+    var gridName = this.action.gridName;
+    // var isRelated = this.action.isRelated;
+    var relatedFieldName = this.action.relatedFieldName;
+    var masterRecordId = this.action.masterRecordId;
+    var initialValues = this.action.initialValues
+    if(!initialValues){
+      initialValues = {};
+      initialValues[relatedFieldName] = masterRecordId
+    }
+    const appId = Session.get("app_id");
+    const page = Steedos.Page.getPage('form', appId, object_name);
+    const title = t('New') + ' ' + object.label;
+    const options = {
+      gridName: gridName
+    };
+    const props = {
+      width: "unset",
+      style: {
+        top: "50px",
+        bottom: "50px",
+        left: "50px",
+        right: "50px",
+        padding: "0px",
+        margin: "0px",
+        width: "unset"
+      }, 
+      className: "absolute"
+    };
+    Steedos.Page.render(SteedosUI.Modal, page, Object.assign({}, options, {
+      appId: appId,
+      objectName: object_name,
+      title: title,
+      data: initialValues,
+    }), {
+      props: props
+    });
   }
 }
