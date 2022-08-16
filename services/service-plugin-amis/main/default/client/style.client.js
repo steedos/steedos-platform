e315478ad98f14ce31760162fd54d771e49e9d7f
殷亮辉 <yinlianghui@hotoa.com>
2022-08-16 16:57:03 +0800

    #2256 表格样式问题处理

--- services/service-plugin-amis/main/default/client/style.client.js
+++ services/service-plugin-amis/main/default/client/style.client.js
@@ -8,6 +8,10 @@
                 /*使用绝对定位显示弹出窗口时，内层content最大高度不用限制*/
                 max-height: unset;
             }
+            .page-form-object_layouts .steedos-amis-form .antd-Collapse .antd-Table-table .antd-Form--normal{
+                /*格式内的单元格空白太多*/
+                display: block!important;
+            }
         </style>`);
         $("head").append(styleCss);
     } catch (error) {
