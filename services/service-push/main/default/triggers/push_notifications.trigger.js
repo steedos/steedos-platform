836ac606aa4c7280014ea062dac9b4a1afca4a09
chenzhipei <18605199364@163.com>
2022-09-07 01:17:59 +0800

    更新service-push推送readme

--- services/service-push/main/default/triggers/push_notifications.trigger.js
+++ services/service-push/main/default/triggers/push_notifications.trigger.js
@@ -5,6 +5,13 @@ module.exports = {
 
     beforeInsert: async function(){
         let doc = this.doc;
+        // 转为int
+        doc.badge = parseInt(doc.badge);
+        
+        if(isNaN(doc.badge)){
+            throw new Meteor.Error(500, `请传入正确的badge`);
+        }
+
         let find_query = {
             fields: ['user'],
             filters: [['mobile','=',doc.owner_mobile],['space','=',doc.space]]
@@ -17,8 +24,7 @@ module.exports = {
             doc.created_by = space_user.user;
             doc.modified_by = space_user.user;
         }
-         
-        // console.log("doc: ",doc);
+        
     },
 
     beforeUpdate: async function(){
@@ -27,9 +33,11 @@ module.exports = {
 
     afterInsert: async function(){
         let doc = this.doc;
-
-        if (!doc.badge){
-            throw new Meteor.Error(500, `缺少badge，请传入正确的badge`);
+        
+        doc.badge = parseInt(doc.badge);
+        
+        if (isNaN(doc.badge)){
+            throw new Meteor.Error(500, `请传入正确的badge`);
         }
 
         if (!doc.name){
@@ -45,19 +53,19 @@ module.exports = {
             body: doc.body,
             related_to: {
                 o: "service_push",
-                ids: [doc._id],
+                ids: [doc._id]
             },
             related_name: doc.name,
             from: null,
             space: doc.space,
         }
-        console.log("message: ", message);
 
         // 发送pc端推送和移动端推送
         await Creator.addNotifications(message, null, doc.owner);
 
         
-        if (doc.app_id && doc.badge){
+        if (doc.app_id){
+
             let app = await objectql.getObject('apps').findOne(doc.app_id, {fields: ['_id']});
             if (app){
                 let find_query = {
@@ -67,7 +75,6 @@ module.exports = {
                 let badge = await objectql.getObject('steedos_keyvalues').findOne(find_query);
                 if (badge){
                     badge.value[app._id] = doc.badge;
-                    console.log("badge: ",badge);
                     await objectql.getObject('steedos_keyvalues').update(badge._id, {"value": badge.value})
                 }else{
                     let value = {}
@@ -78,7 +85,7 @@ module.exports = {
                     }
                     value[app._id] = doc.badge;
                     insertDoc.value = value;
-                    await objectql.getObject('steedos_keyvalues').insert(insertDoc)
+                    await objectql.getObject('steedos_keyvalues').insert(insertDoc);
                 }
             }
         }
