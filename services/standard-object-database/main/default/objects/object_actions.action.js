9fdd238db51c6acf223c3b582f55c3883cb3129c
baozhoutao <baozhoutao@hotoa.com>
2022-09-22 15:04:15 +0800

    添加 按钮设计器显隐 #3826

--- services/standard-object-database/main/default/objects/object_actions.action.js
+++ services/standard-object-database/main/default/objects/object_actions.action.js
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-09-21 18:19:06
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-09-22 14:42:05
+ * @LastEditTime: 2022-09-22 15:01:31
  * @Description: 
  */
 module.exports = {
@@ -10,17 +10,20 @@ module.exports = {
     document.location = Steedos.absoluteUrl(`/api/amisButtonDesign?id=${record_id}&object=${this.record.object}`);
   },
   showDesignVisible: function (object_name, record_id, record_permissions) {
-      var perms, record;
-      perms = {};
-      if (record_permissions) {
-          perms = record_permissions;
-      } else {
-          record = Creator.getObjectRecord(object_name, record_id);
-          record_permissions = Creator.getRecordPermissions(object_name, record, Meteor.userId());
-          if (record_permissions) {
-              perms = record_permissions;
-          }
+      var perms;
+      var record = Creator.getObjectRecord(object_name, record_id);
+      if(record.type === 'amis_button'){
+        perms = {};
+        if (record_permissions) {
+            perms = record_permissions;
+        } else {
+            record_permissions = Creator.getRecordPermissions(object_name, record, Meteor.userId());
+            if (record_permissions) {
+                perms = record_permissions;
+            }
+        }
+        return perms["allowEdit"];
       }
-      return perms["allowEdit"];
+      return false;
   }
 }
