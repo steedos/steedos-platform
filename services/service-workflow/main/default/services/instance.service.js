95cab574b8e38624f2ffdf9fe02107bc8d4b3da1
baozhoutao <baozhoutao@hotoa.com>
2022-09-24 10:59:19 +0800

    审批王新增 相关文件接口
    https://github.com/steedos/steedos-webapp-nextjs/issues/25

---
+++ services/service-workflow/main/default/services/instance.service.js
@@ -0,0 +1,133 @@
+const objectql = require('@steedos/objectql');
+const _ = require('lodash');
+module.exports = {
+  name: "instance",
+  actions: {
+    getRelatedInstances: {
+      graphql: {
+        query:
+          "getRelatedInstances(flowId: String, state: String, keywords: String, top: Int, skip: Int, sort: String): [instances]",
+      },
+      async handler(ctx) {
+        const userSession = ctx.meta.user;
+        const { resolveInfo } = ctx.meta;
+        const { flowId, state, keywords , top , skip , sort} = ctx.params;
+
+        const filters = await this.getRelatedFilters(flowId, state, keywords, userSession)
+
+        let fields = [];
+
+        const fieldNames = objectql.getGraphqlFields(resolveInfo);
+
+        if (!_.isEmpty(fieldNames)) {
+            fields = fieldNames;
+        };
+
+        const query = {
+            filters: filters, 
+            fields: fields,
+            top: top,
+            skip: skip,
+            sort: sort,
+        }
+
+        return await objectql.getObject('instances').find(query, null)
+      },
+    },
+    getRelatedInstances__count: {
+        graphql: {
+            query:
+            "getRelatedInstances__count(flowId: String, state: String, keywords: String): Int"
+        },
+        async handler(ctx) {
+            const userSession = ctx.meta.user;
+            const { flowId, state, keywords } = ctx.params;
+            const filters = await this.getRelatedFilters(flowId, state, keywords, userSession);
+            const query = {
+                filters: filters
+            }
+    
+            return await objectql.getObject('instances').count(query, null)
+          },
+    }
+  },
+  methods: {
+    getRelatedFilters: {
+        async handler(flowId, state, keywords, userSession){
+            const filters = [];
+            const defaultFilters = await this.getRelatedDefaultFilters(userSession);
+            const userFilters = [];
+
+            if(!_.isEmpty(defaultFilters)){
+                filters.push(defaultFilters);
+            }
+
+            if(flowId){
+                userFilters.push(['flow', '=', flowId]);
+            };
+
+            if(state){
+                userFilters.push(['state', '=', state]);
+            }else{
+                userFilters.push(['state', 'in', ['pending', 'completed']]);
+            }
+
+            if(keywords){
+                userFilters.push([
+                    ['name', 'contains', keywords], 
+                    'or', 
+                    ['applicant_name', 'contains', keywords], 
+                    'or', 
+                    ['flow_name', 'contains', keywords], 
+                    'or', 
+                    ['current_step_name', 'contains', keywords]
+                ])
+            };
+
+            if(!_.isEmpty(userFilters)){
+                filters.push(userFilters);
+            };
+            return filters;
+        }
+    },
+    getRelatedDefaultFilters: {
+        async handler(userSession){
+            const filters = [];
+
+            filters.push(['space', '=', userSession.spaceId]);
+
+            if(!userSession.is_space_admin){
+                const monitorFlows = await objectql.getObject('flows').find({filters: [
+                    ['perms.orgs_can_monitor', 'in',   userSession.organizations_parents],
+                    'or',
+                    ['perms.users_can_monitor', '=', userSession.userId]
+                ], fields: ['_id']});
+                
+                const adminFlows = await objectql.getObject('flows').find({filters: [
+                    ['perms.orgs_can_admin', 'in',   userSession.organizations_parents],
+                    'or',
+                    ['perms.users_can_admin', '=', userSession.userId]
+                ], fields: ['_id']})
+                
+                filters.push([
+                    ['submitter', '=', userSession.userId],
+                    'or',
+                    ['applicant', '=', userSession.userId],
+                    'or',
+                    ['inbox_users', '=', userSession.userId],
+                    'or',
+                    ['outbox_users', '=', userSession.userId],
+                    'or',
+                    ['cc_users', '=', userSession.userId],
+                    'or',
+                    ['inbox_users', '=', userSession.userId],
+                    'or',
+                    ['flow', 'in', _.uniq(_.concat(_.map(monitorFlows, '_id'), _.map(adminFlows, '_id')))],
+                ])
+
+            }
+            return filters;
+        },
+    }
+  }
+};
