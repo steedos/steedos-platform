fbb2e97c89c6d37102e6af02051d4b99727e41ba
Byran <sunhaolin@hotoa.com>
2022-10-25 10:38:35 +0800

    graphql删除不存在的记录时报错"Cannot read property 'space' of null" #3883

--- packages/standard-objects/base.recordRecentView.trigger.js
+++ packages/standard-objects/base.recordRecentView.trigger.js
@@ -2,6 +2,7 @@ const objectql = require('@steedos/objectql');
 
 const afterDeleteRecentView = async function () {
     const { previousDoc, object_name, id } = this;
+    if (!previousDoc) return;
     await objectql.getObject('object_recent_viewed').directDelete({
         filters: [['space', '=', previousDoc.space],['record/o', '=', object_name],['record/ids', '=', [id]]]
     });
