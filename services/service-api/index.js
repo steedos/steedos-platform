4587b4375aa429dc5204145146d2e2fe3bafeb0e
baozhoutao <baozhoutao@hotoa.com>
2022-11-14 10:47:17 +0800

    add gzip

--- services/service-api/index.js
+++ services/service-api/index.js
@@ -381,7 +381,19 @@ module.exports = {
 	},
 	created(){
 		const express = require("express");
+		const compression = require('compression');
 		const app = express();
+		app.use(compression()) //{ filter: shouldCompress }
+
+		// function shouldCompress (req, res) {
+		// 	if (req.headers['x-no-compression']) {
+		// 	  // don't compress responses with this request header
+		// 	  return false
+		// 	}
+		   
+		// 	// fallback to standard filter function
+		// 	return compression.filter(req, res)
+		// }
 		this.app = app;
 	},
 	async started (){
