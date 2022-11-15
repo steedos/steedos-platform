c72daeb20da49a44d0447ae336d95053fd35e232
Byran <sunhaolin@hotoa.com>
2022-11-15 11:54:22 +0800

    工作流规则增加 过滤条件 字段用于 时间触发器过滤对象记录
    https://github.com/steedos/steedos-platform/issues/1063

--- steedos-packages/workflow_time_trigger/main/default/schedule/time_trigger.js
+++ steedos-packages/workflow_time_trigger/main/default/schedule/time_trigger.js
@@ -2,7 +2,7 @@
  * @Author: sunhaolin@hotoa.com
  * @Date: 2022-11-12 13:18:28
  * @LastEditors: sunhaolin@hotoa.com
- * @LastEditTime: 2022-11-12 20:21:17
+ * @LastEditTime: 2022-11-15 11:44:07
  * @Description: 工作流时间触发器定时任务，用于将工作流规则中配置的时间触发器放入工作流操作执行队列
  */
 'use strict';
@@ -11,6 +11,7 @@
 const schedule = require('node-schedule');
 const objectql = require('@steedos/objectql');
 const moment = require('moment');
+const _eval = require('eval')
 
 module.exports = {
     run: async function () {
@@ -42,45 +43,53 @@ module.exports = {
                  * 将过滤出的记录和配置的工作流操作，放到工作流操作执行队列，由单独的定时器执行
                  */
                 for (const ruleDoc of workflowRuleDocs) {
-                    const { time_triggers, object_name, _id } = ruleDoc
-                    const obj = objectql.getObject(object_name)
-                    if (time_triggers && obj) {
-                        for (const tTrigger of time_triggers) {
-                            const { number, unit, type, date_field, updates_field_actions, workflow_notifications_actions } = tTrigger
-                            if (updates_field_actions || workflow_notifications_actions) { // 配置了工作流操作才处理
-                                let triggerTime;
-                                if (type == 'earlier_than') {
-                                    triggerTime = now.add(number, unit)
-                                }
-                                else if (type == 'later_than') {
-                                    triggerTime = now.add(-number, unit)
-                                }
-                                else {
-                                    console.error('time_trigger schedule:', 'invalid type in time_triggers', `workflow_rule _id ${_id}`);
-                                    continue
-                                }
-                                const docs = await obj.find({
-                                    filters: [
-                                        // TODO 首先应该查找符合工作流规则的记录
-                                        [date_field, '>=', triggerTime.toDate()],
-                                        [date_field, '<=', triggerTime.add(intervalMinitues, 'minute').toDate()]
-                                    ]
-                                })
-                                // 将过滤出的记录和配置的工作流操作，放到工作流操作执行队列，由单独的定时器执行
-                                for (const doc of docs) {
-                                    await queueObj.insert({
-                                        'record_id': doc._id,
-                                        'updates_field_actions': updates_field_actions,
-                                        'workflow_notifications_actions': workflow_notifications_actions,
-                                        '_excuted': false,
-                                        '_excuting': false,
+                    try {
+                        const { time_triggers, object_name, _id } = ruleDoc
+                        const filters = _eval(`module.exports = ${ruleDoc.filters}`) || []
+                        const obj = objectql.getObject(object_name)
+                        if (time_triggers && obj) {
+                            for (const tTrigger of time_triggers) {
+                                const { number, unit, type, date_field, updates_field_actions, workflow_notifications_actions } = tTrigger
+                                if (updates_field_actions || workflow_notifications_actions) { // 配置了工作流操作才处理
+                                    let triggerTime;
+                                    if (type == 'earlier_than') {
+                                        triggerTime = now.add(number, unit)
+                                    }
+                                    else if (type == 'later_than') {
+                                        triggerTime = now.add(-number, unit)
+                                    }
+                                    else {
+                                        console.error('time_trigger schedule:', 'invalid type in time_triggers', `workflow_rule _id ${_id}`);
+                                        continue
+                                    }
+                                    const docs = await obj.find({
+                                        filters: [
+                                            filters, // 首先应该查找符合过滤条件的记录
+                                            [date_field, '>=', triggerTime.toDate()],
+                                            [date_field, '<=', triggerTime.add(intervalMinitues, 'minute').toDate()]
+                                        ]
                                     })
+                                    // 将过滤出的记录和配置的工作流操作，放到工作流操作执行队列，由单独的定时器执行
+                                    for (const doc of docs) {
+                                        await queueObj.insert({
+                                            'record_id': doc._id,
+                                            'updates_field_actions': updates_field_actions,
+                                            'workflow_notifications_actions': workflow_notifications_actions,
+                                            '_excuted': false,
+                                            '_excuting': false,
+                                        })
+                                    }
+
+
                                 }
-
-
                             }
                         }
+                    } catch (error) {
+                        console.error(error.stack);
+                        console.error('time_trigger schedule:', error.message);
+                        console.error('ruleDoc:', ruleDoc);
                     }
+
                 }
 
 
