7db6a2ca6fb7e9fc29dac964ac10c517b39f863f
Byran <sunhaolin@hotoa.com>
2022-12-01 19:31:04 +0800

    SaaS版：主数据 & demo数据初始化 #3961

---
+++ packages/data-import/src/types/userSessionType.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: sunhaolin@hotoa.com
+ * @Date: 2022-11-30 16:16:08
+ * @LastEditors: sunhaolin@hotoa.com
+ * @LastEditTime: 2022-11-30 16:16:47
+ * @Description: 
+ */
+export type userSessionType = {
+    spaceId: string
+    userId: string
+    company_id: string
+    company_ids: string[]
+}
