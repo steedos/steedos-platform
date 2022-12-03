148f94b00f536fc4a64447030ad511e49e476fa4
Byran <sunhaolin@hotoa.com>
2022-12-03 20:00:47 +0800

    迁移 spaces.object.js、spaces.actions.js、spaces.object.yml
    工作区初始化逻辑从meteor迁移至单独软件包 #3990

---
+++ services/standard-space/main/default/services/spaces.service.js
@@ -0,0 +1,262 @@
+/*
+ * @Author: sunhaolin@hotoa.com
+ * @Date: 2022-12-02 16:53:23
+ * @LastEditors: sunhaolin@hotoa.com
+ * @LastEditTime: 2022-12-03 17:30:35
+ * @Description: 
+ */
+"use strict";
+const _ = require("lodash");
+const { getObject, getSteedosConfig } = require("@steedos/objectql")
+const metadataCore = require('@steedos/metadata-core');
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+module.exports = {
+	name: 'spaces',
+	namespace: "steedos",
+	mixins: [],
+	/**
+	 * Settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+		/**
+		 * @api {post} /service/api/spaces/register_tenant 注册工作区
+		 * @apiName register_tenant
+		 * @apiGroup spaces.service.js
+		 * @apiParam {String} name 工作区名称
+		 * @apiSuccess {Object} 返回创建后的工作区记录
+		 */
+		register_tenant: {
+			rest: {
+				method: "POST",
+				path: "/register/tenant"
+			},
+			async handler(ctx) {
+				this.broker.logger.warn('[service][spaces]===>', '/service/api/spaces/register_tenant', ctx.params.name)
+				return await this.register_tenant(ctx.params.name, ctx.meta.user)
+			}
+		},
+		/**
+		 * @api {call} addSpaceUsers 工作区添加用户
+		 * @apiName addSpaceUsers
+		 * @apiGroup spaces.service.js
+		 * @apiParam {String} spaceId 工作区ID
+		 * @apiParam {String} userId 用户ID
+		 * @apiParam {Boolean} user_accepted 是否有效
+		 * @apiParam {String} organization_id 组织ID
+		 * @apiSuccess {Boolean} 是否添加成功
+		 */
+		addSpaceUsers: {
+			async handler(ctx) {
+				this.broker.logger.warn('[service][spaces]===>', 'addSpaceUsers', ctx.params)
+				return await this.addSpaceUsers(ctx.params.spaceId, ctx.params.userId, ctx.params.user_accepted, ctx.params.organization_id)
+			}
+		}
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+		/**
+		 * 注册工作区
+		 */
+		async register_tenant(spaceName, userSession) {
+			const spaceObj = getObject('spaces')
+
+			const config = getSteedosConfig();
+			let tenant = {
+				name: "Steedos",
+				logo_url: undefined,
+				background_url: undefined,
+				enable_create_tenant: true,
+				enable_register: true,
+				enable_forget_password: true
+			}
+
+			if (config.tenant) {
+				_.assignIn(tenant, config.tenant)
+			}
+
+			if (!tenant.enable_create_tenant) {
+				throw new Error(500, "禁止注册企业")
+			}
+
+			if (!spaceName) {
+				throw new Error("名称不能为空")
+			}
+
+			if (userSession) {
+				const userId = userSession.userId
+				const spaceId = await spaceObj._makeNewID()
+				const spaceDoc = {
+					_id: spaceId,
+					space: spaceId,
+					name: spaceName,
+					owner: userId
+				}
+
+				await spaceObj.insert(spaceDoc);
+				const newSpace = await spaceObj.findOne(spaceId);
+				return newSpace
+			} else {
+				if (!userSession) {
+					throw new Error(401, "");
+				}
+			}
+		},
+		/**
+		 * 工作区添加用户
+		 */
+		async addSpaceUsers(spaceId, userId, user_accepted, organization_id) {
+			const now = new Date();
+			const suObj = getObject('space_users');
+			const spaceObj = getObject('spaces')
+			const orgObj = getObject('organizations')
+
+			const suDoc = (await suObj.directFind({
+				filters: [
+					['space', '=', spaceId],
+					['user', '=', userId]
+				]
+			}))[0]
+			// 如果工作区已存在此用户则不重复添加
+			if (suDoc) {
+				return;
+			}
+
+			let profile = 'user';
+
+			const space = await spaceObj.findOne(spaceId, { fields: ['default_profile', 'default_organization'] })
+			if (space) {
+				if (space.default_profile) {
+					profile = space.default_profile
+				}
+				if (!organization_id && space.default_organization) {
+					organization_id = space.default_organization
+				}
+			}
+
+			if (!organization_id) {
+				const rootOrg = (await orgObj.find({
+					filters: [
+						['space', '=', spaceId],
+						['parent', '=', null]
+					]
+				}))[0]
+				organization_id = rootOrg._id
+			}
+
+			//company_id,company_ids,organizations_parents由triggers维护
+			const spaceUsersDoc = {
+				user: userId,
+				user_accepted: user_accepted,
+				organization: organization_id,
+				organizations: [organization_id],
+				profile: profile,
+				space: spaceId,
+				owner: userId,
+				created_by: userId,
+				created: now,
+				modified_by: userId,
+				modified: now
+			}
+			const newsuDoc = await suObj.insert(spaceUsersDoc)
+
+			if (this.isSpaceAdmin(spaceId, userId)) {
+				await suObj.directUpdate(newsuDoc._id, {
+					profile: 'admin'
+				})
+			}
+			// 如果开发环境且创建第一条space_users记录时, 自动生成api key 并写入.env.local 文件
+			if (process.env.NODE_ENV != 'production' && (await suObj.count({ filters: [['space', '=', spaceId]] })) === 1) {
+				// 创建api key
+				getObject('api_keys').insert({
+					api_key: process.env.STEEDOS_INITIAL_API_KEY || Random.secret(),
+					space: spaceId,
+					owner: userId,
+					active: true
+				}).then(function (record) {
+					// 写入.env.local 文件
+					const rootUrl = metadataCore.getRootUrl();
+					if (!rootUrl) {
+						rootUrl = process.env.ROOT_URL
+					}
+					if (!rootUrl) {
+						rootUrl = 'http://localhost:5000'
+					}
+					const server = rootUrl
+					if (!server.startsWith('http')) {
+						server = `http://${server}`
+					}
+					if (server.endsWith('/')) {
+						server = server.substring(0, server.length - 1);
+					}
+					metadataCore.saveSourceConfig({
+						server: server,
+						apikey: record.api_key
+					})
+				}).catch(function (error) {
+					console.log(error)
+				})
+			}
+			return true;
+		},
+
+		/**
+		 * 是否工作区管理员
+		 * @param {*} spaceId 
+		 * @param {*} userId 
+		 * @return boolean
+		 */
+		async isSpaceAdmin(spaceId, userId) {
+			const spaceObj = getObject('spaces')
+			const space = await spaceObj.findOne(spaceId, { fields: ['admins'] })
+			if (space?.admins) {
+				return space.admins.indexOf(userId) >= 0
+			}
+			return false
+		}
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	async started() {
+		this.broker.logger.warn('[service][spaces]===>', 'started')
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	async stopped() {
+
+	}
+};
---
+++ services/standard-space/main/default/triggers/spaces.trigger.js
@@ -0,0 +1,194 @@
+/*
+ * @Author: sunhaolin@hotoa.com
+ * @Date: 2022-12-03 11:19:39
+ * @LastEditors: sunhaolin@hotoa.com
+ * @LastEditTime: 2022-12-03 19:03:38
+ * @Description: 
+ */
+"use strict";
+// @ts-check
+
+const objectql = require('@steedos/objectql');
+const validator = require('validator');
+const _ = require("lodash");
+
+module.exports = {
+    listenTo: 'spaces',
+
+    beforeInsert: async function () {
+        console.log('==>>> spaces beforeInsert')
+
+        let { doc, userId } = this
+        if (!userId && doc.owner) {
+            userId = doc.owner;
+        }
+        doc.created_by = userId;
+        doc.created = new Date();
+        doc.modified_by = userId;
+        doc.modified = new Date();
+        doc.enable_register = false;
+        doc.services = doc.services || {};
+        if (!userId) {
+            throw new Error(400, "spaces_error_login_required");
+        }
+
+        if (doc._id) {
+            doc.space = doc._id
+        }
+
+        doc.owner = userId;
+        return doc.admins = [userId];
+    },
+
+    beforeUpdate: async function () {
+        console.log('==>>> spaces beforeUpdate')
+
+        const { doc, userId, id } = this
+        const spaceDoc = await objectql.getObject('spaces').findOne(id)
+        if (spaceDoc.owner !== userId) {
+            throw new Error(400, "spaces_error_space_owner_only");
+        }
+        if (doc.owner) {
+            if (!doc.admins) {
+                doc.admins = doc.admins;
+            } else if (doc.admins.indexOf(doc.owner) < 0) {
+                return doc.admins.push(doc.owner);
+            }
+        }
+
+        if (_.has(doc, 'admins') && _.isEmpty(doc.admins)) {
+            throw new Error(400, "spaces_error_space_admins_required");
+        }
+    },
+
+    beforeDelete: async function () {
+        throw new Error(400, "spaces_error_space_readonly");
+    },
+
+    afterInsert: async function () {
+        console.log('==>>> spaces afterInsert')
+
+        const { doc: spaceDoc } = this
+        const spaceObj = objectql.getObject('spaces')
+        // 创建第一个space时, 更新Tenant配置
+        const spaces = await spaceObj.count({});
+        if (spaces === 1) {
+            const config = objectql.getSteedosConfig();
+            config.setTenant({ _id: spaceDoc._id, enable_create_tenant: validator.toBoolean(process.env.STEEDOS_TENANT_ENABLE_SAAS || 'false', true), enable_register: spaceDoc.enable_register });
+        }
+
+        let spaceName = spaceDoc.name;
+        let spaceId = spaceDoc._id;
+        let userId = spaceDoc.owner;
+        let now = new Date();
+
+        let companyDB = objectql.getObject('company')
+        let companyDoc = {
+            _id: spaceId,
+            name: spaceName,
+            organization: spaceId,
+            company_id: spaceId,
+            space: spaceId,
+            owner: userId,
+            created_by: userId,
+            created: now,
+            modified_by: userId,
+            modified: now
+        }
+        // 新建分部
+        await companyDB.directInsert(companyDoc);
+
+        let orgDB = objectql.getObject('organizations')
+        let orgDoc = {
+            _id: spaceId,
+            name: spaceName,
+            fullname: spaceName,
+            is_company: true,
+            users: [userId],
+            company_id: spaceId,
+            space: spaceId,
+            owner: userId,
+            created_by: userId,
+            created: now,
+            modified_by: userId,
+            modified: now
+        }
+        // 新建根组织
+        await orgDB.directInsert(orgDoc);
+
+        // 新建用户
+        await objectql.getSteedosSchema().broker.call(`spaces.addSpaceUsers`, {
+            spaceId,
+            userId,
+            user_accepted: true,
+            organization_id: orgDoc._id
+        })
+
+        // 发出工作区初始化完成事件
+        objectql.broker.emit(`space.initialized`, spaceDoc);
+    },
+
+    afterUpdate: async function () {
+        const { doc, id, previousDoc } = this
+        const orgObj = objectql.getObject('organizations')
+        const suObj = objectql.getObject('space_users')
+        const spaceObj = objectql.getObject('spaces')
+        // 工作区修改后，该工作区的根部门的name也要修改，根部门和子部门的fullname也要修改
+        if (doc.name) {
+            const spaceDoc = await spaceObj.findOne(id)
+            // 直接修改根部门名字，跳过验证
+            const rootOrg = (await orgObj.find({
+                filters: [
+                    ['space', '=', id],
+                    ['parent', '=', null]
+                ]
+            }))[0]
+
+            await orgObj.directUpdate(rootOrg._id, {
+                name: spaceDoc.name,
+                fullname: spaceDoc.name
+            })
+
+            const children = await orgObj.find({
+                filters: [
+                    ['parents', '=', rootOrg._id]
+                ]
+            });
+
+            const broker = objectql.getSteedosSchema().broker
+            for (const child of children) {
+                const childFullname = await broker.call('organizations.calculateFullname', { orgId: child._id })
+                await orgObj.directUpdate(child._id, {
+                    fullname: childFullname
+                })
+            }
+        }
+
+        if (_.has(doc, 'admins')) {
+            const setAdmins = doc.admins || []
+            const removedAdmin = _.difference(previousDoc.admins, setAdmins);
+            if (!_.isEmpty(removedAdmin)) {
+                await suObj.updateMany([
+                    ['space', '=', id],
+                    ['user', 'in', removedAdmin]
+                ], {
+                    profile: 'user'
+                })
+            }
+            const addedAdmin = _.difference(setAdmins, previousDoc.admins);
+            if (!_.isEmpty(addedAdmin)) {
+                await suObj.updateMany([
+                    ['space', '=', id],
+                    ['user', 'in', addedAdmin]
+                ], {
+                    profile: 'admin'
+                })
+            }
+        }
+    },
+
+    afterDelete: async function () {
+
+    },
+
+}
