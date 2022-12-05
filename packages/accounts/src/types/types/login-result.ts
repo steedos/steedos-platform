3f9422a70caac7f7650ac5108516d460e753fbe2
baozhoutao <baozhoutao@hotoa.com>
2022-12-05 09:14:53 +0800

    fix: 登录接口返回用户所属工作区 #3980

--- packages/accounts/src/types/types/login-result.ts
+++ packages/accounts/src/types/types/login-result.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2022-08-12 14:07:01
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2022-12-05 09:09:03
+ * @Description: 
+ */
 import { Tokens } from './tokens';
 
 export interface LoginResult {
@@ -6,4 +13,5 @@ export interface LoginResult {
   tokens: Tokens;
   user: any;
   space?: string;
+  spaces?: Array<any>
 }
