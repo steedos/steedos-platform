cfc89ad56216c5929f1911758f1fd16293ee4499
庄建国 <9340100@qq.com>
2022-12-19 08:42:57 +0000

    tailwind: base

--- services/service-plugin-amis/main/default/client/tailwind.client.js
+++ services/service-plugin-amis/main/default/client/tailwind.client.js
@@ -11,17 +11,25 @@ try {
   //   tailwindScript.setAttribute("src", Steedos.absoluteUrl('/tailwind/tailwind.js'));
   //   document.getElementsByTagName("head")[0].appendChild(tailwindScript);
   // }
+
+  const head = document.head || document.getElementsByTagName('head')[0]
+
   let tailwindBase = document.createElement("link");
   tailwindBase.setAttribute("rel", "stylesheet");
   tailwindBase.setAttribute("type", "text/css");
   tailwindBase.setAttribute("href", Steedos.absoluteUrl("/tailwind/tailwind-base.css"));
-  document.getElementsByTagName("head")[0].appendChild(tailwindBase);
+
+  if (head.firstChild) {
+    head.insertBefore(tailwindBase, head.firstChild)
+  } else {
+    head.appendChild(tailwindBase)
+  }
 
   let tailwindStyle = document.createElement("link");
   tailwindStyle.setAttribute("rel", "stylesheet");
   tailwindStyle.setAttribute("type", "text/css");
   tailwindStyle.setAttribute("href", Steedos.absoluteUrl("/tailwind/tailwind-steedos.css"));
-  document.getElementsByTagName("head")[0].appendChild(tailwindStyle);
+  head.appendChild(tailwindStyle)
 } catch (error) {
   console.error(error)
 };
