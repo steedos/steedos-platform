70bd854bbdf0a1e28740f0b3ddac2351429e1252
庄建国 <9340100@qq.com>
2022-12-19 07:41:50 +0000

    taillwind: build css for creator

--- services/service-plugin-amis/main/default/client/tailwind.client.js
+++ services/service-plugin-amis/main/default/client/tailwind.client.js
@@ -1,16 +1,21 @@
 try {
   // 加载 tailwind
-  if(typeof BigInt === 'undefined') {
-      let tailwindStyle = document.createElement("link");
-      tailwindStyle.setAttribute("rel", "stylesheet");
-      tailwindStyle.setAttribute("type", "text/css");
-      tailwindStyle.setAttribute("href", Steedos.absoluteUrl("/tailwind/tailwind.css"));
-      document.getElementsByTagName("head")[0].appendChild(tailwindStyle);
-  }else{
-    let tailwindScript = document.createElement("script");
-    tailwindScript.setAttribute("src", Steedos.absoluteUrl('/tailwind/tailwind.js'));
-    document.getElementsByTagName("head")[0].appendChild(tailwindScript);
-  }
+  // if(typeof BigInt === 'undefined') {
+  //     let tailwindStyle = document.createElement("link");
+  //     tailwindStyle.setAttribute("rel", "stylesheet");
+  //     tailwindStyle.setAttribute("type", "text/css");
+  //     tailwindStyle.setAttribute("href", Steedos.absoluteUrl("/tailwind/tailwind.css"));
+  //     document.getElementsByTagName("head")[0].appendChild(tailwindStyle);
+  // }else{
+  //   let tailwindScript = document.createElement("script");
+  //   tailwindScript.setAttribute("src", Steedos.absoluteUrl('/tailwind/tailwind.js'));
+  //   document.getElementsByTagName("head")[0].appendChild(tailwindScript);
+  // }
+  let tailwindStyle = document.createElement("link");
+  tailwindStyle.setAttribute("rel", "stylesheet");
+  tailwindStyle.setAttribute("type", "text/css");
+  tailwindStyle.setAttribute("href", Steedos.absoluteUrl("/tailwind/tailwind-steedos.css"));
+  document.getElementsByTagName("head")[0].appendChild(tailwindStyle);
 } catch (error) {
   console.error(error)
 };
