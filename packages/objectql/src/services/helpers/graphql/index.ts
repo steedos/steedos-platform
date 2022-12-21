7ca6dbb8fabc0c453d29ce187144e16b74eb98ce
snow <2291335922@qq.com>
2022-12-21 10:09:51 +0000

    不改服务端，改nextjs

--- packages/objectql/src/services/helpers/graphql/index.ts
+++ packages/objectql/src/services/helpers/graphql/index.ts
@@ -1064,7 +1064,7 @@ function formatBasicFieldValue(valueType, field, value, objectConfig, userSessio
         case 'html':
             return value || "";
         case 'boolean':
-            return value ? "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'><path stroke-linecap='round' stroke-linejoin='round' d='M4.5 12.75l6 6 9-13.5' /></svg>" : ""
+            return value ? "√" : ""
         case 'date':
             return value ? moment.utc(value).format("YYYY-MM-DD") : '';
         case 'datetime':
