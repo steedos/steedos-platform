17351bad46d1bcf9f334e5763bc78755cab496ba
Byran <sunhaolin@hotoa.com>
2023-01-17 10:10:01 +0800

    fix 申请单打开样式异常，使用@steedos/service-meteor-package-loader包未加载public静态文件导致
    新流程引擎，已审核，列表有重复 #4076

--- services/service-workflow/package.service.js
+++ services/service-workflow/package.service.js
@@ -2,7 +2,7 @@
 const Fiber = require("fibers");
 const project = require('./package.json');
 const serviceName = project.name;
-const packageLoader = require('@steedos/service-meteor-package-loader');
+const packageLoader = require('@steedos/service-package-loader');
 const { excuteTriggers } = require('./main/default/utils/trigger');
 
 /**
