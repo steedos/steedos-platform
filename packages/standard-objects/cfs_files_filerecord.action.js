575493701d90084abdcaab83204165e16ae0c52c
chenzhipei <18605199364@163.com>
2023-02-16 15:06:18 +0000

    #4217 合并附件预览代码

--- packages/standard-objects/cfs_files_filerecord.action.js
+++ packages/standard-objects/cfs_files_filerecord.action.js
@@ -1,16 +1,12 @@
 module.exports = {
   online_preview: function (object_name, record_id) {
-    var file, fileId, ref, url;
+    var file, fileId, ref, download_url, file_name;
     file = this.record;
     fileId = file != null ? (ref = file._id) != null ? ref : void 0 : void 0;
     
-    url = window.location.origin + Steedos.absoluteUrl("/api/files/files/" + fileId + "/" + file.original.name);
-    
-    // 图片和网页类型附件用浏览器打开
-    if (Creator.isImageAttachment(file.original.name) || Creator.isHtmlAttachment(file.original.name)){
-      Steedos.openWindow(url);
-    }else{
-      Creator.officeOnlinePreview(url,file.original.name);
-    }
+    download_url = window.location.origin + Steedos.absoluteUrl("/api/files/files/" + fileId + "/" + file.original.name);
+    file_name = file.original.name;
+
+    SteedosUI.previewFile({download_url, file_name, isPreviewButton:true})
   }
 }
