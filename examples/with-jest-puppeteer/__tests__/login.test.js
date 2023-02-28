698189c8afe18ebef18f4619eb32a6a81edd7901
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2023-02-28 10:26:08 +0800

    jest

--- examples/with-jest-puppeteer/__tests__/login.test.js
+++ examples/with-jest-puppeteer/__tests__/login.test.js
@@ -1,25 +1,28 @@
 const timeout = 30000; 
 
 describe('Login', () => {
+  
   beforeAll(async () => {
     await page.goto(URL, {waitUntil: 'networkidle0'});
   }, timeout);
 
   describe('Test page title and header', () => {
     test('page title', async () => {
-      const title = await page.title(); expect(title).toBe('登录您的账户 | 华炎软件'); 
+      const title = await page.title(); 
+      console.log(title)
+      await page.waitForTimeout(5000);
+      expect(title).toBe('登录您的账户 | 华炎软件'); 
     }, timeout); 
     test('Header', async () => { 
       const headerOne = await page.$('h2'); 
-      const header = await page.evaluate(headerOne => headerOne.innerHTML, headerOne); expect(header).toBe("登录您的账户"); 
+      const header = await page.evaluate(headerOne => headerOne.innerHTML, headerOne); 
+      expect(header).toBe("登录您的账户"); 
     }, timeout); 
     test('Login', async () => { 
-      const userName = await page.$('#loginId'); 
-      userName.value = 'test';
-      const password = await page.$('#password'); 
-      password.value = '123456';
-      const submit = await page.$('[type=submit]'); 
-      submit.click();
+      await page.type('#loginId', 'test@example.com');
+      await page.type('#password', '123456');
+      await page.waitForTimeout(1000);
+      await page.$eval('[type=submit]', el => el.click())
       await page.waitForTimeout(5000);
       const msg = await page.$('#client-snackbar').innerText;
       expect(msg).toBe("账号或密码错。"); 
--- examples/with-jest-puppeteer/jest-puppeteer.config.js
+++ examples/with-jest-puppeteer/jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   launch: { 
-    headless: false, 
+    headless: true, 
     slowMo: 30, 
   } 
 }
