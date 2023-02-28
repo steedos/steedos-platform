640f454332b539dfb461be0c52ab8b3db71dd88d
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2023-02-28 09:51:31 +0800

    login test

--- examples/with-jest-puppeteer/__tests__/login.test.js
+++ examples/with-jest-puppeteer/__tests__/login.test.js
@@ -13,5 +13,16 @@ describe('Login', () => {
       const headerOne = await page.$('h2'); 
       const header = await page.evaluate(headerOne => headerOne.innerHTML, headerOne); expect(header).toBe("登录您的账户"); 
     }, timeout); 
+    test('Login', async () => { 
+      const userName = await page.$('#loginId'); 
+      userName.value = 'test';
+      const password = await page.$('#password'); 
+      password.value = '123456';
+      const submit = await page.$('[type=submit]'); 
+      submit.click();
+      await page.waitForTimeout(5000);
+      const msg = await page.$('#client-snackbar').innerText;
+      expect(msg).toBe("账号或密码错。"); 
+    }, timeout); 
   });
 });
