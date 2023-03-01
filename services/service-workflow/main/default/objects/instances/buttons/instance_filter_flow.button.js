2248795d098fed527607bfe09cb5eb341c4b7442
殷亮辉 <yinlianghui@hotoa.com>
2023-03-01 23:36:57 +0800

    不显示审批的过滤器按钮

--- services/service-workflow/main/default/objects/instances/buttons/instance_filter_flow.button.js
+++ services/service-workflow/main/default/objects/instances/buttons/instance_filter_flow.button.js
@@ -1,13 +1,14 @@
 /*
  * @Author: baozhoutao@steedos.com
  * @Date: 2023-01-18 13:29:34
- * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2023-01-18 13:31:04
+ * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
+ * @LastEditTime: 2023-03-01 23:31:21
  * @Description: 
  */
 
 module.exports = {
     instance_filter_flowVisible: function (object_name, record_id, permission, data) {
-        return !!window.Creator
+        return false;
+        // return !!window.Creator
     }
 }
