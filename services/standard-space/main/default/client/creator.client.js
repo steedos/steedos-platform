dd5fb880ed712fcd2addfe72c24fe952ad7b6ccf
baozhoutao <baozhoutao@hotoa.com>
2023-03-02 12:15:43 +0800

    仅Meteor 环境下, 才生成Creator变量

--- services/standard-space/main/default/client/creator.client.js
+++ services/standard-space/main/default/client/creator.client.js
@@ -1,20 +1,29 @@
-if(!window.Creator){
-    window.Creator = {};
-}
-
-Creator.getObject = (objectName)=>{
-    if(!objectName){
-        if(window.Session){
-            objectName = Session.get("object_name")
-        }
-        if(!objectName){
-            throw new Error('miss objectName')
-        }
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2023-02-27 19:09:19
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2023-03-02 12:06:51
+ * @Description: 
+ */
+if(window.Meteor){
+    if(!window.Creator){
+        window.Creator = {};
     }
-    return getUISchemaSync(objectName);
-}
-
-Creator.getPermissions = (object_name, spaceId, userId)=>{
-    return Creator.getObject(object_name).permissions;
+    
+    Creator.getObject = (objectName)=>{
+        if(!objectName){
+            if(window.Session){
+                objectName = Session.get("object_name")
+            }
+            if(!objectName){
+                throw new Error('miss objectName')
+            }
+        }
+        return getUISchemaSync(objectName);
+    }
+    
+    Creator.getPermissions = (object_name, spaceId, userId)=>{
+        return Creator.getObject(object_name).permissions;
+    }
 }
 	
