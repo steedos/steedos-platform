f49e0135ddd4ec7b54fa466cdc317c5622061873
baozhoutao <baozhoutao@hotoa.com>
2023-03-03 09:22:37 +0800

    fix bug: 新建查询报错,无法选择数据源

--- services/standard-space/main/default/client/creator.client.js
+++ services/standard-space/main/default/client/creator.client.js
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2023-02-27 19:09:19
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2023-03-02 12:06:51
+ * @LastEditTime: 2023-03-03 09:11:56
  * @Description: 
  */
 if(window.Meteor){
@@ -24,6 +24,18 @@ if(window.Meteor){
     
     Creator.getPermissions = (object_name, spaceId, userId)=>{
         return Creator.getObject(object_name).permissions;
+    }
+
+    Creator.getCollection = (objectName)=>{
+        if(!objectName){
+            if(window.Session){
+                objectName = Session.get("object_name")
+            }
+            if(!objectName){
+                throw new Error('miss objectName')
+            }
+        }
+        return db[objectName]
     }
 }
 	
