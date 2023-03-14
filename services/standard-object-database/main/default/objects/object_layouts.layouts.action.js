5515ef994162ed68f68ab00f01063f8ffe6b4497
廖大雪 <2291335922@qq.com>
2023-03-14 09:20:03 +0800

    2.3 编辑页面布局没有保存按钮(启用默认的新建按钮) #4471

--- services/standard-object-database/main/default/objects/object_layouts.layouts.action.js
+++ services/standard-object-database/main/default/objects/object_layouts.layouts.action.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 廖大雪 2291335922@qq.com
+ * @Date: 2023-01-09 15:34:24
+ * @LastEditors: 廖大雪 2291335922@qq.com
+ * @LastEditTime: 2023-03-14 09:15:15
+ * @FilePath: /steedos-platform/services/standard-object-database/main/default/objects/object_layouts.layouts.action.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 module.exports = {
   listenTo: 'object_layouts',
 
@@ -16,38 +24,38 @@ module.exports = {
   customizeVisible: function(object_name, record_id, record_permissions, record){
       return Creator.baseObject.actions.standard_new.visible() && record.is_system;
   },
-  standard_new: function (object_name, record_id, fields){
-		var object = Creator.getObject(object_name);
-    var gridName = this.action.gridName;
-    // var isRelated = this.action.isRelated;
-    var relatedFieldName = this.action.relatedFieldName;
-    var masterRecordId = this.action.masterRecordId;
-    var initialValues = this.action.initialValues
-    if(!initialValues){
-      initialValues = {};
-      initialValues[relatedFieldName] = masterRecordId
-    }
-    const appId = Session.get("app_id");
-    const page = Steedos.Page.getPage('form', appId, object_name);
-    const title = t('New') + ' ' + object.label;
-    if (page && page.schema) {
-      const options = {
-        gridName: gridName
-      };
-      const fullScreenProps = {
-        "width": "96%", 
-        className: "!max-w-[96%]"
-      };
-      Steedos.Page.render(SteedosUI.Modal, page, Object.assign({}, options, {
-        appId: appId,
-        objectName: object_name,
-        title: title,
-        data: initialValues,
-      }), {
-        props: fullScreenProps
-      });
-    }
-  },
+  // standard_new: function (object_name, record_id, fields){
+	// 	var object = Creator.getObject(object_name);
+  //   var gridName = this.action.gridName;
+  //   // var isRelated = this.action.isRelated;
+  //   var relatedFieldName = this.action.relatedFieldName;
+  //   var masterRecordId = this.action.masterRecordId;
+  //   var initialValues = this.action.initialValues
+  //   if(!initialValues){
+  //     initialValues = {};
+  //     initialValues[relatedFieldName] = masterRecordId
+  //   }
+  //   const appId = Session.get("app_id");
+  //   const page = Steedos.Page.getPage('form', appId, object_name);
+  //   const title = t('New') + ' ' + object.label;
+  //   if (page && page.schema) {
+  //     const options = {
+  //       gridName: gridName
+  //     };
+  //     const fullScreenProps = {
+  //       "width": "96%", 
+  //       className: "!max-w-[96%]"
+  //     };
+  //     Steedos.Page.render(SteedosUI.Modal, page, Object.assign({}, options, {
+  //       appId: appId,
+  //       objectName: object_name,
+  //       title: title,
+  //       data: initialValues,
+  //     }), {
+  //       props: fullScreenProps
+  //     });
+  //   }
+  // },
   /*
   standard_edit: function (object_name, record_id, fields){
 		var object = Creator.getObject(object_name);
