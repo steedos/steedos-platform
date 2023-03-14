412e417a21b64a85586c0d695908da6cdb3ce17c
李海龙 220 <lihailong@steedos.com>
2023-03-14 01:54:56 +0000

    增加了表单设计器显隐控制：当前流程是否绑定对象决定表单设计器是否显示 #4339

--- services/service-workflow/main/default/objects/flows/buttons/showDesign.button.js
+++ services/service-workflow/main/default/objects/flows/buttons/showDesign.button.js
@@ -1,21 +1,17 @@
+/*
+ * @Author: 李海龙 220 lihailong@steedos.com
+ * @Date: 2023-03-13 06:22:57
+ * @LastEditors: 李海龙 220 lihailong@steedos.com
+ * @LastEditTime: 2023-03-13 10:25:51
+ */
 module.exports = {
     showDesign: function (object_name, record_id) {
         //这里/api/amisFormDesign?PagId 改为 id
         document.location = Steedos.absoluteUrl(`/api/amisFormDesign?id=${record_id}`);
     },
     showDesignVisible: function (object_name, record_id, record_permissions) {
-        var perms, record;
-        perms = {};
-        if (record_permissions) {
-            perms = record_permissions;
-        } else {
-            record = Creator.getObjectRecord(object_name, record_id);
-            record_permissions = Creator.getRecordPermissions(object_name, record, Meteor.userId());
-            if (record_permissions) {
-                perms = record_permissions;
-            }
-        }
-        return perms["allowEdit"];
+        const record = Creator.getObjectRecord(object_name, record_id);
+        return !(record.object_name);
     }
     
 }
