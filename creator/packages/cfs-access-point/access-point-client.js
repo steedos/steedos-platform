f3cd64e7a7d0e0146c55102bfec81d2c6b9044e6
baozhoutao <baozhoutao@hotoa.com>
2023-03-15 16:34:42 +0800

    fix: APP登录系统后，会报跨域问题
    https://github.com/steedos/steedos-platform/issues/4528

--- creator/packages/cfs-access-point/access-point-client.js
+++ creator/packages/cfs-access-point/access-point-client.js
@@ -43,7 +43,8 @@ if (FS.HTTP.storage) {
   // client time - this includes lag and timezone
   Meteor.startup(function() {
     // Call the server method an get server time
-    HTTP.get(rootUrlPathPrefix + '/cfs/servertime', function(error, result) {
+    const _rootUrl = rootUrlPathPrefix.endsWith("/") ? rootUrlPathPrefix.substr(0, rootUrlPathPrefix.length-1) : rootUrlPathPrefix
+    HTTP.get(_rootUrl + '/cfs/servertime', function(error, result) {
       if (!error) {
         // Update our server time diff
         var dateNew = new Date(+result.content);
