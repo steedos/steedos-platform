3f5212e38abc72ff1869f33edfc43f90b7348b7d
baozhoutao <baozhoutao@hotoa.com>
2023-03-30 18:03:49 +0800

    先临时关闭 数据导入按钮

--- packages/standard-objects/base.client.js
+++ packages/standard-objects/base.client.js
@@ -198,6 +198,7 @@ Steedos.StandardObjects = {
             },
             standard_import_data:{
                 visible: function(objectName){
+                    return false;
                     var allowCreate = Creator.baseObject.actions.standard_new.visible.apply(this, arguments);
                     var objectName = objectName || this.objectName || FlowRouter.current().params.object_name;
                     if(allowCreate){
