47f06dd0078afad4c12c2ffa6104d8654350ee76
李海龙 <lihailong@steedos.com>
2023-04-06 10:27:05 +0800

    新建流程: 增加启用amis表单按钮判断新旧表单设计器的显隐
    按钮字段enable_amisform，通过修改发送适配器 后端获取布尔值判断，也通过新建流程时是否绑定对象判断是否显示添加字段按钮

--- services/service-workflow/main/default/objects/flows/buttons/showDesign.button.js
+++ services/service-workflow/main/default/objects/flows/buttons/showDesign.button.js
@@ -1,8 +1,8 @@
 /*
  * @Author: 李海龙 220 lihailong@steedos.com
  * @Date: 2023-03-13 06:22:57
- * @LastEditors: 李海龙 220 lihailong@steedos.com
- * @LastEditTime: 2023-03-13 10:25:51
+ * @LastEditors: 李海龙 lihailong@steedos.com
+ * @LastEditTime: 2023-04-06 10:20:29
  */
 module.exports = {
     showDesign: function (object_name, record_id) {
@@ -11,7 +11,16 @@ module.exports = {
     },
     showDesignVisible: function (object_name, record_id, record_permissions) {
         const record = Creator.getObjectRecord(object_name, record_id);
-        return !(record.object_name);
+        return record.enable_amisform;
+    },
+	// 通过判断该流程是否绑定对象，决定流程详情页-标题面板上是否显示“添加字段”按钮
+    addFormFields: function (object_name, record_id) {
+        //这里/api/amisFormDesign?PagId 改为 id
+        document.location = Steedos.absoluteUrl(`/api/amisFormDesign?id=${record_id}`);
+    },
+    addFormFieldsVisable: function (object_name, record_id, record_permissions) {
+        const record = Creator.getObjectRecord(object_name, record_id);
+        return record.object_name;
     }
-    
+
 }
