483b0d793a3de6f5c1ba5859f1d9cdba54057fd1
殷亮辉 <yinlianghui@hotoa.com>
2023-04-26 07:17:15 +0000

    build creator

--- server/bundle/programs/server/packages/modules.js
+++ server/bundle/programs/server/packages/modules.js
@@ -466,7 +466,7 @@ module.useNode();
                                                                                                       //
 module.exports = {
   "name": "@steedos/objectql",
-  "version": "2.2.63",
+  "version": "2.2.70-beta.1",
   "main": "lib/index.js"
 };
 
@@ -493,7 +493,7 @@ module.useNode();
                                                                                                       //
 module.exports = {
   "name": "@steedos/core",
-  "version": "2.2.63",
+  "version": "2.2.70-beta.1",
   "main": "lib/index.js"
 };
 
@@ -520,7 +520,7 @@ module.useNode();
                                                                                                       //
 module.exports = {
   "name": "@steedos/service-meteor-package-loader",
-  "version": "2.2.63",
+  "version": "2.2.70-beta.1",
   "main": "index.js"
 };
 
@@ -547,7 +547,7 @@ module.useNode();
                                                                                                       //
 module.exports = {
   "name": "@steedos/service-api",
-  "version": "2.2.63",
+  "version": "2.2.70-beta.1",
   "main": "index.js"
 };
 
@@ -574,7 +574,7 @@ module.useNode();
                                                                                                       //
 module.exports = {
   "name": "@steedos/service-metadata-server",
-  "version": "2.2.63",
+  "version": "2.2.70-beta.1",
   "main": "index.js"
 };
 
@@ -601,7 +601,7 @@ module.useNode();
                                                                                                       //
 module.exports = {
   "name": "@steedos/service-package-registry",
-  "version": "2.2.63",
+  "version": "2.2.70-beta.1",
   "main": "package.service.js"
 };
 
@@ -628,7 +628,7 @@ module.useNode();
                                                                                                       //
 module.exports = {
   "name": "@steedos/service-pages",
-  "version": "2.2.63",
+  "version": "2.2.70-beta.1",
   "main": "package.service.js"
 };
 
@@ -655,7 +655,7 @@ module.useNode();
                                                                                                       //
 module.exports = {
   "name": "@steedos/i18n",
-  "version": "2.2.63",
+  "version": "2.2.70-beta.1",
   "main": "lib/index.js"
 };
 
@@ -682,7 +682,7 @@ module.useNode();
                                                                                                       //
 module.exports = {
   "name": "@steedos/auth",
-  "version": "2.2.63",
+  "version": "2.2.70-beta.1",
   "main": "lib/index.js"
 };
 
@@ -709,7 +709,7 @@ module.useNode();
                                                                                                       //
 module.exports = {
   "name": "@steedos/service-workflow",
-  "version": "2.2.63",
+  "version": "2.2.70-beta.1",
   "main": "package.service.js"
 };
 
