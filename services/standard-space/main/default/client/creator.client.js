7f3b960b735963448399e068cc325b578991bbf8
baozhoutao <baozhoutao@hotoa.com>
2023-04-27 11:38:51 +0800

    FlowRouter.reload 等效于 window.location.reload
    [Bug]: 人员详细页启用按钮点击后页面未刷新，仍显示启用按钮，停用同样 #4819

--- services/standard-space/main/default/client/creator.client.js
+++ services/standard-space/main/default/client/creator.client.js
@@ -1,8 +1,8 @@
 /*
  * @Author: baozhoutao@steedos.com
  * @Date: 2023-02-27 19:09:19
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-04-25 14:05:15
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2023-04-27 11:28:37
  * @Description: 
  */
 if(window.Meteor){
@@ -46,6 +46,9 @@ if(window.Meteor){
     window.refreshGrid = ()=>{
         return FlowRouter.reload();
     };
+    
+    FlowRouter.reload = function(){window.location.reload()}
+
     if(!window.signOut){
         window.signOut = window.Steedos.logout;
     }
