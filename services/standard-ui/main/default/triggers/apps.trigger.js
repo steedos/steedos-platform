27cfde81890c7b28b05bda141c8f203d2118d17b
baozhoutao <baozhoutao@hotoa.com>
2023-05-06 11:57:16 +0800

    fix [Bug]: 设置》简档权限集，新建/编辑 时的 授权应用，可选项缺少了 代码里只开放给手机的应用 #4891

--- services/standard-ui/main/default/triggers/apps.trigger.js
+++ services/standard-ui/main/default/triggers/apps.trigger.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2022-08-05 14:17:44
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2023-05-06 11:52:46
+ * @Description: 
+ */
 const _ = require('underscore');
 const clone = require("clone");
 const objectql = require("@steedos/objectql");
@@ -38,7 +45,8 @@ module.exports = {
             let allApps = clone(await objectql.getAppConfigs(this.spaceId));
             let apps = {}
             _.each(allApps, function(app){
-                if(app.is_creator){
+                if(app.is_creator || app.mobile){
+                    apps[app._id] = app
                     apps[app._id] = app
                 }
             })
@@ -49,46 +57,10 @@ module.exports = {
                     self.data.values.push(Object.assign({code: app._id}, clone(app), baseRecord));
                 }
             })
-            // console.log(`find this.query`, self.query, objectql.getSteedosSchema().metadataDriver.find(self.data.values, this.query, spaceId))
             self.data.values = objectql.getSteedosSchema().metadataDriver.find(self.data.values, self.query, spaceId);
         }
     },
-    afterAggregate: async function () {
-        const { spaceId } = this;
-        let query = InternalData.parserFilters(this.query.filters);
-        let isSystem = query.is_system;
-        if(!_.isEmpty(isSystem) || _.isBoolean(isSystem)){
-            if(_.isObject(isSystem) && isSystem["$ne"]){
-                return;
-            }
-        }
-        if(_.isArray(this.data.values)){
-            let lng = await getLng(this.userId);
-            let self = this;
-            let allApps = clone(await objectql.getAppConfigs(this.spaceId));
-            let apps = {}
-            _.each(allApps, function(app){
-                if(app.is_creator){
-                    apps[app._id] = app
-                }
-            })
-            i18n.translationApps(lng, apps)
-            _.each(apps, function(app){
-                app.name = app.label
-                if(!_.find(self.data.values, function(item){return item.code === app._id || item._id === app._id})){
-                    self.data.values.push(Object.assign({code: app._id}, clone(app), baseRecord));
-                }
-            })
-        }
-        // 获取的apps根据保存的值进行过滤
-        // const allData = this.data.values;
-        // const firstFilterKey = _.keys(query)[0];
-        // this.data.values = _.filter(allData, (item)=>{
-        //     return item[firstFilterKey] === query[firstFilterKey];
-        // })
-        // console.log(`afterAggregate this.query`, this.query, objectql.getSteedosSchema().metadataDriver.find(this.data.values, this.query, spaceId))
-        this.data.values = objectql.getSteedosSchema().metadataDriver.find(this.data.values, this.query, spaceId);
-    },
+    
     afterCount: async function () {
         try {
             this.query.fields.push('name');
@@ -104,32 +76,8 @@ module.exports = {
         if(id && _.isEmpty(this.data.values)){
             let lng = await getLng(this.userId);
             let app = await objectql.getAppConfig(id);
-            // let allApps = clone(await objectql.getAppConfigs(this.spaceId));
-            // let apps = {}
-            // _.each(allApps, function(app){
-            //     if(app._id === id && app.is_creator){
-            //         apps[app._id] = app
-            //     }
-            // })
             i18n.translationApp(lng, app._id, app)
             Object.assign(this.data.values, Object.assign({code: app._id}, clone(app), baseRecord))
-            // let sefl = this;
-            // _.each(apps, function(app){
-            //     app.name = app.label
-            //     Object.assign(sefl.data.values, Object.assign({code: app._id}, clone(app), baseRecord))
-            // })
         }
-    },
-    // afterInsert: async function () {
-    //     const record = await this.getObject('apps').findOne(this.doc._id);
-    //     await objectql.addAppConfig(record, SERVICE_NAME)
-    // },
-    // afterUpdate: async function () {
-    //     const record = await this.getObject('apps').findOne(this.id);
-    //     await objectql.addAppConfig(record, SERVICE_NAME)
-    // },
-    // afterDelete: async function(){
-    //     let id = this.id;
-    //     objectql.removeApp(id)
-    // }
+    }
 }
