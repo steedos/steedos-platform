830af95dcc46ba3ac1abf237943d23e627feabaf
baozhoutao <baozhoutao@hotoa.com>
2023-05-06 19:08:27 +0800

    对象权限数据加载优化, 防止db中出现各种奇怪的数据, 比如name不存在的情况.

--- services/standard-object-database/main/default/objects/3.permission_objects.observe.object.js
+++ services/standard-object-database/main/default/objects/3.permission_objects.observe.object.js
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-08-05 14:17:44
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2023-05-05 14:13:57
+ * @LastEditTime: 2023-05-06 18:58:32
  * @Description: 
  */
 
@@ -12,30 +12,31 @@ const _ = require('lodash');
 let permissionObjectsLoadSetTimeoutId = null;
 Meteor.startup(function () {
     var _change, _remove;
-    const permissionSets = Creator.getCollection("permission_set").find({}).fetch();
     _change = function (document) {
         if(permissionObjectsLoadSetTimeoutId){
             clearTimeout(permissionObjectsLoadSetTimeoutId);
             permissionObjectsLoadSetTimeoutId = null;
         }
         permissionObjectsLoadSetTimeoutId = setTimeout(()=>{
-            objectql.getObject("permission_objects").directFind({}).then((records)=>{
-                records = _.map(records, (doc)=>{
-                    if(_.includes(['admin', 'user', 'customer', 'supplier'], doc.permission_set_id)){
-                        doc.name = doc.permission_set_id
-                    }else{
-                        const record = _.find(permissionSets, (item)=>{
-                            return doc.permission_set_id == item._id
-                        })
-                        if(record){
-                            doc.name = record.name;
+            objectql.getObject("permission_set").find({}).then((permissionSets)=>{
+                objectql.getObject("permission_objects").directFind({}).then((records)=>{
+                    records = _.map(records, (doc)=>{
+                        if(_.includes(['admin', 'user', 'customer', 'supplier'], doc.permission_set_id)){
+                            doc.name = doc.permission_set_id
                         }else{
-                            doc.name = _.last(doc.name.split('.'));
+                            const record = _.find(permissionSets, (item)=>{
+                                return doc.permission_set_id == item._id
+                            })
+                            if(record){
+                                doc.name = record.name;
+                            }else{
+                                doc.name = _.last(doc.name.split('.')) || doc.permission_set_id;
+                            }
                         }
-                    }
-                    return doc;
+                        return doc;
+                    })
+                    cachers.getCacher('permission_objects').set('permission_objects', _.groupBy(records, 'space'));
                 })
-                cachers.getCacher('permission_objects').set('permission_objects', _.groupBy(records, 'space'));
             })
         }, 1000 * 3)
     };
