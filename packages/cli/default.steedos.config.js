30cfb4daf376999eb6b2b39018ea4cea535afd0d
baozhoutao <baozhoutao@hotoa.com>
2023-05-10 09:38:05 +0800

    amis sdk默认使用2.9.0版本

--- packages/cli/default.steedos.config.js
+++ packages/cli/default.steedos.config.js
@@ -16,7 +16,7 @@ if(_.isEmpty(process.env.STEEDOS_UNPKG_URL)) {
 }
 
 if(_.isEmpty(process.env.STEEDOS_AMIS_VERSION)) {
-	process.env.STEEDOS_AMIS_VERSION = '2.7.2';
+	process.env.STEEDOS_AMIS_VERSION = '2.9.0';
 }
 
 if(_.isEmpty(process.env.STEEDOS_PUBLIC_PAGE_ASSETURLS)) {
