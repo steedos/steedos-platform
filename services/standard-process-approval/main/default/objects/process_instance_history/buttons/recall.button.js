299f8ad71e70b7d54da35852c0782180df16546d
Byran <sunhaolin@hotoa.com>
2023-05-15 16:14:30 +0800

    Update recall.button.js

--- services/standard-process-approval/main/default/objects/process_instance_history/buttons/recall.button.js
+++ services/standard-process-approval/main/default/objects/process_instance_history/buttons/recall.button.js
@@ -1,6 +1,5 @@
 module.exports = {
-    recallVisible: function(object_name, record_id, record_permissions, props){
-      console.error("recallVisible3333", props)
-      return Steedos.ProcessManager.allowRecall(props.record.target_object.o, props.record.target_object.ids[0])
-    },
-  }
+  recallVisible: function (object_name, record_id, record_permissions, props) {
+    return Steedos.ProcessManager.allowRecall(props.record.target_object.o, props.record.target_object.ids[0])
+  },
+}
