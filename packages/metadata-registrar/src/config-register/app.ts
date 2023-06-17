af897b324340ba9e04e299c3767ad3e055732402
baozhoutao <baozhoutao@hotoa.com>
2023-06-17 19:30:00 +0800

    fix [Bug]: graphql接口获取的apps数据有时候异常 #5088

--- packages/metadata-registrar/src/config-register/app.ts
+++ packages/metadata-registrar/src/config-register/app.ts
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2023-05-27 17:47:51
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2023-05-30 12:22:02
+ * @LastEditTime: 2023-06-17 18:17:31
  * @Description: 
  */
 import { getConfigsFormFiles } from '../config';
@@ -10,8 +10,7 @@ import { registerApp } from '../metadata-register/app';
 
 export const registerPackageApps = async (packagePath: string, packageServiceName: string)=>{
     const apps = getConfigsFormFiles('app', packagePath);
-    for (const apiName in apps) {
-        const appConfig = apps[apiName];
+    for (const appConfig of apps) {
         await registerApp(broker, packageServiceName, Object.assign(appConfig, {
             is_system: true, record_permissions: {
             allowEdit: false,
