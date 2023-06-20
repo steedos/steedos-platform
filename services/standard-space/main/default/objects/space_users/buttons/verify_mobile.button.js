19cca65ac827929ee4caee6cf5d0db3bcbdc9afe
tujiajun <tujiajun@steedos.com>
2023-06-20 09:31:11 +0800

    [Feature]: 个人设置中添加设置邮箱、手机号按钮 #5084 #5085

---
+++ services/standard-space/main/default/objects/space_users/buttons/verify_mobile.button.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: sunhaolin@hotoa.com
+ * @Date: 2022-12-12 11:32:06
+ * @LastEditors: sunhaolin@hotoa.com
+ * @LastEditTime: 2022-12-13 10:16:32
+ * @Description: 
+ */
+module.exports = {
+    verify_mobile: function (object_name, record_id) {
+        Steedos.openWindow('/accounts/a/#/verify/mobile')
+    },
+
+    verify_mobileVisible: function (object_name, record_id, record_permissions, data) {
+        var space_userId = db.space_users.findOne({ user: Steedos.userId(), space: Steedos.spaceId() })._id
+        if (space_userId === record_id) {
+            return true;
+        }else{
+            return false;
+        }
+    },
+}
