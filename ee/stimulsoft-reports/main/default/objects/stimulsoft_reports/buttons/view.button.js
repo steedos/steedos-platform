8a52120474ecb71416faac3d4c9a88088f270df4
baozhoutao <baozhoutao@hotoa.com>
2023-08-08 17:18:21 +0800

    remove @steedos/ee_stimulsoft-reports

--- ee/stimulsoft-reports/main/default/objects/stimulsoft_reports/buttons/view.button.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    view: function (object_name, record_id) {
-        // window.open(`/api/stimulsoft-reports/view/${record_id}`)
-        Steedos.StimulsoftReports.printOnLine(record_id, {});
-    },
-    viewVisible: function () {
-        return true
-    }
-}
