b11e2923f0955d4aa8e1937fb15c44518f4d233b
殷亮辉 <yinlianghui@hotoa.com>
2023-08-09 17:36:56 +0800

    #5204 实现流程、步骤和连线的合法性校验功能构架

--- services/service-workflow/main/default/client/workflow_designer.client.js
+++ services/service-workflow/main/default/client/workflow_designer.client.js
@@ -2,7 +2,7 @@
  * @Author: 殷亮辉 yinlianghui@hotoa.com
  * @Date: 2023-07-30 16:49:42
  * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2023-07-30 17:13:22
+ * @LastEditTime: 2023-08-09 17:35:31
  */
 ; (function () {
     function getIconName(type) {
@@ -21,13 +21,14 @@
         }
     }
 
-    function getStepLabel(step) {
+    function getStepLabel(step, isValiFailed) {
         const iconName = getIconName(step.step_type);
         return React.createElement(
             'div',
             { className: 'graph-node' },
             React.createElement('div', { className: 'step-icon ' + iconName }),
-            React.createElement('span', { className: 'step-name' }, step.name)
+            React.createElement('span', { className: 'step-name' }, step.name),
+            isValiFailed ? React.createElement('span', { className: 'step-vali-failed-icon' }, step.name) : null
         )
     }
 
@@ -35,7 +36,57 @@
         return crypto.randomUUID();
     }
 
+    function valiStepAndGetErrMsg(step){
+		//验证步骤合法性，如果不合法则返回错误信息，反之则返回空字符
+		var reMsg = "";
+        var stepName = step.name;
+        if(stepName.trim().length == 0){
+            //请输入步骤名称
+            reMsg = "Step.Name.NotEmpty";
+        }
+        else{
+            
+        }
+        return reMsg;
+    }
+
+    function valiFlow(context, doAction, event) {
+        var isFlowValid = true;
+        // 优先从event.data下取setNodes函数而不是从event.data.reactFlowInstance中取可以避免死循环
+        var setNodes = event.data.setNodes || event.data.reactFlowInstance.setNodes;
+        setNodes(function (nds) {
+            return nds.map((node) => {
+                node.data = {
+                    ...node.data
+                };
+                var reMsg = valiStepAndGetErrMsg(node.data);
+                var isValiFailed = !!reMsg.length;
+                if(isValiFailed){
+                    isFlowValid = false;
+                }
+                node.data.label = Steedos.getWorkflowStepLabel(node.data, isValiFailed);
+                return node;
+            })
+        });
+        var flowInfo = event.data.flow;
+        if(isFlowValid && flowInfo && flowInfo.name.trim().length == 0){
+            isFlowValid = false;
+        }
+        // TODO:等所有的校验功能都完成后这句话放能放开，否则会把错误的is_valid保存到数据库中
+        flowInfo.is_valid = isFlowValid;
+        doAction({
+            "componentId": "service_react_flow",
+            "actionType": "setValue",
+            "args": {
+                "value": {
+                    "flow": flowInfo
+                }
+            }
+        });
+    }
+
     Steedos.getWorkflowStepIconName = getIconName;
     Steedos.getWorkflowStepLabel = getStepLabel;
     Steedos.getWorkflowUUID = getUUID;
+    Steedos.validateWokflow = valiFlow;
 })();
