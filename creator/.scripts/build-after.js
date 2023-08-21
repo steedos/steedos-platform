3d270ea80f9d8341840d899afc47c30f478de9aa
baozhoutao <baozhoutao@hotoa.com>
2023-08-21 14:31:20 +0800

    fix [Bug]:手机端 input-month、select等部分组件在微页面中不能正常使用 #5317

--- creator/.scripts/build-after.js
+++ creator/.scripts/build-after.js
@@ -12,3 +12,7 @@ execSync('rm -rf ../server/bundle/*');
 
 execSync('cp -rf .dist/bundle/* ../server/bundle/');
 execSync('cp -rf .scripts/build-fix/* ../server/bundle/');
+
+// /web.browser.legacy 使用和 web.browser一样的代码.
+execSync('rm -rf ../server/bundle/programs/web.browser.legacy/*');
+execSync('cp -rf ../server/bundle/programs/web.browser/* ../server/bundle/programs/web.browser.legacy/');
