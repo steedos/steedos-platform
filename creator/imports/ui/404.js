227e0693cef69ef342767c82a8134ab574c982e7
baozhoutao <baozhoutao@hotoa.com>
2023-08-25 15:42:05 +0800

    notFound 页面显示异常页面地址

--- creator/imports/ui/404.js
+++ creator/imports/ui/404.js
@@ -2,7 +2,7 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-05-19 11:38:30
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2023-08-22 17:45:14
+ * @LastEditTime: 2023-08-25 15:10:39
  * @Description: 
  */
 import './404.html';
@@ -16,5 +16,8 @@ Template.notFound.helpers({
 	},
 	notFoundMessageBody: function() {
 	  return t("creator_not_found_message_body");
+	},
+	currentPath: function(){
+		return FlowRouter.current().context.pathname
 	}
 });
