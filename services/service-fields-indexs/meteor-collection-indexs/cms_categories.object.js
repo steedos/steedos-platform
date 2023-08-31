13339838710b416fb26f71037643bff1c486cdd7
baozhoutao <baozhoutao@hotoa.com>
2023-08-31 10:28:06 +0800

    调整cms 对象索引

--- services/service-fields-indexs/meteor-collection-indexs/cms_categories.object.js
+++ services/service-fields-indexs/meteor-collection-indexs/cms_categories.object.js
@@ -1,4 +1,11 @@
-if (Meteor.isServer) {
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2022-03-28 09:35:35
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2023-08-31 10:27:06
+ * @Description: 
+ */
+if (Meteor.isServer && b.cms_categories) {
   db.cms_categories._ensureIndex({
     "site": 1,
     "parent": 1
