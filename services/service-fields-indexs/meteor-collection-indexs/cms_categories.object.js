809f80ec6de2450248b5f38ec98711bff7b18f5b
baozhoutao <baozhoutao@hotoa.com>
2023-09-02 10:14:24 +0800

    Update cms_categories.object.js

--- services/service-fields-indexs/meteor-collection-indexs/cms_categories.object.js
+++ services/service-fields-indexs/meteor-collection-indexs/cms_categories.object.js
@@ -2,10 +2,10 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-03-28 09:35:35
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2023-08-31 10:27:06
+ * @LastEditTime: 2023-09-02 10:14:08
  * @Description: 
  */
-if (Meteor.isServer && b.cms_categories) {
+if (Meteor.isServer && db.cms_categories) {
   db.cms_categories._ensureIndex({
     "site": 1,
     "parent": 1
