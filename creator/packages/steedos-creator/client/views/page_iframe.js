13bb7ef07a4d9b1c7e7a30e7b9048442c7b0e005
baozhoutao <baozhoutao@hotoa.com>
2023-09-11 17:10:14 +0800

    完善平台能力
    https://github.com/steedos/steedos-platform-ee/issues/26

--- creator/packages/steedos-creator/client/views/page_iframe.js
+++ creator/packages/steedos-creator/client/views/page_iframe.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: baozhoutao@steedos.com
+ * @Date: 2023-08-30 15:26:07
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2023-09-09 15:11:09
+ * @Description: 
+ */
 
 var onNavClick=function (event) {
     const { data } = event;
@@ -38,3 +45,6 @@ Template.page_iframe.onDestroyed(function(){
 	window.removeEventListener('message', onNavClick)
 })
 
+Template.page_iframe.onRendered(function(){
+	window.Steedos.setDocumentTitle({pageName: null, tabName: null})
+})
