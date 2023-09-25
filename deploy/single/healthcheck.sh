38b002aa7425b60ffc9d8e0fc60c30478884667a
庄建国 <9340100@qq.com>
2023-09-25 09:32:28 +0000

    docker scripts

---
+++ deploy/single/healthcheck.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+healthy=true
+
+if [ -f "/data/.env" ]; then
+  export $(cat /data/.env | xargs)
+elif [ -f "/home/.env" ]; then
+  export $(cat /home/.env | xargs)
+else
+  echo "No .env file found"
+  healthy=false
+fi
+
+if [[ $(curl -Lfk -s -w "%{http_code}\n" http://localhost:3000/ -o /dev/null) -ne 200 ]]; then
+  echo 'ERROR: Steedos is not running';
+  healthy=false
+fi
+
+if [[ $(redis-cli -a $REDIS_PASSWORD --no-auth-warning  ping) != 'PONG' ]]; then
+    echo 'ERROR: Redis is down';
+    healthy=false
+fi
+
+if [ $healthy == true ]; then
+  exit 0
+else
+  exit 1
+fi
