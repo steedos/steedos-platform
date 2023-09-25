6b8ecc4364e2594309140e30e09daa3138c6d0d6
庄建国 <9340100@qq.com>
2023-09-25 10:54:23 +0000

    single docker

--- deploy/single/entrypoint.sh
+++ deploy/single/entrypoint.sh
@@ -1,8 +1,16 @@
-[[ -z "${REDIS_URL}" ]] && export CACHER=redis://localhost:6379
+#!/bin/bash
+
+declare -a ENV_VARS=("JWT_SECRET" "REDIS_PASSWORD")
+
+[[ -z "${CACHER}" ]] && export CACHER=redis://localhost:6379
 [[ -z "${TRANSPORTER}" ]] && export TRANSPORTER=redis://localhost:6379/1
 
-sudo service mongod start
-redis-server --requirepass $REDIS_PASSWORD > /dev/stdout 2>&1 &
+mkdir /data/db
+mkdir /data/db
+mongod --bind_ip_all --replSet steedos --logpath /var/log/mongodb/mongod.log --fork
+mongo --host localhost:27017 --eval "rs.initiate({ _id: 'steedos', members: [{_id: 0, host: 'localhost:27017'}]})"
 
-cd /app
-npm start
+redis-server --daemonize yes
+
+# Handle CMD command
+exec "$@"
