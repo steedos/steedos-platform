37efc3f7ff407a9274240aafb2e028509527cbe0
庄建国 <9340100@qq.com>
2023-09-26 02:13:51 +0000

    docker scripts

--- deploy/single/entrypoint.sh
+++ deploy/single/entrypoint.sh
@@ -1,16 +1,200 @@
 #!/bin/bash
 
-declare -a ENV_VARS=("JWT_SECRET" "REDIS_PASSWORD")
+set -e
 
-[[ -z "${CACHER}" ]] && export CACHER=redis://localhost:6379
-[[ -z "${TRANSPORTER}" ]] && export TRANSPORTER=redis://localhost:6379/1
+stacks_path=/steedos-stacks
 
-mkdir /data/db
-mkdir /data/db
-mongod --bind_ip_all --replSet steedos --logpath /var/log/mongodb/mongod.log --fork
-mongo --host localhost:27017 --eval "rs.initiate({ _id: 'steedos', members: [{_id: 0, host: 'localhost:27017'}]})"
+export SUPERVISORD_CONF_TARGET="$TMP/supervisor-conf.d/"  # export for use in supervisord.conf
+export MONGODB_TMP_KEY_PATH="$TMP/mongodb-key"  # export for use in supervisor process mongodb.conf
 
-redis-server --daemonize yes
+mkdir -pv "$SUPERVISORD_CONF_TARGET"
+
+init_env_file() {
+  CONF_PATH="/steedos-stacks/configuration"
+  ENV_PATH="$CONF_PATH/docker.env"
+  APP_PATH="/app"
+
+  # Build an env file with current env variables. We single-quote the values, as well as escaping any single-quote characters.
+  printenv | grep -E '^STEEDOS_|^MONGO_|ROOT_URL|CACHER|TRANSPORTER|PORT|NODE_ENV' | sed "s/'/'\\\''/g; s/=/='/; s/$/'/" > "$APP_PATH/.env"
+
+  echo "Initialize .env file"
+  if ! [[ -e "$ENV_PATH" ]]; then
+    # Generate new docker.env file when initializing container for first time or in Heroku which does not have persistent volume
+    echo "Generating default configuration file"
+    mkdir -p "$CONF_PATH"
+    local default_steedos_mongodb_user="steedos"
+    local generated_steedos_mongodb_password=$(
+      tr -dc A-Za-z0-9 </dev/urandom | head -c 13
+      echo ""
+    )
+    local generated_steedos_encryption_password=$(
+      tr -dc A-Za-z0-9 </dev/urandom | head -c 13
+      echo ""
+    )
+    local generated_steedos_encription_salt=$(
+      tr -dc A-Za-z0-9 </dev/urandom | head -c 13
+      echo ""
+    )
+    local generated_steedos_supervisor_password=$(
+      tr -dc A-Za-z0-9 </dev/urandom | head -c 13
+      echo ''
+    )
+    bash "$TEMPLATES_PATH/env.sh" "$default_steedos_mongodb_user" "$generated_steedos_mongodb_password" "$generated_steedos_encryption_password" "$generated_steedos_encription_salt" "$generated_steedos_supervisor_password" > "$ENV_PATH"
+  fi
+
+
+  echo "Load environment configuration"
+  set -o allexport
+  . "$ENV_PATH"
+  . "$APP_PATH/.env"
+  set +o allexport
+}
+
+
+check_mongodb_uri() {
+  echo "Checking MONGO_URL"
+  isUriLocal=1
+  if [[ $MONGO_URL == *"localhost"* || $MONGO_URL == *"127.0.0.1"* ]]; then
+    echo "Detected local MongoDB"
+    isUriLocal=0
+  fi
+}
+
+
+init_mongodb() {
+  if [[ $isUriLocal -eq 0 ]]; then
+    echo "Initializing local database"
+    MONGO_DB_PATH="$stacks_path/data/mongodb"
+    MONGO_LOG_PATH="$MONGO_DB_PATH/log"
+    MONGO_DB_KEY="$MONGO_DB_PATH/key"
+    mkdir -p "$MONGO_DB_PATH"
+    touch "$MONGO_LOG_PATH"
+
+    if [[ ! -f "$MONGO_DB_KEY" ]]; then
+      openssl rand -base64 756 > "$MONGO_DB_KEY"
+    fi
+    use-mongodb-key "$MONGO_DB_KEY"
+  fi
+}
+
+
+init_replica_set() {
+  echo "Checking initialized database"
+  shouldPerformInitdb=1
+  for path in \
+    "$MONGO_DB_PATH/WiredTiger" \
+    "$MONGO_DB_PATH/journal" \
+    "$MONGO_DB_PATH/local.0" \
+    "$MONGO_DB_PATH/storage.bson"; do
+    if [ -e "$path" ]; then
+      shouldPerformInitdb=0
+      break
+    fi
+  done
+
+  if [[ $isUriLocal -gt 0 && -f /proc/cpuinfo ]] && ! grep --quiet avx /proc/cpuinfo; then
+    echo "====================================================================================================" >&2
+    echo "==" >&2
+    echo "== AVX instruction not found in your CPU. Steedos's embedded MongoDB may not start. Please use an external MongoDB instance instead." >&2
+    echo "==" >&2
+    echo "====================================================================================================" >&2
+  fi
+
+  if [[ $shouldPerformInitdb -gt 0 && $isUriLocal -eq 0 ]]; then
+    echo "Initializing Replica Set for local database"
+    # Start installed MongoDB service - Dependencies Layer
+    mongod --fork --port 27017 --dbpath "$MONGO_DB_PATH" --logpath "$MONGO_LOG_PATH"
+    echo "Waiting 10s for MongoDB to start"
+    sleep 10
+    # echo "Creating MongoDB user"
+    # mongosh "127.0.0.1/steedos" --eval "db.createUser({
+    #     user: '$STEEDOS_MONGODB_USER',
+    #     pwd: '$STEEDOS_MONGODB_PASSWORD',
+    #     roles: [{
+    #         role: 'root',
+    #         db: 'admin'
+    #     }, 'readWrite']
+    #   }
+    # )"
+    echo "Enabling Replica Set"
+    mongod --dbpath "$MONGO_DB_PATH" --shutdown || true
+    mongod --fork --port 27017 --dbpath "$MONGO_DB_PATH" --logpath "$MONGO_LOG_PATH" --replSet steedos --keyFile "$MONGODB_TMP_KEY_PATH" --bind_ip localhost
+    echo "Waiting 10s for MongoDB to start with Replica Set"
+    sleep 10
+    mongo "$MONGO_URL" --eval 'rs.initiate()'
+    mongod --dbpath "$MONGO_DB_PATH" --shutdown || true
+  fi
+
+  if [[ $isUriLocal -gt 0 ]]; then
+    echo "Checking Replica Set of external MongoDB"
+
+    if appsmithctl check-replica-set; then
+      echo "MongoDB ReplicaSet is enabled"
+    else
+      echo -e "\033[0;31m***************************************************************************************\033[0m"
+      echo -e "\033[0;31m*      MongoDB Replica Set is not enabled                                             *\033[0m"
+      echo -e "\033[0;31m*      Please ensure the credentials provided for MongoDB, has 'readWrite' role.      *\033[0m"
+      echo -e "\033[0;31m***************************************************************************************\033[0m"
+      exit 1
+    fi
+  fi
+}
+
+use-mongodb-key() {
+  # We copy the MongoDB key file to `$MONGODB_TMP_KEY_PATH`, so that we can reliably set its permissions to 600.
+  # Why? When the host machine of this Docker container is Windows, file permissions cannot be set on files in volumes.
+  # So the key file should be somewhere inside the container, and not in a volume.
+  mkdir -pv "$(dirname "$MONGODB_TMP_KEY_PATH")"
+  cp -v "$1" "$MONGODB_TMP_KEY_PATH"
+  chmod 600 "$MONGODB_TMP_KEY_PATH"
+}
+
+
+configure_supervisord() {
+  local supervisord_conf_source="/app/supervisord"
+  if [[ -n "$(ls -A "$SUPERVISORD_CONF_TARGET")" ]]; then
+    rm -f "$SUPERVISORD_CONF_TARGET"/*
+  fi
+
+  # Disable services based on configuration
+  if [[ -z "${DYNO}" ]]; then
+    if [[ $isUriLocal -eq 0 ]]; then
+      cp "$supervisord_conf_source/mongodb.conf" "$SUPERVISORD_CONF_TARGET"
+    fi
+    if [[ $REDIS_URL == *"localhost"* || $REDIS_URL == *"127.0.0.1"* ]]; then
+      cp "$supervisord_conf_source/redis.conf" "$SUPERVISORD_CONF_TARGET"
+      mkdir -p "$stacks_path/data/redis"
+    fi
+    if ! [[ -e "/steedos-stacks/ssl/fullchain.pem" ]] || ! [[ -e "/steedos-stacks/ssl/privkey.pem" ]]; then
+      cp "$supervisord_conf_source/cron.conf" "$SUPERVISORD_CONF_TARGET"
+    fi
+  fi
+
+}
+
+init_env_file
+
+
+if [[ -z "${DYNO}" ]]; then
+  # Don't run MongoDB if running in a Heroku dyno.
+  init_mongodb
+  init_replica_set
+else
+  # These functions are used to limit heap size for Backend process when deployed on Heroku
+  get_maximum_heap
+  setup_backend_heap_arg
+  # set the hostname for heroku dyno
+  export HOSTNAME="heroku_dyno"
+fi
+
+
+configure_supervisord
+
+# Ensure the restore path exists in the container, so an archive can be copied to it, if need be.
+mkdir -p /steedos-stacks/data/{backup,restore}
+
+# Create sub-directory to store services log in the container mounting folder
+mkdir -p /steedos-stacks/logs/{supervisor,steedos,cron,mongodb,redis}
 
 # Handle CMD command
 exec "$@"
