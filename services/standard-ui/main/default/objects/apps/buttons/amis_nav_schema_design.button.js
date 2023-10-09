ad152c71e59d74083aea977380af2b13689601aa
baozhoutao <baozhoutao@hotoa.com>
2023-10-09 15:46:24 +0800

    fix [Bug]: 应用删除页面报错“Cannot read property '_id' of undefined” #5534

--- services/standard-ui/main/default/objects/apps/buttons/amis_nav_schema_design.button.js
+++ services/standard-ui/main/default/objects/apps/buttons/amis_nav_schema_design.button.js
@@ -2,30 +2,30 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2023-04-11 14:05:42
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2023-04-11 14:08:41
+ * @LastEditTime: 2023-10-09 15:44:47
  * @Description: 
  */
 module.exports = {
-    amis_nav_schema_design: function (object_name, record_id) {
-      document.location = Steedos.absoluteUrl(`/api/amisAppNavSchemaDesign?id=${record_id}&object=${this.record.object_name}&assetUrls=${Builder.settings.assetUrls}&locale=${Builder.settings.locale}`);
-    },
-    amis_nav_schema_designVisible: function (object_name, record_id, record_permissions) {
-        var perms= {};
-        var record = Creator.getObjectRecord(object_name, record_id);
-        if(!record){
-          return false;
-        }
-        if(!record.enable_nav_schema){
-          return false;
-        }
-        if (record_permissions) {
-          perms = record_permissions;
-        } else {
-            record_permissions = Creator.getRecordPermissions(object_name, record, Meteor.userId());
-            if (record_permissions) {
-                perms = record_permissions;
-            }
-        }
-        return perms["allowEdit"];
+  amis_nav_schema_design: function (object_name, record_id) {
+    document.location = Steedos.absoluteUrl(`/api/amisAppNavSchemaDesign?id=${record_id}&object=${this.record.object_name}&assetUrls=${Builder.settings.assetUrls}&locale=${Builder.settings.locale}`);
+  },
+  amis_nav_schema_designVisible: function (object_name, record_id, record_permissions, data) {
+    var perms = {};
+    var record = data.record;
+    if (!record) {
+      return false;
     }
+    if (!record.enable_nav_schema) {
+      return false;
+    }
+    if (record_permissions) {
+      perms = record_permissions;
+    } else {
+      record_permissions = Creator.getRecordPermissions(object_name, record, Meteor.userId());
+      if (record_permissions) {
+        perms = record_permissions;
+      }
+    }
+    return perms["allowEdit"];
   }
+}
