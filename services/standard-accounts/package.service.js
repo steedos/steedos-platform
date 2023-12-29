df6d5711453a95ae5e424b837478a9ebe960ef9a
孙浩林 <sunhaolin@steedos.com>
2023-12-29 15:14:59 +0800

    [Task]: 标准对象中的 accounts, contacts 分拆到单独的软件包 @steedos/standard-accounts #6054

---
+++ services/standard-accounts/package.service.js
@@ -0,0 +1,76 @@
+/*
+ * @Author: sunhaolin@hotoa.com
+ * @Date: 2022-12-02 13:17:06
+ * @LastEditors: 孙浩林 sunhaolin@steedos.com
+ * @LastEditTime: 2023-12-29 11:28:53
+ * @Description: 
+ */
+"use strict";
+const project = require('./package.json');
+const packageName = project.name;
+const packageLoader = require('@steedos/service-meteor-package-loader');
+
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+module.exports = {
+    name: packageName,
+    namespace: "steedos",
+    mixins: [packageLoader],
+    /**
+     * Settings
+     */
+    settings: {
+        packageInfo: {
+            path: __dirname,
+            name: packageName,
+            isPackage: false
+        }
+    },
+
+    /**
+     * Dependencies
+     */
+    dependencies: ['~packages-standard-objects'],
+
+    /**
+     * Actions
+     */
+    actions: {
+
+    },
+
+    /**
+     * Events
+     */
+    events: {
+
+    },
+
+    /**
+     * Methods
+     */
+    methods: {
+
+    },
+
+    /**
+     * Service created lifecycle event handler
+     */
+    created() {
+    },
+
+    /**
+     * Service started lifecycle event handler
+     */
+    async started() {
+
+    },
+
+    /**
+     * Service stopped lifecycle event handler
+     */
+    async stopped() {
+
+    }
+};
