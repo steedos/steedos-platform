ebc038a83aada142190a3af8c5557c6a4626815b
涂佳俊 <tujiajun@steedos.com>
2024-02-18 15:07:20 +0800

    objectLayout.field_groups不存在时不覆盖对象上的field_groups#6140

--- packages/objectql/src/types/object.ts
+++ packages/objectql/src/types/object.ts
@@ -1623,7 +1623,9 @@ export class SteedosObjectType extends SteedosObjectProperties {
             }
         })
         objectConfig.fields = this.getAccessFields(objectConfig.fields, objectLayout, objectConfig.permissions)
-        objectConfig.field_groups = objectLayout && objectLayout.field_groups
+        if(objectLayout && objectLayout.field_groups) {
+            objectConfig.field_groups = objectLayout && objectLayout.field_groups
+        }
         
         if (spaceProcessDefinition.length > 0) {
             objectConfig.enable_process = true
