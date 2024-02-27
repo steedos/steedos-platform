177276deba27846002ebc5e7aecd055bf4f61331
殷亮辉 <yinlianghui@hotoa.com>
2024-02-27 18:38:30 +0800

    process.env.STEEDOS_WIDGETS_VERSION = '3.6.4-beta.5';

--- packages/cli/default.steedos.config.js
+++ packages/cli/default.steedos.config.js
@@ -33,7 +33,7 @@ if(_.isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL =  process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if(_.isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-	process.env.STEEDOS_WIDGETS_VERSION = '3.6.4-beta.4';
+	process.env.STEEDOS_WIDGETS_VERSION = '3.6.4-beta.5';
 }
 
 if(_.isEmpty(process.env.STEEDOS_PUBLIC_PAGE_ASSETURLS)) {
