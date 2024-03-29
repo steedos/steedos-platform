a76fcea33c0b001a97b21b5fdfa462db3fa03f44
baozhoutao <baozhoutao@hotoa.com>
2024-03-29 13:22:03 +0800

    fix 性能优化: open api find 最大返回数据量 从5000调整到500 #6660

--- services/service-rest/consts.js
+++ services/service-rest/consts.js
@@ -1,11 +1,11 @@
 /*
  * @Author: sunhaolin@hotoa.com
  * @Date: 2023-06-12 17:20:40
- * @LastEditors: sunhaolin@hotoa.com
- * @LastEditTime: 2023-06-15 18:38:17
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2024-03-29 13:17:38
  * @Description: 
  */
 module.exports = {
-    QUERY_DOCS_TOP: 5000,
+    QUERY_DOCS_TOP: 500,
     REQUEST_SUCCESS_STATUS: 0,
 }
