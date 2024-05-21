417aef7c500ebc480c23aa88b12c11a02359c89b
殷亮辉 <yinlianghui@hotoa.com>
2024-05-21 12:17:44 +0800

    [Feature]: 新建主子表字段时，最大主子表层级深度从3改为6 #6867

--- services/service-metadata-objects/src/master-detail/masterDetailActionHandler.ts
+++ services/service-metadata-objects/src/master-detail/masterDetailActionHandler.ts
@@ -5,7 +5,7 @@ import _ = require('lodash')
 // 其3层指的是A-B-C-D，它们都有父子关系，A作为最顶层，该对象上不可以再创建主表子表关系字段，但是B、C、D上可以；
 // 或者如果当前对象上创建的主表子表关系字段指向的对象是D，那么也会超过3层的层级限制；
 // 又或者中间加一层M先连接B再连接C，形成A-B-M-C-D，也会超过3层的层级限制；
-export const MAX_MASTER_DETAIL_LEAVE = 3;
+export const MAX_MASTER_DETAIL_LEAVE = 6;
 
 /**
  * 判断传入的paths中最大层级深度
