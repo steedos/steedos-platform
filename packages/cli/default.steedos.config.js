3b0545725cc14bf29be795e422f078a8b7d290ad
baozhoutao <baozhoutao@hotoa.com>
2024-10-21 16:32:01 +0800

    Update default.steedos.config.js

--- packages/cli/default.steedos.config.js
+++ packages/cli/default.steedos.config.js
@@ -38,7 +38,7 @@ if (_.isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (_.isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-	process.env.STEEDOS_WIDGETS_VERSION = 'v6.3.11-beta.9';
+	process.env.STEEDOS_WIDGETS_VERSION = 'v6.3.11-beta.10';
 }
 
 if (_.isEmpty(process.env.STEEDOS_PUBLIC_PAGE_ASSETURLS)) {
