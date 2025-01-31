26ccb11753b36bd9202ca0068072466b3970891f
Jack Zhuang <9340100@qq.com>
2025-01-31 15:21:36 +0800

    作废循环依赖，新增环境变量，global['SteedosBroker']

--- packages/metadata-core/src/load/loadPackage.ts
+++ packages/metadata-core/src/load/loadPackage.ts
@@ -8,11 +8,27 @@ import * as _ from 'underscore';
 import { syncMatchFiles } from '../';
 // import * as path from 'path';
 const path = require("path");
-import { getSteedosSchema, loadService } from '@steedos/objectql';
+// import { getSteedosSchema, loadService } from '@steedos/objectql';
+
+
+function clearRequireCache(filename) {
+    /* istanbul ignore next */
+    Object.keys(require.cache).forEach(function (key) {
+        if (key == filename) {
+            delete require.cache[key];
+        }
+    });
+};
+
+function loadService(broker, filename){
+    clearRequireCache(filename);
+    return broker.loadService(filename);
+}
+
 
 export async function loadPackage(packagePath){
-    let schema = getSteedosSchema();
-    let broker = schema.broker;
+    // let schema = getSteedosSchema();
+    let broker = global['SteedosBroker']; //schema.broker;
     if(broker){
         const filePatten = [
           path.join(packagePath, "**", "package.service.js"),
