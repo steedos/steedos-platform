63a2d1a51d5e5f920de5e0873348ac16da834365
baozhoutao <baozhoutao@hotoa.com>
2025-02-13 05:52:31 +0000

    upgrade widgets: v6.3.12-beta.16

--- packages/cli/default.steedos.config.js
+++ packages/cli/default.steedos.config.js
@@ -38,7 +38,7 @@ if (_.isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (_.isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-	process.env.STEEDOS_WIDGETS_VERSION = 'v6.3.12-beta.12';
+	process.env.STEEDOS_WIDGETS_VERSION = 'v6.3.12-beta.16';
 }
 
 if (_.isEmpty(process.env.STEEDOS_PUBLIC_PAGE_ASSETURLS)) {
