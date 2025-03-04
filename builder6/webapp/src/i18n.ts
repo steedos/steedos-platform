13bda70b26b8d44d652fbb7db8c7f1812283b5ca
baozhoutao <baozhoutao@hotoa.com>
2025-03-04 16:48:17 +0800

    fix(core):  add sprintf support to i18n #7130

--- builder6/webapp/src/i18n.ts
+++ builder6/webapp/src/i18n.ts
@@ -1,25 +1,27 @@
-import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
-import ICU from 'i18next-icu';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import ICU from "i18next-icu";
+import { initReactI18next } from "react-i18next";
+import sprintf from "i18next-sprintf-postprocessor";
 
 i18n
+  .use(sprintf)
   .use(ICU)
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
-    supportedLngs: ['en', 'zh-CN'],
+    fallbackLng: "en",
+    supportedLngs: ["en", "zh-CN"],
     debug: false,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
     backend: {
-      loadPath: `${import.meta.env.VITE_B6_ROOT_URL}/locales/{{lng}}/{{ns}}`
-    }
+      loadPath: `${import.meta.env.VITE_B6_ROOT_URL}/locales/{{lng}}/{{ns}}`,
+    },
   });
-  
+
 export default i18n;
