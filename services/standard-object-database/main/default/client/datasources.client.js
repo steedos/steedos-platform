367c5e456546b4bc943c60081c848445965c256e
baozhoutao <baozhoutao@hotoa.com>
2025-03-16 18:57:12 +0800

    chore: Remove unused code #7130

--- services/standard-object-database/main/default/client/datasources.client.js
+++
@@ -1,7 +0,0 @@
-Meteor.startup(function() {
-    return Tracker.autorun(function(c) {
-      if (Session.get("spaceId")) {
-        return Meteor.subscribe("datasources", Session.get("spaceId"));
-      }
-    });
-  });
