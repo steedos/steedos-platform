d0011a31cb3ee118d4b48471de6ed0f985a04bae
baozhoutao <baozhoutao@hotoa.com>
2025-04-11 11:49:48 +0800

    fix: 应用程序: 新建、修改、删除等按钮问题 #7130

--- services/standard-process-approval/main/default/client/core.client.js
+++ services/standard-process-approval/main/default/client/core.client.js
@@ -1,34 +1,3 @@
-/*
- * @Author: baozhoutao@steedos.com
- * @Date: 2022-12-13 10:41:08
- * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2023-02-26 16:30:11
- * @Description: 
- */
-
-Steedos.getObjectsOptions = function (filterFunction) {
-    var options = [];
-    _.each(Steedos.getDisplayObjects(filterFunction), function (v, k) {
-        options.push({ label: v.label, value: v.name, icon: v.icon })
-    })
-    return options;
-}
-
-Steedos.getDisplayObjects = function(filterFunction){
-    var objects = [];
-    _.each(Creator.objectsByName, function (object, k) {
-        var filterReturn = true;
-        if (filterFunction && _.isFunction(filterFunction)) {
-            filterReturn = filterFunction(object);
-        }
-        if (filterReturn && !object.hidden && !_.includes(['cfs_instances_filerecord'], object.name)) {
-            objects.push(object)
-        }
-    })
-    objects.sort(Creator.sortingMethod.bind({key:"label"}))
-    return objects;
-}
-
 Steedos.getFieldDataTypes = function (field) {
     if(field.type === "select"){
         return [
