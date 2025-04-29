b8f2a32b926f3f3dc41230613427b02eb6c64d7a
殷亮辉 <yinlianghui@hotoa.com>
2025-04-29 17:02:25 +0800

    fix: t is not defiend

--- services/standard-process-approval/main/default/triggers/process_instance_history.trigger.js
+++ services/standard-process-approval/main/default/triggers/process_instance_history.trigger.js
@@ -1,5 +1,12 @@
+/*
+ * @Author: 殷亮辉 yinlianghui@hotoa.com
+ * @Date: 2025-04-14 09:47:57
+ * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
+ * @LastEditTime: 2025-04-29 11:24:43
+ */
 const _ = require("underscore");
 const auth = require("@steedos/auth");
+const steedosI18n = require("@steedos/i18n");
 
 const getLng = async function(userId){
   const userSession = await auth.getSessionByUserId(userId);
@@ -8,7 +15,7 @@ const getLng = async function(userId){
 
 const getEmptyNodeTitle = async function(userId){
   let lng = await getLng(userId);
-  return t('process_approval_empty_node_title', {}, lng);
+  return steedosI18n.t('process_approval_empty_node_title', {}, lng);
 }
 
 module.exports = {
