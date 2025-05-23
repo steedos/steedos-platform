08e22a1b34198a034d3345997baa1bbb194018fd
baozhoutao <baozhoutao@hotoa.com>
2025-05-23 13:22:25 +0800

    fix: 默认不使用主题 #7130

--- services/service-core-objects/main/default/client/css.client.js
+++ services/service-core-objects/main/default/client/css.client.js
@@ -1,4 +1,5 @@
 var today = new Date();
 var v = today.getFullYear().toString() + (today.getMonth() + 1).toString().padStart(2, '0') + today.getDate().toString().padStart(2, '0');
 
-loadCss(Steedos.absoluteUrl('/topic.css?v='+v));
+loadCss(Steedos.absoluteUrl('/platform.css?v='+v));
+// loadCss(Steedos.absoluteUrl('/topic.css?v='+v));
