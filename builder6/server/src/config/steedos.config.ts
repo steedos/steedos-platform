469f7a791d1a304b53446dba2a64349d599be897
baozhoutao <baozhoutao@hotoa.com>
2025-05-24 14:51:43 +0800

    fix: 升级widgets版本

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -57,7 +57,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "v6.10.1-beta.23";
+  process.env.STEEDOS_WIDGETS_VERSION = "v6.10.1-beta.25";
 }
 
 if (isEmpty(process.env.STEEDOS_PUBLIC_PAGE_ASSETURLS)) {
