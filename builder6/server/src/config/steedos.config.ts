88863d32808d49cafbf8ce9d55546babc12dcef5
殷亮辉 <yinlianghui@hotoa.com>
2025-06-17 21:59:08 +0800

    feat: STEEDOS_WIDGETS_VERSION = "v6.10.1-beta.33"

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -57,7 +57,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "v6.10.1-beta.32";
+  process.env.STEEDOS_WIDGETS_VERSION = "v6.10.1-beta.33";
 }
 
 if (isEmpty(process.env.STEEDOS_PUBLIC_PAGE_ASSETURLS)) {
