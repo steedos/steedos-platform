06a541911dbb4fbc97b60a4f883e23a13a852db9
baozhoutao <baozhoutao@hotoa.com>
2025-07-04 11:30:39 +0800

    fix: 统一defaultsDeep函数参数

--- packages/utils/src/defaultsDeep.ts
+++ packages/utils/src/defaultsDeep.ts
@@ -3,25 +3,29 @@
  * @Date: 2024-04-14 11:32:34
  * @LastEditors: baozhoutao@steedos.com
  * @LastEditTime: 2024-04-14 11:32:36
- * @Description: 
+ * @Description:
  */
 import _ = require("lodash");
 
-
-export function defaultsDeep(obj, sources) {
-    let output = {};
-    _.toArray(arguments).reverse().forEach(item=> {
-        _.mergeWith(output, item, (objectValue, sourceValue) => {
-            if(_.isArray(sourceValue)){
-                if(_.isArray(objectValue)){
-                    return _.uniqBy(_.compact(_.concat(objectValue, sourceValue)), 'name')
-                }else{
-                    return sourceValue
-                }
-            }else{
-                return undefined;
-            }
-        });
+export function defaultsDeep(obj, ...sources: any[]) {
+  let output = {};
+  _.toArray(arguments)
+    .reverse()
+    .forEach((item) => {
+      _.mergeWith(output, item, (objectValue, sourceValue) => {
+        if (_.isArray(sourceValue)) {
+          if (_.isArray(objectValue)) {
+            return _.uniqBy(
+              _.compact(_.concat(objectValue, sourceValue)),
+              "name",
+            );
+          } else {
+            return sourceValue;
+          }
+        } else {
+          return undefined;
+        }
+      });
     });
-    return output;
-};
+  return output;
+}
