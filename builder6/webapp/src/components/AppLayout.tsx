1c7df07354d8059b3848222fe96f50e4fd64dbcc
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-08-05 14:01:46 +0800

    feat: 优化左侧菜单样式

--- builder6/webapp/src/components/AppLayout.tsx
+++ builder6/webapp/src/components/AppLayout.tsx
@@ -15,7 +15,7 @@ export const AppLayout = (props) => {
     <>
       <GlobalLinkInterceptor></GlobalLinkInterceptor>
       <AppHeader />
-      <div className="creator-content-wrapper sm:bg-gray-50" id="main">
+      <div className="creator-content-wrapper" id="main">
         {children}
       </div>
     </>
