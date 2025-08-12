14e11507d5db3d5a0f82c9c79b904c71545d4475
殷亮辉 <yinlianghui@hotoa.com>
2025-08-12 17:55:25 +0800

    fix: #7198 对象自定义按钮，老的按钮移除不显示

--- services/service-core-objects/main/default/objects/objects/buttons/custom.button.js
+++ services/service-core-objects/main/default/objects/objects/buttons/custom.button.js
@@ -1,39 +1,10 @@
+/*
+ * @Author: 殷亮辉 yinlianghui@hotoa.com
+ * @Date: 2025-08-12 17:48:56
+ * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
+ * @LastEditTime: 2025-08-12 17:50:22
+ */
 module.exports = {
-
-    custom: function(object_name, record_id) {
-        $(document.body).addClass('loading');
-        let url = `/graphql`;
-        const obj = Creator.odata.get("objects", record_id);
-        delete obj.record_permissions;
-        delete obj["@odata.context"]
-        delete obj["@odata.editLink"]
-        delete obj["@odata.etag"]
-        delete obj["@odata.id"]
-        delete obj.idFieldName;
-        let options = {
-            type: 'post',
-            async: true,
-            data: JSON.stringify({
-                query: `mutation{objects__upsert(id: "${obj.name}", doc: ${JSON.stringify(JSON.stringify(obj))}){_id,name}}`
-            }),
-            success: function(data) {
-                SteedosUI.notification.success({
-                    message: '对象已自定义。'
-                });
-                // SteedosUI.router.go({}, "/app/" + Session.get("app_id") + "/" + object_name + "/view/" + data.data.objects__upsert._id);
-                FlowRouter.reload()
-                $(document.body).removeClass('loading');
-            },
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
-                SteedosUI.notification.error({
-                    message: '操作失败',
-                    description: t(XMLHttpRequest.responseJSON.error),
-                });
-                $(document.body).removeClass('loading');
-            }
-        };
-        Steedos.authRequest(url, options);
-    },
     customVisible: function(object_name, record_id, permission, data) {
         if (Steedos.settings.public.enable_saas) {
             return false;
