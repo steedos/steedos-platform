3881c2ae70939b2c36062652be6c95513940e819
殷亮辉 <yinlianghui@hotoa.com>
2025-08-25 11:33:29 +0800

    fix: 打开微页面设计器，url中少了当前语言参数值，传入了locale=undefined，造成页面没有国际化
    https://github.com/steedos/steedos-platform/issues/7401

--- services/service-pages/main/default/objects/pages/buttons/showDesign.button.js
+++ services/service-pages/main/default/objects/pages/buttons/showDesign.button.js
@@ -1,7 +1,17 @@
+/*
+ * @Author: 殷亮辉 yinlianghui@hotoa.com
+ * @Date: 2025-07-07 16:57:38
+ * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
+ * @LastEditTime: 2025-08-25 11:24:10
+ */
 module.exports = {
     showDesign: function (object_name, record_id) {
         // Steedos.openWindow();
-        document.location = Steedos.absoluteUrl(`/api/pageDesign?pageId=${record_id}&assetUrls=${Builder.settings.assetUrls}&locale=${Builder.settings.locale}`);
+        var locale = Builder.settings.context?.user?.language || window.navigator.language;
+        if(locale === 'en' || locale.startsWith('en-')){
+            locale = 'en-us'
+        }
+        document.location = Steedos.absoluteUrl(`/api/pageDesign?pageId=${record_id}&assetUrls=${Builder.settings.assetUrls}&locale=${locale}`);
     },
     showDesignVisible: function (object_name, record_id, record_permissions) {
         var perms, record;
