316a35a7e878ca20eeadf776b199abaceaa5c0d4
baozhoutao <baozhoutao@hotoa.com>
2025-08-29 13:08:10 +0800

    fix: 非saas环境下, 工作区管理员=超级管理员

--- packages/auth/src/auth-middleware.ts
+++ packages/auth/src/auth-middleware.ts
@@ -3,46 +3,76 @@
  * @Date: 2022-07-11 17:35:55
  * @LastEditors: baozhoutao@steedos.com
  * @LastEditTime: 2024-04-26 10:48:24
- * @Description: 
+ * @Description:
  */
 import { setRequestUser } from "./session";
 import * as core from "express-serve-static-core";
 import { compact, split, includes } from "lodash";
 interface Request extends core.Request {
-    user: any;
+  user: any;
 }
 
-export const requireAuthentication = async (req: Request, res: any, next: () => void) => {
-    await setRequestUser(req, res, function () {
-        if (req.user) {
-            next();
-        }
-        else {
-            res.status(401).send({ status: 'error', message: 'You must be logged in to do this.' });
-        }
-    });
+export const requireAuthentication = async (
+  req: Request,
+  res: any,
+  next: () => void,
+) => {
+  await setRequestUser(req, res, function () {
+    if (req.user) {
+      next();
+    } else {
+      res
+        .status(401)
+        .send({
+          status: "error",
+          message: "You must be logged in to do this.",
+        });
+    }
+  });
+};
 
-}
+export const authentication = async (
+  req: Request,
+  res: any,
+  next: () => void,
+) => {
+  await setRequestUser(req, res, function () {
+    next();
+  });
+};
 
-export const authentication = async (req: Request, res: any, next: () => void) => {
-    await setRequestUser(req, res, function () {
+export const superAdminAuthentication = async (
+  req: Request,
+  res: any,
+  next: () => void,
+) => {
+  await setRequestUser(req, res, function () {
+    if (req.user) {
+      const { userId, is_space_admin } = req.user;
+
+      if (process.env.STEEDOS_TENANT_ENABLE_SAAS != "true" && is_space_admin) {
         next();
-    });
-}
+        return;
+      }
 
-export const superAdminAuthentication= async (req: Request, res: any, next: () => void) => {
-    await setRequestUser(req, res, function () {
-        if (req.user) {
-            const { userId } = req.user;
-            const superAdmins = compact(split(process.env.STEEDOS_SUPER_ADMIN, ','));
-            if(includes(superAdmins, userId)){
-                next();
-            }else{
-                res.status(403).send({ status: 'error', message: 'You do not have permission to do this.' });
-            }
-        }
-        else {
-            res.status(401).send({ status: 'error', message: 'You must be logged in to do this.' });
-        }
-    });
-}
+      const superAdmins = compact(split(process.env.STEEDOS_SUPER_ADMIN, ","));
+      if (includes(superAdmins, userId)) {
+        next();
+      } else {
+        res
+          .status(403)
+          .send({
+            status: "error",
+            message: "You do not have permission to do this.",
+          });
+      }
+    } else {
+      res
+        .status(401)
+        .send({
+          status: "error",
+          message: "You must be logged in to do this.",
+        });
+    }
+  });
+};
