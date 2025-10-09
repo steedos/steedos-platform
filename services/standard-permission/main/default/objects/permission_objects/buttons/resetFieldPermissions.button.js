28745032491d5a9cf63ddcc50b95fccf2007a505
baozhoutao <baozhoutao@hotoa.com>
2025-10-09 18:14:35 +0800

    fix: 转化对象权限格式. 优化分组 #7159

--- services/standard-permission/main/default/objects/permission_objects/buttons/resetFieldPermissions.button.js
+++ services/standard-permission/main/default/objects/permission_objects/buttons/resetFieldPermissions.button.js
@@ -1,14 +1,11 @@
-/*
- * @Author: 殷亮辉 yinlianghui@hotoa.com
- * @Date: 2025-07-07 16:57:38
- * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
- * @LastEditTime: 2025-08-11 14:55:44
- */
-const _ = require("underscore");
 module.exports = {
-    resetFieldPermissions: function (object_name, record_id) {
+
+    resetFieldPermissions: function(object_name, record_id) {
         var doc = Creator.odata.get(object_name, record_id);
-        var result = Steedos.authRequest(`/api/v4/${object_name}/${record_id}/resetFieldPermissions`, { type: 'get', async: false });
+        var result = Steedos.authRequest(`/api/v4/${object_name}/${record_id}/resetFieldPermissions`, {
+            type: 'get',
+            async: false
+        });
         if (result.error) {
             toastr.error(t(result.error));
         } else {
@@ -16,7 +13,7 @@ module.exports = {
             FlowRouter.reload();
         }
     },
-    resetFieldPermissionsVisible: function (object_name, record_id, record_permissions, data) {
+    resetFieldPermissionsVisible: function(object_name, record_id, record_permissions, data) {
         var record = data && data.record;
         if (!record) {
             record = {}
@@ -24,4 +21,5 @@ module.exports = {
         return Steedos.Object.base.actions.standard_new.visible() && !record.is_system;
         // return !(Steedos.Object.base.actions.standard_new.visible() && record.is_system);
     }
+
 }
