1bc3845eca17f196efc448e7f940922aa7b0a075
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-10-22 17:56:49 +0800

    fix STEEDOS_WIDGETS_VERSION = "6.10.3"

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -52,7 +52,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "v6.10.3";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.3";
 }
 
 if (isEmpty(process.env.STEEDOS_PUBLIC_PAGE_ASSETURLS)) {
