7c58454628e0fec10a67c91fa9f2362952ef79be
baozhoutao <baozhoutao@hotoa.com>
2025-10-23 11:50:13 +0800

    Revert "fix: 完善记录修改消息."
    This reverts commit 685dde190e66cad93eacb44cf1ba7b9023bb80ce.

--- builder6/webapp/src/components/AmisRender.tsx
+++ builder6/webapp/src/components/AmisRender.tsx
@@ -7,7 +7,6 @@
  */
 import { Builder, builder, BuilderComponent } from '@builder6/react';
 import { useNavigate } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 
 const normalizeLink = (to, location = window.location) => {
   to = to || "";
@@ -50,7 +49,6 @@ const normalizeLink = (to, location = window.location) => {
 export const AmisRender = function ({schema = {}, data = {}, env = {}}) {
   // console.log(`AmisRender`, schema, data, env)
   const navigate = useNavigate(); 
-  const browserHistory = createBrowserHistory();
 
   if(!(window as any).goBack){
     (window as any).goBack = ()=>{
@@ -60,10 +58,6 @@ export const AmisRender = function ({schema = {}, data = {}, env = {}}) {
 
   if(!(window as any).navigate){
     (window as any).navigate = navigate;
-  }
-
-  if(!(window as any).browserHistory){
-    (window as any).browserHistory = browserHistory;
   }
 
   let locale = Builder.settings.context?.user?.language || Builder.settings.default_language;
