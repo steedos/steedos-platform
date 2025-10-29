017600a7f1063c15a048f20ab454b2221b43202e
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-10-29 14:29:23 +0800

    fix: webapp 源码运行

--- builder6/webapp/src/components/intl_provider/intl_provider.tsx
+++ builder6/webapp/src/components/intl_provider/intl_provider.tsx
@@ -108,7 +108,7 @@ export default class IntlProvider extends React.PureComponent<IntlProviderProps,
             const self = this;
             _window.lodash = _window._;
             _window.loadJs('/steedos-init.js', ()=>{
-                _window.loadJs(`${Builder.settings.context.rootUrl}/client_scripts.js`, ()=>{
+                _window.loadJs(`${import.meta.env.VITE_B6_ROOT_URL}/client_scripts.js`, ()=>{
                     self.setState({ settings: settingsData, loading: false });
                 });
             });
