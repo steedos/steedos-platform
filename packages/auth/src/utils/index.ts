bb427405ba8b1b5944f36107eef956ff7f413630
孙浩林 <sunhaolin@steedos.com>
2025-11-04 09:34:49 +0800

    fix: 钉钉单点登录报错，未登录过的用户没有services.resume.loginTokens属性 #147

--- packages/auth/src/utils/index.ts
+++ packages/auth/src/utils/index.ts
@@ -2,62 +2,64 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2024-06-16 17:46:33
  * @LastEditors: 孙浩林 sunhaolin@steedos.com
- * @LastEditTime: 2025-02-18 10:30:13
- * @Description: 
+ * @LastEditTime: 2025-11-04 09:32:47
+ * @Description:
  */
-import crypto = require('crypto');
-import { default as Random } from './random';
-import { getSteedosSchema } from '@steedos/objectql';
-import { setCookie, clearCookie } from '@steedos/utils';
+import crypto = require("crypto");
+import { default as Random } from "./random";
+import { getSteedosSchema } from "@steedos/objectql";
+import { setCookie, clearCookie } from "@steedos/utils";
 
 export const hashLoginToken = function (loginToken) {
-  const hash = crypto.createHash('sha256');
+  const hash = crypto.createHash("sha256");
   hash.update(loginToken);
-  return hash.digest('base64');
-}
+  return hash.digest("base64");
+};
 
 export const generateStampedLoginToken = function () {
   return {
     token: Random.secret(),
-    when: new Date
+    when: new Date(),
   };
-}
+};
 
 export const hashStampedToken = function (stampedToken) {
   const hashedStampedToken = Object.keys(stampedToken).reduce(
-    (prev, key) => key === 'token' ?
-      prev :
-      { ...prev, [key]: stampedToken[key] },
+    (prev, key) =>
+      key === "token" ? prev : { ...prev, [key]: stampedToken[key] },
     {},
-  )
+  );
   return {
     ...hashedStampedToken,
-    hashedToken: hashLoginToken(stampedToken.token)
+    hashedToken: hashLoginToken(stampedToken.token),
   };
-}
+};
 
 export const insertHashedLoginToken = async function (userId, hashedToken) {
-  let userObject = getSteedosSchema().getObject('users')
-  let user = await userObject.findOne(userId, { fields: ['services'] })
-  if(!user['services']){
-    user['services'] = {}
+  let userObject = getSteedosSchema().getObject("users");
+  let user = await userObject.findOne(userId, { fields: ["services"] });
+  if (!user["services"]) {
+    user["services"] = {};
   }
-  if(!user['services']['resume']){
-    user['services']['resume'] = {loginTokens: []}
+  if (!user["services"]["resume"]) {
+    user["services"]["resume"] = { loginTokens: [] };
   }
-  user['services']['resume']['loginTokens'].push(hashedToken)
-  let data = { services: user['services'] }
+  if (!user["services"]["resume"]["loginTokens"]) {
+    user["services"]["resume"]["loginTokens"] = [];
+  }
+  user["services"]["resume"]["loginTokens"].push(hashedToken);
+  let data = { services: user["services"] };
   return await userObject.update(userId, data);
-}
+};
 
 export const setAuthCookies = function (req, res, userId, authToken, spaceId?) {
   let options = {
     maxAge: 90 * 60 * 60 * 24 * 1000,
     httpOnly: true,
-    overwrite: true
-  }
+    overwrite: true,
+  };
   setCookie(req, res, "X-User-Id", userId, options as any);
-  setCookie(req, res, "X-Auth-Token", authToken, options as any)
+  setCookie(req, res, "X-Auth-Token", authToken, options as any);
 
   if (spaceId) {
     setCookie(req, res, "X-Space-Id", spaceId, options as any);
@@ -65,27 +67,26 @@ export const setAuthCookies = function (req, res, userId, authToken, spaceId?) {
   }
 
   return;
-}
-
+};
 
 export const clearAuthCookies = function (req, res) {
   let options = {
     maxAge: 0,
     httpOnly: true,
-    overwrite: true
-  }
+    overwrite: true,
+  };
 
-  clearCookie(req, res, "X-User-Id", options as any)
-  clearCookie(req, res, "X-Auth-Token", options as any)
+  clearCookie(req, res, "X-User-Id", options as any);
+  clearCookie(req, res, "X-Auth-Token", options as any);
 
-  clearCookie(req, res, "X-Access-Token", options as any)
-  clearCookie(req, res, "X-Space-Token", options as any)
+  clearCookie(req, res, "X-Access-Token", options as any);
+  clearCookie(req, res, "X-Space-Token", options as any);
 
   return;
-}
+};
 
 export function isExpried(expiredAt: number) {
   return expiredAt <= new Date().getTime();
 }
 
-export { default as Random } from './random';
+export { default as Random } from "./random";
