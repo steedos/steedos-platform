309aa68394930632b969c02bb84853f416d6d4a3
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-11-20 18:36:07 +0800

    feat: 优化左侧导航

--- builder6/webapp/src/components/AppHeader.tsx
+++ builder6/webapp/src/components/AppHeader.tsx
@@ -136,7 +136,7 @@ export const getHeaderSchema = (props) => {
                 body: [
                     {
                         type: "wrapper",
-                        className: 'sidebar-wrapper pl-2 pr-0 pt-2 pb-16 fixed z-20 h-full h-fill flex flex-col overflow-y-auto block',
+                        className: 'sidebar-wrapper bg-white border-r px-2 pr-0 pt-2 pb-16 fixed z-20 h-full h-fill flex flex-col overflow-y-auto block',
                         visibleOn: '${!!app}',
                         body: [
                             {
