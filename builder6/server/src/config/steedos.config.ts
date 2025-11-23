b2e7be75a6a698b501e2832f4a21e28c25d316f1
baozhoutao <baozhoutao@hotoa.com>
2025-11-23 13:45:40 +0800

    fix: 升级资产、plugin-workflow

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -57,7 +57,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "6.10.13";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.14";
 }
 
 if (isEmpty(process.env.STEEDOS_PUBLIC_PAGE_ASSETURLS)) {
