5de65846a7907df221c6796f818c2f24f79fc15c
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-11-24 08:53:30 +0000

    fix: docker build

--- deploy/enterprise/fs/opt/steedos/templates/docker.env.sh
+++ deploy/enterprise/fs/opt/steedos/templates/docker.env.sh
@@ -11,16 +11,11 @@ NODERED_PASSWORD="$5"
 cat <<EOF
 
 MONGO_URL=mongodb://$MONGO_USER:$MONGO_PASSWORD@localhost:27017/steedos?authSource=admin
-MONGO_OPLOG_URL=mongodb://$MONGO_USER:$MONGO_PASSWORD@localhost:27017/local?authSource=admin
-# MONGO_URL=mongodb://localhost:27017/steedos
-# MONGO_OPLOG_URL=mongodb://localhost:27017/local
+# MONGO_OPLOG_URL=mongodb://$MONGO_USER:$MONGO_PASSWORD@localhost:27017/local?authSource=admin
 
 STEEDOS_MONGODB_URI=mongodb://$MONGO_USER:$MONGO_PASSWORD@localhost:27017/steedos?authSource=admin
 STEEDOS_MONGODB_USER=$MONGO_USER
 STEEDOS_MONGODB_PASSWORD=$MONGO_PASSWORD
-
-STEEDOS_TABLES_MONGO_URL=mongodb://$MONGO_USER:$MONGO_PASSWORD@localhost:27017/tables?authSource=admin
-
 TRANSPORTER=redis://localhost:6379
 CACHER=redis://localhost:6379/1
 
