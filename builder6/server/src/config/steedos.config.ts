4eb5c31ba6ffda33244ed1a7f051ac394ff54ce8
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-11-27 18:37:47 +0800

    feat: 企业版默认启用 内置 unpkg 和 mongodb log

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -13,6 +13,11 @@ const clone = require("clone");
 const validator = require("validator");
 const yaml = require("js-yaml");
 
+if (process.env.STEEDOS_LICENSE) {
+  process.env.B6_LOG_MONGO_ENABLED = "true";
+  process.env.STEEDOS_UNPKG_URL = "/unpkg";
+}
+
 if (isEmpty(process.env.STEEDOS_TENANT_ENABLE_PASSWORD_LOGIN)) {
   process.env.STEEDOS_TENANT_ENABLE_PASSWORD_LOGIN = "true";
 }
