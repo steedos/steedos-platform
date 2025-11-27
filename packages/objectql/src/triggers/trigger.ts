ec675154618d09d9a7585ad4d93de475bf4ff8d1
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-11-27 17:09:35 +0800

    [Feature]: 对象触发器和函数支持直接使用原生 mongodb 语法查询数据库 #7507

--- packages/objectql/src/triggers/trigger.ts
+++ packages/objectql/src/triggers/trigger.ts
@@ -3,35 +3,31 @@
  * @Date: 2023-04-23 13:35:17
  * @LastEditors: baozhoutao@steedos.com
  * @LastEditTime: 2024-11-25 10:41:20
- * @Description: 
+ * @Description:
  */
-const { NodeVM } = require('vm2');
-const _  = require('lodash');
+const { NodeVM } = require("vm2");
+const _ = require("lodash");
 import { ObjectId } from "mongodb";
-import axios from 'axios';
+import axios from "axios";
+const objectql = require("@steedos/objectql");
 
-function str2function(
-    contents,
-    ...args
-  ) {
-    try {
-      let fn = new Function(...args, contents);
-      return fn;
-    } catch (e) {
-      console.warn(e);
-      return null;
-    }
+function str2function(contents, ...args) {
+  try {
+    let fn = new Function(...args, contents);
+    return fn;
+  } catch (e) {
+    console.warn(e);
+    return null;
+  }
 }
 
-const sendPost = async (url, body, options)=>{
-    try {
-        return await axios.post(url, body, options);
-    } catch (error) {
-        throw new Error(`请求失败(${error.message}): ${url}`)
-    }
-}
-
-
+const sendPost = async (url, body, options) => {
+  try {
+    return await axios.post(url, body, options);
+  } catch (error) {
+    throw new Error(`请求失败(${error.message}): ${url}`);
+  }
+};
 
 /**
  * 
@@ -52,84 +48,104 @@ const sendPost = async (url, body, options)=>{
             }
         }
  */
-const runUrlTrigger = async (trigger, thisArg, args)=>{
-    if(!trigger.url){
-        throw new Error(`触发器「${trigger.name}」缺少URL`)
-    }
+const runUrlTrigger = async (trigger, thisArg, args) => {
+  if (!trigger.url) {
+    throw new Error(`触发器「${trigger.name}」缺少URL`);
+  }
 
-    const headers = {
-        'Content-Type': 'application/json',
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  if (trigger.authentication_type == "header") {
+    headers[trigger.authentication_header_key] =
+      trigger.authentication_header_value;
+  }
+
+  const { data: rsBody, status: httpStatus } = await sendPost(
+    trigger.url,
+    args && args.length > 0 ? args[0].params : {},
+    { headers },
+  );
+  if (httpStatus !== 200) {
+    throw new Error(`请求失败: ${trigger.url}`);
+  }
+  let { error, data } = rsBody;
+
+  if (error && error.message) {
+    throw new Error(error.message);
+  }
+  return data;
+};
+
+export const runTriggerFunction = async (trigger, thisArg, ...args) => {
+  if (trigger.type === "url") {
+    return await runUrlTrigger(trigger, thisArg, args);
+  }
+  const db = objectql.getDataSource("default").adapter;
+  const npm = {
+    _: require("lodash"),
+    moment: require("moment"),
+    validator: require("validator"),
+    filters: require("@steedos/filters"),
+  };
+
+  // ----- code trigger -----
+  const vm = new NodeVM({
+    sandbox: {
+      str2function,
+      global: npm,
+      npm,
+      services: (global as any).services,
+      objects: (global as any).objects,
+      makeNewID: () => {
+        return new ObjectId().toHexString();
+      },
+      db,
+    },
+    require: {
+      external: true,
+      root: "./",
+    },
+    env: process.env,
+  });
+  const triggerFileName = `${trigger.listenTo}.${trigger.name}.trigger.js`;
+  let triggerInSandbox = vm.run(
+    `module.exports = async function(ctx){${trigger.handler}};`,
+    triggerFileName,
+  );
+  try {
+    const runTrigger = async function () {
+      return new Promise((resolve, reject) => {
+        triggerInSandbox
+          .apply(thisArg, args)
+          .then((res) => {
+            resolve(res);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
     };
-
-    if(trigger.authentication_type == 'header'){
-        headers[trigger.authentication_header_key] = trigger.authentication_header_value
+    const res: any = await runTrigger();
+    return res;
+  } catch (error) {
+    const source = error.stack;
+    const errorStack = source
+      .substring(source.indexOf("(") + 1, source.indexOf(")"))
+      .replace(
+        triggerFileName,
+        "对象「" + trigger.listenTo + "」的「" + trigger.name + "」触发器",
+      )
+      .replace(":", " 行 ")
+      .replace(":", " 列 ");
+    const newError = new Error(error.message);
+    newError.stack = `Object Trigger Error: ${error.message}\n    at ${errorStack}`;
+    if (process.env.NODE_ENV === "development") {
+      newError.message = newError.stack;
     }
-
-    const { data: rsBody, status: httpStatus } = await sendPost(trigger.url, args && args.length > 0 ? args[0].params : {}, { headers });
-    if(httpStatus !== 200){
-        throw new Error(`请求失败: ${trigger.url}`)
-    }
-    let { error, data } = rsBody;
-
-    if(error && error.message){
-        throw new Error(error.message)
-    }
-    return data
-}
-
-
-export const runTriggerFunction = async (trigger, thisArg, ...args)=>{
-    
-    if(trigger.type === 'url'){
-        return await runUrlTrigger(trigger, thisArg, args)
-    }
-
-    // ----- code trigger -----
-    const vm = new NodeVM({
-        sandbox: {
-            str2function,
-            global: {
-                _: require('lodash'),
-                moment: require('moment'),
-                validator: require('validator'),
-                // dateFNS: require('date-fns'),
-                Filters: require('@steedos/filters'),
-                filters: require('@steedos/filters')
-            },
-            services: (global as any).services,
-            objects: (global as any).objects,
-            makeNewID: ()=>{
-                return new ObjectId().toHexString();
-            }
-        },
-        require: {
-            external: true,
-            root: './'
-        },
-        env: process.env
-    });
-    const triggerFileName = `${trigger.listenTo}.${trigger.name}.trigger.js`;
-    let triggerInSandbox =  vm.run(`module.exports = async function(ctx){${trigger.handler}};`, triggerFileName);
-    try {
-        const runTrigger = async function(){
-            return new Promise((resolve, reject)=>{
-                triggerInSandbox.apply(thisArg, args).then((res)=>{
-                    resolve(res)
-                }).catch((error)=>{
-                    reject(error)
-                })
-            });
-        }
-        const res: any = await runTrigger();
-        return res;
-    } catch (error) {
-        const source = error.stack;
-        const errorStack = source.substring(source.indexOf("(")+1,source.indexOf(")")).replace(triggerFileName, "对象「" + trigger.listenTo + "」的「" + trigger.name +"」触发器").replace(":"," 行 ").replace(":"," 列 ");
-        const newError = new Error(error.message);
-        newError.stack = `Object Trigger Error: ${error.message}\n    at ${errorStack}`;
-        if(process.env.NODE_ENV === 'development'){
-            newError.message = newError.stack
-        }
-        throw newError
-    }
-}
+    console.log(error);
+    console.log(newError);
+    throw newError;
+  }
+};
