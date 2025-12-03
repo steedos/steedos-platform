e89b4146a844383aa0793ca1efc686db9a5a493f
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-12-03 13:43:28 +0800

    feat: request has been deprecated, see https://github.com/request/request/issues/3142

--- packages/cli/src/auth/index.ts
+++ packages/cli/src/auth/index.ts
@@ -1,22 +1,51 @@
-const request = require('request');
-const _ = require('underscore');
-import {getMetaDataSpaceId, getMetaDataAuthToken, getMetaDataUrl, loadENV, getMetaDataSpaceToken, getAuthorization, getAPIAuthorization} from '@steedos/metadata-core';
+import axios, { AxiosRequestConfig } from "axios";
+import _ from "lodash";
+import {
+  getMetaDataSpaceId,
+  getMetaDataAuthToken,
+  getMetaDataUrl,
+  loadENV,
+  getMetaDataSpaceToken,
+  getAuthorization,
+  getAPIAuthorization,
+} from "@steedos/metadata-core";
+
 loadENV();
 
-export function authRequest(url: string, requestOptions, requestCallback){
+export async function authRequest(
+  url: string,
+  requestOptions: any,
+  requestCallback?: (error: any, response: any, body: any) => void,
+) {
+  const metadataURL = getMetaDataUrl();
+  const fullUrl = `${metadataURL}${url}`;
 
-    var metadataURL = getMetaDataUrl();
+  if (!requestOptions.headers) {
+    requestOptions.headers = {};
+  }
 
-    requestOptions.url = `${metadataURL}${url}`
-    
-    // requestOptions.form.space = getMetaDataSpaceId();
+  if (!_.has(requestOptions.headers, "Authorization")) {
+    requestOptions.headers["Authorization"] = getAPIAuthorization();
+  }
 
-    if(!_.has(requestOptions, 'headers')){
-        requestOptions.headers = {};
+  try {
+    const response = await axios({
+      url: fullUrl,
+      method: requestOptions.method || "GET",
+      headers: requestOptions.headers,
+      params: requestOptions.qs, // request → axios 差异
+      data: requestOptions.body || requestOptions.form, // 支持 body/form
+      timeout: requestOptions.timeout,
+    });
+
+    if (requestCallback) {
+      requestCallback(null, response, response.data);
     }
-
-    if(!_.has(requestOptions.headers, 'Authorization')){
-        requestOptions.headers["Authorization"] = getAPIAuthorization();
+    return response;
+  } catch (error) {
+    if (requestCallback) {
+      requestCallback(error, error.response, error.response?.data);
     }
-    return request(requestOptions, requestCallback);
+    throw error;
+  }
 }
