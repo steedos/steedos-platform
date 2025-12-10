fef254f727ec669d448ab0994e8b17e789a3bc9e
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-12-10 18:22:32 +0800

    feat: 合并编译 steedos-widgets 中的 tailwind 样式

--- builder6/webapp/tailwind.config.js
+++ builder6/webapp/tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./index.html",
     '../../services/**/*.{json,client.js}',
-    '../../packages/**/*.{json,client.js}'
+    '../../packages/**/*.{json,client.js}',
+    "../../node_modules/@steedos-widgets/amis-object/dist/*.js",
+    "../../../steedos-widgets/packages/@steedos-widgets/amis-object/dist/*.js",
   ],
   darkMode: 'selector',
   theme: {
