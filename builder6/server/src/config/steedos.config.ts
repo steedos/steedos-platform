196040f2f6f8ebd851c1bf108151b6d52609e62e
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-12-11 15:34:13 +0800

    fix: antd.js 必须在资产包中加载，否则设计器崩了。

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -62,7 +62,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "6.10.34-beta.3";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.34-beta.4";
 }
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_ADDITIONAL)) {
