6096896900ad0f50e2035d24ae0311e091049e27
Jack Zhuang <9340100@qq.com>
2025-12-12 18:24:16 +0800

    [Bug]: dayjs 加载方式调整. 目前是从公共cdn加载的. https://cdn.jsdelivr.net/npm/dayjs@1.11.19/dayjs.min.js #8220

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -62,7 +62,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "6.10.34-beta.5";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.34-beta.6";
 }
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_ADDITIONAL)) {
