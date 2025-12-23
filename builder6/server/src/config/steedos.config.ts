eafdddba79c6358913e9b7079c7f00522df18398
殷亮辉 <yinlianghui@hotoa.com>
2025-12-23 14:57:50 +0800

    chore: upgrade widgets to 6.10.41

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -60,7 +60,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "6.10.39";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.41";
 }
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_ADDITIONAL)) {
