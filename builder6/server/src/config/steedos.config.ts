fb484399bfb758fd38e4c88dd828a51010ada549
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-12-25 14:49:24 +0800

    feat: STEEDOS_WIDGETS_VERSION = "6.10.44"

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -60,7 +60,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "6.10.42-beta.2";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.44";
 }
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_ADDITIONAL)) {
