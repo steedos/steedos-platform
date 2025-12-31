e2a88411eaa992c424ea2221be45af9cec5367b5
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-12-31 16:38:32 +0800

    feat: 支持动态 schema

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -64,7 +64,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "6.10.47";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.48";
 }
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_ADDITIONAL)) {
