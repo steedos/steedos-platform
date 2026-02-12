d5313f898793706d7abc6481c62e0cfa9fbe25cc
殷亮辉 <yinlianghui@hotoa.com>
2026-02-12 12:48:25 +0800

    chore: upgrade steedos-widgets version to 6.10.52-beta.14

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -64,7 +64,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "6.10.52-beta.12";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.52-beta.14";
 }
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_ADDITIONAL)) {
