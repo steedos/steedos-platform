9295f880f15ee69bfdee1549fcbba116f384128f
殷亮辉 <yinlianghui@hotoa.com>
2026-03-19 12:28:58 +0800

    feat: bump plugin-workflow to ^3.0.43, widgets to 6.10.52-beta.24

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -64,7 +64,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "6.10.52-beta.23";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.52-beta.24";
 }
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_ADDITIONAL)) {
