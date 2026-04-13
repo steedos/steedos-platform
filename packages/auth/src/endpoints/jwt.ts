afa6ae168637b715608d2b91609e6ef907120c8d
yinlianghui <yinlianghui@hotoa.com>
2026-04-13 14:59:58 +0800

    revert jwt.ts 改动，非必要改动还原 #8337

--- packages/auth/src/endpoints/jwt.ts
+++ packages/auth/src/endpoints/jwt.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: baozhoutao@steedos.com
  * @Date: 2022-03-28 09:35:34
- * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2022-11-16 17:15:17
+ * @LastEditors: yinlianghui yinlianghui@hotoa.com
+ * @LastEditTime: 2026-04-13 14:58:43
  * @Description: 
  */
 import { hashStampedToken, insertHashedLoginToken, hashLoginToken, setAuthCookies } from '../utils';
@@ -98,7 +98,7 @@ export const jwtSSO = async (req, res) => {
       redirectUrl = decodeURIComponent(redirectUrl);
     }
     if(redirectUrl){
-      redirectUrl = `/home/${spaceId}?redirect_uri=${encodeURIComponent(redirectUrl)}`
+      redirectUrl = `/home/${spaceId}?redirect_uri=${redirectUrl}`
     }else{
       redirectUrl = `/home/${spaceId}`
     }
