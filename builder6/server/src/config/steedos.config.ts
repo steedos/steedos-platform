7374cbad62f260d4d07203866c4946bdaea8685a
包周涛 <baozhoutao@hotoa.com>
2026-05-06 22:45:01 +0800

    Bump Steedos widget and workflow versions

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -64,7 +64,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "6.10.53-beta.6";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.53-beta.7";
 }
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_ADDITIONAL)) {
