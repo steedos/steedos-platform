3b7ee525c23838ae7f07e280af403dd7ffd72880
殷亮辉 <yinlianghui@hotoa.com>
2026-06-18 12:50:04 +0800

    feat: 升级 plugin-workflow 至 3.0.91，widgets 至 6.10.54-beta.7

--- builder6/server/src/config/steedos.config.ts
+++ builder6/server/src/config/steedos.config.ts
@@ -64,7 +64,7 @@ if (isEmpty(process.env.STEEDOS_AMIS_URL)) {
 process.env.STEEDOS_AMIS_URL = process.env.STEEDOS_AMIS_URL.replace(/\/+$/, "");
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_VERSION)) {
-  process.env.STEEDOS_WIDGETS_VERSION = "6.10.54-beta.6";
+  process.env.STEEDOS_WIDGETS_VERSION = "6.10.54-beta.7";
 }
 
 if (isEmpty(process.env.STEEDOS_WIDGETS_ADDITIONAL)) {
